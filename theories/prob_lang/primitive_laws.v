c166b1c62d721eca995fe9dc08747c79965ddff6
Simon Gregersen <gregersen@cs.au.dk>
2022-11-30 10:40:04 +0100

    weakestpre theory

--- theories/prob_lang/primitive_laws.v
+++ theories/prob_lang/primitive_laws.v
@@ -52,110 +52,110 @@ Notation "l ↪{# q } v" := (l ↪{ DfracOwn q } v)%I
 Notation "l ↪ v" := (l ↪{ DfracOwn 1 } v)%I
   (at level 20, format "l  ↪  v") : bi_scope.
 
-Section lifting.
-Context `{!prelocGS Σ}.
-Implicit Types P Q : iProp Σ.
-Implicit Types Φ Ψ : val → iProp Σ.
-Implicit Types σ : state.
-Implicit Types v : val.
-Implicit Types l : loc.
+(* Section lifting. *)
+(* Context `{!prelocGS Σ}. *)
+(* Implicit Types P Q : iProp Σ. *)
+(* Implicit Types Φ Ψ : val → iProp Σ. *)
+(* Implicit Types σ : state. *)
+(* Implicit Types v : val. *)
+(* Implicit Types l : loc. *)
 
-(** Recursive functions: we do not use this lemmas as it is easier to use Löb
-induction directly, but this demonstrates that we can state the expected
-reasoning principle for recursive functions, without any visible ▷. *)
-Lemma wp_rec_löb s E f x e Φ Ψ :
-  □ ( □ (∀ v, Ψ v -∗ WP (rec: f x := e)%V v @ s; E {{ Φ }}) -∗
-     ∀ v, Ψ v -∗ WP (subst' x v (subst' f (rec: f x := e) e)) @ s; E {{ Φ }}) -∗
-  ∀ v, Ψ v -∗ WP (rec: f x := e)%V v @ s; E {{ Φ }}.
-Proof.
-  iIntros "#Hrec". iLöb as "IH". iIntros (v) "HΨ".
-  iApply lifting.wp_pure_step_later; first done.
-  iNext. iApply ("Hrec" with "[] HΨ"). iIntros "!>" (w) "HΨ".
-  iApply ("IH" with "HΨ").
-Qed.
+(* (** Recursive functions: we do not use this lemmas as it is easier to use Löb *)
+(* induction directly, but this demonstrates that we can state the expected *)
+(* reasoning principle for recursive functions, without any visible ▷. *) *)
+(* Lemma wp_rec_löb s E f x e Φ Ψ : *)
+(*   □ ( □ (∀ v, Ψ v -∗ WP (rec: f x := e)%V v @ s; E {{ Φ }}) -∗ *)
+(*      ∀ v, Ψ v -∗ WP (subst' x v (subst' f (rec: f x := e) e)) @ s; E {{ Φ }}) -∗ *)
+(*   ∀ v, Ψ v -∗ WP (rec: f x := e)%V v @ s; E {{ Φ }}. *)
+(* Proof. *)
+(*   iIntros "#Hrec". iLöb as "IH". iIntros (v) "HΨ". *)
+(*   iApply lifting.wp_pure_step_later; first done. *)
+(*   iNext. iApply ("Hrec" with "[] HΨ"). iIntros "!>" (w) "HΨ". *)
+(*   iApply ("IH" with "HΨ"). *)
+(* Qed. *)
 
-(** Heap *)
-Lemma wp_alloc s E v :
-  {{{ True }}} Alloc (Val v) @ s; E {{{ l, RET LitV (LitLoc l); l ↦ v }}}.
-Proof.
-  iIntros (Φ) "_ HΦ".
-  iApply wp_lift_atomic_head_step; [done|].
-  iIntros (σ1) "[Hh Ht] !#".
-  iSplit; [by eauto with head_step|].
-  iIntros "!> /=" (e2 σ2 Hs); inv_head_step.
-  iMod ((ghost_map_insert (fresh_loc σ1.(heap)) v) with "Hh") as "[$ Hl]".
-  { apply not_elem_of_dom, fresh_loc_is_fresh. }
-  iIntros "!>". iFrame. by iApply "HΦ".
-Qed.
+(* (** Heap *) *)
+(* Lemma wp_alloc s E v : *)
+(*   {{{ True }}} Alloc (Val v) @ s; E {{{ l, RET LitV (LitLoc l); l ↦ v }}}. *)
+(* Proof. *)
+(*   iIntros (Φ) "_ HΦ". *)
+(*   iApply wp_lift_atomic_head_step; [done|]. *)
+(*   iIntros (σ1) "[Hh Ht] !#". *)
+(*   iSplit; [by eauto with head_step|]. *)
+(*   iIntros "!> /=" (e2 σ2 Hs); inv_head_step. *)
+(*   iMod ((ghost_map_insert (fresh_loc σ1.(heap)) v) with "Hh") as "[$ Hl]". *)
+(*   { apply not_elem_of_dom, fresh_loc_is_fresh. } *)
+(*   iIntros "!>". iFrame. by iApply "HΦ". *)
+(* Qed. *)
 
-Lemma wp_load s E l dq v :
-  {{{ ▷ l ↦{dq} v }}} Load (Val $ LitV $ LitLoc l) @ s; E {{{ RET v; l ↦{dq} v }}}.
-Proof.
-  iIntros (Φ) ">Hl HΦ".
-  iApply wp_lift_atomic_head_step; [done|].
-  iIntros (σ1) "[Hh Ht] !#".
-  iDestruct (ghost_map_lookup with "Hh Hl") as %?.
-  iSplit; [by eauto with head_step|].
-  iIntros "!> /=" (e2 σ2 Hs); inv_head_step.
-  iFrame. iModIntro. by iApply "HΦ".
-Qed.
+(* Lemma wp_load s E l dq v : *)
+(*   {{{ ▷ l ↦{dq} v }}} Load (Val $ LitV $ LitLoc l) @ s; E {{{ RET v; l ↦{dq} v }}}. *)
+(* Proof. *)
+(*   iIntros (Φ) ">Hl HΦ". *)
+(*   iApply wp_lift_atomic_head_step; [done|]. *)
+(*   iIntros (σ1) "[Hh Ht] !#". *)
+(*   iDestruct (ghost_map_lookup with "Hh Hl") as %?. *)
+(*   iSplit; [by eauto with head_step|]. *)
+(*   iIntros "!> /=" (e2 σ2 Hs); inv_head_step. *)
+(*   iFrame. iModIntro. by iApply "HΦ". *)
+(* Qed. *)
 
-Lemma wp_store s E l v' v :
-  {{{ ▷ l ↦ v' }}} Store (Val $ LitV (LitLoc l)) (Val v) @ s; E
-  {{{ RET LitV LitUnit; l ↦ v }}}.
-Proof.
-  iIntros (Φ) ">Hl HΦ".
-  iApply wp_lift_atomic_head_step; [done|].
-  iIntros (σ1) "[Hh Ht] !#".
-  iDestruct (ghost_map_lookup with "Hh Hl") as %?.
-  iSplit; [by eauto with head_step|].
-  iIntros "!> /=" (e2 σ2 Hs); inv_head_step.
-  iMod (ghost_map_update with "Hh Hl") as "[$ Hl]".
-  iFrame. iModIntro. by iApply "HΦ".
-Qed.
+(* Lemma wp_store s E l v' v : *)
+(*   {{{ ▷ l ↦ v' }}} Store (Val $ LitV (LitLoc l)) (Val v) @ s; E *)
+(*   {{{ RET LitV LitUnit; l ↦ v }}}. *)
+(* Proof. *)
+(*   iIntros (Φ) ">Hl HΦ". *)
+(*   iApply wp_lift_atomic_head_step; [done|]. *)
+(*   iIntros (σ1) "[Hh Ht] !#". *)
+(*   iDestruct (ghost_map_lookup with "Hh Hl") as %?. *)
+(*   iSplit; [by eauto with head_step|]. *)
+(*   iIntros "!> /=" (e2 σ2 Hs); inv_head_step. *)
+(*   iMod (ghost_map_update with "Hh Hl") as "[$ Hl]". *)
+(*   iFrame. iModIntro. by iApply "HΦ". *)
+(* Qed. *)
 
-(** Tapes  *)
-Lemma wp_alloc_tape s E :
-  {{{ True }}} AllocTape @ s; E {{{ α, RET LitV (LitLbl α); α ↪ [] }}}.
-Proof.
-  iIntros (Φ) "_ HΦ".
-  iApply wp_lift_atomic_head_step; [done|].
-  iIntros (σ1) "[Hh Ht] !# /=".
-  iSplit; [by eauto with head_step|].
-  iIntros "!>" (e2 σ2 Hs); inv_head_step.
-  iMod (ghost_map_insert (fresh_loc σ1.(tapes)) with "Ht") as "[$ Hl]".
-  { apply not_elem_of_dom, fresh_loc_is_fresh. }
-  iFrame. iModIntro. by iApply "HΦ".
-Qed.
+(* (** Tapes  *) *)
+(* Lemma wp_alloc_tape s E : *)
+(*   {{{ True }}} AllocTape @ s; E {{{ α, RET LitV (LitLbl α); α ↪ [] }}}. *)
+(* Proof. *)
+(*   iIntros (Φ) "_ HΦ". *)
+(*   iApply wp_lift_atomic_head_step; [done|]. *)
+(*   iIntros (σ1) "[Hh Ht] !# /=". *)
+(*   iSplit; [by eauto with head_step|]. *)
+(*   iIntros "!>" (e2 σ2 Hs); inv_head_step. *)
+(*   iMod (ghost_map_insert (fresh_loc σ1.(tapes)) with "Ht") as "[$ Hl]". *)
+(*   { apply not_elem_of_dom, fresh_loc_is_fresh. } *)
+(*   iFrame. iModIntro. by iApply "HΦ". *)
+(* Qed. *)
 
-Lemma wp_flip s E α b bs :
-  {{{ ▷ α ↪ (b :: bs) }}} Flip (Val $ LitV $ LitLbl α) @ s; E
-  {{{ RET LitV (LitBool b); α ↪ bs }}}.
-Proof.
-  iIntros (Φ) ">Hl HΦ".
-  iApply wp_lift_atomic_head_step; [done|].
-  iIntros (σ1) "[Hh Ht] !#".
-  iDestruct (ghost_map_lookup with "Ht Hl") as %?.
-  iSplit; [by eauto with head_step|].
-  simpl.
-  iIntros "!>" (e2 σ2 Hs); inv_head_step; last first.
-  { destruct_or?; simplify_map_eq. }
-  iMod (ghost_map_update with "Ht Hl") as "[$ Hl]".
-  iFrame. iModIntro. by iApply "HΦ".
-Qed.
+(* Lemma wp_flip s E α b bs : *)
+(*   {{{ ▷ α ↪ (b :: bs) }}} Flip (Val $ LitV $ LitLbl α) @ s; E *)
+(*   {{{ RET LitV (LitBool b); α ↪ bs }}}. *)
+(* Proof. *)
+(*   iIntros (Φ) ">Hl HΦ". *)
+(*   iApply wp_lift_atomic_head_step; [done|]. *)
+(*   iIntros (σ1) "[Hh Ht] !#". *)
+(*   iDestruct (ghost_map_lookup with "Ht Hl") as %?. *)
+(*   iSplit; [by eauto with head_step|]. *)
+(*   simpl. *)
+(*   iIntros "!>" (e2 σ2 Hs); inv_head_step; last first. *)
+(*   { destruct_or?; simplify_map_eq. } *)
+(*   iMod (ghost_map_update with "Ht Hl") as "[$ Hl]". *)
+(*   iFrame. iModIntro. by iApply "HΦ". *)
+(* Qed. *)
 
-Lemma wp_flip_empty s E α :
-  {{{ ▷ α ↪ [] }}} Flip (Val $ LitV $ LitLbl α) @ s; E
-  {{{ b, RET LitV (LitBool b); α ↪ [] }}}.
-Proof.
-  iIntros (Φ) ">Hl HΦ".
-  iApply wp_lift_atomic_head_step; [done|].
-  iIntros (σ1) "[Hh Ht] !# /=".
-  iDestruct (ghost_map_lookup with "Ht Hl") as %?.
-  iSplit; [by eauto with head_step|].
-  Unshelve. 2: { exact inhabitant. }
-  iIntros "!>" (e2 σ2 Hs); inv_head_step.
-  iFrame. iModIntro. by iApply "HΦ".
-Qed.
+(* Lemma wp_flip_empty s E α : *)
+(*   {{{ ▷ α ↪ [] }}} Flip (Val $ LitV $ LitLbl α) @ s; E *)
+(*   {{{ b, RET LitV (LitBool b); α ↪ [] }}}. *)
+(* Proof. *)
+(*   iIntros (Φ) ">Hl HΦ". *)
+(*   iApply wp_lift_atomic_head_step; [done|]. *)
+(*   iIntros (σ1) "[Hh Ht] !# /=". *)
+(*   iDestruct (ghost_map_lookup with "Ht Hl") as %?. *)
+(*   iSplit; [by eauto with head_step|]. *)
+(*   Unshelve. 2: { exact inhabitant. } *)
+(*   iIntros "!>" (e2 σ2 Hs); inv_head_step. *)
+(*   iFrame. iModIntro. by iApply "HΦ". *)
+(* Qed. *)
 
-End lifting.
+(* End lifting. *)
