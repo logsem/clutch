10f5680c52b74a7887b891db7097a8075bfc63bc
Philipp G. Haselwarter <philipp@haselwarter.org>
2022-12-06 10:22:56 +0100

    presampled lazy/eager coin using locks
    The locks are axiomatised, as the standard construction using CAS is not
    expressible in our language. Could use Peterson's solution instead.

--- theories/examples/lazy.v
+++ theories/examples/lazy.v
@@ -1,7 +1,9 @@
 From stdpp Require Import namespaces.
+From iris Require Import invariants.
 From iris.proofmode Require Import proofmode.
 From iris.prelude Require Import options.
 From self Require Import spec_rules rel_rules rel_tactics notation types proofmode model spec_tactics.
+From self.examples Require Import lock.
 
 Notation "'match:' e0 'with' | 'NONE' => e1 | 'SOME' x => e2 'end'" :=
   (Match e0 BAnon e1 x%binder e2)
@@ -12,185 +14,109 @@ Notation "'match:' e0 'with' | 'SOME' x => e2 | 'NONE' => e1 'end'" :=
 
 Definition lazy α : expr :=
   let:"s" := ref NONE in
-  (λ:"_u", match: !"s" with
-           | NONE => let: "b" := flip #lbl:α in
-                     "s" <- SOME "b" ;;
-                     "b"
-           | SOME "b" => "b" end).
+  let: "lk" := newlock #() in
+  (λ:"_u",
+    acquire "lk" ;;
+    let:"r" := match: !"s" with
+               | NONE => let: "b" := flip #lbl:α in
+                         "s" <- SOME "b" ;;
+                         "b"
+               | SOME "b" => "b" end
+    in
+    release "lk" ;;
+    "r").
 
 Definition eager α : expr :=
   let:"b" := flip #lbl:α in
   (λ:"_u", "b").
 
-
 Section proofs.
   Context `{!prelocGS Σ}.
+  Context `{!lockG Σ}.
 
   Definition coinN := nroot.@"coins".
 
+  (** Lazy coin (with lock) refines eager coin *)
+  Definition I l α b bs := (l ↦ NONEV ∗ α ↪ (b::bs) ∨ l ↦ SOMEV #b)%I.
+
   (* Warmup: the flips have already been resolved. *)
-  Lemma lazy_eager_refinement α b bs :
+  Lemma lazy_eager_refinement α β b bs bs' :
     (* TODO: should instead assume that REL α << αₛ : lrel_tape *)
-    α ↪ₛ (b::bs) ∗ α ↪ (b::bs)
-    -∗ REL lazy α << eager α : lrel_unit → lrel_bool.
-  Proof.
-    iIntros "[Hαs Hα]".
+    α ↪ (b::bs) ∗ β ↪ₛ (b::bs')
+    -∗ REL lazy α << eager β : lrel_unit → lrel_bool.
+  Proof using lockG0 prelocGS0 Σ.
+    iIntros "[Hα Hβ]".
     rewrite /lazy /eager.
     rel_alloc_l l as "Hl".
     do 2 rel_pure_l.
+    rel_flip_r.
+    rel_apply_l (refines_newlock_l coinN (I l α b bs) with "[-]").
+    { unfold I. iLeft. iFrame. }
+    iIntros (lk γ) "!> #Hlk /=". rel_pures_l. rel_pures_r.
+    rel_arrow ; iIntros (v1 v2) "_".
+    rel_pure_r ; rel_pure_l.
+    rel_apply_l (refines_acquire_l with "Hlk").
+    iIntros "!> Hlocked HI".
+    rel_pures_l.
+    iDestruct "HI" as "[[l_None α_b] | l_Some_b]" ; rel_load_l ; rel_pures_l ;
+      last first.
+    - rel_apply_l (refines_release_l with "Hlk Hlocked [-]") ;
+        [iFrame | iNext].
+      rel_pures_l. rel_values.
+    - rel_bind_l (Flip _).
+      iApply (refines_wp_l [_;_]).
+      iApply (wp_flip with "α_b").
+      iIntros "!> α_bs /=".
+      rel_pures_l. rel_store_l. rel_pures_l.
+      (* same as before *)
+      rel_apply_l (refines_release_l with "Hlk Hlocked [-]") ;
+        [iFrame | iNext].
+      rel_pures_l. rel_values.
+  Qed.
 
-    rel_bind_r (Flip _).
-    rewrite refines_eq /refines_def.
-    iIntros (K) "HCs".
-    rewrite {1} /refines_right.
-    rewrite -fill_app.
-    iPoseProof ((step_flip _ _ α) with "[Hαs HCs]") as "> (Hs & Hb & Hαs)" ; auto.
-    { now iFrame. }
-    (* iModIntro. *)
-    rewrite fill_app.
-    fold refines_right.
-    iAssert (spec_ctx ∗ ⤇ fill K (fill [AppRCtx (λ: "b" "_u", "b")] #b))%I with "[Hs Hb]" as "HH"
-    ; [iFrame|].
-    replace (spec_ctx ∗ ⤇ fill K (fill [AppRCtx (λ: "b" "_u", "b")] #b))%I with
-      (refines_right K (fill [AppRCtx (λ: "b" "_u", "b")] #b)) by reflexivity.
-    iRevert (K) "HH".
-    rewrite -/refines_def.
-
-    (* TODO: move to rel_tactics *)
-    replace (∀ a : list (ectxi_language.ectx_item prob_ectxi_lang),
-                refines_right a (fill [AppRCtx (λ: "b" "_u", "b")] #b) ={⊤}=∗
-                                                                          WP λ: "_u",
-               match: ! #l with
-                 InjL <> => let: "b" := flip #lbl:α in #l <- InjR "b";; "b"
-               | InjR "b" => "b"
-               end
-                 {{ v, ∃ v' : val, refines_right a v' ∗ (() → lrel_bool)%lrel v v' }}
-            )%I
-      with
-      (REL
-         λ: "_u",
-        match: ! #l with
-          InjL <> => let: "b" := flip #lbl:α in #l <- InjR "b";; "b"
-        | InjR "b" => "b"
-        end
-          << (fill [AppRCtx (λ: "b" "_u", "b")] #b)
-        : () → lrel_bool)%I.
-    2:{ rewrite refines_eq /refines_def ; reflexivity. }
-    simpl.
-
-    rel_pure_l.
-    do 3 rel_pure_r.
-
-    iMod (invariants.inv_alloc coinN _
-
-                    (l ↦ NONEV ∗ α ↪ (b::bs) ∨ l ↦ SOMEV #b)%I
-
-           (* (∃ (b : bool), *)
-           (*     is_locked_r lk false *)
-           (*       ∗ ce ↦ #b *)
-           (*       ∗ (cl ↦ₛ NONEV ∨ cl ↦ₛ SOMEV #b) *)
-           (* )%I *)
+  Lemma eager_lazy_refinement α β b bs bs' :
+    α ↪ (b::bs) ∗ β ↪ₛ (b::bs')
+    -∗ REL eager α << lazy β : lrel_unit → lrel_bool.
+  Proof using lockG0 prelocGS0 Σ.
+    iIntros "[Hα Hβ]".
+    rewrite /lazy /eager.
+    rel_alloc_r l as "Hl".
+    do 2 rel_pure_r.
+    rel_flip_l.
+    rel_apply_r (refines_newlock_r).
+    iIntros (lk) "Hlk".
+    iMod (inv_alloc coinN _
+           (is_locked_r lk false ∗
+              (β ↪ₛ (b::bs') ∗ l ↦ₛ NONEV ∨ l ↦ₛ SOMEV #b))%I
             with "[-]") as "#Hinv".
-    { iNext. iLeft. iFrame. }
-
-    iApply refines_arrow.
-    (* rel_arrow. *)
-    iIntros (v1 v2) "!> _".
-
+    { iNext. iFrame. iLeft. iFrame. }
+    rel_pures_l ; rel_pures_r.
+    rel_arrow ; iIntros (v1 v2) "_".
     rel_pure_r.
-
-    (* iInv coinN as "HHH" "Hclose". *)
-
-    (* rel_pure_l doesn't give us a ▷ in the goal, this does. *)
-    rel_pure_l.
-    rel_load_l_atomic.
-    iInv coinN as "> [[H G] | H]" "Hclose"; first last.
-
-    (* The second branch of lazy, amounting to #b << #b : bool. *)
-    - iModIntro. iExists _.
-      iFrame.
-      iNext.
-      iIntros "Hl".
-      rel_pures_l.
-      rel_values.
-      iMod ("Hclose" with "[Hl]").
-      + eauto.
-      + eauto.
-    - iModIntro.
-      iExists _.
-      iFrame.
-      iNext.
-      iIntros "Hl".
-      rel_pures_l.
-      rel_bind_l (Flip _).
-
-      (* Should be [iApply refines_wp_l], but that one only seems to work with the top mask.
-         Let's replay that proof and see where it gets stuck. *)
-      rewrite refines_eq /refines_def.
-      iIntros (j) "Hs /=".
-
-      iMod ("Hclose" with "[Hl]").
-
-
-      iMod wp_flip.
-
-
-    iModIntro. iApply wp_bind.
-    iApply (wp_wand with "He").
-    iIntros (v) "Hv".
-    by iMod ("Hv" with "Hs").
-
-    iApply refines_wp_l.
-
-    iApply wp_flip.
-
-    rel_load_l.
-
-
-
-  iStartProof;
-  rel_reshape_cont_l
-
-    ltac:(fun K e' =>
-      (* unify e' ef; *)
-      (* (iApply refines_pure_l || idtac "nope") ; *)
-      eapply (tac_rel_pure_l K e');
-      [reflexivity                  (** e = fill K e' *)
-      |iSolveTC                     (** PureClosed ϕ e' e2 *)
-      | .. ]);
-      [try prob_lang.proofmode.solve_vals_compare_safe                (** φ *)
-      |
-        (* first [left; split; reflexivity              (** Here we decide if the mask E is ⊤ *) *)
-        (*      | right; reflexivity]                  (**    (m = n ∧ E = ⊤) ∨ (m = 0) *) *)
-        ..
-      |iSolveTC                                     (** IntoLaters *)
-      |simpl; reflexivity           (** eres = fill K e2 *)
-      |rel_finish                   (** new goal *)]
-  (* || fail "rel_pure_l: cannot find the reduct" *).
-
-Unshelve.
-
-
-    rel_bind_l (_ _). iApply refines_pure_l ; auto ; simpl.
-
-    rel_pure_l.
-
-
-    rel_pure_l ; rel_pure_r ; clear v1 v2.
-
-    iInv coinN as "HHH" "Hclose".
-
-    rel_load_l.
-
-    rel_load_l_atomic.
-
+    (* We need to open Hinv in order to acquire the lock on the right. In order
+       to use the invariant, we need to step on the left to produce a later. *)
+    rel_bind_l (_ _).
+    iApply refines_atomic_l .
+    iInv coinN as "Hlk" "Hclose".
     iModIntro.
-    iExists _.
-    iApply bi.later_sep_1.
-    iNext.
-    iDestruct "HHH" as "[[ Hl Hα ] | Hl ]".
-    - iFrame.
-      iIntros "Hl".
-      repeat rel_pure_l.
-      admit.
-    - iIntros.
+    wp_pures.
+    iIntros "!> /=".
+    rel_bind_r (acquire _).
+    iDestruct "Hlk" as "[Hlk [(Hβ & Hl) | Hl]]" ;
+      rel_apply_r (refines_acquire_r with "Hlk");
+      iIntros "Hlk" ;
+      rel_pures_r ; rel_load_r ; rel_pures_r.
+    - rel_bind_r (Flip _).
+      iApply refines_step_r.
+      iIntros (K) "[??]".
+      iMod (step_flip with "[$]") as "(? & ? & Hβ)" ; [solve_ndisj|].
+      iModIntro.
+      iExists _. iFrame. simpl.
+      rel_pures_r. rel_store_r. rel_pures_r.
+      rel_apply_r (refines_release_r with "Hlk [-]") ; iIntros "Hlk".
+      rel_pures_r. rel_values.
+      iMod ("Hclose" with "[Hlk Hl]") ; [iFrame | eauto].
+    - rel_apply_r (refines_release_r with "Hlk [-]") ; iIntros "Hlk".
+      rel_pures_r. rel_values.
+      iMod ("Hclose" with "[Hlk Hl]") ; eauto with iFrame.
+Qed.
