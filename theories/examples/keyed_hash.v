93fb4546fd8cc2d4b2472b7033a2d597dd7001de
Joseph Tassarotti <jt4767@nyu.edu>
2022-12-29 22:09:08 -0500

    Checkpoint more silly arithmetic for keyed hash example

--- theories/examples/keyed_hash.v
+++ theories/examples/keyed_hash.v
@@ -24,22 +24,25 @@ Section keyed_hash.
   (* we assume the key space / value space are integers in the range {0, ..., 2^n - 1} for some power n. *)
   Context (MAX_KEYS_POW : nat).
   Context (MAX_VALS_POW : nat).
-  
+
   Definition MAX_KEYS : nat := (Nat.pow 2 MAX_KEYS_POW) - 1.
   Definition MAX_VALS : nat := (Nat.pow 2 MAX_VALS_POW) - 1.
   Definition MAX_KEYS_Z : Z := (Z.pow 2 MAX_KEYS_POW) - 1.
   Definition MAX_VALS_Z : Z := (Z.pow 2 MAX_VALS_POW) - 1.
-  
+
   Definition MAX_HASH_DOM : nat := ((Nat.pow 2 (MAX_KEYS_POW + MAX_VALS_POW)) - 1).
-  
+
   Definition enc : val :=
     λ: "k" "v", ("k" ≪ #MAX_VALS_POW) + "v".
-  
+
   Definition enc_gallina (k : nat) (v: nat) : nat :=
     (Nat.shiftl k MAX_VALS_POW) + v.
-  
+
+  Definition val_of_enc_gallina (x: nat) : nat :=
+    x `mod` (Nat.pow 2 MAX_VALS_POW).
+
   Definition key_of_enc_gallina (x: nat) : nat :=
-    (Nat.shiftr x MAX_VALS_POW).
+    (Nat.shiftr (x - val_of_enc_gallina x) MAX_VALS_POW).
 
   Lemma pow2_nonzero: ∀ x, 0 < 2 ^ x.
   Proof.
@@ -47,61 +50,123 @@ Section keyed_hash.
   Qed.
 
   Definition not_in_key x k : Prop := (¬ ∃ v, enc_gallina k v = x).
-  
+
+  Lemma val_of_enc_gallina_spec1 k v :
+    v <= MAX_VALS →
+    val_of_enc_gallina (enc_gallina k v) = v.
+  Proof.
+    rewrite /val_of_enc_gallina /enc_gallina/MAX_VALS.
+    rewrite ?Nat.shiftr_div_pow2 ?Nat.shiftl_mul_pow2.
+    intros Hv.
+    specialize (pow2_nonzero MAX_VALS_POW) => ?.
+    rewrite plus_comm Nat.mod_add; last lia.
+    rewrite Nat.mod_small; lia.
+  Qed.
+
+  Lemma val_of_enc_gallina_spec2 (x : nat) :
+    val_of_enc_gallina x <= MAX_VALS.
+  Proof.
+    specialize (pow2_nonzero MAX_VALS_POW) => ?.
+    cut (x `mod` (Nat.pow 2 MAX_VALS_POW) < Nat.pow 2 MAX_VALS_POW).
+    { rewrite /val_of_enc_gallina/MAX_VALS. lia. }
+    apply Nat.mod_upper_bound. lia.
+  Qed.
+
   Lemma key_of_enc_gallina_spec1 (x : nat) k v :
     v <= MAX_VALS →
     key_of_enc_gallina (enc_gallina k v) = k.
   Proof.
-    rewrite /key_of_enc_gallina /enc_gallina/MAX_VALS.
+    intros Hle.
+    rewrite /key_of_enc_gallina val_of_enc_gallina_spec1 //.
+    rewrite /enc_gallina/MAX_VALS.
+    rewrite Nat.add_sub.
+    rewrite Nat.shiftr_shiftl_r //.
+    assert (MAX_VALS_POW - MAX_VALS_POW = 0) as -> by lia.
+    rewrite Nat.shiftr_0_r //.
+  Qed.
+
+  Lemma Nat_div_sub_mod x k :
+   0 < k ->
+   x `div` k = (x - x `mod` k) `div` k.
+  Proof.
+    intros Hlt.
+    rewrite Nat.mod_eq; last by lia.
+    replace (x - (x - k * x `div` k)) with (k * x `div` k); last first.
+    { remember (x `div` k) as y. cut (k * y <= x); try lia.
+      rewrite Heqy. apply Nat.mul_div_le; lia. }
+    rewrite Nat.mul_comm Nat.div_mul; lia.
+  Qed.
+
+  Lemma enc_gallina_inv x :
+    enc_gallina (key_of_enc_gallina x) (val_of_enc_gallina x) = x.
+  Proof.
+    rewrite /enc_gallina/key_of_enc_gallina/val_of_enc_gallina.
     rewrite ?Nat.shiftr_div_pow2 ?Nat.shiftl_mul_pow2.
-    intros Hv.
+    symmetry.
+    rewrite {1}(Nat.div_mod_eq x (Nat.pow 2 MAX_VALS_POW)).
+    f_equal.
+    rewrite Nat.mul_comm.
+    rewrite Nat_div_sub_mod //. apply pow2_nonzero.
+  Qed.
+
+  Lemma enc_gallina_mono1 k1 k2 v1 v2:
+    k1 < k2 -> v1 <= v2 -> enc_gallina k1 v1 < enc_gallina k2 v2.
+  Proof.
+    intros Hlt Hle.
+    rewrite /enc_gallina.
+    rewrite ?Nat.shiftr_div_pow2 ?Nat.shiftl_mul_pow2.
+    specialize (pow2_nonzero MAX_VALS_POW) => Hnz.
+    remember (2 ^ MAX_VALS_POW) as z.
+    cut (k1 * z < k2 * z); first by lia.
+    apply Mult.mult_lt_compat_r_stt; auto.
+  Qed.
+
+  Lemma enc_gallina_mono2 k1 k2 v1 v2:
+    k1 < k2 -> v1 <= MAX_VALS -> v2 <= MAX_VALS -> enc_gallina k1 v1 < enc_gallina k2 v2.
+  Proof.
+    intros Hlt Hle1 Hle2.
+    rewrite /enc_gallina.
+    specialize (pow2_nonzero MAX_VALS_POW) => Hnz.
+    rewrite ?Nat.shiftr_div_pow2 ?Nat.shiftl_mul_pow2.
+    apply (Nat.lt_le_trans _ (k2 * 2 ^ MAX_VALS_POW)); last by lia.
+    apply (Nat.lt_le_trans _ ((k1 + 1) * 2 ^ MAX_VALS_POW)); last first.
+    { apply Nat.mul_le_mono_r; lia. }
+    ring_simplify.
+    rewrite /MAX_VALS in Hle1. lia.
+  Qed.
+
+  Lemma enc_gallina_mono2_inv k1 k2 v1 v2:
+    enc_gallina k1 v1 <= enc_gallina k2 v2 ->
+    v1 <= MAX_VALS ->
+    v2 <= MAX_VALS ->
+    k1 <= k2.
+  Proof.
+    intros Henc Hle1 Hle2.
+    destruct (decide (k2 < k1)) as [Hlt|]; try lia.
+    specialize (enc_gallina_mono2 _ _ _ _ Hlt Hle2 Hle1). lia.
+  Qed.
+
+  Lemma enc_hits_max :
+    enc_gallina MAX_KEYS MAX_VALS = MAX_HASH_DOM.
+  Proof.
+    rewrite /enc_gallina/MAX_HASH_DOM/MAX_KEYS/MAX_VALS.
+    rewrite ?Nat.shiftr_div_pow2 ?Nat.shiftl_mul_pow2.
+    rewrite Nat.pow_add_r.
     specialize (pow2_nonzero MAX_VALS_POW) => ?.
-    rewrite Nat.div_add_l; last lia.
-    rewrite Nat.div_small; lia.
+    specialize (pow2_nonzero MAX_KEYS_POW) => ?.
+    assert (2 ^ MAX_KEYS_POW = (2 ^ MAX_KEYS_POW - 1) + 1) as Hsub1 by lia.
+    lia.
   Qed.
 
   Lemma key_of_enc_gallina_spec2 (x : nat) :
     x <= MAX_HASH_DOM →
     key_of_enc_gallina x <= MAX_KEYS.
   Proof.
-    rewrite /key_of_enc_gallina /enc_gallina/MAX_VALS/MAX_KEYS/MAX_HASH_DOM.
-    rewrite ?Nat.shiftr_div_pow2 ?Nat.shiftl_mul_pow2.
-    specialize (pow2_nonzero MAX_VALS_POW) => ?.
-    intros Hle.
-    transitivity (MAX_HASH_DOM `div` 2 ^ MAX_VALS_POW); last first.
-    { cut (MAX_HASH_DOM `div` 2 ^ MAX_VALS_POW = MAX_KEYS).
-      { intros ->. rewrite /MAX_KEYS. lia. }
-      symmetry. Search Nat.div "mod".
-      rewrite /MAX_HASH_DOM.
-  Abort.
-  (*
-
-
-    cut (x `div` 2 ^ MAX_VALS_POW + 1 ≤ 2 ^ MAX_KEYS_POW); try lia; [].
-    rewrite plus_comm -Nat.div_add_l; last lia.
-    transitivity (1 * 2 ^ MAX_VALS_POW + (2 ^ (MAX_KEYS_POW + MAX_VALS_POW) - 1) 
-
-    { assert (x `div` 2 ^ MAX_VALS_POW + 2 ^ MAX_VALS_POW `div` 2 ^ MAX_VALS_POW
-                = ((1 * 2^MAX_VALS_POW + x) `div` 2^MAX_VALS_POW)).
-      { rewrite plus_comm. rewrite Nat.div_add_l; last lia.
-
-
-    rewrite Z
-
-
-    etransitivity.
-    { eapply (Nat.div_le_mono _ MAX_HASH_DOM); last eapply Hle; lia. }
-    rewrite /MAX_HASH_DOM.
-    transitivity (Z.to_nat ((Z.pow 2 (MAX_KEYS_POW + MAX_VALS_POW) - 1) / (2 ^ MAX_VALS_POW))%Z).
-    { admit. }
-    rewrite /Z.sub.
-    rewrite Zplus_comm.
-    rewrite Z.pow_add_r; try lia.
-    { rewrite Z_div_plus; try lia.
-      rewrite Z
-      rewrite -?Nat2Z.id -?Nat2Z.inj_add Nat2Z.inj_mul Nat2Z.inj_pow.
-  Abort.
-   *)
+    intros.
+    eapply (enc_gallina_mono2_inv _ _ (val_of_enc_gallina x) (MAX_VALS));
+      auto using val_of_enc_gallina_spec2.
+    rewrite enc_gallina_inv enc_hits_max //.
+  Qed.
 
   Lemma enc_gallina_range k v :
     k < S MAX_KEYS →
@@ -110,7 +175,7 @@ Section keyed_hash.
   Proof.
     rewrite /enc_gallina/MAX_KEYS/MAX_VALS/MAX_HASH_DOM.
     rewrite ?Nat.shiftl_mul_pow2.
-    assert (Hsub_le: ∀ a b, 0 < b -> a < S (b - 1) → a <= b - 1). 
+    assert (Hsub_le: ∀ a b, 0 < b -> a < S (b - 1) → a <= b - 1).
     { intros. lia. }
     assert (Hsub: ∀ x, 0 < x -> S (x - 1) = x).
     { intros. lia. }
@@ -131,12 +196,12 @@ Section keyed_hash.
   Qed.
 
   Definition init_keyed_hash : val :=
-    λ: "_", 
+    λ: "_",
       let: "f" := init_hash #MAX_HASH_DOM in
       (λ: "k" "v", "f" (enc "k" "v")).
 
   Context `{!prelogrelGS Σ}.
-  
+
   Lemma wp_enc_spec (k v : nat) E :
     {{{ ⌜ k <= MAX_KEYS ∧ v ≤ MAX_VALS ⌝ }}}
       enc #k #v @ E
@@ -150,7 +215,7 @@ Section keyed_hash.
     iApply "HΦ". auto.
   Qed.
 
-  
+
   Definition fin_hash_dom_space : Type := fin (S (MAX_HASH_DOM)).
   Definition fin_key_space : Type := fin (S (MAX_KEYS)).
   Definition fin_val_space : Type := fin (S (MAX_VALS)).
@@ -168,15 +233,15 @@ Section keyed_hash.
     refine (@nat_to_fin (enc_gallina (fin_to_nat k) (fin_to_nat v)) _ _).
     apply (enc_gallina_range); apply fin_to_nat_lt.
   Defined.
-  
+
   Definition keyed_hash_inv (γ : gname) (f : val) : iProp Σ :=
-    ∃ (f0 : val) (mphys : gmap nat bool) (mghost : gmap fin_hash_dom_space (option bool)), 
+    ∃ (f0 : val) (mphys : gmap nat bool) (mghost : gmap fin_hash_dom_space (option bool)),
       ⌜ f = (λ: "k" "v", f0 (enc "k" "v"))%V ⌝ ∗
       ⌜ ∀ x b, mphys !! (fin_to_nat x) = Some b → mghost !! x = Some (Some b) ⌝ ∗
       ⌜ ∀ x, mphys !! (fin_to_nat x) = None → mghost !! x = Some (None) ⌝ ∗
       ghost_map_auth γ 1 mghost ∗
       hashfun MAX_HASH_DOM f0 mphys.
-  
+
   (* This encoding is annoying to work with because we don't have good lemmas for
      "set products" and big_sepS over such products. *)
   (*
@@ -184,7 +249,7 @@ Section keyed_hash.
     ⌜ ∀ x, x ∈ dom m → x <= MAX_VALS ⌝ ∗
     [∗ set] v ∈ fin_to_set (fin_val_space), (enc_gallina_fin k v) ↪[γ] (m !! (fin_to_nat v)).
    *)
-  
+
   Definition not_in_key_fin x k : Prop := (¬ ∃ v, enc_gallina_fin k v = x).
 
   (* This encoding is equivalent to the above in some sense but ends up being more workable
@@ -193,10 +258,10 @@ Section keyed_hash.
      to the problem here. *)
   Definition khashfun_own γ k (m : gmap nat bool) : iProp Σ :=
     ⌜ ∀ x, x ∈ dom m → x <= MAX_VALS ⌝ ∗
-    [∗ set] kv ∈ fin_to_set (fin_hash_dom_space), 
+    [∗ set] kv ∈ fin_to_set (fin_hash_dom_space),
       (∃ v, ⌜ enc_gallina_fin k v = kv ⌝ ∗ kv ↪[γ] (m !! (fin_to_nat v))) ∨ ⌜ not_in_key_fin kv k ⌝.
-                                                                              
- 
+
+
   Lemma wp_init_keyed_hash E :
     {{{ True }}}
       init_keyed_hash #() @ E
