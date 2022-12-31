7d81ece764fa426c3a5af74a9ffd2785115326c1
Joseph Tassarotti <jt4767@nyu.edu>
2022-12-30 22:23:38 -0500

    wp for init rng generator and running a generator

--- theories/examples/split_rng.v
+++ theories/examples/split_rng.v
@@ -29,27 +29,140 @@ Section rng.
 
   Definition init_rng_gen : val :=
     λ: "_",
-      let: "f" := (init_keyed_hash MAX_RNGS_POW MAX_SAMPLES_POW) in
+      let: "f" := (init_keyed_hash MAX_RNGS_POW MAX_SAMPLES_POW) #() in
       let: "key_cntr" := ref #0 in
       (* We return an rng "generator" that gets a fresh key from key_cntr (if available) and returns
          an rng function using that key *)
       λ: "_",
         let: "k" := !"key_cntr" in
         if: #MAX_RNGS < "k" then
-          NONEV
+          NONE
         else
           "key_cntr" <- "k" + #1;;
           let: "sample_cntr" := ref #0 in
-          (λ: "_",
-            let: "v" := !"sample_cntr" in
-            if: #MAX_SAMPLES < "v" then
-              #false
-            else
-              let: "b" := "f" "k" "v" in
-              "sample_cntr" <- "v" + #1;;
-              "b").
+          SOME (λ: "_",
+               let: "v" := !"sample_cntr" in
+               if: #MAX_SAMPLES < "v" then
+                 #false
+               else
+                 let: "b" := "f" "k" "v" in
+                 "sample_cntr" <- "v" + #1;;
+                 "b").
+
+  Definition hash_rng_gen_specialized (f: val) (key_cntr: loc) : val :=
+      λ: "_",
+        let: "k" := ! #key_cntr in
+        if: #MAX_RNGS < "k" then
+          NONE
+        else
+          #key_cntr <- "k" + #1;;
+          let: "sample_cntr" := ref #0 in
+          SOME (λ: "_",
+               let: "v" := !"sample_cntr" in
+               if: #MAX_SAMPLES < "v" then
+                 #false
+               else
+                 let: "b" := f "k" "v" in
+                 "sample_cntr" <- "v" + #1;;
+                 "b").
+
+  Definition hash_rng_specialized (f : val) (k : nat) (c : loc) : val :=
+    (λ: "_",
+      let: "v" := !#c in
+      if: #MAX_SAMPLES < "v" then
+        #false
+      else
+        let: "b" := f #k "v" in
+        #c <- "v" + #1;;
+        "b").
+
 
   Context `{!prelogrelGS Σ}.
 
+  (* TODO: it would be better to wrap this ghost_mapG with keyed_mapG *)
+  Context {GHOST_MAP: ghost_mapG Σ (fin_hash_dom_space MAX_RNGS_POW MAX_SAMPLES_POW) (option bool)}.
+
+  Definition khashN := nroot.@"khash".
+
+  Definition is_keyed_hash γ f :=
+    na_inv prelogrelGS_nais khashN (keyed_hash_auth MAX_RNGS_POW MAX_SAMPLES_POW γ f).
+
+  (* Putting is_keyed_hash seems like it makes the definition but then this is not timeless *)
+
+  Definition hash_rng (n: nat) (g: val) : iProp Σ :=
+    ∃ h k c m γ, ⌜ g = hash_rng_specialized h (fin_to_nat k) c ⌝ ∗
+             ⌜ ∀ x, n <= x → x ∉ dom m ⌝ ∗
+             khashfun_own MAX_RNGS_POW MAX_SAMPLES_POW γ k m ∗
+             is_keyed_hash γ h ∗
+             c ↦ #n.
+
+  Definition hash_rng_gen (n: nat) (f: val) : iProp Σ :=
+    ∃ h kcntr γ, ⌜ f = hash_rng_gen_specialized h kcntr ⌝ ∗
+                  is_keyed_hash γ h ∗
+                  kcntr ↦ #n ∗
+                  [∗ set] k ∈ fin_to_set (fin_key_space MAX_RNGS_POW),
+                     (⌜ n <= fin_to_nat k ⌝ → khashfun_own _ MAX_SAMPLES_POW γ k ∅).
+
+  Lemma wp_init_rng_gen E :
+    {{{ True }}}
+      init_rng_gen #() @ E
+    {{{ (f: val), RET f; hash_rng_gen 0 f }}}.
+  Proof.
+    iIntros (Φ) "_ HΦ".
+    rewrite /init_rng_gen.
+    wp_pures.
+    wp_apply (wp_init_keyed_hash with "[//]").
+    iIntros (h) "H". iDestruct "H" as (γ) "(Hauth&Hks)".
+    wp_pures.
+    wp_alloc key_cntr as "Hkc".
+    wp_pures.
+    iAssert (|={E}=> is_keyed_hash γ h)%I with "[Hauth]" as ">#His_keyed".
+    { iApply na_inv_alloc. iNext. eauto. }
+    iModIntro. iApply "HΦ".
+    iExists _, _, _. iFrame "# ∗".
+    iSplit; first by eauto.
+    iApply (big_sepS_mono with "Hks").
+    iIntros (x Helem) "$"; auto.
+  Qed.
+
+  Lemma wp_run_rng_gen k f E :
+    k <= MAX_RNGS →
+    {{{ hash_rng_gen k f }}}
+      f #() @ E
+    {{{ (g: val), RET (SOMEV g); hash_rng_gen (S k) f ∗ hash_rng 0 g }}}.
+  Proof.
+    iIntros (Hle Φ) "Hgen HΦ".
+    iDestruct "Hgen" as (h kcntr γ ->) "(#His&Hknctr&Hks)".
+
+    iEval (rewrite /hash_rng_gen_specialized). wp_pures.
+    wp_load. wp_pures.
+    case_bool_decide; first by lia.
+    wp_pures.
+    wp_store.
+    wp_alloc sample_cntr as "Hsc".
+    wp_pures.
+    iModIntro.
+    rewrite /init_rng_gen.
+    iApply "HΦ".
+    assert (Hlt: k < S MAX_RNGS) by lia.
+    set (k' := (nat_to_fin Hlt : fin_key_space MAX_RNGS_POW)).
+    iDestruct (big_sepS_delete _ _ k' with "Hks") as "(Hk&Hks)".
+    { apply elem_of_fin_to_set. }
+    iSplitL "Hks Hknctr".
+    - iExists _, _, _. iSplit; first eauto. iFrame "#".
+      assert (Z.of_nat k + 1 = Z.of_nat (S k))%Z as -> by lia.
+      iFrame "Hknctr".
+      iApply (big_sepS_delete _ _ k').
+      { apply elem_of_fin_to_set. }
+      iSplitR "Hks".
+      * iIntros (Hle'). iExFalso. rewrite /k' fin_to_nat_to_fin in Hle'. lia.
+      * iApply (big_sepS_mono with "Hks"). iIntros (??) "H %Hle'".
+        iApply "H". iPureIntro; lia.
+    - iExists _, k', _, ∅, _. iFrame "Hsc #". iSplit.
+      { rewrite /hash_rng_specialized. rewrite /k' fin_to_nat_to_fin //. }
+      iSplit.
+      { iPureIntro. set_solver. }
+      iApply "Hk". rewrite /k' fin_to_nat_to_fin; auto.
+  Qed.
 
 End rng.
