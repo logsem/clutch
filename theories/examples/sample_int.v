3ce7a8cc3eab98767e756b1e35433304c2c0eb11
Joseph Tassarotti <jt4767@nyu.edu>
2023-01-02 08:09:05 -0500

    Coupling lemma for int tape

--- theories/examples/sample_int.v
+++ theories/examples/sample_int.v
@@ -11,12 +11,12 @@ From self.prelude Require Import base.
 
    It works by just sampling n individual bits and shifting them to
    form the integer. The sampler is parameterized by a tape to use.
-   One can see the bits on the tape as giving a (possibly prefix) of a
-   big endian encoding of integers to be sampled.
+   One can see the bits on the tape as giving a
+   big endian encoding of integers to be sampled (or a partial prefix thereof).
 
-   Currently we only support using the built-in flip
-   () as the primitive for sampling bits, though in principle one
-   could paramterize by a function for generating bits.
+   Currently we only support using the built-in flip as the primitive
+   for sampling bits, though in principle one could paramterize by a
+   function for generating bits.
 
  *)
 
@@ -53,6 +53,46 @@ Section int.
     | O => []
     | S n' => (Z.testbit z n') :: (Z_to_bool_list z n')
     end.
+
+  Fixpoint bool_list_to_Z (bs : list bool) : Z :=
+    match bs with
+    | [] => 0
+    | b :: bs =>
+        Z.shiftl (Z.b2z b) (length bs) + bool_list_to_Z bs
+    end.
+
+  Lemma pow2_nonzero: ∀ x, 0 < 2 ^ x.
+  Proof.
+    intros x. induction x => /=; try lia.
+  Qed.
+
+  Lemma bool_list_to_Z_lower_bound bs :
+    (0 ≤ bool_list_to_Z bs)%Z.
+  Proof.
+    induction bs as [| b bs] => //=.
+    rewrite Z.shiftl_mul_pow2; last by lia.
+    specialize (pow2_nonzero (length bs)).
+    destruct b => //=; try lia.
+  Qed.
+
+  Lemma bool_list_to_Z_upper_bound bs :
+    (bool_list_to_Z bs < 2 ^ length bs)%Z.
+  Proof.
+    induction bs as [| b bs] => //=.
+    rewrite Z.shiftl_mul_pow2; last by lia.
+    remember (length bs) as n.
+    replace (2 ^ (S n)%Z)%Z with (2^n * 2)%Z; last first.
+    { replace (Z.of_nat (S n)) with (1 + Z.of_nat n)%Z by lia.
+      rewrite Z.pow_add_r //; try lia. }
+    destruct b => //=; try lia.
+  Qed.
+
+  Lemma bool_list_to_Z_upper_bound' bs :
+    (bool_list_to_Z bs ≤ Z.ones (length bs))%Z.
+  Proof.
+    specialize (bool_list_to_Z_upper_bound bs).
+    rewrite Z.ones_equiv; lia.
+  Qed.
 
   (*
   Compute (Z_to_bool_list 0 5).
@@ -99,6 +139,51 @@ Section int.
     rewrite Z.shiftl_mul_pow2; last by lia.
     rewrite Z.mul_comm. f_equal.
     apply Z.testbit_spec'; lia.
+  Qed.
+
+  Instance b2z_inj :
+    Inj eq eq (Z.b2z).
+  Proof. intros [|] [|] => //=. Qed.
+
+  Lemma Z_to_bool_list_mod_pow2 z n :
+    Z_to_bool_list z n =
+    Z_to_bool_list (z `mod` 2^n) n.
+  Proof.
+    revert z.
+    induction n => //= z.
+    f_equal; last first.
+    { rewrite IHn. rewrite [a in _ = a]IHn.
+      f_equal. symmetry.
+      replace (2 ^ (S n)%Z)%Z with (2^n * 2)%Z; last first.
+      { replace (Z.of_nat (S n)) with (1 + Z.of_nat n)%Z by lia.
+        rewrite Z.pow_add_r //; try lia. }
+      rewrite Z.rem_mul_r; try lia.
+      rewrite Z.mul_comm.
+      rewrite Z.mod_add; last by lia.
+      rewrite Zmod_mod //.
+    }
+    symmetry. apply Z.mod_pow2_bits_low; lia.
+  Qed.
+
+  Lemma bool_list_to_Z_to_bool_list bs :
+    Z_to_bool_list (bool_list_to_Z bs) (length bs) = bs.
+  Proof.
+    induction bs as [| b bs IH] => //=.
+    f_equal.
+    - apply (inj Z.b2z).
+      rewrite Z.testbit_spec'; last by lia.
+      rewrite Z.shiftl_mul_pow2; last by lia.
+      specialize (pow2_nonzero (length bs)) => ?.
+      rewrite Z.div_add_l; last by lia.
+      rewrite Z.div_small; last first.
+      { split; auto using bool_list_to_Z_lower_bound, bool_list_to_Z_upper_bound. }
+      destruct b => //=.
+    - rewrite -[a in _ = a]IH.
+      rewrite Z_to_bool_list_mod_pow2. f_equal.
+      rewrite Z.shiftl_mul_pow2; last by lia.
+      rewrite Z.add_comm Z_mod_plus; last by lia.
+      apply Z.mod_small.
+      { split; auto using bool_list_to_Z_lower_bound, bool_list_to_Z_upper_bound. }
   Qed.
 
   Definition int_tape α (zs: list Z) : iProp Σ :=
@@ -173,6 +258,10 @@ Section int.
     flat_map f (x :: l) = (f x) ++ flat_map f l.
   Proof. rewrite //=. Qed.
 
+  Lemma flat_map_singleton {A B: Type} (f: A → list B) (x: A) :
+    flat_map f [x] = (f x).
+  Proof. rewrite /= app_nil_r //. Qed.
+
   Lemma Zland_ones_small (z: Z) (n : nat):
     (0 ≤ z < 2 ^ n)%Z →
     Z.land z (Z.ones n) = z.
@@ -215,6 +304,47 @@ Section int.
     rewrite Zland_ones_small in Heq; last auto.
     rewrite Heq.
     iFrame. eauto.
+  Qed.
+
+  Lemma wp_couple_int_tapes_eq E e α αₛ zs zsₛ Φ:
+    to_val e = None →
+    nclose specN ⊆ E →
+    spec_ctx ∗ spec_int_tape αₛ zsₛ ∗ int_tape α zs ∗
+    ((∃ z: Z, (⌜ 0 ≤ z ≤ MAX_INT ⌝%Z ∗ spec_int_tape αₛ (zsₛ ++ [z]) ∗ int_tape α (zs ++ [z])))
+              -∗ WP e @ E {{ Φ }}) -∗
+    WP e @ E {{ Φ }}.
+  Proof.
+    iIntros (Hnv HN) "(#Hctx&Hαₛ&Hα&Hwp)".
+    iDestruct "Hα" as (?) "Hα".
+    iDestruct "Hαₛ" as (?) "Hαₛ".
+    iApply (wp_couple_tapesN_eq _ (NUM_BITS)); try done.
+    iFrame "Hα Hαₛ Hctx".
+    iDestruct 1 as (bs Hlen) "(Hαₛ&Hα)".
+    iApply "Hwp".
+
+    assert (Hrange: (0 ≤ bool_list_to_Z bs ≤ MAX_INT)%Z).
+    { split.
+      - apply bool_list_to_Z_lower_bound.
+      - rewrite /MAX_INT -Hlen. apply bool_list_to_Z_upper_bound'.
+    }
+    iExists (bool_list_to_Z bs). iSplit; first done.
+    iSplitL "Hαₛ".
+    { rewrite /spec_int_tape.
+      rewrite ?flat_map_app ?flat_map_singleton.
+      iSplit.
+      { iPureIntro; apply Forall_app; split; auto. econstructor; eauto.
+        rewrite /MAX_INT ?Z.ones_equiv in Hrange. lia. }
+      rewrite -Hlen bool_list_to_Z_to_bool_list.
+      iFrame.
+    }
+    { rewrite /int_tape.
+      rewrite ?flat_map_app ?flat_map_singleton.
+      iSplit.
+      { iPureIntro; apply Forall_app; split; auto. econstructor; eauto.
+        rewrite /MAX_INT ?Z.ones_equiv in Hrange. lia. }
+      rewrite -Hlen bool_list_to_Z_to_bool_list.
+      iFrame.
+    }
   Qed.
 
 End int.
