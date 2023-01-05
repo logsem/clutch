535444ec26833562d0db9ec990fbdc8357f8148d
Simon Gregersen <gregersen@cs.au.dk>
2023-01-05 18:35:17 +0100

    working contextual_refinement_alt

--- theories/typing/contextual_refinement_alt.v
+++ theories/typing/contextual_refinement_alt.v
@@ -1,7 +1,9 @@
 From Autosubst Require Import Autosubst.
+From Coquelicot Require Import Rcomplements Rbar Series Lim_seq Hierarchy.
 From self.prob_lang Require Export lang.
 From self.prob_lang Require Import tactics.
 From self.typing Require Export contextual_refinement.
+From self.prob Require Import distribution.
 
 (** Alternative formulation of contextual refinement without restricting to
     contexts of the ground type but only observing termination through their
@@ -11,17 +13,112 @@ Definition ctx_refines_alt (Γ : stringmap type)
   typed_ctx K Γ τ ∅ τ' →
   (SeriesC (lim_exec_val (fill_ctx K e, σ₀)) <= SeriesC (lim_exec_val (fill_ctx K e', σ₀)))%R.
 
+Notation SeqV e1 e2 := (LamV BAnon e2%E e1%E).
+
+Lemma prim_step_true_no_val e σ n :
+  to_val e = None →
+  prim_step_or_val ((SeqV e #true)%E, σ) ≫= exec_val n =
+    (prim_step e σ) ≫= (λ '(e', σ'), exec_val n ((SeqV e' #true)%E, σ')).
+Proof.
+  intros He.
+  rewrite 1!prim_step_or_val_no_val /=; [|done].
+  replace (SeqV e #true)%E with (fill [(AppRCtx (LamV <> #true)%E)] e); [|done].
+  rewrite fill_dmap //=.
+  rewrite /dmap.
+  rewrite -dbind_assoc -/exec_val.
+  eapply dbind_eq; [|done].
+  intros [e' σ'] Hs.
+  rewrite dret_id_left //.
+Qed.
+
+Lemma prim_step_true_val e σ n v :
+  to_val e = Some v →
+  prim_step_or_val ((SeqV e #true)%E, σ) ≫= exec_val n =
+    exec_val n ((of_val #true)%E, σ).
+Proof.
+  intros He.
+  rewrite 1!prim_step_or_val_no_val /=; [|done].
+  apply of_to_val in He. rewrite -He.
+  rewrite head_prim_step_eq /=; last first.
+  { eexists (_, _); simpl.
+    eapply head_step_support_equiv_rel.
+    by econstructor. }
+  erewrite det_head_step_singleton; [|by econstructor]; simpl.
+  rewrite dret_id_left -/exec_val //.
+Qed.
+
+Lemma exec_val_SeriesC_SeqV_true e σ n :
+  exec_val (S n) (SeqV e #true, σ) #true = SeriesC (exec_val n (e, σ)).
+Proof.
+  move: e σ; induction n; intros e σ.
+  - rewrite exec_val_Sn.
+    destruct (to_val e) eqn:Heq.
+    + setoid_rewrite prim_step_true_val; eauto; simpl.
+      rewrite Heq dret_mass
+        dret_1_1; auto.
+    + setoid_rewrite prim_step_true_no_val; eauto; simpl.
+      rewrite Heq.
+      rewrite SeriesC_0; auto.
+      rewrite /pmf/=/dbind_pmf.
+      setoid_rewrite SeriesC_0; auto.
+      intros (? & ?).
+      rewrite Rmult_0_r; auto.
+  - setoid_rewrite exec_val_Sn.
+    destruct (to_val e) eqn:Heq.
+    + rewrite (prim_step_true_val _ _ _ v); auto.
+      rewrite {1}/prim_step_or_val/= Heq.
+      assert (SeriesC (exec_val n (e, σ)) = SeriesC (dret (e, σ) ≫= exec_val n)) as Haux.
+      { apply SeriesC_ext; intro. rewrite dret_id_left. auto. }
+      rewrite -Haux.
+      rewrite exec_val_unfold /= Heq.
+      rewrite dret_mass.
+      rewrite dret_1_1; auto.
+    + rewrite prim_step_true_no_val; auto.
+      rewrite prim_step_or_val_no_val; auto.
+      rewrite /pmf/=/dbind_pmf.
+      rewrite distr_double_swap.
+      apply SeriesC_ext; intro.
+      rewrite SeriesC_scal_l.
+      rewrite (Rmult_eq_compat_l (prim_step e σ n0)
+                 ((let '(e', σ') := n0 in prim_step (SeqV e' #true) σ' ≫= exec_val n) #true)
+                 (SeriesC (exec_val n n0))); auto.
+      destruct n0. rewrite -IHn.
+      rewrite exec_val_Sn.
+      rewrite prim_step_or_val_no_val //.
+Qed.
+
+Lemma lim_exec_val_SeriesC_SeqV_true e σ :
+  SeriesC (lim_exec_val (e, σ)) = (lim_exec_val (SeqV e #true, σ)) #true.
+Proof.
+  rewrite lim_exec_val_rw.
+  erewrite SeriesC_ext;
+    [ | intro; apply lim_exec_val_rw].
+  simpl.
+  rewrite (MCT_seriesC _ (λ n, SeriesC (exec_val n (e,σ)))
+             (Sup_seq (λ n0 : nat, SeriesC (λ n : val, exec_val n0 (e, σ) n)))) .
+  - rewrite (mon_sup_succ (λ n : nat, exec_val n ((SeqV e #true)%E, σ) #true)).
+    + f_equal. apply Sup_seq_ext; intro n.
+      rewrite (exec_val_SeriesC_SeqV_true e σ n); auto.
+    + intro; apply exec_val_mon.
+  - intros; auto.
+  - intros; apply exec_val_mon.
+  - intros; exists 1; intros; auto.
+  - intros. apply SeriesC_correct; auto.
+  - rewrite (Rbar_le_sandwich 0 1); auto.
+    + apply Sup_seq_correct.
+    + apply (Sup_seq_minor_le _ _ 0%nat); simpl; auto.
+    + apply upper_bound_ge_sup; intro; simpl; auto.
+Qed.
+
 Lemma ctx_refines_impl_alt Γ e1 e2 τ :
-  (Γ ⊨ e1 ≤ctx≤ e2 : τ) →
-  ctx_refines_alt Γ e1 e2 τ.
+  (Γ ⊨ e1 ≤ctx≤ e2 : τ) → ctx_refines_alt Γ e1 e2 τ.
 Proof.
   intros H K σ0 τ' Hty.
-  pose (K' := (CTX_AppR (λ: <>, #true)%E):: K).
-  cut (∀ e, lim_exec_val ((e;; #true)%E, σ0) #true = SeriesC (lim_exec_val (e, σ0))).
+  pose (K' := (CTX_AppR (LamV BAnon #true)%E):: K).
+  cut (∀ e, lim_exec_val (SeqV e #true, σ0) #true = SeriesC (lim_exec_val (e, σ0))).
   - intros Heq. rewrite -2!Heq.
     eapply (H K' σ0 true).
     repeat econstructor; eauto.
   - intros e.
-    (* TODO: needs some more facts about [lim_exec_val]*)
-
-Admitted.
+    rewrite lim_exec_val_SeriesC_SeqV_true //.
+Qed.
