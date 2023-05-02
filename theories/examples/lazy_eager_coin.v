f7a09b13f76e8d491e774526cfe0fcb99912b142
Simon Gregersen <gregersen@cs.au.dk>
2023-05-02 14:59:14 +0200

    flip library and recover lazy_eager_coin.v

--- theories/examples/lazy_eager_coin.v
+++ theories/examples/lazy_eager_coin.v
@@ -4,6 +4,7 @@ From self.prob_lang Require Import notation proofmode primitive_laws spec_rules.
 From self.logrel Require Import model rel_rules rel_tactics.
 From self.typing Require Import soundness.
 From self.prelude Require Import base.
+From self.examples.lib Require Import flip.
 Set Default Proof Using "Type*".
 
 (** The lazy/eager coins, without tapes *)
@@ -11,24 +12,24 @@ Definition lazy : expr :=
   let: "s" := ref NONEV in
   λ: <>,
     match: !"s" with
-    | NONE => let: "b" := flip #() in
+    | NONE => let: "b" := flip in
               "s" <- SOME "b" ;;
               "b"
     | SOME "b" => "b"
     end.
 
 Definition eager : expr :=
-  let: "b" := flip #() in
+  let: "b" := flip in
   λ: <>, "b".
 
 (** An intetermedaite version of [lazy] that uses a tape to allow presampling
     bits during the proof *)
 Definition lazy_with_tape : expr :=
-  let: "α" := alloc #1%nat in
+  let: "α" := allocB in
   let: "s" := ref NONEV in
   λ: <>,
     match: !"s" with
-    | NONE => let: "b" := flip "α" in
+    | NONE => let: "b" := flipL "α" in
               "s" <- SOME "b" ;;
               "b"
     | SOME "b" => "b"
@@ -41,17 +42,17 @@ Section logical_ref.
 
   (** lazy << lazy_with_tape << eager *)
   Lemma lazy_lazy_with_tape_rel :
-    ⊢ REL lazy << lazy_with_tape : () → lrel_nat.
+    ⊢ REL lazy << lazy_with_tape : () → lrel_bool.
   Proof.
     rewrite /lazy /lazy_with_tape.
     rel_alloc_l l as "Hl".
     rel_pures_l.
-    rel_alloctape_r α as "Hα".
+    rel_allocBtape_r α as "Hα".
     rel_pures_r.
     rel_alloc_r l' as "Hl'".
     rel_pures_r.
-    set (P := (α ↪ₛ (1%nat; []) ∗ (l ↦ NONEV ∗ l' ↦ₛ NONEV ∨
-               ∃ (b : fin 2), l ↦ SOMEV #b ∗ l' ↦ₛ SOMEV #b))%I).
+    set (P := (α ↪ₛB [] ∗ (l ↦ NONEV ∗ l' ↦ₛ NONEV ∨
+               ∃ b : bool, l ↦ SOMEV #b ∗ l' ↦ₛ SOMEV #b))%I).
     iApply (refines_na_alloc P coinN).
     iSplitL.
     { iFrame. iLeft. iFrame. }
@@ -64,9 +65,9 @@ Section logical_ref.
     iIntros "[(Hα & [[Hl Hl'] | >[%b [Hl Hl']]]) Hclose]".
     - rel_load_l. rel_pures_l.
       rel_load_r. rel_pures_r.
-      rel_bind_l (rand _ from _)%E.
-      rel_bind_r (rand _ from _)%E.
-      iApply (refines_couple_rands_r with "[-$Hα]").
+      rel_bind_l flip.
+      rel_bind_r (flipL _)%E.
+      iApply (refines_couple_flip_flipL with "[-$Hα]").
       iIntros (b) "Hα /=".
       rel_pures_l. rel_store_l. rel_pures_l.
       rel_pures_r. rel_store_r. rel_pures_r.
@@ -81,16 +82,17 @@ Section logical_ref.
   Qed.
 
   Lemma lazy_with_tape_eager_rel :
-    ⊢ REL lazy_with_tape << eager : () → lrel_nat.
+    ⊢ REL lazy_with_tape << eager : () → lrel_bool.
   Proof.
     rewrite /lazy_with_tape /eager.
-    rel_alloctape_l α as "Hα". rel_pures_l.
-    rel_bind_r (rand _ from _)%E.
-    iApply (refines_couple_tape_rand with "[$Hα]"); [done|].
+    rel_allocBtape_l α as "Hα".
+    rel_pures_l.
+    rel_bind_r flip.
+    iApply (refines_couple_tape_flip with "[$Hα]"); [solve_ndisj|done|].
     iIntros (b) "Hα /=".
     rel_pures_r.
     rel_alloc_l l as "Hl". rel_pures_l.
-    set (P := ((α ↪ (1; [b]) ∗ l ↦ NONEV) ∨ (l ↦ SOMEV #b))%I).
+    set (P := ((α ↪B [b] ∗ l ↦ NONEV) ∨ (l ↦ SOMEV #b))%I).
     iApply (refines_na_alloc P coinN).
     iSplitL.
     { iModIntro. iLeft. iFrame. }
@@ -101,7 +103,7 @@ Section logical_ref.
     iApply (refines_na_inv with "[$Hinv]"); [done|].
     iIntros "[[[Hα Hl] | Hl] Hclose]".
     - rel_load_l. rel_pures_l.
-      rel_rand_l. rel_pures_l.
+      rel_flipL_l. rel_pures_l.
       rel_store_l. rel_pures_l.
       iApply (refines_na_close with "[- $Hclose]").
       iSplitL; [|rel_values].
@@ -114,16 +116,16 @@ Section logical_ref.
 
   (** eager << lazy_with_tape << lazy *)
   Lemma eager_lazy_with_tape_rel :
-    ⊢ REL eager << lazy_with_tape : () → lrel_nat.
+    ⊢ REL eager << lazy_with_tape : () → lrel_bool.
   Proof.
     rewrite /lazy_with_tape /eager.
-    rel_alloctape_r α as "Hα". rel_pures_r.
-    rel_bind_l (rand _ from _ )%E.
-    iApply (refines_couple_rand_tape with "[$Hα]").
+    rel_allocBtape_r α as "Hα". rel_pures_r.
+    rel_bind_l flip.
+    iApply (refines_couple_flip_tape with "[$Hα]").
     iIntros (b) "Hα /=".
     rel_pures_r.
     rel_alloc_r l as "Hl". rel_pures_r.
-    set (P := ((α ↪ₛ (1; [b]) ∗ l ↦ₛ NONEV) ∨ (l ↦ₛ SOMEV #b))%I).
+    set (P := ((α ↪ₛB [b] ∗ l ↦ₛ NONEV) ∨ (l ↦ₛ SOMEV #b))%I).
     iApply (refines_na_alloc P coinN).
     iSplitL.
     { iModIntro. iLeft. iFrame. }
@@ -134,7 +136,7 @@ Section logical_ref.
     iApply (refines_na_inv with "[$Hinv]"); [done|].
     iIntros "[[>[Hα Hl] | >Hl] Hclose]".
     - rel_load_r. rel_pures_r.
-      rel_rand_r. rel_pures_r.
+      rel_flipL_r. rel_pures_r.
       rel_store_r. rel_pures_r.
       iApply (refines_na_close with "[- $Hclose]").
       iSplitL; [|rel_values].
@@ -146,14 +148,14 @@ Section logical_ref.
   Qed.
 
   Lemma lazy_with_tape_lazy_rel :
-    ⊢ REL lazy_with_tape << lazy : () → lrel_nat.
+    ⊢ REL lazy_with_tape << lazy : () → lrel_bool.
   Proof.
     rewrite /lazy /lazy_with_tape.
     rel_alloc_r l' as "Hl'". rel_pures_l.
-    rel_alloctape_l α as "Hα". rel_pures_l.
+    rel_allocBtape_l α as "Hα". rel_pures_l.
     rel_alloc_l l as "Hl". rel_pures_l.
-    set (P := (α ↪ (1; []) ∗ (l ↦ NONEV ∗ l' ↦ₛ NONEV ∨
-               ∃ (b: fin 2), l ↦ SOMEV #b ∗ l' ↦ₛ SOMEV #b))%I).
+    set (P := (α ↪B [] ∗ (l ↦ NONEV ∗ l' ↦ₛ NONEV ∨
+               ∃ (b : bool), l ↦ SOMEV #b ∗ l' ↦ₛ SOMEV #b))%I).
     iApply (refines_na_alloc P coinN).
     iSplitL.
     { iFrame. iLeft. iFrame. }
@@ -166,9 +168,9 @@ Section logical_ref.
     iIntros "[(Hα & [[Hl Hl'] | >[%b [Hl Hl']]]) Hclose]".
     - rel_load_l. rel_pures_l.
       rel_load_r. rel_pures_r.
-      rel_bind_l (rand _ from _)%E.
-      rel_bind_r (rand _ from _)%E.
-      iApply (refines_couple_rands_l with "[-$Hα]").
+      rel_bind_l (flipL _)%E.
+      rel_bind_r flip.
+      iApply (refines_couple_flipL_flip with "[-$Hα]"); [solve_ndisj|].
       iIntros (b) "Hα /=".
       rel_pures_l. rel_store_l. rel_pures_l.
       rel_pures_r. rel_store_r. rel_pures_r.
@@ -185,7 +187,7 @@ Section logical_ref.
 End logical_ref.
 
 Theorem lazy_eager_refinement :
-  ∅ ⊨ lazy ≤ctx≤ eager : () → TNat.
+  ∅ ⊨ lazy ≤ctx≤ eager : () → TBool.
 Proof.
   eapply ctx_refines_transitive.
   - eapply (refines_sound prelogrelΣ).
@@ -195,7 +197,7 @@ Proof.
 Qed.
 
 Theorem eager_lazy_refinement :
-  ∅ ⊨ eager ≤ctx≤ lazy : () → TNat.
+  ∅ ⊨ eager ≤ctx≤ lazy : () → TBool.
 Proof.
   eapply ctx_refines_transitive.
   - eapply (refines_sound prelogrelΣ).
@@ -205,7 +207,7 @@ Proof.
 Qed.
 
 Theorem eager_lazy_equiv :
-  ∅ ⊨ lazy =ctx= eager : () → TNat.
+  ∅ ⊨ lazy =ctx= eager : () → TBool.
 Proof.
   split.
   - apply lazy_eager_refinement.
--- theories/examples/lib/flip.v
+++ theories/examples/lib/flip.v
@@ -1,30 +1,34 @@
-(** * Derived laws for [flip] *)
+(** * Derived laws for a fair coin flip *)
 From iris.base_logic Require Import invariants na_invariants.
+From iris.proofmode Require Import coq_tactics ltac_tactics sel_patterns environments reduction proofmode.
 From self.prob_lang Require Import notation proofmode primitive_laws spec_rules spec_tactics.
 From self.logrel Require Import model rel_rules rel_tactics.
 From self.prelude Require Import base.
-From self.examples.lib Require Import conversion. 
+From self.examples.lib Require Import conversion.
 
-Definition flipL (e : expr) := int_to_bool (rand #1%nat from e)%E.
-Definition flip := flipL #().
+Definition flipL : val := λ: "e", (int_to_bool (rand #1%nat from "e")).
+Definition flip : expr := flipL #().
 Definition allocB := alloc #1%nat.
 
-Notation "l ↪b bs" := (l ↪ (1; bool_to_fin <$> (bs : list bool)))%I
-  (at level 20, format "l  ↪b  bs") : bi_scope.
-Notation "l ↪ₛb bs" := (l ↪ₛ (1; bool_to_fin <$> (bs : list bool)))%I
-  (at level 20, format "l  ↪ₛb  bs") : bi_scope.
+Definition tapeB (bs : list bool) : tape := (1; bool_to_fin <$> bs).
+
+Notation "l ↪B bs" := (l ↪ tapeB bs)%I
+  (at level 20, format "l  ↪B  bs") : bi_scope.
+Notation "l ↪ₛB bs" := (l ↪ₛ tapeB bs)%I
+  (at level 20, format "l  ↪ₛB  bs") : bi_scope.
 
 Section specs.
   Context `{clutchRGS Σ}.
-   
+
   Lemma wp_allocB_tape E :
-    {{{ True }}} allocB @ E {{{ α, RET #lbl:α; α ↪b [] }}}.
+    {{{ True }}} allocB @ E {{{ α, RET #lbl:α; α ↪B [] }}}.
   Proof. iIntros (Φ) "_ HΦ". by iApply wp_alloc_tape. Qed.
 
   Lemma wp_flip E :
     {{{ True }}} flip @ E {{{ (b : bool), RET #(LitBool b); True }}}.
   Proof.
     iIntros (Φ) "_ HΦ". rewrite /flip /flipL.
+    wp_pures.
     wp_bind (rand _ from _)%E.
     wp_apply (wp_rand 1 with "[//]").
     iIntros (?) "_ /=".
@@ -34,10 +38,11 @@ Section specs.
   Qed.
 
   Lemma wp_flip_tape E α b bs :
-    {{{ ▷ α ↪b (b :: bs) }}} flipL #lbl:α @ E {{{ RET #(LitBool b); α ↪b bs }}}.
+    {{{ ▷ α ↪B (b :: bs) }}} flipL #lbl:α @ E {{{ RET #(LitBool b); α ↪B bs }}}.
   Proof.
-    iIntros (Φ) ">Hl HΦ". rewrite /flip /flipL. 
-    wp_bind (rand _ from _)%E.   
+    iIntros (Φ) ">Hl HΦ". rewrite /flip /flipL.
+    wp_pures.
+    wp_bind (rand _ from _)%E.
     wp_apply (wp_rand_tape 1 with "Hl").
     iIntros "Hl /=".
     wp_apply (wp_int_to_bool with "[//]").
@@ -47,9 +52,10 @@ Section specs.
   Qed.
 
   Lemma wp_flip_tape_empty E α :
-    {{{ ▷ α ↪b [] }}} flipL #lbl:α @ E {{{ b, RET #(LitBool b); α ↪b [] }}}.
+    {{{ ▷ α ↪B [] }}} flipL #lbl:α @ E {{{ b, RET #(LitBool b); α ↪B [] }}}.
   Proof.
     iIntros (Φ) ">Hl HΦ". rewrite /flip /flipL.
+    wp_pures.
     wp_bind (rand _ from _)%E.
     wp_apply (wp_rand_tape_empty with "Hl").
     iIntros (n) "Hl /=".
@@ -60,40 +66,39 @@ Section specs.
 
   Lemma refines_right_allocB_tape E K :
     nclose specN ⊆ E →
-    refines_right K allocB ={E}=∗ ∃ α, refines_right K (#lbl: α) ∗ α ↪ₛb [].
+    refines_right K allocB ={E}=∗ ∃ α, refines_right K (#lbl: α) ∗ α ↪ₛB [].
   Proof.
     iIntros (?) "(?&?)".
     iMod (step_alloctape with "[$]") as (α) "[? ?]"; [done|].
-    iModIntro; iExists _; iFrame. done. 
-  Qed. 
+    iModIntro; iExists _; iFrame. done.
+  Qed.
 
   Lemma refines_right_flip_tape E K α (b : bool) bs :
     nclose specN ⊆ E →
-    α ↪ₛb (b :: bs) -∗
-    refines_right K (flipL #lbl:α) ={E}=∗ refines_right K #(LitBool b) ∗ α ↪ₛb bs.
+    α ↪ₛB (b :: bs) -∗
+    refines_right K (flipL #lbl:α) ={E}=∗ refines_right K #(LitBool b) ∗ α ↪ₛB bs.
   Proof.
-    iIntros (?) "Hα Hr". rewrite /flipL.    
+    iIntros (?) "Hα Hr". rewrite /flipL.
+    tp_pures.
     tp_bind (rand _ from _ )%E.
     rewrite refines_right_bind.
     iDestruct "Hr" as "[#Hinv Hr]".
     iMod (step_rand with "[$]") as "(_ & Hj & Hl) /="; [done|].
     iMod (refines_right_int_to_bool with "[$Hinv $Hj]") as "Hr"; [done|].
     rewrite Z_to_bool_of_nat bool_to_fin_to_nat_inv.
-    by iFrame "Hr Hl". 
+    by iFrame "Hr Hl".
   Qed.
 
   (** * Tape allocation  *)
-  Lemma refines_allocB_tape_l K E N z t A :
-    TCEq N (Z.to_nat z) →
-    (▷ (∀ α : loc, α ↪b [] -∗ REL fill K (of_val #lbl:α) << t @ E : A))%I
+  Lemma refines_allocB_tape_l K E t A :
+    (▷ (∀ α : loc, α ↪B [] -∗ REL fill K (of_val #lbl:α) << t @ E : A))%I
     -∗ REL fill K allocB << t @ E : A.
-  Proof. iIntros (?) "?". by iApply refines_alloctape_l. Qed.
+  Proof. iIntros "?". by iApply refines_alloctape_l. Qed.
 
-  Lemma refines_allocB_tape_r E K N z t A :
-    TCEq N (Z.to_nat z) →
-    (∀ α : loc, α ↪ₛb [] -∗ REL t << fill K (of_val #lbl:α) @ E : A)%I
+  Lemma refines_allocB_tape_r E K t A :
+    (∀ α : loc, α ↪ₛB [] -∗ REL t << fill K (of_val #lbl:α) @ E : A)%I
     -∗ REL t << fill K allocB @ E : A.
-  Proof. iIntros (?) "?". by iApply refines_alloctape_r. Qed. 
+  Proof. iIntros "?". by iApply refines_alloctape_r. Qed.
 
   (** * Unlabelled flip *)
   Lemma refines_flip_l E K t A :
@@ -103,30 +108,31 @@ Section specs.
     iIntros "Hlog".
     iApply refines_wp_l.
     wp_apply (wp_flip with "[//]").
-    eauto. 
+    eauto.
   Qed.
 
   (* TODO: [refines_flip_r] *)
-  
+
   (** * Labelled flip  *)
   Lemma refines_flipL_empty_l K E α A e :
-    α ↪b [] ∗
-      (∀ (b : bool), α ↪b [] -∗ REL fill K (Val #b) << e @ E : A)
+    α ↪B [] ∗
+      (∀ (b : bool), α ↪B [] -∗ REL fill K (Val #b) << e @ E : A)
     ⊢ REL fill K (flipL #lbl:α) << e @ E : A.
   Proof.
     iIntros "[Hα H]".
     iApply refines_wp_l.
-    by wp_apply (wp_flip_tape_empty with "Hα"). 
-  Qed.     
+    by wp_apply (wp_flip_tape_empty with "Hα").
+  Qed.
 
   Lemma refines_flipL_empty_r K E α A e :
+    nclose specN ⊆ E →
     to_val e = None →
-    α ↪ₛb [] ∗
-      (∀ (b : bool), α ↪ₛb [] -∗ REL e << fill K (Val #b) @ E : A)
+    α ↪ₛB [] ∗
+      (∀ b : bool, α ↪ₛB [] -∗ REL e << fill K (Val #b) @ E : A)
     ⊢ REL e << fill K (flipL #lbl:α) @ E : A.
   Proof.
-    iIntros (ev) "[Hα H]". rewrite /flipL. 
-    rel_bind_r (rand _ from _)%E.
+    iIntros (? ev) "[Hα H]". rewrite /flipL.
+    rel_pures_r.
     rel_apply_r refines_rand_empty_r; [done|].
     iFrame.
     iIntros (n) "Hα".
@@ -136,10 +142,10 @@ Section specs.
     iModIntro; iExists _; iFrame.
     by iApply "H".
   Qed.
-  
+
   Lemma refines_flipL_l E K α b bs t A :
-    (▷ α ↪b (b :: bs) ∗
-     ▷ (α ↪b bs -∗ REL fill K (of_val #b) << t @ E : A))
+    (▷ α ↪B (b :: bs) ∗
+     ▷ (α ↪B bs -∗ REL fill K (of_val #b) << t @ E : A))
     -∗ REL fill K (flipL #lbl:α) << t @ E : A.
   Proof.
     iIntros "[Hα Hlog]".
@@ -148,40 +154,41 @@ Section specs.
   Qed.
 
   Lemma refines_flipL_r E K α b bs t A :
-    α ↪ₛb (b :: bs)
-    -∗ (α ↪ₛb bs -∗ REL t << fill K (of_val #b) @ E : A)
+    α ↪ₛB (b :: bs)
+    -∗ (α ↪ₛB bs -∗ REL t << fill K (of_val #b) @ E : A)
     -∗ REL t << (fill K (flipL #lbl:α)) @ E : A.
   Proof.
     iIntros "Hα Hlog".
     iApply refines_step_r.
     iIntros (k) "Hk".
-    iMod (refines_right_flip_tape with "[$] [$]") as "[? ?]"; [done|]. 
+    iMod (refines_right_flip_tape with "[$] [$]") as "[? ?]"; [done|].
     iModIntro; iExists _; iFrame. iApply ("Hlog" with "[$]").
   Qed.
 
   (** * Coupling rules (equality) *)
   Lemma refines_couple_flip_tapes E e1 e2 A α αₛ bs bsₛ :
     to_val e1 = None →
-    (αₛ ↪ₛb bsₛ ∗ α ↪b bs ∗
-       (∀ b, αₛ ↪ₛb (bsₛ ++ [b]) ∗ α ↪b (bs ++ [b]) -∗ REL e1 << e2 @ E : A))
+    (αₛ ↪ₛB bsₛ ∗ α ↪B bs ∗
+       (∀ b, αₛ ↪ₛB (bsₛ ++ [b]) ∗ α ↪B (bs ++ [b]) -∗ REL e1 << e2 @ E : A))
     ⊢ REL e1 << e2 @ E : A.
   Proof.
     iIntros (?) "(Hαs & Hα & Hlog)".
     iApply refines_couple_tapes; [done|iFrame].
     iIntros (n) "[? ?]".
     destruct (bool_to_fin_surj n) as [b <-].
-    rewrite -list_fmap_singleton -!fmap_app. 
-    iApply ("Hlog" $! b). iFrame. 
+    rewrite -list_fmap_singleton -!fmap_app.
+    iApply ("Hlog" $! b). iFrame.
   Qed.
 
   Lemma refines_couple_tape_flip K' E α A bs e :
+    nclose specN ⊆ E →
     to_val e = None →
-    α ↪b bs ∗
-      (∀ b, α ↪b (bs ++ [b]) -∗ REL e << fill K' (of_val #b) @ E : A)
+    α ↪B bs ∗
+      (∀ b, α ↪B (bs ++ [b]) -∗ REL e << fill K' (of_val #b) @ E : A)
     ⊢ REL e << fill K' flip @ E : A.
   Proof.
-    iIntros (?) "[Hα Hcnt]". rewrite /flip/flipL.
-    rel_bind_r (rand _ from _)%E.    
+    iIntros (??) "[Hα Hcnt]". rewrite /flip/flipL.
+    rel_pures_r.
     rel_apply_r refines_couple_tape_rand; [done|iFrame].
     iIntros (n) "Hα".
     destruct (bool_to_fin_surj n) as [b <-].
@@ -192,16 +199,16 @@ Section specs.
     iMod (refines_right_int_to_bool with "[$]"); [done|].
     iModIntro; iExists _; iFrame.
     rewrite Z_to_bool_of_nat bool_to_fin_to_nat_inv //.
-  Qed.     
+  Qed.
 
   Lemma refines_couple_flip_tape K E α A bs e :
-    α ↪ₛb bs ∗
-      (∀ b, α ↪ₛb (bs ++ [b]) -∗ REL fill K (of_val #b) << e @ E : A)
+    α ↪ₛB bs ∗
+      (∀ b, α ↪ₛB (bs ++ [b]) -∗ REL fill K (of_val #b) << e @ E : A)
     ⊢ REL fill K flip << e @ E : A.
   Proof.
     iIntros "[Hαs Hcnt]". rewrite /flip/flipL.
-    rel_bind_l (rand _ from _)%E.    
-    rel_apply_l refines_couple_rand_tape; iFrame.    
+    rel_pures_l.
+    rel_apply_l refines_couple_rand_tape; iFrame.
     iIntros (n) "Hα".
     destruct (bool_to_fin_surj n) as [b <-].
     rewrite -list_fmap_singleton -!fmap_app.
@@ -210,15 +217,17 @@ Section specs.
     wp_apply (wp_int_to_bool with "[//]").
     iIntros "_".
     rewrite Z_to_bool_of_nat bool_to_fin_to_nat_inv //.
-  Qed.     
+  Qed.
 
   Lemma refines_couple_flipL_flip K K' E α A :
-    α ↪b [] ∗
-      (∀ b : bool, α ↪b [] -∗ REL fill K (of_val #b) << fill K' (of_val #b) @ E : A)
+    nclose specN ⊆ E →
+    α ↪B [] ∗
+      (∀ b : bool, α ↪B [] -∗ REL fill K (of_val #b) << fill K' (of_val #b) @ E : A)
     ⊢ REL fill K (flipL #lbl:α) << fill K' flip @ E : A.
   Proof.
-    iIntros "(Hα & H)".
-    iApply refines_couple_tape_flip.
+    iIntros (?) "(Hα & H)".
+    rel_pures_l.
+    iApply refines_couple_tape_flip; [done| |].
     { rewrite fill_not_val //. }
     iFrame => /=. iIntros (b) "Hα".
     iApply (refines_flipL_l _ _ _ b []).
@@ -226,8 +235,8 @@ Section specs.
   Qed.
 
   Lemma refines_couple_flip_flipL K K' E α A :
-    α ↪ₛb [] ∗
-      (∀ b : bool, α ↪ₛb [] -∗ REL fill K (of_val #b) << fill K' (of_val #b) @ E : A)
+    α ↪ₛB [] ∗
+      (∀ b : bool, α ↪ₛB [] -∗ REL fill K (of_val #b) << fill K' (of_val #b) @ E : A)
     ⊢ REL fill K flip << fill K' (flipL #lbl:α) @ E : A.
   Proof.
     iIntros "(Hα & H)".
@@ -239,10 +248,12 @@ Section specs.
   Qed.
 
   Lemma refines_couple_flip_flip K K' E A :
+    nclose specN ⊆ E →
     (∀ b : bool, REL fill K (of_val #b) << fill K' (of_val #b) @ E : A)
     ⊢ REL fill K flip << fill K' flip @ E : A.
   Proof.
-    iIntros "Hcnt". rewrite /flip/flipL.
+    iIntros (?) "Hcnt". rewrite /flip/flipL.
+    rel_pures_l. rel_pures_r.
     rel_bind_l (rand _ from _)%E.
     rel_bind_r (rand _ from _)%E.
     rel_apply_l refines_couple_rands_lr.
@@ -254,12 +265,128 @@ Section specs.
     iIntros (K'') "Hr".
     iMod (refines_right_int_to_bool with "[$]"); [done|].
     iModIntro; iExists _; iFrame.
-    done. 
-  Qed.     
+    done.
+  Qed.
 
   (* TODO: non-equality couplings *)
-  
-End specs.   
 
+End specs.
 
-  
+Lemma tac_rel_allocBtape_l_simpl `{!clutchRGS Σ} K ℶ1 ℶ2 e t A E :
+  e = fill K allocB →
+  MaybeIntoLaterNEnvs 1 ℶ1 ℶ2 →
+  (envs_entails ℶ2 (∀ (α : loc),
+     (α ↪B [] -∗ refines E (fill K (of_val #lbl:α)) t A))) →
+  envs_entails ℶ1 (refines E e t A).
+Proof. intros ???. by eapply tac_rel_alloctape_l_simpl. Qed.
+
+Tactic Notation "rel_allocBtape_l" ident(l) "as" constr(H) :=
+  rel_pures_l;
+  first
+    [rel_reshape_cont_l ltac:(fun K e' =>
+       eapply (tac_rel_allocBtape_l_simpl K);
+       [reflexivity (** e = fill K (Alloc e') *)
+       |idtac..])
+    |fail 1 "rel_allocBtape_l: cannot find 'allocB'"];
+  [iSolveTC        (** IntoLaters *)
+  |iIntros (l) H; rel_finish  (** new goal *)].
+
+Lemma tac_rel_allocBtape_r `{!clutchRGS Σ} K' ℶ E e t A :
+  t = fill K' allocB →
+  nclose specN ⊆ E →
+  envs_entails ℶ (∀ α, α ↪ₛB [] -∗ refines E e (fill K' #lbl:α) A) →
+  envs_entails ℶ (refines E e t A).
+Proof. intros ???. by eapply tac_rel_alloctape_r. Qed.
+
+Tactic Notation "rel_allocBtape_r" ident(l) "as" constr(H) :=
+  rel_pures_r;
+  first
+    [rel_reshape_cont_r ltac:(fun K e' =>
+       eapply (tac_rel_allocBtape_r K);
+       [reflexivity  (** t = K'[alloc t'] *)
+       |idtac..])
+    |fail 1 "rel_allocBtape_r: cannot find 'allocB'"];
+  [solve_ndisj || fail "rel_allocBtape_r: cannot prove 'nclose specN ⊆ ?'"
+  |iIntros (l) H; rel_finish  (** new goal *)].
+
+Tactic Notation "rel_allocBtape_r" :=
+  let l := fresh in
+  let H := iFresh "H" in
+  rel_alloctape_r l as H.
+
+Tactic Notation "rel_allocBtape_l" :=
+  let l := fresh in
+  let H := iFresh "H" in
+  rel_alloctape_l l as H.
+
+Lemma tac_rel_flip_l `{!clutchRGS Σ} K ℶ1 ℶ2 i1 (α : loc) n ns e t tres A E :
+  t = fill K (flipL #lbl:α) →
+  envs_lookup i1 ℶ1 = Some (false, α ↪B (n::ns))%I →
+  envs_simple_replace i1 false (Esnoc Enil i1 (α ↪B ns)) ℶ1 = Some ℶ2 →
+  tres = fill K (of_val #n) →
+  envs_entails ℶ2 (refines E tres e A) →
+  envs_entails ℶ1 (refines E t e A).
+Proof.
+  rewrite envs_entails_unseal.
+  intros ???? Hg.
+  subst t tres.
+  rewrite envs_simple_replace_sound //; simpl.
+  rewrite right_id.
+  rewrite Hg.
+  rewrite -(refines_flipL_l _ K).
+  rewrite -bi.later_sep.
+  apply bi.later_intro.
+Qed.
+
+Lemma tac_rel_flip_r `{!clutchRGS Σ} K ℶ1 ℶ2 E i1 (α : loc) n ns e t tres A :
+  t = fill K (flipL (#lbl:α)) →
+  nclose specN ⊆ E →
+  envs_lookup i1 ℶ1 = Some (false, α ↪ₛB (n::ns))%I →
+  envs_simple_replace i1 false (Esnoc Enil i1 (α ↪ₛB ns)) ℶ1 = Some ℶ2 →
+  tres = fill K (of_val #n) →
+  envs_entails ℶ2 (refines E e tres A) →
+  envs_entails ℶ1 (refines E e t A).
+Proof.
+  rewrite envs_entails_unseal.
+  intros ????? Hg.
+  subst t tres.
+  rewrite envs_simple_replace_sound //; simpl.
+  rewrite right_id.
+  rewrite (refines_flipL_r _ K) //.
+  rewrite Hg.
+  apply bi.wand_elim_l.
+Qed.
+
+Tactic Notation "rel_flipL_l" :=
+  let solve_mapsto _ :=
+    let α := match goal with |- _ = Some (_, (?α ↪B _)%I) => α end in
+    iAssumptionCore || fail "rel_flip_l: cannot find" α "↪B ?" in
+  rel_pures_l;
+  first
+    [rel_reshape_cont_l ltac:(fun K e' =>
+       eapply (tac_rel_flip_l K); [reflexivity|..])
+    |fail 1 "rel_flip_l: cannot find 'flip'"];
+  (* the remaining goals are from tac_rel_flip_l (except for the first one, which has already been solved by this point) *)
+  [solve_mapsto ()
+  |pm_reflexivity || fail "rel_flip_l: this should not happen O-:"
+  |reflexivity
+  |rel_finish  (** new goal *)].
+
+Tactic Notation "rel_flipL_r" :=
+  let solve_mapsto _ :=
+    let l := match goal with |- _ = Some (_, (?l ↪ₛB _)%I) => l end in
+    iAssumptionCore || fail "rel_flip_r: cannot find" l "↪ₛB ?" in
+  rel_pures_r;
+  first
+    [rel_reshape_cont_r ltac:(fun K e' =>
+       eapply (tac_rel_flip_r K); [reflexivity|..])
+    |fail 1 "rel_flip_r: cannot find 'flip'"];
+  (* the remaining goals are from tac_rel_flip_r (except for the first one, which has already been solved by this point) *)
+  [solve_ndisj || fail "rel_flip_r: cannot prove 'nclose specN ⊆ ?'"
+  |solve_mapsto ()
+  |pm_reflexivity || fail "rel_flip_r: this should not happen O-:"
+  |reflexivity
+  |rel_finish  (** new goal *)].
+
+Global Opaque flip flipL allocB tapeB.
+
