dea7cfdaf5a8868a74921837b8f87278f53f3aaf
Simon Gregersen <gregersen@cs.au.dk>
2023-06-07 13:03:39 +0200

    wip

---
+++ theories/program_logic/tpr_weakestpre.v
@@ -0,0 +1,414 @@
+From Coq Require Export Reals Psatz.
+From iris.proofmode Require Import base proofmode.
+From iris.base_logic.lib Require Export fancy_updates.
+From iris.bi Require Export weakestpre fixpoint big_op.
+From iris.prelude Require Import options.
+
+From clutch.prelude Require Import stdpp_ext iris_ext.
+From clutch.prob Require Export couplings distribution.
+From clutch.program_logic Require Export exec language.
+
+Import uPred.
+
+Local Open Scope R.
+
+(** [irisGS] specifies the interface for the resource algebras implementing the
+    [state] and [cfg] of a [language] [Λ]. For the purposes of defining the
+    weakest precondition, we only need [irisGS] to give meaning to invariants,
+    and provide predicates describing valid states via [state_interp] and valid
+    specification configurations via [spec_interp]. *)
+Class irisGS (Λ : language) (Σ : gFunctors) := IrisG {
+  iris_invGS :> invGS_gen HasNoLc Σ;
+  state_interp : state Λ → iProp Σ;
+}.
+Global Opaque iris_invGS.
+Global Arguments IrisG {Λ Σ}.
+
+Class spec (A : Type) `{Countable A} (Σ : gFunctors) := Spec {
+  spec_step   : A → distr A;
+  spec_interp : A → iProp Σ;                                                                
+}.
+
+(** * The weakest precondition  *)
+Definition rwp_step {Σ Λ A} `{spec A Σ} `{!irisGS Λ Σ} E s (e1 : expr Λ) (Z : cfg Λ → A → iProp Σ) : iProp Σ :=
+  ∀ σ1 a1,
+    state_interp σ1 ∗ spec_interp a1 ={E,∅}=∗
+    ⌜if s is NotStuck then reducible e1 σ1 else True⌝ ∗
+    ((∃ R, ⌜Rcoupl (prim_step e1 σ1) (dret a1) R⌝ ∗
+          ∀ ρ2, ⌜R ρ2 a1⌝ ={∅}=∗ Z ρ2 a1) ∨                  
+    (∃ R, ⌜Rcoupl (prim_step e1 σ1) (spec_step a1) R⌝ ∗
+          ∀ ρ2, ⌜R ρ2 a1⌝ ={∅}=∗ ▷ Z ρ2 a1)).
+                                                  
+Definition rwp_pre {Σ A Λ} `{spec A Σ} `{!irisGS Λ Σ} (s : stuckness)
+    (rwp : coPset -d> expr Λ -d> (val Λ -d> iProp Σ) -d> iProp Σ) :
+    coPset -d> expr Λ -d> (val Λ -d> iProp Σ) -d> iProp Σ := λ E e1 Φ,
+  match to_val e1 with
+  | Some v => ∀ σ a, state_interp σ ∗ spec_interp a ={E}=∗ state_interp σ ∗ spec_interp a ∗ Φ v
+  | None => rwp_step E s e1 (λ '(e2, σ2) a2, |={∅,E}=> state_interp σ2 ∗ spec_interp a2 ∗ rwp E e2 Φ)
+  end%I.
+
+Lemma rwp_pre_mono {Σ A Λ} `{spec A Σ} `{!irisGS Λ Σ}  s (wp1 wp2 : coPset → expr Λ → (val Λ → iProp Σ) → iProp Σ) :
+  ⊢ ((□ ∀ E e Φ, wp1 E e Φ -∗ wp2 E e Φ) →
+      ∀ E e Φ, rwp_pre s wp1 E e Φ -∗ rwp_pre s wp2 E e Φ)%I.
+Proof. 
+  iIntros "#H"; iIntros (E e1 Φ) "Hwp". rewrite /rwp_pre /rwp_step.
+  destruct (to_val e1) as [v|]; first done.
+  iIntros (σ1 a1) "Hσ". iMod ("Hwp" with "Hσ") as "[% Hwp]". iModIntro.
+  iSplit; [done|].
+  iDestruct "Hwp" as "[(% & % & HR) | (% & % & HR)]".
+  - iLeft. iExists _. iSplit; [done|].
+    iIntros ([e2 σ2] HR). iMod ("HR" $! (e2,σ2) HR) as "HR".
+    iModIntro. iMod "HR" as "($ & $ & Hwp1)".
+    iModIntro. iApply ("H" with "Hwp1").
+  - iRight. iExists _. iSplit; [done|].
+    iIntros ([e2 σ2] HR). iMod ("HR" $! (e2,σ2) HR) as "HR".
+    iIntros "!# !>". iMod "HR" as "($ & $ & Hwp1)".
+    iModIntro. iApply ("H" with "Hwp1").
+Qed.     
+
+(* Uncurry [rwp_pre] and equip its type with an OFE structure *)
+Definition rwp_pre' {Σ A Λ} `{spec A Σ} `{!irisGS Λ Σ} (s : stuckness) :
+  (prodO (prodO (leibnizO coPset) (exprO Λ)) (val Λ -d> iProp Σ) → iProp Σ) →
+   prodO (prodO (leibnizO coPset) (exprO Λ)) (val Λ -d> iProp Σ) → iProp Σ
+  := uncurry3 ∘ rwp_pre s ∘ curry3.
+
+Local Instance exec_coupl_pre_mono {Λ A Σ} `{spec A Σ} `{!irisGS Λ Σ} (s : stuckness) :
+  BiMonoPred (rwp_pre' s).
+Proof. 
+  constructor.
+  - iIntros (wp1 wp2 ? ?) "#H"; iIntros ([[E e1] Φ]); iRevert (E e1 Φ).
+    iApply rwp_pre_mono. iIntros "!#" (E e Φ). iApply ("H" $! (E,e,Φ)).
+  - intros wp Hwp n [[E1 e1] Φ1] [[E2 e2] Φ2]
+      [[?%leibniz_equiv ?%leibniz_equiv] ?]; simplify_eq/=.
+    rewrite /curry3 /rwp_pre /rwp_step.
+    do 21 (f_equiv || case_match || done).
+    + by apply pair_ne.
+    + f_equiv. by apply pair_ne.
+Qed.
+
+Class Rwp (Λ : language) (PROP A : Type) :=
+  rwp : A → coPset → expr Λ → (val Λ → PROP) → PROP.
+Arguments rwp {_ _ _ _} _ _ _%E _%I.
+#[global]
+Instance: Params (@rwp) 7 := {}.
+
+(* take the least fixpoint of the above definition *)
+Definition rwp_def {Σ A Λ} `{spec A Σ} `{!irisGS Λ Σ} (s : stuckness) (E : coPset)
+  (e : expr Λ) (Φ : val Λ → iProp Σ) : iProp Σ
+  := bi_least_fixpoint (rwp_pre' s) (E,e,Φ).
+Definition rwp_aux {Σ A Λ} `{spec A Σ} `{!irisGS Λ Σ} : seal (@rwp_def Σ A Λ _ _ _ _). by eexists. Qed.
+#[global]
+Instance rwp' {Σ A Λ} `{spec A Σ} `{!irisGS Λ Σ} : Rwp Λ (iProp Σ) stuckness := rwp_aux.(unseal).
+
+Definition rwp_eq {Σ A Λ} `{spec A Σ} `{!irisGS Λ Σ} : rwp = @rwp_def Σ A Λ _ _ _ _ := (rwp_aux.(seal_eq)).
+
+(** Notations for refinement weakest preconditions *)
+(** Notations without binder -- only parsing because they overlap with the
+notations with binder. *)
+Notation "'RWP' e @ s ; E ⟨⟨ Φ ⟩ ⟩" := (rwp s E e%E Φ)
+  (at level 20, e, Φ at level 200, only parsing) : bi_scope.
+Notation "'RWP' e @ E ⟨⟨ Φ ⟩ ⟩" := (rwp NotStuck E e%E Φ)
+  (at level 20, e, Φ at level 200, only parsing) : bi_scope.
+Notation "'RWP' e @ E ? ⟨⟨ Φ ⟩ ⟩" := (rwp MaybeStuck E e%E Φ)
+  (at level 20, e, Φ at level 200, only parsing) : bi_scope.
+Notation "'RWP' e ⟨⟨ Φ ⟩ ⟩" := (rwp NotStuck ⊤ e%E Φ)
+  (at level 20, e, Φ at level 200, only parsing) : bi_scope.
+Notation "'RWP' e ? ⟨⟨ Φ ⟩ ⟩" := (rwp MaybeStuck ⊤ e%E Φ)
+  (at level 20, e, Φ at level 200, only parsing) : bi_scope.
+
+
+(** Notations with binder.  The indentation for the inner format block is chosen
+such that *if* one has a single-character mask (e.g. [E]), the second line
+should align with the binder(s) on the first line. *)
+Notation "'RWP' e @ s ; E ⟨⟨ v , Q ⟩ ⟩" := (rwp s E e%E (λ v, Q))
+  (at level 20, e, Q at level 200,
+   format "'[' 'RWP'  e  '/' '[          ' @  s ;  E  ⟨⟨  v ,  Q  ⟩ ⟩ ']' ']'") : bi_scope.
+Notation "'RWP' e @ E ⟨⟨ v , Q ⟩ ⟩" := (rwp NotStuck E e%E (λ v, Q))
+  (at level 20, e, Q at level 200,
+   format "'[' 'RWP'  e  '/' '[       ' @  E  ⟨⟨  v ,  Q  ⟩ ⟩ ']' ']'") : bi_scope.
+Notation "'RWP' e @ E ? ⟨⟨ v , Q ⟩ ⟩" := (rwp MaybeStuck E e%E (λ v, Q))
+  (at level 20, e, Q at level 200,
+   format "'[' 'RWP'  e  '/' '[        ' @  E  ? ⟨⟨  v ,  Q  ⟩ ⟩ ']' ']'") : bi_scope.
+Notation "'RWP' e ⟨⟨ v , Q ⟩ ⟩" := (rwp NotStuck ⊤ e%E (λ v, Q))
+  (at level 20, e, Q at level 200,
+   format "'[' 'RWP'  e  '/' '[   ' ⟨⟨  v ,  Q  ⟩ ⟩ ']' ']'") : bi_scope.
+Notation "'RWP' e ? ⟨⟨ v , Q ⟩ ⟩" := (rwp MaybeStuck ⊤ e%E (λ v, Q))
+  (at level 20, e, Q at level 200,
+   format "'[' 'RWP'  e  '/' '[    ' ? ⟨⟨  v ,  Q  ⟩ ⟩ ']' ']'") : bi_scope.
+
+Section rwp.
+Context {Σ A Λ} `{spec A Σ} `{!irisGS Λ Σ}.
+Implicit Types s : stuckness.
+Implicit Types P : iProp Σ.
+Implicit Types Φ : val Λ → iProp Σ.
+Implicit Types v : val Λ.
+Implicit Types e : expr Λ.
+Implicit Types a : A.
+Implicit Types b : bool.
+
+(* Weakest pre *)
+Lemma rwp_unfold s E e Φ :
+  RWP e @ s; E ⟨⟨ Φ ⟩⟩ ⊣⊢ rwp_pre s (rwp (PROP:=iProp Σ) s) E e Φ.
+Proof. by rewrite rwp_eq /rwp_def least_fixpoint_unfold. Qed.
+
+Lemma rwp_strong_ind Ψ s :
+  (∀ n E e, Proper (pointwise_relation _ (dist n) ==> dist n) (Ψ E e)) →
+  ⊢ (□ (∀ e E Φ, rwp_pre s (λ E e Φ, Ψ E e Φ ∧ RWP e @ s; E ⟨⟨ Φ ⟩⟩) E e Φ -∗ Ψ E e Φ) →
+        ∀ e E Φ, RWP e @ s; E ⟨⟨ Φ ⟩⟩ -∗ Ψ E e Φ)%I.
+Proof.
+  iIntros (HΨ). iIntros "#IH" (e E Φ) "H". rewrite rwp_eq.
+  set (Ψ' := uncurry3 Ψ :
+    prodO (prodO (leibnizO coPset) (exprO Λ)) (val Λ -d> iProp Σ) → iProp Σ).
+  assert (NonExpansive Ψ').
+  { intros n [[E1 e1] Φ1] [[E2 e2] Φ2]
+      [[?%leibniz_equiv ?%leibniz_equiv] ?]; simplify_eq/=. by apply HΨ. }
+  iApply (least_fixpoint_ind _ Ψ' with "[] H").
+  iIntros "!#" ([[??] ?]) "H". by iApply "IH".
+Qed.
+
+Lemma rwp_ind Ψ s :
+  (∀ n E e, Proper (pointwise_relation _ (dist n) ==> dist n) (Ψ E e)) →
+  ⊢ (□ (∀ e E Φ, rwp_pre s (λ E e Φ, Ψ E e Φ) E e Φ -∗ Ψ E e Φ)
+      → ∀ e E Φ, RWP e @ s; E ⟨⟨ Φ ⟩⟩ -∗ Ψ E e Φ)%I.
+Proof.
+  iIntros (HΨ) "#H". iApply rwp_strong_ind. iIntros "!>" (e E Φ) "Hrwp".
+  iApply "H". iApply (rwp_pre_mono with "[] Hrwp"). clear.
+  iIntros "!>" (E e Φ) "[$ _]".
+Qed.
+
+Global Instance rwp_ne s E e n :
+  Proper (pointwise_relation _ (dist n) ==> dist n) (rwp (PROP:=iProp Σ) s E e).
+Proof.
+  intros Φ1 Φ2 HΦ. rewrite !rwp_eq. by apply (least_fixpoint_ne _), pair_ne, HΦ.
+Qed.
+
+Global Instance rwp_proper s E e :
+  Proper (pointwise_relation _ (≡) ==> (≡)) (rwp (PROP:=iProp Σ) s E e).
+Proof.
+  by intros Φ Φ' ?; apply equiv_dist=>n; apply rwp_ne=>v; apply equiv_dist.
+Qed.
+
+Lemma rwp_value' s E Φ v : Φ v ⊢ RWP of_val v @ s; E ⟨⟨ Φ ⟩⟩.
+Proof. iIntros "HΦ". rewrite rwp_unfold /rwp_pre to_of_val. by iIntros (??) "[$ $]". Qed.
+
+Lemma rwp_strong_mono' s1 s2 E1 E2 e Φ Ψ :
+  s1 ⊑ s2 → E1 ⊆ E2 →
+  RWP e @ s1; E1 ⟨⟨ Φ ⟩⟩ -∗
+  (∀ σ a v, state_interp σ ∗ spec_interp a ∗ Φ v ={E2}=∗
+            state_interp σ ∗ spec_interp a ∗ Ψ v) -∗
+  RWP e @ s2; E2 ⟨⟨ Ψ ⟩⟩.
+Proof.
+  iIntros (? HE) "H HΦ". iRevert (E2 Ψ HE) "HΦ"; iRevert (e E1 Φ) "H".
+  iApply rwp_ind; first solve_proper.
+  iIntros "!#" (e E1 Φ) "IH"; iIntros (E2 Ψ HE) "HΦ".
+  rewrite !rwp_unfold /rwp_pre /rwp_step.
+  destruct (to_val e) as [v|] eqn:?.
+  { iIntros (??) "H".
+    iSpecialize ("IH" with "[$]").
+    iMod (fupd_mask_mono with "IH") as "(?&?&?)"; [done|].
+    iApply ("HΦ" with "[$]"). }
+  iIntros (σ1 a1) "Hσ". iMod (fupd_mask_subseteq E1) as "Hclose"; first done.
+  iMod ("IH" with "[$]") as "IH". iModIntro. iDestruct "IH" as (b) "IH". 
+  iSplit; [by destruct s1, s2|].
+  iDestruct "IH" as "[(% & % & HR) | (% & % & HR)]".
+  - iLeft. iExists _. iSplit; [done|].
+    iIntros ([e2 σ2] HR). iMod ("HR" $! (e2,σ2) HR) as "HR".
+    iModIntro. iMod "HR" as "($ & $ & Hwp1)".
+    iMod "Hclose". iModIntro. 
+    by iApply "Hwp1".
+  - iRight. iExists _. iSplit; [done|]. 
+    iIntros ([e2 σ2] HR). iMod ("HR" $! (e2,σ2) HR) as "HR".    
+    iModIntro. iModIntro. iMod "HR" as "($ & $ & Hwp1)".
+    iMod "Hclose". iModIntro. 
+    by iApply "Hwp1".
+Qed. 
+
+Lemma rwp_strong_mono s1 s2 E1 E2 e Φ Ψ :
+  s1 ⊑ s2 → E1 ⊆ E2 →
+  RWP e @ s1; E1 ⟨⟨ Φ ⟩⟩ -∗ (∀ v, Φ v ={E2}=∗ Ψ v) -∗ RWP e @ s2; E2 ⟨⟨ Ψ ⟩⟩.
+Proof.
+  iIntros (??) "Hrwp H". iApply (rwp_strong_mono' with "[$]"); auto.
+  iIntros (???) "($&$&HΦ)". by iApply "H".
+Qed.
+
+Lemma fupd_rwp s E e Φ : (|={E}=> RWP e @ s; E ⟨⟨ Φ ⟩⟩) ⊢ RWP e @ s; E ⟨⟨ Φ ⟩⟩.
+Proof.
+  rewrite rwp_unfold /rwp_pre. iIntros "H". destruct (to_val e) as [v|] eqn:?.
+  { by iMod "H". }
+  iIntros (σ1 a) "HS". iMod "H". by iApply "H".
+Qed.
+Lemma fupd_rwp' s E e Φ :
+  (∀ σ a, state_interp σ ∗ spec_interp a ={E}=∗ state_interp σ ∗ spec_interp a) ∗
+  RWP e @ s; E ⟨⟨ Φ ⟩⟩ ⊢ RWP e @ s; E ⟨⟨ Φ ⟩⟩.
+Proof.
+  iIntros "[H Hrwp]".
+  iEval (rewrite rwp_unfold /rwp_pre). destruct (to_val e) as [v|] eqn:Heq.
+  { iIntros. iSpecialize ("H" with "[$]"). rewrite rwp_unfold /rwp_pre Heq.
+    iMod "H" as "[H1 H2]". by iMod ("Hrwp" with "[$]") as "$". }
+  iIntros (σ1 a) "HS".
+  iSpecialize ("H" with "[$]"). rewrite rwp_unfold /rwp_pre Heq.
+  iMod "H" as "(H1&H2)". by iMod ("Hrwp" with "[$]") as "$".
+Qed.
+Lemma rwp_fupd s E e Φ : RWP e @ s; E ⟨⟨ v, |={E}=> Φ v ⟩⟩ ⊢ RWP e @ s; E ⟨⟨ Φ ⟩⟩.
+Proof. iIntros "H". iApply (rwp_strong_mono s s E with "H"); auto. Qed.
+
+Lemma rwp_fupd' s E e Φ :
+  RWP e @ s; E ⟨⟨ v, ∀ σ a, state_interp σ ∗ spec_interp a ={E}=∗
+                            state_interp σ ∗ spec_interp a ∗ Φ v⟩⟩
+  ⊢ RWP e @ s; E ⟨⟨ Φ ⟩⟩.
+Proof.
+  iIntros "H". iApply (rwp_strong_mono' s s E with "H"); auto. iIntros (???) "(?&?&H)".
+  by iMod ("H" with "[$]").
+Qed.
+
+Lemma rwp_atomic E1 E2 e s Φ `{!Atomic WeaklyAtomic e} :
+  (|={E1,E2}=> RWP e @ s; E2 ⟨⟨ v, |={E2,E1}=> Φ v ⟩⟩) ⊢ RWP e @ s; E1 ⟨⟨ Φ ⟩⟩.
+Proof.
+  iIntros "H". rewrite !rwp_unfold /rwp_pre.
+  destruct (to_val e) as [v|] eqn:He.
+  { iIntros. iMod ("H"). iMod ("H" with "[$]") as "($&$&$)". }
+  iIntros (σ1 a1) "Hσ". iMod "H".
+  iMod ("H" $! σ1 with "Hσ") as "[% H]". iModIntro.
+  iSplit; [by destruct s|].
+  iDestruct "H" as "[(% & % & HR) | (% & % & HR)]".
+  - iLeft. iExists _. iSplit; [done|].
+    iIntros ([e2 σ2] HR). iMod ("HR" $! (e2,σ2) HR) as "HR".
+    iModIntro. iMod "HR" as "($ & $ & Hwp1)".
+    rewrite rwp_unfold /rwp_pre. destruct (to_val e2) as [v2|] eqn:He2.
+    + admit.
+    + specialize (atomic _ _ _ _ _ Hstep) as []; congruence. 
+
+    
+    iMod "Hclose". iModIntro. 
+    by iApply "Hwp1".
+  - iRight. iExists _. iSplit; [done|]. 
+    iIntros ([e2 σ2] HR). iMod ("HR" $! (e2,σ2) HR) as "HR".    
+    iModIntro. iModIntro. iMod "HR" as "($ & $ & Hwp1)".
+    iMod "Hclose". iModIntro. 
+    by iApply "Hwp1".
+  
+  iDestruct "H" as (b) "H". iExists b. iNext. iMod "H" as "[$ H]"; iModIntro.
+  iIntros (e2 σ2 efs κ Hstep). iSpecialize ("H" with "[//]"). iMod "H".
+  iDestruct "H" as "(Hsrc & Hσ & H & Hefs)".
+  rewrite rwp_unfold /rwp_pre. destruct (to_val e2) as [v2|] eqn:He2.
+  - rewrite rwp_unfold /rwp_pre He2.
+    destruct b.
+    * iDestruct "Hsrc" as (??) "H'". iMod ("H" with "[$]") as "(Hsrc&$&H)".
+      iFrame. iMod "H". iIntros "!>".
+      iSplitL "Hsrc"; first eauto.
+      iIntros (???) "(?&?) !>". iFrame.
+    * iMod ("H" with "[$]") as "(Hsrc&$&H)".
+      iFrame. iMod "H". iIntros "!>".
+      iIntros (???) "(?&?) !>". iFrame.
+  - specialize (atomic _ _ _ _ _ Hstep) as []; congruence.
+Qed.
+
+Lemma rwp_bind K `{!LanguageCtx K} s E e Φ :
+  RWP e @ s; E ⟨⟨ v, RWP K (of_val v) @ s; E ⟨⟨ Φ ⟩⟩ ⟩⟩ ⊢ RWP K e @ s; E ⟨⟨ Φ ⟩⟩.
+Proof.
+  revert Φ. cut (∀ Φ', RWP e @ s; E ⟨⟨ Φ' ⟩⟩ -∗ ∀ Φ,
+  (∀ v, Φ' v -∗ RWP K (of_val v) @ s; E ⟨⟨ Φ ⟩⟩) -∗ RWP K e @ s; E ⟨⟨ Φ ⟩⟩).
+  { iIntros (help Φ) "H". iApply (help with "H"); auto. }
+  iIntros (Φ') "H". iRevert (e E Φ') "H". iApply rwp_strong_ind; first solve_proper.
+  iIntros "!#" (e E1 Φ') "IH". iIntros (Φ) "HΦ".
+  rewrite /rwp_pre /rwp_step.
+  destruct (to_val e) as [v|] eqn:He.
+  { apply of_to_val in He as <-. iApply fupd_rwp'.
+    iIntros. iMod ("IH" with "[$]") as "($&$&H)".
+      by iApply "HΦ". }
+  rewrite rwp_unfold /rwp_pre /rwp_step fill_not_val //.
+  iIntros (σ1 n a) "H". iMod ("IH" with "H") as "IH". iModIntro.
+  iDestruct "IH" as (b) "IH". iExists b. iNext.
+  iMod "IH" as "[% IH]"; iModIntro. iSplit.
+  { iPureIntro. destruct s; last done.
+    unfold reducible in *. naive_solver eauto using fill_step. }
+  iIntros (e2 σ2 efs κ Hstep).
+  destruct (fill_step_inv e σ1 κ e2 σ2 efs) as (e2'&->&?); auto.
+  iMod ("IH" $! e2' σ2 efs with "[//]") as "($ & $ & IH & IHfork)". iIntros "!>".
+  iSplitL "IH HΦ".
+  - iDestruct "IH" as "[IH _]". by iApply "IH".
+  - by setoid_rewrite bi.and_elim_r.
+Qed.
+
+
+Lemma rwp_bind_inv K `{!LanguageCtx K} s E e Φ :
+  RWP K e @ s; E ⟨⟨ Φ ⟩⟩ ⊢ RWP e @ s; E ⟨⟨ v, RWP K (of_val v) @ s; E ⟨⟨ Φ ⟩⟩ ⟩⟩.
+Proof.
+  iIntros "H". remember (K e) as e' eqn:He'.
+  iRevert (e He'). iRevert (e' E Φ) "H". iApply rwp_strong_ind; first solve_proper.
+  iIntros "!#" (e' E1 Φ) "IH". iIntros (e ->).
+  rewrite !rwp_unfold {2}/rwp_pre.
+  destruct (to_val e) as [v|] eqn:He.
+  { iIntros (???) "($&$)". iModIntro. apply of_to_val in He as <-. rewrite !rwp_unfold.
+    iApply (rwp_pre_mono with "[] IH"). by iIntros "!#" (E e Φ') "[_ ?]". }
+  rewrite /rwp_pre fill_not_val //.
+  iIntros (σ1 κs n) "Hσ". iMod ("IH" with "[$]") as (b) "IH". iModIntro.
+  iExists b. iNext. iMod "IH" as "[% IH]"; iModIntro. iSplit.
+  { destruct s; eauto using reducible_fill. }
+  iIntros (e2 σ2 efs κ Hstep).
+  iMod ("IH" $! (K e2) σ2 efs κ with "[]") as "(Hsrc & Hσ & IH & IHefs)"; eauto using fill_step.
+  iModIntro. iFrame "Hsrc Hσ". iSplitR "IHefs".
+  - iDestruct "IH" as "[IH _]". by iApply "IH".
+  - by setoid_rewrite bi.and_elim_r.
+Qed.
+
+(** * Derived rules *)
+Lemma rwp_mono s E e Φ Ψ : (∀ v, Φ v ⊢ Ψ v) → RWP e @ s; E ⟨⟨ Φ ⟩⟩ ⊢ RWP e @ s; E ⟨⟨ Ψ ⟩⟩.
+Proof.
+  iIntros (HΦ) "H"; iApply (rwp_strong_mono with "H"); auto.
+  iIntros (v) "?". by iApply HΦ.
+Qed.
+Lemma rwp_stuck_mono s1 s2 E e Φ :
+  s1 ⊑ s2 → RWP e @ s1; E ⟨⟨ Φ ⟩⟩ ⊢ RWP e @ s2; E ⟨⟨ Φ ⟩⟩.
+Proof. iIntros (?) "H". iApply (rwp_strong_mono with "H"); auto. Qed.
+Lemma rwp_stuck_weaken s E e Φ :
+  RWP e @ s; E ⟨⟨ Φ ⟩⟩ ⊢ RWP e @ E ?⟨⟨ Φ ⟩⟩.
+Proof. apply rwp_stuck_mono. by destruct s. Qed.
+Lemma rwp_mask_mono s E1 E2 e Φ : E1 ⊆ E2 → RWP e @ s; E1 ⟨⟨ Φ ⟩⟩ ⊢ RWP e @ s; E2 ⟨⟨ Φ ⟩⟩.
+Proof. iIntros (?) "H"; iApply (rwp_strong_mono with "H"); auto. Qed.
+Global Instance rwp_mono' s E e :
+  Proper (pointwise_relation _ (⊢) ==> (⊢)) (rwp (PROP:=iProp Σ) s E e).
+Proof. by intros Φ Φ' ?; apply rwp_mono. Qed.
+
+Lemma rwp_value s E Φ e v : IntoVal e v → Φ v ⊢ RWP e @ s; E ⟨⟨ Φ ⟩⟩.
+Proof. intros <-. by apply rwp_value'. Qed.
+Lemma rwp_value_fupd' s E Φ v : (|={E}=> Φ v) ⊢ RWP of_val v @ s; E ⟨⟨ Φ ⟩⟩.
+Proof. intros. by rewrite -rwp_fupd -rwp_value'. Qed.
+Lemma rwp_value_fupd s E Φ e v `{!IntoVal e v} :
+  (|={E}=> Φ v) ⊢ RWP e @ s; E ⟨⟨ Φ ⟩⟩.
+Proof. intros. rewrite -rwp_fupd -rwp_value //. Qed.
+(*
+Lemma rwp_value_inv s E Φ e v : IntoVal e v → RWP e @ s; E ⟨⟨ Φ ⟩⟩ ={E}=∗ Φ v.
+Proof. intros <-. by apply rwp_value_inv'. Qed.
+*)
+
+Lemma rwp_frame_l s E e Φ R : R ∗ RWP e @ s; E ⟨⟨ Φ ⟩⟩ ⊢ RWP e @ s; E ⟨⟨ v, R ∗ Φ v ⟩⟩.
+Proof. iIntros "[? H]". iApply (rwp_strong_mono with "H"); auto with iFrame. Qed.
+Lemma rwp_frame_r s E e Φ R : RWP e @ s; E ⟨⟨ Φ ⟩⟩ ∗ R ⊢ RWP e @ s; E ⟨⟨ v, Φ v ∗ R ⟩⟩.
+Proof. iIntros "[H ?]". iApply (rwp_strong_mono with "H"); auto with iFrame. Qed.
+
+Lemma rwp_wand s E e Φ Ψ :
+  RWP e @ s; E ⟨⟨ Φ ⟩⟩ -∗ (∀ v, Φ v -∗ Ψ v) -∗ RWP e @ s; E ⟨⟨ Ψ ⟩⟩.
+Proof.
+  iIntros "Hwp H". iApply (rwp_strong_mono with "Hwp"); auto.
+  iIntros (?) "?". by iApply "H".
+Qed.
+Lemma rwp_wand_l s E e Φ Ψ :
+  (∀ v, Φ v -∗ Ψ v) ∗ RWP e @ s; E ⟨⟨ Φ ⟩⟩ ⊢ RWP e @ s; E ⟨⟨ Ψ ⟩⟩.
+Proof. iIntros "[H Hwp]". iApply (rwp_wand with "Hwp H"). Qed.
+Lemma rwp_wand_r s E e Φ Ψ :
+  RWP e @ s; E ⟨⟨ Φ ⟩⟩ ∗ (∀ v, Φ v -∗ Ψ v) ⊢ RWP e @ s; E ⟨⟨ Ψ ⟩⟩.
+Proof. iIntros "[Hwp H]". iApply (rwp_wand with "Hwp H"). Qed.
+Lemma rwp_frame_wand_l s E e Q Φ :
+  Q ∗ RWP e @ s; E ⟨⟨ v, Q -∗ Φ v ⟩⟩ -∗ RWP e @ s; E ⟨⟨ Φ ⟩⟩.
+Proof.
+  iIntros "[HQ HRWP]". iApply (rwp_wand with "HRWP").
+  iIntros (v) "HΦ". by iApply "HΦ".
+Qed.
+
+End rwp.
+
+
+
+
+
