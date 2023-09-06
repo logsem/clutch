1a29aa840b584bd6beba71fae5cba86b73bb4f6e
Simon Gregersen <gregersen@cs.au.dk>
2023-09-06 15:51:59 +0100

    add 'round' numbers to model

--- theories/tpref_logic/examples/lazy_int.v
+++ theories/tpref_logic/examples/lazy_int.v
@@ -8,22 +8,25 @@ From clutch.tpref_logic.examples Require Import flip.
 Section lazy_int.
   (* Context (CHUNCK_SIZE : nat). *)
 
-  Definition mstep (bs : bool * bool) : distr (bool * bool) :=
-    let '(b1, b2) := bs in
-    if bool_decide (b1 ≠ b2) then
-      dzero
-    else
-      dprod fair_coin fair_coin.
+  Definition mstep (bs : nat * bool * bool) : distr (nat * bool * bool) :=
+    let '(fuel, b1, b2) := bs in
+    match fuel with
+    | 0 => dzero
+    | S n =>
+        '(b1', b2') ← dprod fair_coin fair_coin;
+        dret (if bool_decide (b1' ≠ b2') then n else S n, b1', b2')
+    end.
 
-  Definition mto_final (bs : bool * bool) : option (bool * bool) :=
-    let '(b1, b2) := bs in
-    if bool_decide (b1 ≠ b2) then Some (b1, b2) else None.
+  Definition mto_final (bs : nat * bool * bool) : option (bool * bool) :=
+    let '(fuel, b1, b2) := bs in
+    if bool_decide (b1 ≠ b2 ∧ fuel = 0%nat)then Some (b1, b2) else None.
 
   Definition random_walks_mixin : MarkovMixin mstep mto_final.
   Proof.
     constructor.
-    move=> [? ?]=>/= [[[? ?] ?] [? ?]].
-    case_bool_decide; simplify_eq=>//.
+    move=> [[? ?] ?] =>/= [[[? ?] ?] [? ?]].
+    case_bool_decide as Heq; simplify_eq=>//.
+    by destruct Heq as [? ->].
   Qed.
 
   Canonical Structure random_walks : markov := Markov _ _ random_walks_mixin.
@@ -132,13 +135,13 @@ Section lazy_int.
   (* TODO: why is this neccesary?!?! *)
   Definition foo : specG _ _ := (@tprG_specG _ _ _).
   Existing Instance foo.
-  
-  Lemma rwp_coupl_two_tapes ns1 ns2 α1 α2 (e : expr) E (Φ : val → iProp Σ) (b : bool) :
+
+  Lemma rwp_coupl_two_tapes ns1 ns2 α1 α2 N (e : expr) E (Φ : val → iProp Σ) (b : bool) :
     TCEq (to_val e) None →
     α1 ↪ (1%nat; ns1) ∗
     α2 ↪ (1%nat; ns2) ∗
-    specF (b, b) ∗
-    ▷ (∀ b1 b2, specF (b1, b2) -∗
+    specF (S N, b, b) ∗
+    ▷ (∀ b1 b2, specF (if bool_decide (b1 ≠ b2) then N else S N, b1, b2) -∗
                 α1 ↪ (1%nat; ns1 ++ [bool_to_fin b1]) -∗
                 α2 ↪ (1%nat; ns2 ++ [bool_to_fin b2]) -∗
                 WP e @ E {{ Φ }})
@@ -146,14 +149,19 @@ Section lazy_int.
   Proof.
     iIntros (Hv) "(Hα1 & Hα2 & Hspec & Hcnt)".
     iApply (rwp_couple_two_tapes (δ := random_walks) _ _
-              (λ '(n1, n2) '(b1, b2), n1 = bool_to_fin b1 ∧ n2 = bool_to_fin b2)
+              (λ '(n1, n2) '(N', b1, b2),
+                (N' = if bool_decide (b1 ≠ b2) then N else S N) ∧
+                  n1 = bool_to_fin b1 ∧
+                  n2 = bool_to_fin b2)
              with "[$Hα1 $Hα2 $Hspec Hcnt]").
     { rewrite Hv //. }
     { intros ???? => /=.
-      rewrite bool_decide_eq_false_2; auto.
-      eapply Rcoupl_mono; [by apply state_steps_fair_coins_coupl|].
-      intros [] [b1 b2] [= -> ->] =>/=. eauto. }
-    iIntros "!>" (?? [b1 b2] [-> ->]) "Hf1 Hα1 Hα2".
+      (* rewrite bool_decide_eq_false_2; auto. *)
+      rewrite -(dret_id_right (state_step _ _ ≫= _)).
+      eapply Rcoupl_dbind; [|by apply state_steps_fair_coins_coupl].
+      intros [] [b1 b2]  [= -> ->] =>/=.
+      eapply Rcoupl_dret. eauto 6. }
+    iIntros "!>" (?? [[N' b1] b2] (-> & -> & ->)) "Hf1 Hα1 Hα2".
     iApply ("Hcnt" with "Hf1 Hα1 Hα2").
   Qed.
 
@@ -164,7 +172,7 @@ Section lazy_int.
     | Gt => 1
     end.
 
-  Lemma wp_cmp_Z (z1 z2 : Z) E :
+  Lemma wp_cmpZ (z1 z2 : Z) E :
     ⟨⟨⟨ True ⟩⟩⟩
       cmpZ #z1 #z2 @ E
     ⟨⟨⟨ RET #(comparison2z (Z.compare z1 z2)); True%I ⟩⟩⟩.
@@ -180,7 +188,7 @@ Section lazy_int.
       wp_pures; case_bool_decide; [|lia].
       wp_pures. iApply "HΦ"; eauto.
   Qed.
-  
+
   Fixpoint chunk_list (l : loc) (zs : list (fin 2)) : iProp Σ :=
     match zs with
     | [] => l ↦ NONEV
@@ -189,7 +197,7 @@ Section lazy_int.
   end.
 
   Definition chunk_and_tape_list α l zs : iProp Σ :=
-    ∃ zs1 zs2, ⌜zs = zs1 ++ zs2⌝ ∗ chunk_list l zs1 ∗ α ↪ (1%nat; zs2).  
+    ∃ zs1 zs2, ⌜zs = zs1 ++ zs2⌝ ∗ chunk_list l zs1 ∗ α ↪ (1%nat; zs2).
 
   Definition lazy_no (zs : list _) (v : val) : iProp Σ :=
     ∃ (l : loc) (α : loc),
@@ -209,7 +217,7 @@ Section lazy_int.
     { rewrite /=. iExists l'. iFrame. }
     iFrame.
   Qed.
-  
+
   Lemma rwp_get_chunk_cons z zs α l E :
     ⟨⟨⟨ chunk_and_tape_list α l (z :: zs) ⟩⟩⟩
       get_chunk #lbl:α #l @ E
@@ -217,7 +225,7 @@ Section lazy_int.
         chunk_and_tape_list α l' zs ∗
        (chunk_and_tape_list α l' zs -∗ chunk_and_tape_list α l (z :: zs)) ⟩⟩⟩.
   Proof.
-    iIntros (Ψ) "(%zs1 & %zs2 & %Heq & Hl & Hα) HΨ". 
+    iIntros (Ψ) "(%zs1 & %zs2 & %Heq & Hl & Hα) HΨ".
     rewrite /get_chunk.
     wp_pures.
     destruct zs1 as [| z' zs1'].
@@ -238,37 +246,37 @@ Section lazy_int.
       { iIntros "Htail". iApply (chunk_and_tape_list_cons_chunk with "[$] [$]"). }
   Qed.
 
-  Lemma rwp_couple_chunk_and_tape_list α1 α2 l1 l2 zs1 zs2 (e : expr) E (Φ : val → iProp Σ) b :
+  Lemma rwp_couple_chunk_and_tape_list α1 α2 l1 l2 zs1 zs2 N (e : expr) E (Φ : val → iProp Σ) b :
     TCEq (to_val e) None →
     chunk_and_tape_list α1 l1 zs1 ∗
     chunk_and_tape_list α2 l2 zs2 ∗
-    specF (b, b) ∗
-    ▷ (∀ b1 b2, specF (b1, b2) -∗
-                chunk_and_tape_list α1 l1 (zs1 ++ [bool_to_fin b1]) -∗                                  
-                chunk_and_tape_list α2 l2 (zs2 ++ [bool_to_fin b2]) -∗                                  
+    specF (S N, b, b) ∗
+    ▷ (∀ b1 b2, specF (if bool_decide (b1 ≠ b2) then N else S N, b1, b2) -∗
+                chunk_and_tape_list α1 l1 (zs1 ++ [bool_to_fin b1]) -∗
+                chunk_and_tape_list α2 l2 (zs2 ++ [bool_to_fin b2]) -∗
                 WP e @ E {{ Φ }})
     ⊢ WP e @ E {{ Φ }}.
   Proof.
     iIntros (?) "((% & % & % & Hl1 & Hα1) & (% & % & % & Hl2 & Hα2) & Hspec & Hcnt)".
     iApply rwp_coupl_two_tapes.
-    iFrame "Hα1 Hα2 Hspec". 
+    iFrame "Hα1 Hα2 Hspec".
     iIntros "!>" (b1 b2) "Hspec Hα1 Hα2".
     iApply ("Hcnt" with "Hspec [Hl1 Hα1] [Hl2 Hα2]").
     { iExists _, _. iFrame. subst. rewrite app_assoc //. }
-    { iExists _, _. iFrame. subst. rewrite app_assoc //. }    
+    { iExists _, _. iFrame. subst. rewrite app_assoc //. }
   Qed.
-  
-  Lemma rwp_cmp_list α1 α2 l1 l2 E b :
-    ⟨⟨⟨ specF (b, b) ∗ chunk_and_tape_list α1 l1 [] ∗ chunk_and_tape_list α2 l2 [] ⟩⟩⟩
+
+  Lemma rwp_cmp_list N α1 α2 l1 l2 E b :
+    ⟨⟨⟨ specF (S N, b, b) ∗ chunk_and_tape_list α1 l1 [] ∗ chunk_and_tape_list α2 l2 [] ⟩⟩⟩
       cmp_list #lbl:α1 #l1 #lbl:α2 #l2 @ E
     ⟨⟨⟨ (z : Z) (b1 b2 : bool), RET #z;
-        specF (b1, b2) ∗ ⌜b1 ≠ b2⌝
+        specF (N, b1, b2) ∗ ⌜b1 ≠ b2⌝
         (* ⌜ z = comparison2z (digit_list_cmp zs1 zs2) ⌝ ∗ *)
         (* chunk_and_tape_list α1 l1 zs1 ∗ chunk_and_tape_list α2 l2 zs2 *) ⟩⟩⟩.
   Proof.
-    iLöb as "IH" forall (b l1 l2).    
+    iLöb as "IH" forall (b l1 l2).
     iIntros (Ψ) "(Hspec & Hl1 & Hl2) HΨ".
-    wp_rec. wp_pures. 
+    wp_rec. wp_pures.
     iApply (rwp_couple_chunk_and_tape_list α1).
     iFrame.
     iIntros "!>" (b1 b2) "Hspec Hl1 Hl2".
@@ -279,12 +287,15 @@ Section lazy_int.
     wp_apply (rwp_get_chunk_cons with "Hl2").
     iIntros (l2') "(Hl2' & Hl2)".
     wp_pures.
-    wp_apply wp_cmp_Z; [done|]; iIntros "_".
+    wp_apply wp_cmpZ; [done|]; iIntros "_".
     destruct (Z.compare_spec (bool_to_fin b1) (bool_to_fin b2))
-      as [? | Hlt%Z.lt_neq | Hgt%Z.lt_neq] => /=; simplify_eq.
-   - wp_pures. by iApply ("IH" with "[$Hspec $Hl1' $Hl2']"). 
-   - wp_pures. iApply "HΨ". iFrame. iModIntro. iPureIntro. by intros ->.       
-   - wp_pures. iApply "HΨ". iFrame. iModIntro. iPureIntro. by intros ->.
-  Qed. 
-                          
+      as [? | Hlt%Z.lt_neq | Hgt%Z.lt_neq]; simplify_eq=>/=.
+   - wp_pures. rewrite bool_decide_eq_false_2; [|eauto].
+     by iApply ("IH" with "[$Hspec $Hl1' $Hl2']").
+   - wp_pures. rewrite bool_decide_eq_true_2; [|by intros ->].
+     iApply "HΨ". iFrame. iModIntro. iPureIntro. by intros ->.
+   - wp_pures. rewrite bool_decide_eq_true_2; [|by intros ->].
+     iApply "HΨ". iFrame. iModIntro. iPureIntro. by intros ->.
+  Qed.
+
 End lazy_int.
