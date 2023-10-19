d5fd86539b0f7b3b68f72807b1c876ef82f07915
Alejandro Aguirre <alejandro@cs.au.dk>
2023-10-19 14:41:49 +0200

    Added approx coupling rule for rand-rand

--- theories/app_rel_logic/coupling_rules.v
+++ theories/app_rel_logic/coupling_rules.v
@@ -15,11 +15,11 @@ Section rules.
   Implicit Types v : val.
   Implicit Types l : loc.
 
-  Lemma wp_couple_tapes N M E e α αₛ ns nsₛ Φ (ε : nonnegreal) :
+  Lemma wp_couple_tapes (N M : nat) E e α αₛ ns nsₛ Φ (ε : nonnegreal) :
     to_val e = None →
     (∀ σ1, reducible e σ1) →
     nclose specN ⊆ E →
-    (0 < S N <= S M)%R →
+    (N <= M)%R →
     (((S M - S N) / S N) = ε)%R →
     spec_ctx ∗
     ▷ α ↪ (N; ns) ∗ ▷ αₛ ↪ₛ (M; nsₛ) ∗
@@ -81,11 +81,11 @@ Section rules.
     iModIntro. done.
   Qed.
 
-  Lemma wp_couple_tapes_rev N M E e α αₛ ns nsₛ Φ (ε : nonnegreal) :
+  Lemma wp_couple_tapes_rev (N M : nat) E e α αₛ ns nsₛ Φ (ε : nonnegreal) :
     to_val e = None →
     (∀ σ1, reducible e σ1) →
     nclose specN ⊆ E →
-    (0 < S M <= S N)%R →
+    (M <= N)%R →
     (((S N - S M) / S N) = ε)%R →
     spec_ctx ∗
     ▷ α ↪ (N; ns) ∗ ▷ αₛ ↪ₛ (M; nsₛ) ∗
@@ -145,6 +145,76 @@ Section rules.
     iMod ("Hwp" $! (state_upd_tapes <[α:=(N; ns ++ [n]) : tape]> _)
            with "[$Hh1 $Hauth2 $Ht1 $Hε2]") as "[Hred Hwp]".
     iModIntro. done.
+  Qed.
+
+
+  (** * rand(unit, N) ~ rand(unit, M) coupling, N <= M *)
+  Lemma wp_couple_rand_rand (N M : nat) z w K E Φ (ε : nonnegreal) :
+    TCEq N (Z.to_nat z) →
+    TCEq M (Z.to_nat w) →
+    nclose specN ⊆ E →
+    (N <= M)%R ->
+    (((S M - S N) / S N) = ε)%R →
+    refines_right K (rand #w from #()) ∗
+    € ε ∗
+    ▷ (∀ (n : fin (S N)) (m : fin (S M)),
+        ⌜(fin_to_nat n = m)⌝ →
+        refines_right K #m  -∗ WP (Val #n) @ E {{ Φ }})
+    ⊢ WP rand #z from #() @ E {{ Φ }}.
+  Proof.
+    iIntros (-> -> ? HNM Hε) "([#Hinv Hr ] & Hε & Hwp)".
+    iApply wp_lift_step_fupd_couple; [done|].
+    iIntros (σ1 e1' σ1' ε_now) "((Hh1 & Ht1) & Hauth2 & Hε2)".
+    iInv specN as (ρ' e0' σ0' m) ">(Hspec0 & %Hexec & Hauth & Hheap & Htapes)" "Hclose".
+    iDestruct (spec_prog_auth_frag_agree with "Hauth Hr") as %->.
+    iDestruct (spec_interp_auth_frag_agree with "Hauth2 Hspec0") as %<-.
+    iApply fupd_mask_intro; [set_solver|]; iIntros "Hclose'".
+    iSplit.
+    { iPureIntro. apply head_prim_reducible.
+      eexists (Val #0%fin, σ1).
+      apply head_step_support_equiv_rel.
+      by eapply (RandNoTapeS _ _ 0%fin). }
+    iDestruct (ec_supply_bound with "Hε2 Hε") as %Hle.
+    set (ε' := nnreal_minus ε_now ε Hle ).
+    replace ε_now with (nnreal_plus ε ε'); last first.
+    { apply nnreal_ext; simpl; lra. }
+    iApply exec_coupl_det_r; [done|].
+    iApply exec_coupl_prim_steps.
+    iExists (λ '(e2, σ2) '(e2', σ2'),
+        ∃ (n : fin _) (m : fin _),
+        (fin_to_nat n = m) ∧
+        (e2, σ2) = (Val #n, σ1) ∧ (e2', σ2') = (fill K #m, σ0')).
+    iSplit.
+    { iPureIntro. apply head_prim_reducible.
+      eexists (Val #0%fin, σ1).
+      apply head_step_support_equiv_rel.
+      by eapply (RandNoTapeS _ _ 0%fin). }
+    iSplit.
+    { iPureIntro. simpl.
+      rewrite fill_dmap // -(dret_id_right (prim_step _ _)) /=.
+      rewrite /dmap /=.
+      replace ε with (nnreal_plus ε nnreal_zero); last first.
+      { apply nnreal_ext; simpl; lra. }
+      eapply ARcoupl_dbind.
+      1,2:apply cond_nonneg.
+      2:eapply ARcoupl_rand_rand; eauto.
+      intros [] [] (b & ? & ? & [=] & [=])=>/=.
+      apply ARcoupl_dret.
+      simplify_eq. eauto. }
+    iIntros ([] [] (b & b' & Hbb' & [= -> ->] & [= -> ->])).
+    simplify_eq.
+    iMod (spec_interp_update (fill K #b', σ0') with "Hauth2 Hspec0") as "[Hauth2 Hspec0]".
+    iMod (spec_prog_update (fill K #b')  with "Hauth Hr") as "[Hauth Hr]".
+    iMod (ec_decrease_supply with "Hε2 Hε") as "Hε2".
+    do 2 iModIntro.
+    iMod "Hclose'" as "_".
+    iMod ("Hclose" with "[Hauth Hheap Hspec0 Htapes]") as "_".
+    { iModIntro. rewrite /spec_inv.
+      iExists _, _, _, 0. simpl.
+      iFrame. rewrite exec_O dret_1_1 //. }
+    iModIntro. iFrame.
+    iApply "Hwp"; eauto.
+    iSplit; done.
   Qed.
 
 
