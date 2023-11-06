9731f569bec2e3d801cbd13824ea16d5d2ef6989
Markus de Medeiros <markusdemedeiros@outlook.com>
2023-11-06 13:55:22 -0500

    approximate rejection sampler examples

--- theories/examples/approximate_bounded_rejection_sampler.v
+++ theories/examples/approximate_bounded_rejection_sampler.v
@@ -1,119 +1,117 @@
-(* FIXME stop being such a hoarder *)
 From clutch.app_rel_logic Require Export app_weakestpre primitive_laws.
 From clutch.ub_logic Require Export ub_clutch.
 
-(* Require Import clutch.ub_logic.ub_clutch.
-From stdpp Require Import namespaces.
-From iris.proofmode Require Import proofmode.
-From clutch.prelude Require Import stdpp_ext.
-From clutch.app_rel_logic Require Import lifting ectx_lifting.
-From clutch.prob_lang Require Import lang notation tactics metatheory.
-From clutch.rel_logic Require Export spec_ra.
-*)
-
 Set Default Proof Using "Type*".
-
-(* equivalence between the control flow in the bounded rejection sampler,
-   and the unbounded rejection sampler with error credits *)
-
-Context (PRED_MAX_VALUE: nat).
-Definition MAX_VALUE: nat := S PRED_MAX_VALUE.
 
 Section proofs.
   Local Open Scope R.
   Context `{!clutchGS Σ}.
 
-  (** tapes
-  Definition tapeN ns : tape := (PRED_MAX_VALUE; ns).
-  Definition nat_tape l ns : iProp Σ := l ↪ tapeN ns.
-  Notation "l ↪N ns" := (nat_tape l ns) (at level 20, format "l  ↪N  ns") : bi_scope.
-   *)
+  (* In general:
+      S n' = n
+      S m' = m
+     This notation simplifies the proofs. *)
 
-  (* None when the sampler is in a bad case, SOME #() when in good case *)
-  Definition bdd_cf_rejection_sampler (n m : nat) : val :=
+
+  (** PROGRAMS *)
+
+  (** rejection sampler which takes a preset number of attempts *)
+  Definition bdd_rejection_sampler (n' m' : nat) : val :=
     λ: "depth",
-      (* let: "𝛽" := alloc #m in *)
       let: "do_sample" :=
         (rec: "f" "tries_left" :=
            if: ("tries_left" - #1) < #0
             then NONE
-            else let: "next_sample" := (rand #m from #()) in
-                if: (BinOp LtOp "next_sample" #n)
-                then SOME #()
+            else let: "next_sample" := (rand #m' from #()) in
+                if: ("next_sample" ≤ #n')
+                then SOME "next_sample"
                 else "f" ("tries_left" - #1))
       in "do_sample" "depth".
 
-  (* rejection sampler control flow *)
-  Definition ubdd_cf_rejection_sampler (n m : nat) : val :=
+
+  (** rejection sampler that may take an unbounded number of attempts *)
+  Definition ubdd_rejection_sampler (n' m' : nat) : val :=
     λ: "_",
-      (* let: "𝛽" := alloc #m in *)
       let: "do_sample" :=
         (rec: "f" "_" :=
-           let: "next_sample" := (rand #m from #()) in
-           if: (BinOp LtOp "next_sample" #n)
-            then SOME #()
+           let: "next_sample" := (rand #m' from #()) in
+           if: ("next_sample" ≤ #n')
+            then SOME "next_sample"
             else "f" #())
       in "do_sample" #().
 
-  (* PROBLEM 1: can we prove that unbounded_rejection_samlper_cf always returns Some #()? *)
-  Definition ubdd_cf_safe (n m : nat) E :
-    {{{ True }}} ubdd_cf_rejection_sampler  n m #() @ E {{{ v, RET v ; ⌜ v = SOMEV #()⌝ }}}.
+
+
+
+
+  (** PROBLEM 0: show that the unbounded sampler only returns inbounds values *)
+  Definition ubdd_sampler_safe (n' m' : nat) E :
+    {{{ True }}} ubdd_rejection_sampler  n' m' #() @ E {{{ v, RET v ; ⌜exists v' : nat, v = SOMEV #v' /\ (v' < S n')%nat⌝ }}}.
   Proof.
-    iIntros (Φ) "_ HΦ". rewrite /ubdd_cf_rejection_sampler.
-    (* when we add tapes, why does wp_alloc_tape work? Why can we combine rules from the UB and rel logic? *)
-    (* are they two different logics? are they extensions of the same logic? *)
-    (* wp_apply wp_alloc_tape; [done|]; iIntros (𝛽) "H𝛽". *)
+    iIntros (Φ) "_ HΦ". rewrite /ubdd_rejection_sampler.
     do 4 wp_pure.
     iLöb as "IH"; wp_pures.
-
-    (* this is a regular lob induction proof, we never need to spend any error credits *)
+    (* this is a regular lob induction proof, it does not use error credits *)
     wp_apply wp_rand; [done|]; iIntros (n0) "_".
     wp_pures.
-    case_bool_decide.
-    - wp_pures; by iApply "HΦ".
+    case_bool_decide; last first.
     - wp_pure; by wp_apply ("IH" with "HΦ").
+    - wp_pures; iApply "HΦ"; iModIntro; iPureIntro.
+      exists (fin_to_nat n0); split; [auto|lia].
   Qed.
 
-  (* PROBLEM 2: prove that bounded_rejection_samlper_cf returns Some #() with error *)
 
-  Print nat.
 
+
+  (** PROBLEM 1: show the bounded sampler terminates inbounds with some error *)
+
+  (** nnreal exponents *)
   Fixpoint nnreal_nat_exp (r : nonnegreal) (n : nat) : nonnegreal :=
     match n with
     | O    => nnreal_one
     | S n' => nnreal_mult r (nnreal_nat_exp r n')
     end.
 
-  Definition err_factor n m := (nnreal_div (nnreal_nat (m - n)%nat) (nnreal_nat (S m)%nat)).
+  Lemma nnreal_nat_exp_limit : forall (r 𝜀 : nonnegreal), (r < 1) -> exists n, forall n0, (n0 > n)%nat -> (nnreal_nat_exp r n0) < 𝜀.
+  Proof. (* this is true, how to prove it in the nnreal framework? *) Admitted.
 
-  Lemma err_factor_lt1 n m : err_factor n m < 1.
-  Proof. Admitted.
 
-  (* error for the bounded sampler at a given depth *)
+  (** defining error values for each step *)
+
+  Definition err_factor n m := (nnreal_div (nnreal_nat (m - n)%nat) (nnreal_nat m%nat)).
+
+  Lemma err_factor_lt1 n m (Hn : (0 < n)%nat) (Hnm : (n < m)%nat) : err_factor n m < 1.
+  Proof.
+    rewrite /err_factor.
+    assert (Hdenom : (0 < m)%nat) by lia.
+    assert (Hnum : (m - n < m)%nat) by lia.
+    (* numerator is less than the denominator and they are both positive *)
+  Admitted.
+
+  (* error for the bounded sampler with a given number of tries remaining *)
   Definition bdd_cf_error n m depth := nnreal_nat_exp (err_factor n m) depth.
 
-  (* for proofs which iterate to the very end
-     ie, doing 0 samples requires error tolerance of 1
-   *)
+  (* this lemma is for proofs which iterate to the very end
+     ie, doing 0 samples requires error tolerance of 1 *)
   Lemma bdd_cd_error_final n m : bdd_cf_error n m 0 = nnreal_one.
   Proof. by rewrite /bdd_cf_error /nnreal_nat_exp. Qed.
 
-  (* for proofs which iterate up until the last sample
+  (* this lemma is for proofs which iterate up until the last sample
      ie, a rejection sampler to exclude the final recursive step *)
   Lemma bdd_cd_error_penultimate n m : bdd_cf_error n m 1 = err_factor n m.
   Proof. rewrite /bdd_cf_error /nnreal_nat_exp. (* nnreal: _ * 1 = _ *) Admitted.
 
-  (* how much mass to give to each possibility *)
-  Definition bdd_cf_sampling_error n m 𝜀₁ : (fin (S m)) -> nonnegreal
+  (* distribution of error mass 𝜀₁ for a given sample:
+      - zero error given to cases which are inbounds
+      - uniform error to the recursive case *)
+  Definition bdd_cf_sampling_error n m 𝜀₁ : (fin m) -> nonnegreal
     := fun sample =>
          if (sample <? n)%nat
-            (* good case: needs no error *)
             then nnreal_zero
-            (* recursive case: splits the total mass*)
             else (nnreal_div 𝜀₁ (err_factor n m)).
 
-  (* simplify the accumulated error at a given step *)
-  Lemma simplify_accum_err (n m d': nat) (s : fin (S m))  :
+  (* lemma for simplifying accumulated error in the recursive case *)
+  Lemma simplify_accum_err (n m d': nat) (s : fin m)  :
     (s <? n)%nat = false -> bdd_cf_sampling_error n m (bdd_cf_error n m (S d')) s = (bdd_cf_error n m d' ).
   Proof.
     intros Hcase.
@@ -124,8 +122,8 @@ Section proofs.
   Admitted.
 
 
-  (* each sample is <= 1 provided d > 1 (and the total mass is chosed appropriately) *)
-  Lemma sample_err_wf n m d (s : fin (S m)) : bdd_cf_sampling_error n m (bdd_cf_error n m (S d)) s <= 1.
+  (* error distribution is well-formed for each possible sample *)
+  Lemma sample_err_wf n m d (s : fin m) : bdd_cf_sampling_error n m (bdd_cf_error n m (S d)) s <= 1.
   Proof.
     (* it is either 1, or epsilon times something at most 1 *)
     rewrite /bdd_cf_sampling_error.
@@ -138,10 +136,12 @@ Section proofs.
       admit.
   Admitted.
 
-  (* mean mass is preserved *)
-  Lemma sample_err_mean n m 𝜀₁ :
-    SeriesC (λ s : fin (S m), 1 / S m * bdd_cf_sampling_error n m 𝜀₁ s) = 𝜀₁.
+  (* mean of error distribution is preserved *)
+  Lemma sample_err_mean n' m' 𝜀₁ :
+    SeriesC (λ s : fin (S m'), (1 / S m') * bdd_cf_sampling_error (S n') (S m') 𝜀₁ s) = 𝜀₁.
   Proof.
+    remember (S n') as n.
+    remember (S m') as m.
     rewrite /bdd_cf_sampling_error /err_factor.
     (* split the sum into the elements less than n and those greater *)
     (* sum of constant zero is zero *)
@@ -150,67 +150,189 @@ Section proofs.
 
 
 
-  (* no induction: just see if I can correctly use wp_couple_rand_adv_comp to increase the amount of credit *)
-  Definition bdd_cf_approx_safe_example (n m depth : nat) (Hnm : n <= m) E :
+
+  (** warmup: a finite example *)
+  Definition bdd_approx_safe_finite_example (n' m' depth : nat) (Hnm : (S n' < S m')%nat) E :
     (depth = 3%nat) ->
-    {{{ € (bdd_cf_error n m depth) }}} bdd_cf_rejection_sampler n m #depth @ E {{{ v, RET v ; ⌜ v = SOMEV #() ⌝ }}}.
+    {{{ € (bdd_cf_error (S n') (S m') depth) }}} bdd_rejection_sampler n' m' #(S depth) @ E {{{ v, RET v ; ⌜exists v' : nat, v = SOMEV #v' /\ (v' < S n')%nat⌝ }}}.
   Proof.
-    iIntros (-> Φ) "Hcr HΦ"; rewrite /bdd_cf_rejection_sampler.
-    wp_pures.
+    (* make these off by one errors easier to unpack *)
+    remember (S n') as n.
+    remember (S m') as m.
+    assert (Hn : (0 < n)%nat) by lia.
+    assert (Hm : (0 < m)%nat) by lia.
 
-    (* FIXME somewhere in here is shelves some goals, don't do that. *)
+    iIntros (-> Φ) "Hcr HΦ"; rewrite /bdd_rejection_sampler.
+    wp_pures. rewrite Heqm Heqn.
 
-    (* depth=3 sample *)
-    wp_apply (wp_couple_rand_adv_comp _ _ _ _ _ (bdd_cf_sampling_error _ _ _) with "Hcr"); [apply sample_err_wf|apply sample_err_mean|].
+    (* S depth=3 sample *)
+    wp_apply (wp_couple_rand_adv_comp _ _ _ Φ _ (bdd_cf_sampling_error (S n') _ _) with "Hcr").
+    { apply sample_err_wf. }
+    { pose P := (sample_err_mean n' m' (bdd_cf_error (S n') (S m') 3)); apply P. }
     iIntros (sample) "Hcr".
     wp_pures.
-    case_bool_decide; wp_pures; first by iApply "HΦ".
-    rewrite (simplify_accum_err _); last (apply Nat.ltb_nlt; rewrite /not; intro H'; by apply Nat2Z.inj_lt in H').
+    case_bool_decide; wp_pures.
+    { iApply "HΦ"; iModIntro; iPureIntro; exists (fin_to_nat sample); split; [auto|lia]. }
+    rewrite (simplify_accum_err (S n') (S m') _); last (apply Nat.ltb_nlt; by lia).
 
-    (* depth=2 sample *)
-    wp_apply (wp_couple_rand_adv_comp _ _ _ _ _ (bdd_cf_sampling_error _ _ _) with "Hcr"); [apply sample_err_wf|apply sample_err_mean|].
+    (* S depth=2 sample *)
+    wp_apply (wp_couple_rand_adv_comp _ _ _ Φ _ (bdd_cf_sampling_error (S n') _ _) with "Hcr").
+    { apply sample_err_wf. }
+    { pose P := (sample_err_mean n' m' (bdd_cf_error (S n') (S m') 2)); apply P. }
     iIntros (sample') "Hcr".
     wp_pures.
-    case_bool_decide; wp_pures; first by iApply "HΦ".
-    rewrite (simplify_accum_err _); last (apply Nat.ltb_nlt; rewrite /not; intro H'; by apply Nat2Z.inj_lt in H').
+    case_bool_decide; wp_pures.
+    { iApply "HΦ"; iModIntro; iPureIntro; exists (fin_to_nat sample'); split; [auto|lia]. }
+    rewrite (simplify_accum_err (S n') (S m') _); last (apply Nat.ltb_nlt; by lia).
 
-
-    (* depth=1 sample *)
+    (* S depth=1 sample *)
     rewrite bdd_cd_error_penultimate.
-
-    wp_apply (wp_rand_err_list_nat _ m (seq n (m - n))).
+    rewrite -Heqm -Heqn.
+    wp_apply (wp_rand_err_list_nat _ m' (seq n (m - n))).
     iSplitL "Hcr".
     - (* yeesh *)
       rewrite /err_factor.
       replace (length (seq _ _)) with (m - n)%nat by (symmetry; apply seq_length).
-      replace (S m) with (m + 1)%nat by lia.
+      replace (m) with (m' + 1)%nat by lia.
       done.
     - iIntros (sample'') "%Hsample''".
       wp_pures.
       case_bool_decide; wp_pures.
-      + iApply "HΦ". auto.
-      + exfalso; apply H1.
-        remember (sample'' <? n)%nat as P.
-        destruct P.
-        * apply inj_lt; apply Nat.ltb_lt; done.
-        * (* I don't know if that made it any easier lol *)
-          (* anyways, it's true, but I think the rand_err_list_nat needs to be strengthened
-             since we lost the fact that sample'' : fin (S m) along the way *)
-          admit.
-  Admitted.
+      + iApply "HΦ"; iModIntro; iPureIntro; exists (fin_to_nat sample''); split; [auto|lia].
+      + exfalso.
+        rewrite List.Forall_forall in Hsample''.
+        specialize Hsample'' with sample''.
+        apply Hsample''; last reflexivity.
+        rewrite in_seq.
+        split; first lia.
+        replace (n + (m-n))%nat with m by lia.
+        specialize (fin_to_nat_lt sample''); by lia.
+  Qed.
 
 
-
-  Definition bdd_cf_approx_safe (n m depth: nat) (Hnm : n <= m) E :
-    {{{ € (bdd_cf_error n m depth Hnm) }}} bdd_cf_rejection_sampler n m #depth @ E {{{ v, RET v ; ⌜ v = SOMEV #() ⌝ }}}.
+  (** general case for the bounded sampler *)
+  Definition bdd_approx_safe (n' m' depth : nat) (Hnm : (S n' < S m')%nat) E :
+    {{{ € (bdd_cf_error (S n') (S m') (S depth)) }}} bdd_rejection_sampler n' m' #(S depth)@ E {{{ v, RET v ; ⌜exists v' : nat, v = SOMEV #v' /\ (v' < S n')%nat⌝ }}}.
   Proof.
-    iIntros (Φ) "Hcr HΦ"; rewrite /bdd_cf_rejection_sampler.
+    remember (S n') as n.
+    remember (S m') as m.
+    assert (Hn : (0 < n)%nat) by lia.
+    assert (Hm : (0 < m)%nat) by lia.
+
+    iIntros (Φ) "Hcr HΦ"; rewrite /bdd_rejection_sampler.
     do 4 wp_pure.
-    (* invariant: (bdd_cf_error n m depth Hnm) *  ??? = ??? *)
-    iLöb as "IH".
-  Admitted.
+
+    (* induction should reach the base cse when S depth = 1 <=> depth = 0 *)
+    iInduction depth as [|depth' Hdepth'] "IH".
+    - wp_pures.
+      rewrite bdd_cd_error_penultimate.
+      wp_apply (wp_rand_err_list_nat _ m' (seq n (m - n))).
+      iSplitL "Hcr".
+      + rewrite /err_factor.
+        replace (length (seq _ _)) with (m - n)%nat by (symmetry; apply seq_length).
+        replace (m) with (m' + 1)%nat by lia.
+        done.
+      + iIntros (sample'') "%Hsample''".
+        wp_pures.
+        case_bool_decide; wp_pures.
+        * iApply "HΦ"; iModIntro; iPureIntro; exists (fin_to_nat sample''); split; [auto|lia].
+        * exfalso.
+          rewrite List.Forall_forall in Hsample''.
+          specialize Hsample'' with sample''.
+          apply Hsample''; last reflexivity.
+          rewrite in_seq.
+          split; first lia.
+          replace (n + (m-n))%nat with m by lia.
+          specialize (fin_to_nat_lt sample''); by lia.
+    - wp_pures.
+      replace (bool_decide _) with false; last (symmetry; apply bool_decide_eq_false; lia).
+      wp_pures.
+      wp_apply (wp_couple_rand_adv_comp _ _ _ Φ _ (bdd_cf_sampling_error (S n') _ _) with "Hcr").
+      { apply sample_err_wf. }
+      { pose P := (sample_err_mean n' m' (bdd_cf_error (S n') (S m') _)); rewrite Heqn Heqm; by eapply P. }
+      iIntros (sample') "Hcr".
+      wp_pures.
+      case_bool_decide.
+      + wp_pures; iApply "HΦ"; iModIntro; iPureIntro; exists (fin_to_nat sample'); split; [auto|lia].
+      + wp_pure.
+        rewrite (simplify_accum_err (S n') (S m') _); last (apply Nat.ltb_nlt; by lia).
+        wp_bind (#_ - #_)%E. wp_pure.
+        replace (S (S depth') - 1)%Z with (Z.of_nat (S depth')) by lia.
+        rewrite Heqn Heqm.
+        wp_apply ("IH" with "Hcr HΦ").
+  Qed.
 
 
+
+  (** PROBLEM 3: show that the unbounded approximate sampler is safe if we have enough error to eliminate past a depth *)
+  Definition ubdd_approx_safe (n' m' depth : nat) (Hnm : (S n' < S m')%nat) E :
+    {{{ € (bdd_cf_error (S n') (S m') (S depth)) }}} ubdd_rejection_sampler n' m' #() @ E {{{ v, RET v ; ⌜exists v' : nat, v = SOMEV #v' /\ (v' < S n')%nat⌝  }}}.
+  Proof.
+    remember (S n') as n.
+    remember (S m') as m.
+    assert (Hn : (0 < n)%nat) by lia.
+    assert (Hm : (0 < m)%nat) by lia.
+
+    iIntros (Φ) "Hcr HΦ"; rewrite /ubdd_rejection_sampler.
+    do 4 wp_pure.
+
+    iInduction depth as [|depth' Hdepth'] "IH".
+    - wp_pures.
+      rewrite bdd_cd_error_penultimate.
+      wp_apply (wp_rand_err_list_nat _ m' (seq n (m - n))).
+      iSplitL "Hcr".
+      + rewrite /err_factor.
+        replace (length (seq _ _)) with (m - n)%nat by (symmetry; apply seq_length).
+        replace (m) with (m' + 1)%nat by lia.
+        done.
+      + iIntros (sample'') "%Hsample''".
+        wp_pures.
+        case_bool_decide; wp_pures.
+        * iApply "HΦ"; iModIntro; iPureIntro; exists (fin_to_nat sample''); split; [auto|lia].
+        * exfalso.
+          rewrite List.Forall_forall in Hsample''.
+          specialize Hsample'' with sample''.
+          apply Hsample''; last reflexivity.
+          rewrite in_seq.
+          split; first lia.
+          replace (n + (m-n))%nat with m by lia.
+          specialize (fin_to_nat_lt sample''); by lia.
+    - wp_pures.
+      wp_apply (wp_couple_rand_adv_comp _ _ _ Φ _ (bdd_cf_sampling_error (S n') _ _) with "Hcr").
+      { apply sample_err_wf. }
+      { pose P := (sample_err_mean n' m' (bdd_cf_error (S n') (S m') _)); rewrite Heqn Heqm; by eapply P. }
+      iIntros (sample') "Hcr".
+      wp_pures.
+      case_bool_decide.
+      + wp_pures. iApply "HΦ"; iModIntro; iPureIntro; exists (fin_to_nat sample'); split; [auto|lia].
+      + wp_pure.
+        rewrite (simplify_accum_err (S n') (S m') _); last (apply Nat.ltb_nlt; by lia).
+        rewrite Heqn Heqm.
+        wp_apply ("IH" with "Hcr HΦ").
+  Qed.
+
+
+  (** PROBLEM 4: show that any positive error 𝜀 suffices to make the unbounded sampler terminate inbounds *)
+  Theorem ubdd_cf_safety (n' m' : nat) (Hnm : (S n' < S m')%nat) E : forall 𝜀,
+    ⊢ {{{ €𝜀 ∗ ⌜𝜀 > 0 ⌝  }}} ubdd_rejection_sampler n' m' #()@ E {{{ v, RET v ; ⌜exists v' : nat, v = SOMEV #v' /\ (v' < S n')%nat⌝ }}}.
+  Proof.
+    iIntros (𝜀 Φ) " !> (Hcr&%Hcrpos) HΦ".
+    destruct (nnreal_nat_exp_limit (err_factor (S n') (S m')) 𝜀) as [d].
+    - apply err_factor_lt1; lia.
+    - iApply (ubdd_approx_safe with "[Hcr] [HΦ]"); auto.
+      iApply ec_weaken; last iAssumption.
+      rewrite /bdd_cf_error.
+      apply Rlt_le.
+      specialize H with (S d); apply H; lia.
+  Qed.
+
+
+  (* PROBLEM ??: If error credits can rule out nontermination we could show that the unbounded
+     rejection sampler doesn't terminate with probability zero (in the UB logic)  *)
+
+  (* PROBLEM ??: We should be able to prove:
+        - the unbounded sampler is equivalent to the bounded sampler with a given error
+        - the bounded sampler is equivalent to a single sample with a given error
+      in the approximate relational logic. *)
 
 End proofs.
-
