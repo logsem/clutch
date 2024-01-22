1931b323e6f9c487880e2cf99d94d0b2ca667ae1
Hei Li <hei411@gmail.com>
2024-01-22 16:20:52 +0100

    COMPLETED

--- theories/ub_logic/total_adequacy.v
+++ theories/ub_logic/total_adequacy.v
@@ -7,22 +7,246 @@ From clutch.prob Require Import distribution union_bounds.
 Import uPred.
 
 Lemma twp_step_fupd_ineq_prim_step (e : language.expr prob_lang) σ (ε ε1:nonnegreal) (ε2: language.cfg prob_lang -> nonnegreal) R:
+  reducible e σ -> 
   (∃ r, ∀ ρ : language.cfg prob_lang, ε2 ρ <= r) ->
   ε1 + SeriesC
          (λ ρ, prim_step e σ ρ * ε2 ρ) <= ε -> ub_lift (prim_step e σ) R ε1 ->
   (∀ e, R e → 1 - ε2 e <= SeriesC (lim_exec e)) ->
   1 - ε <= SeriesC (λ a, step (e, σ) a * SeriesC (lim_exec a)).
 Proof.
-Admitted.
+  intros Hred Hbound Hsum Hub H.
+  simpl.
+  assert (1 - (ε1 + SeriesC (λ ρ, prim_step e σ ρ * ε2 ρ)) <=
+            SeriesC (λ a, prim_step e σ a * SeriesC (lim_exec a))) as Hint; last first.
+  { etrans; last exact. apply Rminus_le. lra. }
+  rewrite Rcomplements.Rle_minus_l Rplus_comm Rplus_assoc.
+  rewrite <-SeriesC_plus; last first.
+  { eapply ex_seriesC_le; last first.
+    - instantiate (1 := prim_step e σ).
+      apply pmf_ex_seriesC.
+    -  intros. split.
+       + by apply Rmult_le_pos.
+       + rewrite <-Rmult_1_r. by apply Rmult_le_compat_l.
+  }
+  { destruct Hbound as [r ?]. eapply ex_seriesC_le; last first.
+    - instantiate (1 := λ ρ, prim_step e σ ρ * r).
+      apply ex_seriesC_scal_r.
+      apply pmf_ex_seriesC.
+    -  intros. split.
+       + apply Rmult_le_pos; first done. apply cond_nonneg. 
+       + by apply Rmult_le_compat_l.
+  }
+  erewrite SeriesC_ext; last first.
+  { intros n. by rewrite <-Rmult_plus_distr_l. }
+  simpl in Hub, H, R. simpl.
+  assert (forall ρ, Decision (R ρ)) as Hdec.
+  { intros. apply make_decision. }
+  rewrite (SeriesC_ext _
+             (λ ρ, (if bool_decide(R ρ) then prim_step e σ ρ * (ε2 ρ + SeriesC (lim_exec ρ)) else 0)+
+                     if bool_decide (~R ρ) then prim_step e σ ρ * (ε2 ρ + SeriesC (lim_exec ρ)) else 0
+          )); last first.
+  { intros. repeat case_bool_decide; try done.
+    - by rewrite Rplus_0_r.
+    - by rewrite Rplus_0_l.
+  }
+  rewrite SeriesC_plus; last first.
+  { eapply ex_seriesC_filter_pos.
+    - intros. apply Rmult_le_pos; first done.
+      apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+    - destruct Hbound as [r?].
+      eapply ex_seriesC_ext; last first.
+      + eapply pmf_ex_seriesC_mult_fn. shelve.
+      + intros. simpl. f_equal.
+        Unshelve. exists (r+1).
+        intros; split.
+        * apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+        * by apply Rplus_le_compat.
+  }
+  { eapply ex_seriesC_filter_pos.
+    - intros. apply Rmult_le_pos; first done.
+      apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+    - destruct Hbound as [r?].
+      eapply ex_seriesC_ext; last first.
+      + eapply pmf_ex_seriesC_mult_fn. shelve.
+      + intros. simpl. f_equal.
+        Unshelve. exists (r+1).
+        intros; split.
+        * apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+        * by apply Rplus_le_compat.
+  }
+  trans (ε1 +
+  (SeriesC
+     (λ x : expr * state,
+        if bool_decide (R x) then prim_step e σ x else 0) +
+   SeriesC
+     (λ x : expr * state,
+        if bool_decide (¬ R x) then prim_step e σ x * (ε2 x + SeriesC (lim_exec x)) else 0))).
+  2:{ apply Rplus_le_compat_l. apply Rplus_le_compat_r.
+      apply SeriesC_le.
+      - intros. case_bool_decide; last done.
+        split; [apply pmf_pos|].
+        rewrite -{1}(Rmult_1_r (prim_step _ _ _)). apply Rmult_le_compat_l; [apply pmf_pos|].
+        pose proof (H n H0).
+        rewrite Rplus_comm. by rewrite <-Rcomplements.Rle_minus_l.
+      - apply ex_seriesC_filter_pos.
+        + intros. apply Rmult_le_pos; [apply pmf_pos|].
+          apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+        + apply pmf_ex_seriesC_mult_fn. destruct Hbound as [r?]. exists (r+1).
+          intros; split.
+          * apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+          * by apply Rplus_le_compat.
+  }
+  trans (ε1 +
+           (SeriesC (λ x : expr * state, if bool_decide (R x) then prim_step e σ x else 0))); last first.
+  { rewrite -Rplus_assoc. rewrite -{1}(Rplus_0_r (_+_)).
+    apply Rplus_le_compat_l. apply SeriesC_ge_0'.
+    intros. case_bool_decide; try lra.
+    apply Rmult_le_pos; [apply pmf_pos|].
+    apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+  }
+  rewrite /ub_lift in Hub.
+  assert (prob (prim_step e σ) (∽(λ ρ, bool_decide(R ρ)))%P <= ε1).
+  { apply Hub. intros. by apply bool_decide_eq_true_2. }
+  etrans.
+  2: { apply Rplus_le_compat_r. exact. }
+  rewrite /prob. simpl.
+  rewrite <-SeriesC_plus; last first.
+  - apply ex_seriesC_filter_pos; [apply pmf_pos|apply pmf_ex_seriesC].
+  - eapply ex_seriesC_ext.
+    + intros. by rewrite <- bool_decide_not.
+    + apply ex_seriesC_filter_pos; [apply pmf_pos|apply pmf_ex_seriesC].
+  - rewrite (SeriesC_ext _ (λ ρ, prim_step e σ ρ)); last first.
+    { intros. case_bool_decide; simpl.
+      - apply Rplus_0_l.
+      - apply Rplus_0_r.
+    }
+    simpl. apply Req_le_sym.
+    epose proof (@prim_step_mass prob_lang) as K. simpl in K.
+    apply K. apply Hred.
+Qed.
 
 Lemma twp_step_fupd_ineq_state_step (e : language.expr prob_lang) σ l (ε ε1:nonnegreal) (ε2: _ -> nonnegreal) R:
+  l ∈ language.get_active σ -> 
   (∃ r, ∀ ρ : language.cfg prob_lang, ε2 ρ <= r) ->
   ε1 + SeriesC
          (λ ρ, language.state_step σ l ρ * ε2 (e, ρ)) <= ε -> ub_lift (language.state_step σ l) R ε1 ->
   (∀ s, R s → 1 - ε2 (e, s) <= SeriesC (lim_exec (e, s))) ->
   1 - ε <= SeriesC (λ a, state_step σ l a * SeriesC (lim_exec (e, a))).
 Proof.
-Admitted.
+intros Hred Hbound Hsum Hub H.
+  simpl.
+  assert (1 - (ε1 + SeriesC (λ ρ, state_step σ l ρ * ε2 (e, ρ))) <=
+            SeriesC (λ a, state_step σ l a * SeriesC (lim_exec (e, a)))) as Hint; last first.
+  { etrans; last exact. rewrite -Ropp_minus_distr -(Ropp_minus_distr (_+_)).
+    apply Ropp_le_cancel. rewrite !Ropp_involutive Rcomplements.Rle_minus_l.
+    rewrite Rplus_assoc Rplus_opp_l Rplus_0_r. done.
+  }
+  rewrite Rcomplements.Rle_minus_l Rplus_comm Rplus_assoc.
+  rewrite <-SeriesC_plus; last first.
+  { eapply ex_seriesC_le; last first.
+    - instantiate (1 := state_step σ l).
+      apply pmf_ex_seriesC.
+    -  intros. split.
+       + by apply Rmult_le_pos.
+       + rewrite <-Rmult_1_r. by apply Rmult_le_compat_l.
+  }
+  { destruct Hbound as [r ?]. eapply ex_seriesC_le; last first.
+    - instantiate (1 := λ ρ, state_step σ l ρ * r).
+      apply ex_seriesC_scal_r.
+      apply pmf_ex_seriesC.
+    -  intros. split.
+       + apply Rmult_le_pos; first done. apply cond_nonneg. 
+       + by apply Rmult_le_compat_l.
+  }
+  erewrite SeriesC_ext; last first.
+  { intros n. by rewrite <-Rmult_plus_distr_l. }
+  simpl in Hub, H, R. simpl.
+  assert (forall ρ, Decision (R ρ)) as Hdec.
+  { intros. apply make_decision. }
+  rewrite (SeriesC_ext _
+             (λ ρ, (if bool_decide(R ρ) then state_step σ l ρ * (ε2 (e, ρ) + SeriesC (lim_exec (e, ρ))) else 0)+
+                     if bool_decide (~R ρ) then state_step σ l ρ * (ε2 (e, ρ) + SeriesC (lim_exec (e, ρ))) else 0
+          )); last first.
+  { intros. repeat case_bool_decide; try done.
+    - by rewrite Rplus_0_r.
+    - by rewrite Rplus_0_l.
+  }
+  rewrite SeriesC_plus; last first.
+  { eapply ex_seriesC_filter_pos.
+    - intros. apply Rmult_le_pos; first done.
+      apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+    - destruct Hbound as [r?].
+      eapply ex_seriesC_ext; last first.
+      + eapply pmf_ex_seriesC_mult_fn. shelve.
+      + intros. simpl. f_equal.
+        Unshelve. exists (r+1).
+        intros; split.
+        * apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+        * by apply Rplus_le_compat.
+  }
+  { eapply ex_seriesC_filter_pos.
+    - intros. apply Rmult_le_pos; first done.
+      apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+    - destruct Hbound as [r?].
+      eapply ex_seriesC_ext; last first.
+      + eapply pmf_ex_seriesC_mult_fn. shelve.
+      + intros. simpl. f_equal.
+        Unshelve. exists (r+1).
+        intros; split.
+        * apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+        * by apply Rplus_le_compat.
+  }
+  trans (ε1 +
+  (SeriesC
+     (λ x,
+        if bool_decide (R x) then state_step σ l x else 0) +
+   SeriesC
+     (λ x,
+        if bool_decide (¬ R x) then state_step σ l x * (ε2 (e, x) + SeriesC (lim_exec (e, x))) else 0))).
+  2:{ apply Rplus_le_compat_l. apply Rplus_le_compat_r.
+      apply SeriesC_le.
+      - intros. case_bool_decide; last done.
+        split; [apply pmf_pos|].
+        rewrite -{1}(Rmult_1_r (state_step _ _ _)). apply Rmult_le_compat_l; [apply pmf_pos|].
+        pose proof (H n H0).
+        rewrite Rplus_comm. by rewrite <-Rcomplements.Rle_minus_l.
+      - apply ex_seriesC_filter_pos.
+        + intros. apply Rmult_le_pos; [apply pmf_pos|].
+          apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+        + apply pmf_ex_seriesC_mult_fn. destruct Hbound as [r?]. exists (r+1).
+          intros; split.
+          * apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+          * by apply Rplus_le_compat.
+  }
+  trans (ε1 +
+           (SeriesC (λ x, if bool_decide (R x) then state_step σ l x else 0))); last first.
+  { rewrite -Rplus_assoc. rewrite -{1}(Rplus_0_r (_+_)).
+    apply Rplus_le_compat_l. apply SeriesC_ge_0'.
+    intros. case_bool_decide; try lra.
+    apply Rmult_le_pos; [apply pmf_pos|].
+    apply Rplus_le_le_0_compat; [apply cond_nonneg|apply pmf_SeriesC_ge_0].
+  }
+  rewrite /ub_lift in Hub.
+  assert (prob (state_step σ l) (∽(λ ρ, bool_decide(R ρ)))%P <= ε1).
+  { apply Hub. intros. by apply bool_decide_eq_true_2. }
+  etrans.
+  2: { apply Rplus_le_compat_r. exact. }
+  rewrite /prob. simpl.
+  rewrite <-SeriesC_plus; last first.
+  - apply ex_seriesC_filter_pos; [apply pmf_pos|apply pmf_ex_seriesC].
+  - eapply ex_seriesC_ext.
+    + intros. by rewrite <- bool_decide_not.
+    + apply ex_seriesC_filter_pos; [apply pmf_pos|apply pmf_ex_seriesC].
+  - rewrite (SeriesC_ext _ (λ ρ, state_step σ l ρ)); last first.
+    { intros. case_bool_decide; simpl.
+      - apply Rplus_0_l.
+      - apply Rplus_0_r.
+    }
+    simpl. apply Req_le_sym.
+    epose proof (@state_step_mass) as K. simpl in K.
+    apply K. simpl in Hred. rewrite /get_active in Hred.
+    set_solver. 
+Qed.
 
 Section adequacy.
   Context `{!ub_clutchGS Σ}.
@@ -119,6 +343,7 @@ Section adequacy.
           rewrite dbind_mass.
           erewrite SeriesC_ext.
           - eapply (twp_step_fupd_ineq_state_step _ _ _ _ _ (λ _, ε2)); try done.
+            + set_solver.
             + exists ε. intros. trans (ε1 + ε2); last lra. destruct ε1, ε2. simpl. lra.
             + rewrite SeriesC_scal_r. etrans; last exact Hineq.
               pose proof (pmf_SeriesC (language.state_step σ l)).
@@ -149,7 +374,8 @@ Section adequacy.
           simpl. erewrite SeriesC_ext; last by (intros; rewrite dret_id_right).
           rewrite dbind_mass.
           erewrite SeriesC_ext.
-          - by eapply twp_step_fupd_ineq_state_step.
+          - eapply twp_step_fupd_ineq_state_step; try done.
+            set_solver. 
           - intros; simpl. done.
         }    
         iIntros (a HR). iMod ("H" $! a (HR)) as "H".
