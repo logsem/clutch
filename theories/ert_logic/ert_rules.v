cfab88103ba5c6d6416193433b848f72998d76be
Hei Li <hei411@gmail.com>
2024-03-14 15:38:10 +0100

    Add ert_rules + proofmode

---
+++ theories/ert_logic/ert_rules.v
@@ -0,0 +1,12 @@
+(** * ERT rules  *)
+From stdpp Require Import namespaces finite.
+From iris.proofmode Require Import proofmode.
+From clutch.prelude Require Import stdpp_ext.
+From clutch.prob_lang Require Import notation tactics metatheory.
+From clutch.prob_lang Require Export lang.
+From clutch.ub_logic Require Export lifting proofmode ectx_lifting primitive_laws seq_amplification.
+
+Section metatheory.
+
+  Local Open Scope R.
+End metatheory.
---
+++ theories/ert_logic/proofmode.v
@@ -0,0 +1,6 @@
+From clutch.prob_lang Require Import lang notation class_instances tactics.
+From clutch.prob_lang Require Export wp_tactics.
+From clutch.ert_logic Require Import ert_weakestpre primitive_laws.
+From iris.prelude Require Import options.
+
+
