9216a846cebc7967c46e111ec104dc96c77cc7ff
Hei Li <hei411@gmail.com>
2024-03-15 13:26:38 +0100

    possible lifting lemmas

--- theories/ert_logic/lifting.v
+++ theories/ert_logic/lifting.v
@@ -32,140 +32,142 @@ Qed.
 (* TODO: What should this actually say?? *)
 Lemma wp_lift_step_fupd E Φ e1 s :
   to_val e1 = None →
-  (∀ σ1, state_interp σ1
+  (∀ σ1 x1, state_interp σ1 ∗ etc_interp x1
      ={E,∅}=∗
     ⌜reducible (e1, σ1)⌝ ∗
      ∀ e2 σ2,
       ⌜prim_step e1 σ1 (e2, σ2) > 0 ⌝ ={∅}=∗ ▷ |={∅,E}=>
-      state_interp σ2 ∗ WP e2 @ s; E {{ Φ }})
+      ∃ x2:nonnegreal, state_interp σ2 ∗ ⌜(x2 + 1 = x1)%R⌝ ∗ etc_interp x2 ∗ WP e2 @ s; E {{ Φ }})
   ⊢ WP e1 @ s; E {{ Φ }}.
 Proof.
   iIntros (?) "H".
   iApply wp_lift_step_fupd_ERM; [done|].
   iIntros (σ1 x) "[Hσ Hx]".
-  (*
-  iMod ("H" with "Hσ") as "[%Hs [H1 H]]". iModIntro.
+  iMod ("H" with "[$]") as "[%Hs H]". iModIntro.
   iApply (ERM_prim_step e1 σ1).
   (* iExists _.
      iExists nnreal_zero. *)
-  unshelve iExists (nnreal_minus x nnreal_one _).
-  iSplit.
-  { iPureIntro. simpl. done. }
-  iSplit.
-  { iPureIntro. simpl. lra. }
-  iSplit.
-  { iPureIntro.
-    eapply ub_lift_pos_R, ub_lift_trivial.
-    simpl; lra.
-  }
-  iIntros (e2 σ2 (?&?)).
-  iMod ("H" with "[//]")as "H".
-  iIntros "!> !>".
-  by iMod "H" as "[$ $]".
-   *)
+  (* unshelve iExists (nnreal_minus x nnreal_one _). *)
+  (* iSplit. *)
+  (* { iPureIntro. simpl. done. } *)
+  (* iSplit. *)
+  (* { iPureIntro. simpl. lra. } *)
+  (* iSplit. *)
+  (* { iPureIntro. *)
+  (*   eapply ub_lift_pos_R, ub_lift_trivial. *)
+  (*   simpl; lra. *)
+  (* } *)
+  (* iIntros (e2 σ2 (?&?)). *)
+  (* iMod ("H" with "[//]")as "H". *)
+  (* iIntros "!> !>". *)
+  (* by iMod "H" as "[$ $]". *)
 Admitted.
 
 (** Derived lifting lemmas. *)
 Lemma wp_lift_step E Φ e1 s :
   to_val e1 = None →
-  (∀ σ1, state_interp σ1 ={E,∅}=∗
+  (∀ σ1 x1, state_interp σ1 ∗ etc_interp x1 ={E,∅}=∗
     ⌜reducible (e1, σ1)⌝ ∗
     ▷ ∀ e2 σ2,
      ⌜prim_step e1 σ1 (e2, σ2) > 0⌝ ={∅,E}=∗
-      state_interp σ2 ∗
+      ∃ x2:nonnegreal, state_interp σ2 ∗ ⌜(x2 + 1 = x1)%R⌝ ∗ etc_interp x2 ∗
       WP e2 @ s; E {{ Φ }})
   ⊢ WP e1 @ s; E {{ Φ }}.
 Proof.
-  iIntros (?) "H". iApply wp_lift_step_fupd; [done|]. iIntros (?) "Hσ".
-  iMod ("H" with "Hσ") as "[$ H]". iIntros "!>" (???) "!>!>" . by iApply "H".
+  iIntros (?) "H". iApply wp_lift_step_fupd; [done|]. iIntros (??) "[Hσ Hx]".
+  iMod ("H" with "[$]") as "[$ H]". iIntros "!>" (???) "!>!>" . by iApply "H".
 Qed.
 
 Lemma wp_lift_pure_step `{!Inhabited (state Λ)} E E' Φ e1 s :
   (∀ σ1, reducible (e1, σ1)) →
   (∀ σ1 e2 σ2, prim_step e1 σ1 (e2, σ2) > 0 → σ2 = σ1) →
-  (|={E}[E']▷=> ∀ e2 σ, ⌜prim_step e1 σ (e2, σ) > 0⌝ → WP e2 @ s; E {{ Φ }})
+  (∀ x1, etc_interp x1 ={E}[E']▷=∗ ∀ e2 σ, ⌜prim_step e1 σ (e2, σ) > 0⌝ -∗
+                                           ∃ x2:nonnegreal, ⌜(x2 + 1 = x1)%R⌝ ∗ etc_interp x2 ∗
+                                                            WP e2 @ s; E {{ Φ }})
   ⊢ WP e1 @ s; E {{ Φ }}.
 Proof.
   iIntros (Hsafe Hstep) "H". iApply wp_lift_step.
   { by eapply (to_final_None_1 (e1, inhabitant)), reducible_not_final. }
-  iIntros (σ1) "Hσ". iMod "H".
+  iIntros (σ1 x1) "[Hσ Hx]". iMod ("H" with "[$]") as "H".
   iApply fupd_mask_intro; first set_solver. iIntros "Hclose".
   iSplit; [done|].
   iNext. iIntros (e2 σ2 Hprim).
   destruct (Hstep _ _ _ Hprim).
   iMod "Hclose" as "_". iMod "H".
-  iDestruct ("H" with "[//]") as "H". simpl. by iFrame.
+  iDestruct ("H" with "[//]") as "H". iFrame. done.
 Qed.
 
 (* Atomic steps don't need any mask-changing business here, one can *)
 (* use the generic lemmas here. *)
 Lemma wp_lift_atomic_step_fupd {E1 E2 Φ} e1 s :
   to_val e1 = None →
-  (∀ σ1, state_interp σ1 ={E1}=∗
+  (∀ σ1 x1, state_interp σ1 ∗ etc_interp x1 ={E1}=∗
     ⌜reducible (e1, σ1)⌝ ∗
     ∀ e2 σ2, ⌜prim_step e1 σ1 (e2, σ2) > 0⌝ ={E1}[E2]▷=∗
-      state_interp σ2 ∗
+      ∃ x2:nonnegreal, state_interp σ2 ∗ ⌜(x2 + 1 = x1)%R⌝ ∗ etc_interp x2 ∗
       from_option Φ False (to_val e2))
   ⊢ WP e1 @ s; E1 {{ Φ }}.
 Proof.
   iIntros (?) "H".
-  iApply (wp_lift_step_fupd E1 _ e1)=>//; iIntros (σ1) "Hσ1".
-  iMod ("H" $! σ1 with "Hσ1") as "[$ H]".
+  iApply (wp_lift_step_fupd E1 _ e1)=>//; iIntros (σ1 x1) "[Hσ1 Hx1]".
+  iMod ("H" $! σ1 with "[$]") as "[$ H]".
   iApply fupd_mask_intro; first set_solver.
   iIntros "Hclose" (e2 σ2 Hs). iMod "Hclose" as "_".
   iMod ("H" $! e2 σ2 with "[#]") as "H"; [done|].
   iApply fupd_mask_intro; first set_solver. iIntros "Hclose !>".
-  iMod "Hclose" as "_". iMod "H" as "($ & HQ)".
+  iMod "Hclose" as "_". iMod "H" as "(%&?&%&?&?)".
   destruct (to_val e2) eqn:?; last by iExFalso.
-  (* iApply ub_wp_value; last done. by apply of_to_val. *)
-Admitted.
+  iModIntro. iFrame. iExists _. iFrame. iSplit; first done.
+  iApply ert_wp_value; last done. by apply of_to_val.
+Qed.
 
 Lemma wp_lift_atomic_step {E Φ} e1 s :
   to_val e1 = None →
-  (∀ σ1, state_interp σ1 ={E}=∗
+  (∀ σ1 x1, state_interp σ1 ∗ etc_interp x1 ={E}=∗
          ⌜reducible (e1, σ1)⌝ ∗
          ▷ ∀ e2 σ2, ⌜prim_step e1 σ1 (e2, σ2) > 0⌝ ={E}=∗
-                    state_interp σ2 ∗
+                    ∃ x2:nonnegreal, state_interp σ2 ∗ ⌜(x2 + 1 = x1)%R⌝ ∗ etc_interp x2 ∗
                     from_option Φ False (to_val e2))
   ⊢ WP e1 @ s; E {{ Φ }}.
 Proof.
   iIntros (?) "H". iApply wp_lift_atomic_step_fupd; [done|].
-  iIntros (?) "?". iMod ("H" with "[$]") as "[$ H]".
+  iIntros (??) "[??]". iMod ("H" with "[$]") as "[$ H]".
   iIntros "!> *". iIntros (Hstep) "!> !>".
   by iApply "H".
 Qed.
 
-Lemma wp_lift_pure_det_step `{!Inhabited (state Λ)} {E E' Φ} e1 e2 s :
-  (∀ σ1, reducible (e1, σ1)) →
-  (∀ σ1 e2' σ2, prim_step e1 σ1 (e2', σ2) > 0 → σ2 = σ1 ∧ e2' = e2) →
-  (|={E}[E']▷=> WP e2 @ s; E {{ Φ }}) ⊢ WP e1 @ s; E {{ Φ }}.
-Proof.
-  iIntros (? Hpuredet) "H". iApply (wp_lift_pure_step E E'); try done.
-  { naive_solver. }
-  iApply (step_fupd_wand with "H"); iIntros "H".
-  iIntros (e' σ (?&->)%Hpuredet); auto.
-Qed.
+(** TODO FIX THESE*)
+(* Lemma wp_lift_pure_det_step `{!Inhabited (state Λ)} {E E' Φ} e1 e2 s : *)
+(*   (∀ σ1, reducible (e1, σ1)) → *)
+(*   (∀ σ1 e2' σ2, prim_step e1 σ1 (e2', σ2) > 0 → σ2 = σ1 ∧ e2' = e2) → *)
+(*   (|={E}[E']▷=> WP e2 @ s; E {{ Φ }}) ⊢ WP e1 @ s; E {{ Φ }}. *)
+(* Proof. *)
+(*   iIntros (? Hpuredet) "H". iApply (wp_lift_pure_step E E'); try done. *)
+(*   { naive_solver. } *)
+(*   iApply (step_fupd_wand with "H"); iIntros "H". *)
+(*   iIntros (e' σ (?&->)%Hpuredet); auto. *)
+(* Qed. *)
 
-Lemma wp_pure_step_fupd `{!Inhabited (state Λ)} E E' e1 e2 φ n Φ s :
-  PureExec φ n e1 e2 →
-  φ →
-  (|={E}[E']▷=>^n WP e2 @ s; E {{ Φ }}) ⊢ WP e1 @ s; E {{ Φ }}.
-Proof.
-  iIntros (Hexec Hφ) "Hwp". specialize (Hexec Hφ).
-  iInduction Hexec as [e|n e1 e2 e3 [Hsafe ?]] "IH"; simpl; first done.
-  iApply wp_lift_pure_det_step.
-  - done. 
-  - intros σ1 e2' σ2 Hpstep.
-    by injection (pmf_1_supp_eq _ _ _ (pure_step_det σ1) Hpstep).
-  - by iApply (step_fupd_wand with "Hwp").
-Qed.
+(* Lemma wp_pure_step_fupd `{!Inhabited (state Λ)} E E' e1 e2 φ n Φ s : *)
+(*   PureExec φ n e1 e2 → *)
+(*   φ → *)
+(*   (|={E}[E']▷=>^n WP e2 @ s; E {{ Φ }}) ⊢ WP e1 @ s; E {{ Φ }}. *)
+(* Proof. *)
+(*   iIntros (Hexec Hφ) "Hwp". specialize (Hexec Hφ). *)
+(*   iInduction Hexec as [e|n e1 e2 e3 [Hsafe ?]] "IH"; simpl; first done. *)
+(*   iApply wp_lift_pure_det_step. *)
+(*   - done.  *)
+(*   - intros σ1 e2' σ2 Hpstep. *)
+(*     by injection (pmf_1_supp_eq _ _ _ (pure_step_det σ1) Hpstep). *)
+(*   - by iApply (step_fupd_wand with "Hwp"). *)
+(* Qed. *)
 
-Lemma wp_pure_step_later `{!Inhabited (state Λ)} E e1 e2 φ n Φ s :
-  PureExec φ n e1 e2 →
-  φ →
-  ▷^n WP e2 @ s; E {{ Φ }} ⊢ WP e1 @ s; E {{ Φ }}.
-Proof.
-  intros Hexec ?. rewrite -wp_pure_step_fupd //. clear Hexec.
-  induction n as [|n IH]; by rewrite //= -step_fupd_intro // IH.
-Qed.
+(* Lemma wp_pure_step_later `{!Inhabited (state Λ)} E e1 e2 φ n Φ s : *)
+(*   PureExec φ n e1 e2 → *)
+(*   φ → *)
+(*   ▷^n WP e2 @ s; E {{ Φ }} ⊢ WP e1 @ s; E {{ Φ }}. *)
+(* Proof. *)
+(*   intros Hexec ?. rewrite -wp_pure_step_fupd //. clear Hexec. *)
+(*   induction n as [|n IH]; by rewrite //= -step_fupd_intro // IH. *)
+(* Qed. *)
 End lifting.
