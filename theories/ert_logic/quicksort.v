ef8413a39990ed724be7d82a0d401ef0f259624f
Markus de Medeiros <markusdemedeiros@outlook.com>
2024-03-15 09:30:48 -0400

    randomized quicksort implementation

--- theories/ert_logic/quicksort.v
+++ theories/ert_logic/quicksort.v
@@ -8,7 +8,45 @@ Require Import Lra.
 Require Coq.Program.Tactics.
 Require Coq.Program.Wf.
 
-Definition in_place_pivot := #().
+Definition swap :=
+  (λ: "arr" "a" "b",
+      let: "tmp" := !("arr" +ₗ "a") in
+      "a" <- !("arr" +ₗ "b") ;;
+      "b" <- "tmp")%V.
+
+
+Definition in_place_pivot
+  := (λ: "arr" "len" "pivot",
+        (* Swap pivot with the last element *)
+        swap "arr" "pivot" ("len" - #1) ;;
+        let: "pivot_v" := !("arr" +ₗ ("len" - #1)) in
+
+        let: "len_left" := ref #0 in
+        (* rec inv.
+              len_left < next <= len - 1
+              for all 0 <= i < len_left, (arr[i] <= pivot)
+              for all len_left <= i < next, (arr[i] > pivot)
+              ⇒ if (next = len - 1), array is of the form [less than pivot, greater than pivot, pivot]
+        *)
+        (rec: "swap_next" "next" :=
+           if: ("next" = ("len" - #1))
+              then #()
+              else (
+                  if: (!("arr" +ₗ "next") ≤ "pivot_v")
+                    then
+                      (* next belongs in the left list *)
+                      swap "arr" "next" "len_left" ;;
+                      "len_left" <- !("len_left" + #1) ;;
+                      "swap_next" ("next" + #1)
+                    else
+                      "swap_next" ("next" + #1)))
+        #0 ;;
+
+        (* Swap pivot back into the right spot*)
+        swap "arr" !("len_left") ("len" - #1) ;;
+        "len_left")%V.
+
+
 (* let: "pivot_v" := !("arr" +ₗ "pivot") in *)
 
 Definition tc_pivot : nat -> nonnegreal.
@@ -24,35 +62,25 @@ Definition quicksort :=
       then "arr"
       else
         let: "pivot" := rand "len" in
-        let: "r" := in_place_pivot "arr" "pivot" in
+        let: "len_left" := in_place_pivot "arr" "pivot" in
         let: "left" := "arr" in
-        let: "len_left" := Fst "r" in
-        let: "right" := Snd "r" in
+        let: "right" := (#1 + "len_left") in
         let: "len_right" := ("len" - "right" + #1) in
 
         "quicksort" "left" "len_left" ;;
         "quicksort" "right" "len_right" ;;
         "arr")%V.
 
-
-
-
 (* tc_quicksort(len) = (1/len) + 2 * sum(i=0 to len-1)(tc_quicksort i) *)
-
-Definition range (j : nat) : list nat
- := [].
-
-(* a mess *)
-
 Program Fixpoint tc_quicksort (len : nat) {measure len} : nonnegreal
     := match len with
       | 0%nat => nnreal_zero
-      | (S len1)%nat => nnreal_div ((tc_pivot len) + (foldr nnreal_plus nnreal_zero $ map _ $ range len) )%NNR (nnreal_nat len)
+      | (S len1)%nat =>
+          nnreal_plus (tc_pivot len) $
+          nnreal_mult (nnreal_nat 2) $
+          nnreal_div
+            (foldr nnreal_plus nnreal_zero $ map (fun n => (tc_quicksort (fin_to_nat n))) $ fin_enum len1)%NNR
+            (nnreal_nat len)
      end.
-Next Obligation.
-  intros.
-  eapply tc_quicksort.
-  (* want to use H < len from range? *)
-Admitted.
-Next Obligation. Admitted.
-
+Next Obligation. intros. eapply Nat.lt_trans; [eapply fin_to_nat_lt|lia]. Qed.
+Next Obligation. apply Wf.measure_wf, lt_wf. Qed.
