39deab88de68923e125ea294f2785a6f1d5fd4e0
Hei Li <hei411@gmail.com>
2024-03-18 14:04:55 +0100

    WIP: coupon

--- theories/ert_logic/examples/couponcollector.v
+++ theories/ert_logic/examples/couponcollector.v
@@ -1,7 +1,8 @@
 (** * Exact time credit accounting for Coupon collecting *)
-From clutch.ert_logic Require Export expected_time_credits ert_weakestpre.
+From clutch.ert_logic Require Export expected_time_credits ert_weakestpre problang_wp.
 From clutch.prob_lang Require Import notation tactics metatheory lang.
 From Coq Require Export Reals Psatz.
+From Coquelicot Require Export Hierarchy.
 Require Import Lra.
 
 
@@ -24,7 +25,36 @@ Section Coupon.
     λ: "n",
       let: "a" := AllocN #coupon #false in
       let: "cnt" := ref #0 in
-      coupon_helper "a" "cnt". 
+      coupon_helper "a" "cnt".
 
+  
+  Definition harmonic_sum:= sum_n_m (λ x, /INR x).
+
+  Local Lemma coupon_etc_credit_split p:
+    (p≠0)%nat -> (p<coupon)%nat -> p/coupon + (coupon-p)/coupon * (1 + (coupon/p)) = (coupon/p).
+  Proof.
+    intros H1 H2. 
+    rewrite Rmult_plus_distr_l -Rplus_assoc Rmult_1_r.
+    replace (_/_+_) with 1.
+    - replace (_*_) with ((coupon-p)/p).
+      + replace 1 with (p/p).
+        * rewrite -Rdiv_plus_distr.
+          by replace (_+_) with (INR coupon) by lra.
+        * apply Rdiv_diag. replace 0 with (INR 0) by done. by move => /INR_eq.
+      + rewrite Rmult_div_assoc -Rmult_div_swap -Rmult_div_assoc.
+        rewrite Rdiv_diag; first lra.
+        rewrite /coupon. replace 0 with (INR 0) by done.
+        by apply not_INR.
+    - rewrite -Rdiv_plus_distr.
+      replace (p+_) with (INR coupon) by lra.
+      rewrite Rdiv_diag; first lra.
+      rewrite /coupon. replace 0 with (INR 0) by done.
+      by apply not_INR.
+  Qed.
+  
+  Context `{!ert_clutchGS Σ}.
+
+
+  
   
 End Coupon.
