6820ab999c701098edb749f745c11c7d6c6216b1
Hei Li <hei411@gmail.com>
2024-03-19 16:56:36 +0100

    general geo

--- theories/ert_logic/examples/geometric.v
+++ theories/ert_logic/examples/geometric.v
@@ -8,6 +8,8 @@ From Coquelicot Require Export Hierarchy.
 From Coq Require Export Reals Psatz.
 Require Import Lra.
 
+
+Set Default Proof Using "Type*".
 (* Simple example: Flip until heads, expectation should be constant *)
 
 Definition geo
@@ -41,10 +43,112 @@ Section proofs.
       + replace (2-_-_) with 2; last (simpl;lra). iApply ("IH" with "[$]"). done.
   Qed.
       
-      
-    
-  
 End proofs.
+
+Section generalized.
+  
+  Local Hint Resolve pos_INR : core.
+  Local Hint Resolve pos_INR_S: core.
+
+  Context (n m:nat).
+  Hypothesis (Hineq:(0<m<=S n)%nat).
+  Definition geo'
+    := (rec: "g" "_" :=
+          if: ((rand #n) < #m)%E
+          then #()
+          else ("g" #()))%V.
+
+  Definition tc := (S n)/(m).
+
+  Local Lemma SeriesC_geo':
+    SeriesC (λ n0 : fin (S n), if bool_decide (n0 < m) then 0 else 1) = (S n - m)%R.
+  Proof.
+    revert Hineq.
+    induction m as [|m' IH].
+    - intros. lia.
+    - intros. replace (S n - S m')%R with (S n - m' - 1); last first.
+      { rewrite (S_INR m'). lra. }
+      apply Req_minus_r.
+      destruct m'.
+      + rewrite Rminus_0_r.
+        assert (0<S n)%nat as H by lia.
+        rewrite -{2}(SeriesC_singleton (nat_to_fin H) 1).
+        rewrite -SeriesC_plus; try apply ex_seriesC_finite.
+        erewrite SeriesC_ext.
+        * erewrite SeriesC_finite_mass. rewrite fin_card.
+          apply Rmult_1_r.
+        * intros k. simpl. case_bool_decide as H1; case_bool_decide as H2; try lra.
+          -- exfalso. apply H2. destruct (fin_to_nat k) eqn:K.
+             ++ apply fin_to_nat_inj. done.
+             ++ rewrite S_INR in H1. pose proof pos_INR n0. lra.
+          -- exfalso. apply H1. rewrite H2. simpl. lra.
+      + rewrite -IH; last lia.
+        assert (S m' < S n)%nat as H by lia.
+        rewrite -{2}(SeriesC_singleton (nat_to_fin H) 1).
+        rewrite -SeriesC_plus; last first.
+        * apply ex_seriesC_finite.
+        * apply ex_seriesC_finite.
+        * apply SeriesC_ext.
+          intros. case_bool_decide as H1; case_bool_decide as H2; case_bool_decide as H3; simpl; try lra.
+          -- rewrite H2 in H3. rewrite fin_to_nat_to_fin in H3. apply INR_lt in H3. lia.
+          -- exfalso. apply H3. apply lt_INR. apply INR_lt in H1. assert (fin_to_nat n0 ≠ S m'); try lia.
+             intro. apply H2. apply fin_to_nat_inj. rewrite H0. rewrite fin_to_nat_to_fin.
+             done.
+          -- rewrite H2 in H3. rewrite fin_to_nat_to_fin in H3. apply INR_lt in H3. lia.
+          -- exfalso. rewrite H2 in H1. rewrite fin_to_nat_to_fin in H1. apply H1.
+             apply lt_INR. lia.
+          -- exfalso. apply H1. apply INR_lt in H3. apply lt_INR.
+             assert (fin_to_nat n0 ≠ S m')%nat; lia.
+  Qed.
+
+  Local Lemma tc_split:
+    SeriesC (λ n0 : fin (S n), 1 / (S n) * (if bool_decide (n0 < m) then 0 else tc)) = tc - 1.
+  Proof.
+    unfold tc.
+    remember (S n) as n'.
+    erewrite (SeriesC_ext _ (λ n0 : fin n', (if bool_decide (n0 < m) then 0 else 1)*/m)); last first.
+    { intros. case_bool_decide; try lra. rewrite Rmult_div_assoc !Rdiv_def Rmult_1_l Rinv_l.
+      - done. 
+      - intro. subst. pose proof pos_INR_S n. lra.
+    }
+    rewrite SeriesC_scal_r. rewrite <-(Rdiv_diag m) at 1; last first.
+    { replace 0 with (INR 0); last (simpl; lra). apply not_INR. lia. }
+    rewrite -Rdiv_minus_distr. rewrite -Rdiv_def. f_equal. subst.
+    apply SeriesC_geo'.
+  Qed.
+
+  Context `{!ert_clutchGS Σ Costapp}.
+  
+  Lemma wp_geo' E:
+    {{{ ⧖ tc }}}
+      geo' #()@E
+      {{{RET #(); True}}}.
+  Proof.
+    assert (1<=tc).
+    { unfold tc. apply Rcomplements.Rle_div_r.
+      - apply Rlt_gt. replace 0 with (INR 0); last (simpl; lra). apply lt_INR.
+        lia.
+      - rewrite Rmult_1_l. apply le_INR. lia. }
+    iIntros (Φ) "Hx HΦ".
+    iLöb as "IH" forall (Φ) "Hx HΦ".
+    rewrite /geo'. 
+    wp_pure.
+    wp_apply (wp_couple_rand_adv_comp' _ _ _ _ _
+                (λ x, if (bool_decide ((fin_to_nat x) < m))
+                          then 0
+                      else tc) with "[$]").
+    - intros; case_bool_decide; lra.
+    - simpl. rewrite Rplus_0_l. apply tc_split. 
+    - iIntros (v) "Hx". wp_pure.
+      { case_bool_decide; lra. }
+      case_bool_decide as H1; case_bool_decide as H2; wp_pure.
+      + iModIntro. iApply "HΦ". done.
+      + exfalso. apply INR_lt in H1. lia.
+      + exfalso. apply H1. apply lt_INR. lia.
+      + replace (tc -_-_) with tc; last (simpl;lra). iApply ("IH" with "[$]"). done.
+  Qed.
+
+End generalized.
 
 (* Defining these as paramaters until we decide what counts as a tick *)
 
