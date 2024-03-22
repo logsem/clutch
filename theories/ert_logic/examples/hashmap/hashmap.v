150c22a45485137d7c88efeec033beb39bd9db16
Hei Li <hei411@gmail.com>
2024-03-22 17:48:55 +0100

    stronger spec

--- theories/ert_logic/examples/hashmap/hashmap.v
+++ theories/ert_logic/examples/hashmap/hashmap.v
@@ -134,6 +134,47 @@ Section simple_bit_hash.
       * pose proof (fin_to_nat_lt x).
         lia.
   Qed.
+
+  Lemma wp_insert_tc E f m (n : nat) x x2:
+    m !! n = None →
+    SeriesC (λ n0, 1 / S val_size * x2 n0)%R = x ->
+    (∀ n0, (0 <= x2 n0)%R) ->
+    {{{ ⧖ x ∗ hashfun f m 
+    }}}
+      f #n @ E
+    {{{ (v : nat), RET #v; ∃ v': fin (S val_size), ⧖ (x2 (v')) ∗ ⌜fin_to_nat v' = v⌝ ∗ hashfun f (<[ n := v ]>m) }}}.
+  Proof.
+    iIntros (Hlookup Hsum Hpos Φ) "[Hx Hhash] HΦ".
+    iDestruct "Hhash" as (hm ->) "[H %Hbound]".
+    rewrite /compute_hash_specialized.
+    wp_pures.
+    wp_apply (wp_get with "[$]").
+    iIntros (vret) "(Hhash&->)".
+    rewrite lookup_fmap Hlookup /=. wp_pures.
+    wp_bind (rand _)%E.
+    wp_apply (wp_couple_rand_adv_comp' _ _ _ _ _ x2 with "[$Hx]"); try done.
+    { rewrite Hsum. simpl. lra. }
+    iIntros "%v Hx".
+    wp_pures.
+    wp_apply (wp_set with "Hhash").
+    iIntros "Hlist".
+    wp_pures.
+    iModIntro.
+    iApply "HΦ".
+    rewrite /hashfun.
+    iExists v. iFrame. iSplit; first done.
+    iExists _.
+    iSplit; first auto.
+    iSplitL.
+    + rewrite fmap_insert //.
+    + iPureIntro.
+      apply map_Forall_insert_2; last done.
+      split.
+      * lia.
+      * pose proof (fin_to_nat_lt v).
+        lia.
+  Qed.
+    
   
 End simple_bit_hash.
 
