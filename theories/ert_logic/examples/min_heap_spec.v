558f01531117867d0bda02d90ccdce2812738a3a
Simon Gregersen <gregersen@cs.au.dk>
2024-03-26 16:11:08 -0400

    adjust min_heap_spec

--- theories/ert_logic/examples/min_heap_spec.v
+++ theories/ert_logic/examples/min_heap_spec.v
@@ -2,7 +2,7 @@ From clutch.prob_lang Require Import lang notation.
 From clutch.ert_logic Require Export problang_wp.
 Set Default Proof Using "Type*".
 
-Record comparator (K : Type) := Comparator {
+Record comparator (K : Type) (c : Costfun prob_lang) := Comparator {
   cmp :> val;
   cmp_rel : relation K;
   cmp_rel_dec :: RelDecision cmp_rel;
@@ -15,40 +15,40 @@ Record comparator (K : Type) := Comparator {
   wp_cmp `{!ert_clutchGS Σ c} k1 k2 v1 v2 :
     {{{ cmp_has_key k1 v1 ∗ cmp_has_key k2 v2 ∗ ⧖ cmp_cost }}}
       cmp v1 v2
-    {{{ RET #(bool_decide (cmp_rel k1 k2)); True }}};
+    {{{ RET #(bool_decide (cmp_rel k1 k2)); cmp_has_key k1 v1 ∗ cmp_has_key k2 v2 }}};
 }.
+Arguments Comparator {_ _} _ _ {_ _} _ _ _.
+Arguments cmp_rel {_ _}.
+Arguments cmp_cost {_ _}.
+Arguments cmp_has_key {_ _} _ {_ _}.
+Arguments wp_cmp {_ _ _ _ _ _}.
 
-Arguments cmp_rel {_}.
-Arguments cmp_cost {_}.
-Arguments cmp_has_key {_ _ _ _ _}.
-Arguments wp_cmp {_ _ _ _ _}.
-
-Class min_heap := MinHeap {
+Class min_heap `(cmp : comparator K c) := MinHeap {
   heap_new : val;
   heap_insert : val;
   heap_remove : val;
 
-  heap_insert_cost : nat → R → R;
-  heap_remove_cost : nat → R → R;
+  heap_insert_cost : nat → R;
+  heap_remove_cost : nat → R;
 
-  is_min_heap `{!ert_clutchGS Σ c} {K} (cmp : comparator K) (l : list K) (v : val) : iProp Σ;
+  is_min_heap `{!ert_clutchGS Σ c} (l : list K) (v : val) : iProp Σ;
 
-  wp_heap_new `{!ert_clutchGS Σ c} {K} (cmp : comparator K) :
+  wp_heap_new `{!ert_clutchGS Σ c} :
     {{{ True }}}
-      heap_new cmp
-    {{{ v, RET v; is_min_heap cmp [] v }}};
+      heap_new #()
+    {{{ v, RET v; is_min_heap [] v }}};
 
-  wp_heap_insert `{!ert_clutchGS Σ c} {K} (cmp : comparator K) l k v w :
-    {{{ is_min_heap cmp l v ∗ cmp.(cmp_has_key) k w ∗ ⧖ (heap_insert_cost (length l) cmp.(cmp_cost)) }}}
+  wp_heap_insert `{!ert_clutchGS Σ c} l k v w :
+    {{{ is_min_heap l v ∗ cmp.(cmp_has_key) k w ∗ ⧖ (heap_insert_cost (length l)) }}}
       heap_insert v w
-    {{{ v l', RET v; is_min_heap cmp l' v ∗ ⌜l' ≡ₚ k :: l⌝ }}};
+    {{{ v l', RET v; is_min_heap l' v ∗ ⌜l' ≡ₚ k :: l⌝ }}};
 
-  wp_heap_remove `{!ert_clutchGS Σ c} {K} (cmp : comparator K) l v :
-    {{{ is_min_heap cmp l v ∗ ⧖ (heap_remove_cost (length l) cmp.(cmp_cost)) }}}
+  wp_heap_remove `{!ert_clutchGS Σ c} l v :
+    {{{ is_min_heap l v ∗ ⧖ (heap_remove_cost (length l)) }}}
       heap_remove v
     {{{ w, RET w;
-        (⌜w = #()⌝ ∗ ⌜l = []⌝ ∗ is_min_heap cmp [] v) ∨
+        (⌜w = #()⌝ ∗ ⌜l = []⌝ ∗ is_min_heap [] v) ∨
         (∃ k l',
-            ⌜k ∈ l⌝ ∗ ⌜l ≡ₚ k :: l'⌝ ∗ cmp.(cmp_has_key) k w ∗
-            ⌜Forall (cmp.(cmp_rel) k) l⌝ ∗ is_min_heap cmp l' v) }}};
+            ⌜l ≡ₚ k :: l'⌝ ∗ cmp.(cmp_has_key) k w ∗
+            ⌜Forall (cmp.(cmp_rel) k) l⌝ ∗ is_min_heap l' v) }}};
 }.
