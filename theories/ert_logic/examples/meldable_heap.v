d79e13a510be45079300f49d341341ba34ef1588
Markus de Medeiros <markusdemedeiros@outlook.com>
2024-03-27 16:27:06 -0400

    meldable heap amplification lemma

--- theories/ert_logic/examples/meldable_heap.v
+++ theories/ert_logic/examples/meldable_heap.v
@@ -144,6 +144,120 @@ Section program.
           #() (* ??? *) )))%V.
 
 
+  (** Time credit accounting *)
+
+  (* Coq uses a fake version of log, which will be nice for handling edge cases in our derivation *)
+  Lemma ln_0 : (ln 0%R = 0%R).
+  Proof. compute. destruct (Rlt_dec R0 R0); auto. exfalso. lra. Qed.
+
+  Definition tc_meld (k : R) (n : nat)  := (2 * k * (1 + Rlog 2 n))%R.
+
+  Lemma tc_meld_1 (k : R) : (tc_meld k 1 = 2 * k)%R.
+  Proof. rewrite /tc_meld /Rlog ln_1. lra. Qed.
+
+  (* Inductive bound for tc_meld *)
+  (* We will only apply advanced composition in the case that both a and b have size at at least 1 *)
+  Lemma tc_meld_ind (k : R) (a b : nat) :
+      (0 < a) -> (0 < b) -> (0 < k)%R -> (tc_meld k (1 + a + b) >= k + (tc_meld k a + tc_meld k b) / 2)%R.
+  Proof.
+    intros Ha Hb Hk.
+    apply Rle_ge.
+    rewrite /tc_meld.
+
+    (* Divide by k and cancel the 2 factors *)
+    apply (Rmult_le_reg_r (/ k)%R); first by apply Rinv_0_lt_compat.
+    rewrite Rmult_plus_distr_r.
+    rewrite Rinv_r; last lra.
+    rewrite Rdiv_def.
+    rewrite Rmult_assoc.
+    rewrite -Rinv_mult.
+    rewrite Rmult_comm.
+    rewrite Rmult_plus_distr_l.
+    replace  (/ (2 * k) * (2 * k * (1 + Rlog 2 a)))%R with (1 + Rlog 2 a)%R; last first.
+    { rewrite -Rmult_assoc. rewrite Rinv_l; lra. }
+    replace  (/ (2 * k) * (2 * k * (1 + Rlog 2 b)))%R with (1 + Rlog 2 b)%R; last first.
+    { rewrite -Rmult_assoc. rewrite Rinv_l; lra. }
+    replace (2 * k * (1 + Rlog 2 (1 + a + b)%nat) * / k)%R with (2 * (1 + Rlog 2 (1 + a + b)%nat))%R; last first.
+    { rewrite (Rmult_comm _ (/ _)%R) -Rmult_assoc (Rmult_comm _ k) -Rmult_assoc.
+      rewrite Rinv_l; lra. }
+
+    (* Cancel out some 1's *)
+    rewrite Rmult_plus_distr_l Rmult_1_r.
+    do 3 rewrite -Rplus_assoc.
+    replace (1 + 1)%R with 2%R by lra.
+    repeat rewrite Rplus_assoc.
+    repeat apply Rplus_le_compat_l.
+    rewrite Rplus_comm.
+
+    (* Replace 1 with a logarithm *)
+    replace 1%R with (INR 1) by done.
+    rewrite -(Rlog_pow 2); try lra.
+
+    (* Turn Rlog into ln so that we can add them *)
+    rewrite /Rlog.
+    assert (Hln2 : (0 < ln 2)%R).
+    { eapply Rlt_trans; [| eapply ln_lt_2]. lra. }
+    apply (Rmult_le_reg_r (ln 2)); [done|].
+    repeat rewrite (Rmult_plus_distr_r _ _ (ln 2)).
+    repeat rewrite (Rmult_assoc _ _ (ln 2)).
+    repeat (rewrite Rinv_l; last lra).
+    repeat rewrite Rmult_1_r.
+
+
+    (* Simplify the logarithms; apply monotonicity *)
+    rewrite -ln_mult; [| lra | by apply lt_0_INR].
+    rewrite -ln_mult; [ | | by apply lt_0_INR]; last first.
+    { apply Rmult_lt_0_compat; [lra | by apply lt_0_INR]. }
+    rewrite -ln_Rpower.
+    apply Rcomplements.ln_le.
+    { apply Rmult_lt_0_compat; last by apply lt_0_INR.
+      apply Rmult_lt_0_compat; last by apply lt_0_INR.
+      lra.
+    }
+
+    (* Turn the exponent into a product *)
+    replace 2%R with (1%R + 1%R)%R by lra.
+    rewrite Rpower_plus.
+    rewrite Rpower_1; last (apply lt_0_INR; lia).
+    replace (1%R + 1%R)%R with 2%R by lra.
+    rewrite pow_1.
+
+
+    (* Expand the binomial, bound using some of the terms *)
+    repeat rewrite plus_INR.
+    eapply (Rle_trans _ (a * b + b * a)%R); try lra.
+    repeat rewrite Rmult_plus_distr_r.
+    repeat rewrite Rmult_plus_distr_l.
+    repeat rewrite INR_1.
+    assert (0 < a)%R; [by apply lt_0_INR|].
+    assert (0 < b)%R; [by apply lt_0_INR|].
+    repeat rewrite Rmult_1_r.
+    repeat rewrite Rmult_1_l.
+
+    rewrite Rplus_assoc.
+    rewrite -{1}(Rplus_0_l ((_ * _) + (_ * _))%R).
+    apply Rle_plus_plus; first lra.
+
+    rewrite Rplus_assoc.
+    rewrite -{1}(Rplus_0_l ((_ * _) + (_ * _))%R).
+    apply Rle_plus_plus.
+    { apply Rplus_le_le_0_compat; try lra. apply Rmult_le_pos; try lra. }
+
+    apply Rplus_le_compat_l.
+
+    rewrite Rplus_assoc.
+    rewrite -{1}(Rplus_0_l ((_ * _))%R).
+    apply Rle_plus_plus; first lra.
+
+    rewrite -{1}(Rplus_0_r ((_ * _))%R).
+    apply Rplus_le_compat_l.
+
+    apply Rmult_le_pos; try lra.
+
+  Qed.
+
+
+
   Lemma spec_meld_heap_new cmp : {{{ True }}} meld_heap_new #() {{{ v, RET v; (is_meld_heap_ref cmp) [] v }}}.
   Proof.
     iIntros (Φ _) "HΦ".
@@ -162,12 +276,11 @@ Section program.
     constructor.
   Qed.
 
-  Definition tc_meld (n : nat) : R := ln n.
 
   Lemma spec_meld_heap_meld cmp h1 h2 (L1 L2 : list A) :
       {{{ (is_meld_heap_val cmp L1 h1) ∗
           (is_meld_heap_val cmp L2 h2) ∗
-          ⧖ (tc_meld (length L1) + tc_meld (length L2))
+          ⧖ ((cmp_cost cmp) + tc_meld (cmp_cost cmp) (length L1) + tc_meld (cmp_cost cmp) (length L2))
       }}}
         (meld_heap_meld cmp h1 h2)%E
       {{{ v, RET v;
@@ -202,8 +315,12 @@ Section program.
     }
     wp_pures.
     wp_bind (cmp _ _).
+    wp_pures.
 
-    (* Need to apply wp_cmp... how?? *)
+    (* need to destruct the binary tree *)
+    wp_pures.
+
+    Fail wp_apply ((@wp_cmp A CostTick cmp Σ ert_clutchGS0 _ _ _ _ ) with "[]").
   Admitted.
 
 
