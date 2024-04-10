63ea60c5fdfe9589de624188ffc7bddab6668110
Hei Li <hei411@gmail.com>
2024-04-10 15:25:14 +0200

    Add fin lemmas

---
+++ theories/prelude/fin.v
@@ -0,0 +1,40 @@
+From stdpp Require Import fin countable finite.
+From clutch.prelude Require Import base classical.
+Set Default Proof Using "Type*".
+
+Section fin.
+  Lemma enum_fin_seq n: fin_to_nat <$> enum (fin n) = seq 0 n.
+  Proof.
+    induction n.
+    - done.
+    - simpl. f_equal.
+      rewrite <-fmap_S_seq. rewrite <-IHn.
+      simpl. clear.
+      induction (fin_enum n).
+      + done.
+      + simpl. by f_equal.
+  Qed.
+  
+  Lemma fin_to_nat_encode_nat {n:nat} (x:fin n): fin_to_nat x = encode_nat x.
+  Proof.
+    rewrite /encode_nat/encode/=. rewrite Nat2Pos.id.
+    { lia. }
+    rewrite Nat.pred_succ.
+    induction x.
+    - simpl; lia.
+    - simpl. rewrite IHx.
+      rewrite list_find_fmap.
+      erewrite (list_find_ext (eq (FS x) ∘ FS) (eq x)); last first.
+      { intros. split; simpl; naive_solver. }
+      edestruct (list_find (eq x) (fin_enum n)) eqn:H1.
+      + done.
+      + exfalso. 
+        pose proof elem_of_enum x as H.
+        epose proof list_find_elem_of _ _  x _ _ as H0.
+        erewrite H1 in H0. by eapply is_Some_None.
+        Unshelve.
+        * done.
+        * done.
+  Qed.
+
+End fin.
