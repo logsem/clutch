f8628cfcd75d6d10a99a158caca0c3ce58ce2d08
Hei Li <hei411@gmail.com>
2024-04-12 16:06:13 +0200

    add fin lemma

--- theories/prelude/fin.v
+++ theories/prelude/fin.v
@@ -14,6 +14,38 @@ Section fin.
       + done.
       + simpl. by f_equal.
   Qed.
+
+  Lemma seq_enum_fin (n:nat) :
+    (fun x => match le_lt_dec (S n) x with
+           | left _ => 0%fin
+           | right h => nat_to_fin h
+           end) <$> seq 0 (S n) = enum (fin (S n)).
+  Proof.
+    induction n as [|n IHn]; first done.
+    rewrite -cons_seq. rewrite fmap_cons.
+    replace (enum _) with (fin_enum (S (S n))) by done.
+    rewrite /fin_enum. f_equal.
+    replace (_::_) with (enum (fin (S n))) by done.
+    rewrite <-IHn. rewrite <-seq_shift.
+    replace (map _ _) with (S <$> (seq 0 (S n))) by done.
+    rewrite <- !list_fmap_compose. apply Forall_fmap_ext_1.
+    clear.
+    rewrite list.Forall_forall.
+    intros x Hx.
+    rewrite elem_of_seq in Hx.
+    replace (_ x) with (match le_lt_dec (S (S n)) (S x) with
+                | left _ => 0%fin
+                | right h => nat_to_fin h
+                        end) by done.
+    replace ((_∘_) x) with (FS  (match le_lt_dec (S n) x with
+                     | left _ => 0%fin
+                     | right h => nat_to_fin h
+                                 end)) by done.
+    destruct (le_lt_dec (S (S n)) (S x)) eqn:H1;
+      destruct (le_lt_dec (S n) x) eqn:H2; try lia.
+    apply fin_to_nat_inj. simpl. 
+    by rewrite !fin_to_nat_to_fin.
+  Qed.
   
   Lemma fin_to_nat_encode_nat {n:nat} (x:fin n): fin_to_nat x = encode_nat x.
   Proof.
