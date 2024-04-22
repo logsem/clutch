9bec2b85b46f40927d40ba5d9cd12a36f94bdf9e
Hei Li <hei411@gmail.com>
2024-04-22 15:44:30 +0200

    change def of rej samp

--- theories/prob_lang/rejection_sampler_distribution.v
+++ theories/prob_lang/rejection_sampler_distribution.v
@@ -22,24 +22,67 @@ Section rejection_sampler_distr.
 
   Local Hint Resolve pos_INR:core.
   Local Hint Resolve pos_INR_S: core.
-  Lemma rej_samp_distr_f_nonneg (N:nat) s l: size s <= S N ->  0<= rej_samp_distr_f N s l.
+  Lemma rej_samp_distr_f_nonneg (N:nat) s l: 0<= rej_samp_distr_f N s l.
   Proof.
     induction l; simpl; first done.
     repeat case_bool_decide; try done.
     all: rewrite -/(INR (S _)).
-    - intros; apply Rcomplements.Rdiv_le_0_compat; auto.
-    - intros; apply Rmult_le_pos; auto. apply Rcomplements.Rdiv_le_0_compat; auto.
-      apply Rle_0_le_minus. done.
+    - apply Rcomplements.Rdiv_le_0_compat; auto.
+    - apply Rmult_le_pos; auto. apply Rcomplements.Rdiv_le_0_compat; auto.
+      apply Rle_0_le_minus.
+      apply le_INR.
+      etrans; first eapply subseteq_size.
+      + instantiate (1:=list_to_set (fin_enum (S N))).
+        intros ??. rewrite elem_of_list_to_set.
+        replace (fin_enum _) with (enum (fin (S N))) by done.
+        apply elem_of_enum.
+      + replace (fin_enum _) with (enum (fin (S N))) by done.
+        rewrite size_list_to_set; last apply NoDup_enum.
+        rewrite fin.length_enum_fin. done.
   Qed.
 
+  Lemma rej_samp_distr_f_emptyset (N:nat) : is_seriesC (rej_samp_distr_f N ∅) 0.
+  Proof.
+  Admitted.
+
+  Lemma rej_samp_distr_f_is_seriesC (N:nat) s: s≠∅ -> is_seriesC (rej_samp_distr_f N s) 1.
+  Proof.
+  Admitted.
+  
+  Lemma rej_samp_distr_f_ex_seriesC (N:nat) s: ex_seriesC (rej_samp_distr_f N s).
+  Proof.
+    rewrite /rej_samp_distr_f.
+    destruct (decide (s=∅)); eexists _.
+    - subst. apply rej_samp_distr_f_emptyset.
+    - by apply rej_samp_distr_f_is_seriesC.
+  Qed.
+  
+  Lemma rej_samp_distr_f_le_1 (N:nat) s: SeriesC (rej_samp_distr_f N s) <=1.
+  Proof.
+    destruct (decide(s=∅)).
+    - trans 0; last lra.  
+      apply Req_le_sym.
+      symmetry.
+      apply is_seriesC_unique.
+      subst.
+      apply rej_samp_distr_f_emptyset .
+    - apply Req_le_sym.
+      symmetry.
+      apply is_seriesC_unique.
+      apply rej_samp_distr_f_is_seriesC. done.
+  Qed.
+  
   Program Definition rej_samp_distr (N:nat) (s:gset (fin(S N))) :=
     MkDistr (rej_samp_distr_f N s) _ _ _.
   Next Obligation.
-  Admitted.
+    apply rej_samp_distr_f_nonneg.
+    Qed.
   Next Obligation.
-  Admitted.
+    apply rej_samp_distr_f_ex_seriesC.
+  Qed.
   Next Obligation.
-  Admitted.
+    apply rej_samp_distr_f_le_1.
+  Qed.
 
   Definition rej_samp_state_f σ α N (ns: list (fin (S N))) (Hfound:(σ.(tapes)!!α = Some (N;ns))) :=
     (λ l,
