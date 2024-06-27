3721e07b7b09fa91acc7b87a7bbf2d1b59401a53
Philipp G. Haselwarter <philipp@haselwarter.org>
2024-06-27 12:18:25 +0200

    cpa wip

--- theories/paris/examples/prf_cpa.v
+++ theories/paris/examples/prf_cpa.v
@@ -28,9 +28,9 @@ Section proofs.
   Let rand_cipher scheme : expr := Snd (Snd scheme).
 
   Definition q_calls : val :=
-    λ:"Q" "f",
-      let: "r" := ref #0 in
-      λ:"x", if: ! "r" < "Q" then ("r" <- !"r" + #1 ;; "f" "x") else "x".
+    λ:"Q" "f" "g",
+      let: "counter" := ref #0 in
+      λ:"x", if: ! "counter" < "Q" then ("counter" <- !"counter" + #1 ;; "f" "x") else "g" "x".
 
   Definition CPA : val :=
     λ:"b" "adv" "scheme" "Q",
@@ -41,12 +41,14 @@ Section proofs.
           "enc_key"
         else
           rand_cipher "scheme" in
-      let: "oracle" := q_calls "Q" "rr_key_b" in
+      let: "oracle" := q_calls "Q" "rr_key_b" (rand_cipher "scheme") in
       let: "b'" := "adv" "oracle" in
       "b'".
 
   Variable xor : val.
   (* We probably need to assume that forall x, Bij (xor x). *)
+  Variable (xor_sem : fin (S Message) -> fin (S Output) -> fin (S Output)).
+  Variable H_xor : forall x, Bij (xor x).
 
   Definition prf_enc : val :=
     λ:"prf" "key",
@@ -97,7 +99,7 @@ Section proofs.
 
   Definition rf_keygen : val := λ:<>, rand #Key.
   Definition rf_enc : expr := prf_enc random_function.
-  Definition rf_rand_cipher : val := λ:<>, (rand #Input, rand #Output).
+  Definition rf_rand_cipher : val := λ:<>, let:"i" := rand #Input in let:"o" := rand #Output in ("i", "o").
   Definition rf_scheme : expr := (rf_keygen, (rf_enc, rf_rand_cipher)).
 
   Definition CPA_rf : val := λ:"b" "adv", CPA "b" "adv" rf_scheme.
@@ -115,22 +117,30 @@ Section proofs.
   (* TODO: Should we prove a logical refinement, or a WP refinement? *)
   Theorem rf_is_CPA (Q : nat) :
     ↯ (Q * Q / (2 * Input)) ⊢ (REL (CPA #true adv rf_scheme #Q) << (CPA #false adv rf_scheme #Q) : lrel_bool).
-  Proof.
+  Proof with (rel_pures_l ; rel_pures_r).
     iIntros "ε".
     rel_pures_l.
     rewrite /CPA.
-    rewrite /rf_scheme/rf_keygen/rf_enc/rf_rand_cipher/prf_enc/enc/keygen.
+    rewrite /rf_scheme/rf_keygen/rf_enc/prf_enc/enc/keygen.
     rel_pures_l.
     rel_pures_r.
     rel_apply refines_couple_UU.
     iIntros (key) "!>".
     rel_pures_l ; rel_pures_r.
-    rel_bind_l (q_calls _ _)%E.
-    rel_bind_r (q_calls _ _)%E.
+    rewrite /random_function. rel_pures_r.
+    rewrite /init_map. rel_pures_r.
+    rewrite /init_list.
+    rel_alloc_r listref' as "listref'". rel_pures_r.
+    rel_alloc_r mapref' as "mapref'". rel_pures_r.
+    rel_alloc_l listref as "listref". rel_pures_l.
+    rel_alloc_l mapref as "mapref". rel_pures_l.
+    rel_bind_l (q_calls _ _ _)%E.
+    rel_bind_r (q_calls _ _ _)%E.
     unshelve iApply (refines_bind with "[ε] []").
     1:{ exact (interp (TMessage → TCipher) []). }
     2:{
       iIntros (f f') "Hff'".
+      simpl.
       unshelve iApply (refines_app with "[] [Hff']").
       3: rel_values.
       rel_arrow_val.
@@ -146,15 +156,46 @@ Section proofs.
       assumption.
     }
 
-    (* rewrite /random_function. *)
-
     rewrite /q_calls. rel_pures_l. rel_pures_r.
-    rel_alloc_l r as "r". rel_alloc_r rs as "rs". rel_pures_l ; rel_pures_r.
+    rel_alloc_l counter as "counter". rel_alloc_r counter' as "counter'". rel_pures_l ; rel_pures_r.
+    iApply (refines_na_alloc (∃ q : nat , ↯ ((Q-q) * (Q-q) / (2 * Input)) ∗ counter ↦ #q ∗ counter' ↦ₛ #q)%I
+              (nroot.@"cpa")); iFrame.
+    iSplitL.
+    1: { iExists 0. rewrite Rminus_0_r. iFrame. }
 
-    (* Oops, we lose our error credit, no good. *)
+    iIntros "#Hinv".
+
     rel_arrow_val.
     iIntros (??) "#(%msg&->&->)" ; rel_pures_l ; rel_pures_r.
-
+    iApply (refines_na_inv with "[$Hinv]"); [done|].
+    iIntros "(> (%q & ε & counter & counter') & Hclose)".
+    rel_load_l ; rel_load_r...
+    rewrite /rf_rand_cipher.
+    case_bool_decide as Hq...
+    - rel_load_l ; rel_load_r... rel_store_l ; rel_store_r...
+      assert (Z.to_nat msg < S Message) as Hmsg by admit.
+      rel_apply (refines_couple_UU).
+      iIntros (r_in) "!>"...
+      rel_bind_l (Match _ _ _ _ _).
+      rel_bind_r (Rand _ _).
+      unshelve iApply (refines_bind with "[-]").
+      1: exact lrel_int.
+      2: { iIntros (??) "#(%out&->&->)" => /=...  }
+      (* (xor_sem (Fin.of_nat_lt Hmsg)) *)
+      (* 1: rewrite Nat2Z.id. *)
+      give_up.
+    - iApply (refines_na_close with "[-]").
+      iFrame.
+      rel_apply refines_couple_UU.
+      iIntros (?) "!>"...
+      rel_apply refines_couple_UU.
+      iIntros (?) "!>"...
+      rel_values => //.
+      iModIntro.
+      iExists _,_,_,_.
+      repeat iSplit ; try done.
+      all: iExists _ ; done.
+      
 
   Abort.
 
