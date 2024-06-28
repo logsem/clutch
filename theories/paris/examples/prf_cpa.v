a6d0e75c7a3d2a946436ef323553ffd678933e1c
Hei Li <hei411@gmail.com>
2024-06-28 13:50:33 +0200

    close proof

--- theories/paris/examples/prf_cpa.v
+++ theories/paris/examples/prf_cpa.v
@@ -29,7 +29,7 @@ Section defs.
   Definition q_calls : val :=
     λ:"Q" "f" "g",
       let: "counter" := ref #0 in
-      λ:"x", if: (BinOp AndOp (! "counter" < "Q") ("x" < #Message))
+      λ:"x", if: (BinOp AndOp (! "counter" < "Q") (BinOp AndOp (#0 ≤ "x") ("x" < #Message)))
              then ("counter" <- !"counter" + #1 ;; "f" "x")
              else "g" "x".
 
@@ -51,6 +51,17 @@ Section defs.
   (* We probably need to assume that forall x, Bij (xor x). *)
   Variable (xor_sem : fin (S Message) -> fin (S Output) -> fin (S Output)).
   Variable H_xor : forall x, Bij (xor_sem x).
+  Variable (xor_correct_l: forall `{!parisRGS Σ} E K (x : Z) (y : fin (S Message))
+                             (_: (0<=x)%Z)
+                             (Hx : ((Z.to_nat x) < S Message)) e A,
+    (REL (fill K (of_val #(xor_sem (nat_to_fin Hx) (y)))) << e @ E : A)
+    -∗ REL (fill K (xor #x #y)) << e @ E : A).
+  
+  Variable (xor_correct_r: ∀ `{!parisRGS Σ} E K (x : Z) (y : fin (S Message))
+                             (_: (0<=x)%Z)
+                             (Hx : ((Z.to_nat x) < S Message)) e A,
+    (REL e << (fill K (of_val #(xor_sem (nat_to_fin Hx) (y)))) @ E : A)
+    -∗ REL e << (fill K (xor #x #y)) @ E : A).
 
   Definition prf_enc : val :=
     λ:"prf" "key",
@@ -174,16 +185,6 @@ Section defs.
       by iApply ("Hlog" with "[$]").
     Qed.
     
-    Lemma xor_correct_l E K (x : Z) (y : fin (S Message)) (Hx : ((Z.to_nat x) < S Message)) e A :
-      (REL (fill K (of_val #(xor_sem (nat_to_fin Hx) (y)))) << e @ E : A)
-      -∗ REL (fill K (xor #x #y)) << e @ E : A.
-    Admitted.
-    
-    Lemma xor_correct_r E K (x : Z) (y : fin (S Message)) (Hx : ((Z.to_nat x) < S Message)) e A :
-      (REL e << (fill K (of_val #(xor_sem (nat_to_fin Hx) (y)))) @ E : A)
-      -∗ REL e << (fill K (xor #x #y)) @ E : A.
-    Admitted.
-
     Lemma nat_to_fin_list (l:list nat):
       (∀ x, x ∈ l -> (x < S Input)%nat) ->
       ∃ l': (list (fin (S Input))), fin_to_nat <$> l' = l.
@@ -263,7 +264,8 @@ Section defs.
       case_bool_decide as Hm.
       - rel_load_l ; rel_load_r...
         rewrite /rf_rand_cipher.
-        case_bool_decide as Hq...
+        rewrite -bool_decide_and.
+        case_bool_decide as Hq.
         + rel_load_l ; rel_load_r... rel_store_l ; rel_store_r...
           assert (Z.to_nat msg < S Message) as Hmsg by lia.
           pose proof nat_to_fin_list (elements(dom M)) dom_range as [l' Hl'].
@@ -313,7 +315,7 @@ Section defs.
           rel_apply_l (refines_set_l with "[-mapref] [$mapref]").
           iIntros "mapref"...
           rel_bind_l (xor _ _).
-          rel_apply_l xor_correct_l.
+          rel_apply_l xor_correct_l; first done.
           iApply (refines_na_close with "[-]").
           iFrame.
           iSplitL.
@@ -341,7 +343,7 @@ Section defs.
           iModIntro. repeat iSplit ; iPureIntro ; eauto. 
         + iApply (refines_na_close with "[-]").
           iFrame.
-          iSplit.
+          iSplit...
           { done. }
           rel_apply (refines_couple_UU Input).
           iIntros (?) "!>"...
@@ -429,6 +431,7 @@ Section defs.
       iIntros (??) "#(%msg&->&->)" ; rel_pures_l ; rel_pures_r.
       iApply (refines_na_inv with "[$Hinv]"); [done|].
       iIntros "(> (%q & %M & ε & counter & counter' & mapref & %dom_q & %dom_range) & Hclose)".
+      rewrite -bool_decide_and.
       case_bool_decide as Hm.
       - rel_load_l ; rel_load_r...
         rewrite /rf_rand_cipher.
@@ -490,7 +493,7 @@ Section defs.
           rel_apply_r (refines_set_r with "[-mapref] [$mapref]").
           iIntros "mapref"...
           rel_bind_r (xor _ _).
-          rel_apply_r xor_correct_r.
+          rel_apply_r xor_correct_r; first lia.
           iApply (refines_na_close with "[-]").
           iFrame.
           iSplitL.
@@ -625,4 +628,112 @@ Section defs.
     split; lra.
   Qed.
   
-End defs. 
+End defs.
+
+Section implementation.
+  Variable adv : val.
+  Variable adv_typed : (∅ ⊢ₜ adv : TAdv).
+  Definition bit:=64.
+  Definition Output' := 2^bit - 1.
+  Definition Input' := 2^8-1.
+  Definition Key' := 2^8-1.
+  Notation Message' := Output'.
+  Notation Output := (S Output').
+  Lemma Output_pos: 0<Output.
+  Proof. lia. Qed.
+  
+  Definition xor:val :=
+    (λ: "x" "y", let: "sum" := "x" + "y" in
+                 if: "sum" < #Output then "sum" else "sum" - #Output)%V.
+
+  Lemma xor_sem_aux (x y:fin (Output)):
+    (if bool_decide (fin_to_nat x + fin_to_nat y<Output)
+    then fin_to_nat x + fin_to_nat y
+    else fin_to_nat x + fin_to_nat y - Output) < Output.     
+  Proof.
+    case_bool_decide; first done.
+    pose proof fin_to_nat_lt x.
+    pose proof fin_to_nat_lt y.
+    lia.
+  Qed.
+
+  Definition xor_sem x y := nat_to_fin (xor_sem_aux x y).
+  
+  Lemma xor_sem_bij x: Bij (xor_sem x).
+  Proof.
+    split.
+    - intros y y'. rewrite /xor_sem.
+      intros H.
+      apply (f_equal fin_to_nat) in H.
+      rewrite !fin_to_nat_to_fin in H.
+      apply fin_to_nat_inj.
+      pose proof fin_to_nat_lt x.
+      pose proof fin_to_nat_lt y.
+      pose proof fin_to_nat_lt y'.
+      case_bool_decide; case_bool_decide; lia.
+    - rewrite /xor_sem. intros y.
+      pose proof fin_to_nat_lt x.
+      pose proof fin_to_nat_lt y.
+      destruct (decide (x<=y)) eqn:Heqn.
+      + assert (y-x<Output) as K by lia.
+        exists (nat_to_fin K).
+        apply fin_to_nat_inj.
+        rewrite !fin_to_nat_to_fin.
+        case_bool_decide; lia.
+      + assert (Output+y-x<Output) as K by lia.
+        exists (nat_to_fin K).
+        apply fin_to_nat_inj.
+        rewrite !fin_to_nat_to_fin.
+        case_bool_decide; lia.
+  Qed.
+  
+  Lemma xor_correct_l `{!parisRGS Σ} E K (x : Z) (y : fin (S Message'))
+    (_: (0<=x)%Z)
+    (Hx : ((Z.to_nat x) < S Message')) e A:
+    (REL (fill K (of_val #(xor_sem (nat_to_fin Hx) (y)))) << e @ E : A)
+    -∗ REL (fill K (xor #x #y)) << e @ E : A.
+  Proof with rel_pures_l.
+    iIntros "H".
+    rewrite /xor...
+    rewrite /xor_sem. rewrite !fin_to_nat_to_fin.
+    case_bool_decide.
+    - rewrite bool_decide_eq_true_2; last lia...
+      replace (Z.of_nat (Z.to_nat x + fin_to_nat y))%Z with (x + Z.of_nat (fin_to_nat y))%Z; first done.
+      rewrite Nat2Z.inj_add. rewrite Z2Nat.id; lia.
+    - rewrite bool_decide_eq_false_2; last lia...
+      replace (Z.of_nat (Z.to_nat x + fin_to_nat y - S Message'))%Z with (x + Z.of_nat (fin_to_nat y) - Z.of_nat (S Message'))%Z by lia.
+      done.
+  Qed.
+  
+  Lemma xor_correct_r  `{!parisRGS Σ} E K (x : Z) (y : fin (S Message')) 
+    (_: (0<=x)%Z) (Hx : ((Z.to_nat x) < S Message')) e A:
+    (REL e << (fill K (of_val #(xor_sem (nat_to_fin Hx) (y)))) @ E : A)
+    -∗ REL e << (fill K (xor #x #y)) @ E : A.
+  Proof with rel_pures_r.
+    iIntros "H".
+    rewrite /xor...
+    rewrite /xor_sem. rewrite !fin_to_nat_to_fin.
+    case_bool_decide.
+    - rewrite bool_decide_eq_true_2; last lia...
+      replace (Z.of_nat (Z.to_nat x + fin_to_nat y))%Z with (x + Z.of_nat (fin_to_nat y))%Z; first done.
+      rewrite Nat2Z.inj_add. rewrite Z2Nat.id; lia.
+    - rewrite bool_decide_eq_false_2; last lia...
+      replace (Z.of_nat (Z.to_nat x + fin_to_nat y - S Message'))%Z with (x + Z.of_nat (fin_to_nat y) - Z.of_nat (S Message'))%Z by lia.
+      done.
+  Qed.
+
+  Lemma CPA_bound_realistic σ σ' (Q : nat) :
+    (Rabs (((lim_exec (((CPA Output') #true adv (rf_scheme Key' Input' Output' xor) #Q), σ)) #true) -
+             ((lim_exec (((CPA Output') #false adv (rf_scheme Key' Input' Output' xor) #Q), σ')) #true)) <= (Q * Q / (2 * S Input')))%R.
+  Proof.
+    unshelve epose proof CPA_bound Key' Input' Output' xor xor_sem _ _ _ adv _ _ σ σ' Q as H.
+    - apply xor_sem_bij.
+    - intros. by apply xor_correct_l.
+    - intros. by apply xor_correct_r.
+    - done.
+    - apply parisRΣ.
+    - apply subG_parisRGPreS. apply subG_refl.
+    - apply H. 
+  Qed.
+      
+End implementation.
