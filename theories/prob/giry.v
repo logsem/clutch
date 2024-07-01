cb2228a02107a7a82be2ac16b4878d467d048c64
Markus de Medeiros <markusdemedeiros@outlook.com>
2024-06-30 21:27:56 -0400

    half-skeleton for giry meas integrate

--- theories/prob/giry.v
+++ theories/prob/giry.v
@@ -531,17 +531,171 @@ HB.end.
       clear Hr1.
       clear X.
 
-
       (* This is a related measurable function *)
-      Check @ge0_emeasurable_fun_sum d T R setT. (* This sum is over T *)
+      Check @ge0_emeasurable_fun_sum d T R setT. (* This sum is over T.*)
 
-      Search sintegral.
-      Check sintegralE.
+      (* This one could actually be applied, if we can do the right rewrites to the sum? *)
+      (* Reduces the problem to proving measurablility at evey approximation level *)
+      have h_seq (i : nat) (μ : giryM T) : \bar R := 0:%:E.
 
-      (* Can I simplify the sintegral? *)
+
+
+      Check @ge0_emeasurable_fun_sum _ (giryM T) R setT h_seq _.
+
+
+      have Questionable : forall μ : giryType T,
+           (topology.lim
+             (topology.fmap (sintegral μ \o (fun x : nat => nnsfun_approx HMT measurable_mapP x))
+                (@topology.nbhs nat (topology.topology_set_system__canonical__topology_Filtered nat) topology.eventually)))
+        =
+             (topology.lim
+               (topology.fmap (fun n : nat => (\sum_(0 <= i < n) h_seq i μ)%R)
+                  (@topology.nbhs nat (topology.topology_set_system__canonical__topology_Filtered nat) topology.eventually))).
+      { intros AAAA. (* ???? *)
+        simpl.
+        intro μ.
+
+        (* Rewriting sintegral *)
+        have X1 : (comp (sintegral μ) (fun x : nat => nnsfun_approx HMT measurable_mapP x)) =
+                  (fun x : nat => (sintegral μ (nnsfun_approx HMT measurable_mapP x))).
+        { intros What1 What2. rewrite /comp. apply functional_extensionality. intro x. simpl. admit. }
+        erewrite X1. Unshelve. 2: apply AAAA. (* ???? *)
+        clear X1.
+        have X2 : forall n : nat,
+          sintegral μ (nnsfun_approx HMT measurable_mapP n)
+          =
+            (\sum_(x <- finite_support 0 [set r | 0 < r]
+                (fun x : join_Num_POrderedZmodule_between_GRing_Nmodule_and_Order_POrder R =>
+                 (x%:E * μ (nnsfun_approx HMT measurable_mapP n @^-1` [set x]))%E))
+      (x%:E * μ (nnsfun_approx HMT measurable_mapP n @^-1` [set x]))%E)%R.
+        { intros What1 What2 What3.
+          intro n.
+
+          rewrite (@sintegralEnnsfun _ _ R μ (nnsfun_approx HMT measurable_mapP n)).
+          simpl.
+          admit.
+        }
+      rewrite (functional_extensionality _ _ (X2 AAAA AAAA AAAA)).
+      clear X2.
+      simpl.
+      f_equal.
+      f_equal.
+      apply functional_extensionality.
+      intro n.
+      simpl.
+
+      (* have Strange : forall z, h_seq z μ = (z%:E * μ (nnsfun_approx HMT measurable_mapP n @^-1` [set z]))%R.  *)
+
+      (* FIXME: Next step-- is this provable? *)
+      Check [set r | 0 < r].
+      Check index_iota 0 n.
+
+      (* Aside from coercion hell, we should be able to define
+         h_seq = (x%:E * μ (nnsfun_approx HMT measurable_mapP n @^-1` [set x])) *)
+      (* I think that some of this could be avoided by just picking a single RealType? I see these weird
+       types join_Num_POrderedZmodule_between_GRing_Nmodule_and_Order_POrder so think it originates from
+       some inference related to coercions. Plus, it's weird that I can't use the type that's displayed
+       for sintegralEnnsfun... that "finite support" magically pops up when I declare it using the type
+       from sintegralEnnsfun  *)
+      admit.
+      }
+      rewrite (functional_extensionality _ _ (Questionable _)).
+      clear Questionable.
+
+      refine (@ge0_emeasurable_fun_sum _ (giryM T) R setT h_seq _ _).
+      - (* Probably doable for sane h_seq *)
+        admit.
+
+      (* Need to prove h_seq is measurable *)
+      (* mult. measurable is measurable *)
+      (* measure ofnnsfun_approx is measurable? *)
+      (* - Regardless of a lemma for this, there should be a lemma for finite sums, which that is. *)
+      (* Then I just need measuring a set is measurable, which is giryM_eval_def_measurable? Or something like that. *)
+
+      Restart.
+
+
+
+
+
+
+      have H1 : (forall x : T, [set: T] x -> (0%R <= f x)%E) by admit.
+      have H2 : d.-measurable [set: T] by admit.
+
+      Check @is_cvg_sintegral d T R.
+
+      Search (topology.lim (topology.fmap _ _) = _).
+
+      Check @emeasurable_fun_cvg _ (giryM T) R setT _
+        (fun μ : giryType T =>
+           topology.lim
+             (topology.fmap (sintegral μ \o (fun x : nat => nnsfun_approx _ measurable_mapP x))
+                (@topology.nbhs nat (topology.topology_set_system__canonical__topology_Filtered nat) topology.eventually))).
+      (* Maybe, but this is not my main approach. *)
+
+
+      (* I want to pull terms through that limit to eventually get to the limit of approximations of f. *)
+      (* f x is the limit of approximations evaluated at x *)
+      Check @cvg_nnsfun_approx d T R setT H2 f (@measurable_mapP _ _ _ _ f) H1.
+      (* Search topology.lim topology.cvg_to. *)
 
       Search measurable_fun topology.lim.
 
+
+
+
+      Check sequences.congr_lim. (* Only aesthetically useful *)
+
+      (* Relate cvg_lim to lim *)
+      Check topology.cvg_lim.
+
+      (* Approximations converge to the limit *)
+      Check is_cvg_sintegral. (* Limit of sintegral *)
+
+      Check approximation. (* Use on Hypothesis. *)
+
+      (* Can I turn the lim thing into a cvg_to problem? *)
+
+
+      (* Can I simplify the sintegral? *)
+      Check sintegralEnnsfun. (* Turn integral into sum *)
+      Search topology.fmap (_ \o _).
+      Check fun μ => @sintegralE d T R _.
+      (* (μ \o (fun x : nat => nnsfun_approx HMT measurable_mapP x)). *)
+
+
+      Check topology.fmap_comp (sintegral _) (fun x : nat => nnsfun_approx _ measurable_mapP x).
+
+      Search topology.lim topology.fmap topology.eventually.
+      Search topology.lim sintegral.
+
+      Search topology.fmap sintegral.
+      Search measurable_fun.
+
+
+
+
+      (* I don't know if I can do much more with measurable_fun? Maybe MCT? since it's about th emeasurability of a limit? *)
+      Locate cvg_monotone_convergence.
+
+      (* Search measurable_fun.
+      rewrite /measurable_fun.
+      intros _.
+      intros B HB.
+      rewrite setTI.
+       *)
+
+
+(* What is this stuff. Why did I save it.
+measurable_fun_esups:
+measurable_fun_limn_esup:
+measurableT_comp:
+measurable_fun_sups:
+measurable_comp:
+measurable_fun_limn_sup:
+*)
+
+      Search measurable_fun topology.lim.
       rewrite /sintegral/=.
       (**)
 
