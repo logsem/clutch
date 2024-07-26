833164e07d77c28d48c074033f113e869d18c640
Hei Li <hei411@gmail.com>
2024-07-26 16:16:40 +0200

    mdp exec progress

--- theories/prob/mdp.v
+++ theories/prob/mdp.v
@@ -43,3 +43,36 @@ Structure mdp := Mdp {
 #[global] Existing Instance mdpaction_eqdec.
 #[global] Existing Instance mdpaction_count.
 
+Section scheduler.
+  Context {δ : mdp}.
+  Context `{Countable sch_state}.
+  Record scheduler:= {
+      scheduler_f :> sch_state -> mdpstate δ -> distr (sch_state * mdpaction δ)
+    }.
+  Definition scheduler_int_state_f (s:scheduler) i e := lmarg (s i e).
+  Definition scheduler_action_f (s:scheduler) i e := rmarg (s i e).
+
+  Context (sch:scheduler).
+  (* sch_step takes a strict step and returns the whole configuration, including the sch state *)
+  Definition sch_step (p:sch_state*mdpstate δ) : distr(sch_state*mdpstate δ) :=
+    let '(sch_σ, mdp_σ) := p in
+    sch sch_σ mdp_σ ≫= (λ '(sch_σ', mdp_a), dmap (λ mdp_σ', (sch_σ', mdp_σ')) (step mdp_a mdp_σ)).
+
+  Definition sch_stepN (n:nat) p := iterM n sch_step p.
+
+  (** * TODO: Lemmas for sch_stepN *)
+
+  (* sch_step_or_final does a non-strict step, and returns whole configuration *)
+  Definition step_or_final a :=
+    match to_final a.2 with
+    | Some _ => dret a
+    | None => sch_step a
+    end.
+
+  Definition pexec (n:nat) p := iterM n step_or_final p.
+
+  (** * TODO: Lemmas for pexec *)
+
+  
+
+End scheduler.
