4530d33d9d389b7f8d821f69fd16321119599f6e
Hei Li <hei411@gmail.com>
2024-08-01 13:07:17 +0200

    lifting for coneris

---
+++ theories/coneris/adequacy.v
@@ -0,0 +1,16 @@
+From iris.proofmode Require Import base proofmode.
+From iris.bi Require Export weakestpre fixpoint big_op.
+From iris.base_logic.lib Require Import ghost_map invariants fancy_updates.
+From iris.algebra Require Import excl.
+From iris.prelude Require Import options.
+
+From clutch.prelude Require Import stdpp_ext iris_ext.
+From clutch.con_prob_lang Require Import erasure notation.
+From clutch.common Require Export con_language sch_erasable.
+From clutch.base_logic Require Import error_credits.
+From clutch.coneris Require Import weakestpre.
+From clutch.prob Require Import distribution.
+Import uPred.
+
+Section adequacy.
+End adequacy.
