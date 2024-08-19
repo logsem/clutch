a8a624c41b8765898245241d745a9fdd27089524
Hei Li <hei411@gmail.com>
2024-08-19 12:43:40 +0200

    hocap error lemmas

--- theories/coneris/lib/hocap.v
+++ theories/coneris/lib/hocap.v
@@ -22,28 +22,29 @@ Notation "'◯↯' ε '@' γ" := (∃ (x : nonnegreal), ⌜x.(nonneg) = ε%R⌝ 
 Section lemmas.
   Context `{!conerisGS Σ, !hocap_errorGS Σ}.
   (* Helpful lemmas *)
-  (* Lemma hocap_error_alloc (ε:nonnegreal) : *)
-  (*   ⊢ |==> (●↯ ε) ∗ (◯↯ ε). *)
-  (* Proof. *)
-  (*   iMod (own_alloc (●↯ ε ⋅ ◯↯ ε)) as "[??]". *)
-  (*   - by apply excl_auth_valid. *)
-  (*   - by eauto with iFrame. *)
-  (* Qed. *)
+  Lemma hocap_error_alloc (ε:nonnegreal):
+    ⊢ |==>∃ γ, (●↯ ε @ γ) ∗ (◯↯ ε @ γ).
+  Proof.
+    iMod (own_alloc (●E ε ⋅ ◯E ε)) as "[% [??]]". 
+    - by apply excl_auth_valid.
+    - by eauto with iFrame.
+  Qed.
 
-  (* Lemma ghost_var_agree γ b c : *)
-  (*   own γ (●E b) -∗ own γ (◯E c) -∗ ⌜ b = c ⌝. *)
-  (* Proof. *)
-  (*   iIntros "Hγ● Hγ◯". *)
-  (*   by iCombine "Hγ● Hγ◯" gives %<-%excl_auth_agree_L. *)
-  (* Qed. *)
+  Lemma hocap_error_agree γ b c :
+    (●↯ b @ γ) -∗ (◯↯ c @ γ) -∗ ⌜ b = c ⌝.
+  Proof.
+    iIntros "[% [<- Hγ●]] [% [<-Hγ◯]]".
+    by iCombine "Hγ● Hγ◯" gives %<-%excl_auth_agree_L.
+  Qed.
 
-  (* Lemma ghost_var_update γ b' b c : *)
-  (*   own γ (●E b) -∗ own γ (◯E c) ==∗ own γ (●E b') ∗ own γ (◯E b'). *)
-  (* Proof. *)
-  (*   iIntros "Hγ● Hγ◯". *)
-  (*   iMod (own_update_2 _ _ _ (●E b' ⋅ ◯E b') with "Hγ● Hγ◯") as "[$$]". *)
-  (*   { by apply excl_auth_update. } *)
-  (*   done. *)
+  Lemma hocap_error_update γ (b':nonnegreal) b c :
+    (●↯ b @ γ) -∗ (◯↯ c @ γ) ==∗ (●↯ b' @ γ) ∗ (◯↯ b' @ γ).
+  Proof.
+    iIntros "[% [<- Hγ●]] [% [<-Hγ◯]]".
+    iMod (own_update_2 _ _ _ (_ ⋅ _) with "Hγ● Hγ◯") as "[$$]".
+    { by apply excl_auth_update. }
+    done.
+  Qed. 
 End lemmas.
 
 Section HOCAP.
@@ -78,10 +79,10 @@ Section HOCAP.
     ↑hocap_error_nroot ⊆ E ->
     (∀ (ε:nonnegreal),  ((nonneg (ε2 ε true) + nonneg (ε2 ε false))/2 <= (nonneg ε))%R) →
     {{{ error_inv γ∗
-        (∀ (ε:nonnegreal) (b : bool), P ∗ ●↯ ε @ γ ={E∖↑hocap_error_nroot}=∗ ●↯ (ε2 ε b) @ γ ∗ Q (b) ) ∗
+        □(∀ (ε:nonnegreal) (b : bool), P ∗ ●↯ ε @ γ ={E∖↑hocap_error_nroot}=∗ ●↯ (ε2 ε b) @ γ ∗ Q (b) ) ∗
         P }}} flip @ E {{{ (b:bool), RET #b; Q (b)}}}.
   Proof.
-    iIntros (Hsubset Hineq) "%Φ [#Hinv [Hchange HP]] HΦ".
+    iIntros (Hsubset Hineq) "%Φ [#Hinv [#Hchange HP]] HΦ".
     rewrite /flip/flipL.
     wp_pures.
     wp_apply (wp_hocap_rand_adv_comp _ _ _ (λ ε x, if fin_to_nat x =? 1%nat then ε2 ε true else ε2 ε false) P (λ x, Q (fin_to_nat x =? 1%nat)) with "[-HΦ]"); [done|..].
@@ -98,7 +99,26 @@ Section HOCAP.
       inv_fin n; simpl; first done.
       intros n; inv_fin n; simpl; first done.
       intros n; inv_fin n.
-  Qed. 
+  Qed.
+
+  (* Lemma wp_hocap_flip_flip_adv_comp E *)
+  (*   (ε2 : nonnegreal -> (bool*bool) -> nonnegreal) *)
+  (*   (P : iProp Σ) (Q : (bool*bool) -> iProp Σ) γ: *)
+  (*   ↑hocap_error_nroot ⊆ E -> *)
+  (*   (∀ (ε:nonnegreal),  ((nonneg (ε2 ε (true, true)) + nonneg (ε2 ε (true, false)) + *)
+  (*                           (nonneg (ε2 ε (false, true)) + nonneg (ε2 ε (false, false)) *)
+  (*                        ))/4 <= (nonneg ε))%R) → *)
+  (*   {{{ error_inv γ∗ *)
+  (*      □ (∀ (ε:nonnegreal) (b : (bool*bool)), P ∗ ●↯ ε @ γ ={E∖↑hocap_error_nroot}=∗ ●↯ (ε2 ε b) @ γ ∗ Q (b) ) ∗ *)
+  (*       P }}} (flip, flip) @ E {{{ (b:bool*bool), RET (#b.1,#b.2); Q (b)}}}. *)
+  (* Proof. *)
+  (*   iIntros (Hsubset Hineq) "%Φ [#Hinv [#Hchange HP]] HΦ". *)
+  (*   wp_apply (wp_hocap_flip_adv_comp _ (λ x b, nnreal_div (ε2 x (false, b) + ε2 x (true, b))%NNR (mknonnegreal 2 _)) P with "[-HΦ]"); [done|..]. *)
+  (*   - intros ε. simpl. specialize (Hineq ε). lra. *)
+  (*   - iFrame. *)
+  (*     iSplit; first done. *)
+  (*     admit. *)
+  (* Abort. *)
     
 End HOCAP.
 
