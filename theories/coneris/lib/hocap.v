c3413a5b58bb39f383b024cc9e212a6d44ea0478
Simon Gregersen <gregersen@cs.au.dk>
2024-08-22 14:09:34 -0400

    alternative flip hocap spec

--- theories/coneris/lib/hocap.v
+++ theories/coneris/lib/hocap.v
@@ -37,7 +37,7 @@ Section error_lemmas.
   Lemma hocap_error_alloc (ε:nonnegreal):
     ⊢ |==>∃ γ, (●↯ ε @ γ) ∗ (◯↯ ε @ γ).
   Proof.
-    iMod (own_alloc (●E ε ⋅ ◯E ε)) as "[% [??]]". 
+    iMod (own_alloc (●E ε ⋅ ◯E ε)) as "[% [??]]".
     - by apply excl_auth_valid.
     - by eauto with iFrame.
   Qed.
@@ -63,7 +63,7 @@ Section error_lemmas.
   Proof.
     iIntros (->) "$".
   Qed.
-    
+
 End error_lemmas.
 
 Section tapes_lemmas.
@@ -75,7 +75,7 @@ End tapes_lemmas.
 Section HOCAP.
 
   Context `{!conerisGS Σ, !hocap_errorGS Σ}.
-  
+
   Definition error_inv (γ :gname):=
     inv hocap_error_nroot (∃ (ε:nonnegreal), ↯ ε ∗ ●↯ ε @ γ).
 
@@ -120,7 +120,7 @@ Section HOCAP.
       iIntros (ε n) "H".
       iMod ("Hchange" $! _ (fin_to_nat n =? 1%nat)with "[$]") as "[%|[Hε HQ]]".
       + iModIntro. iLeft. iPureIntro. by case_match.
-      + iModIntro. iRight. iFrame. by case_match. 
+      + iModIntro. iRight. iFrame. by case_match.
     - iIntros.
       wp_apply (conversion.wp_int_to_bool); first done.
       iIntros "_".
@@ -132,16 +132,16 @@ Section HOCAP.
 
   (** With tapes *)
   Context `{!hocap_tapesGS Σ}.
-  
+
   Definition tapes_inv (γ :gname):=
     inv hocap_tapes_nroot (∃ m, ●m@γ ∗ [∗ map] α ↦ t ∈ m, α ↪N ( t.1 ; t.2 )  ).
-  Lemma wp_hocap_presample_adv_comp (N : nat)  z E e 
+  Lemma wp_hocap_presample_adv_comp (N : nat)  z E e
      (ε2 : nonnegreal -> fin (S N) -> nonnegreal)
     (P : iProp Σ) (Q : val-> iProp Σ) R γ γ':
     TCEq N (Z.to_nat z) →
     to_val e = None ->
     ↑hocap_error_nroot ⊆ E ->
-    ↑hocap_tapes_nroot ⊆ E -> 
+    ↑hocap_tapes_nroot ⊆ E ->
     (∀ (ε:nonnegreal), SeriesC (λ n, (1 / (S N)) * nonneg (ε2 ε n))%R <= (nonneg ε))%R →
     error_inv γ -∗ tapes_inv γ' -∗
     □(∀ (ε:nonnegreal) (n : fin (S N)) m α ns,
@@ -158,8 +158,37 @@ Section HOCAP.
     iInv "Hinv'" as ">(%m & Hm & Hmauth)" "Hclose'".
     iApply (wp_presample_adv_comp); [done|exact|..].
     iApply fupd_frame_l.
-    - 
+    -
   Abort.
-  
+
+
+
 End HOCAP.
 
+Section HOCAP_alt.
+  Context `{!conerisGS Σ}.
+
+  Lemma wp_flip_exp_hocap  (P : iProp Σ) (Q : bool → iProp Σ) E1 E2 :
+    □ (P ={E1, E2}=∗
+        ∃ ε1 ε2, ⌜((nonneg (ε2 true) + nonneg (ε2 false))/2 <= nonneg ε1)%R⌝ ∗
+                 ↯ ε1 ∗ (∀ (b : bool), ↯ (ε2 b) ={E2, E1}=∗ Q b)) -∗
+    {{{ P }}} flip @ E1 {{{ (b : bool), RET #b; Q b}}}.
+  Proof.
+    iIntros "#Hvs". iIntros (Ψ) "!# HP HΨ".
+    rewrite /flip /flipL.
+    wp_pures.
+    wp_bind (rand _)%E.
+    iMod ("Hvs" with "HP") as (ε1 ε2) "(% & Hε1 & HQ)".
+    set (ε2' := ε2 ∘ fin_to_bool).
+    iApply (wp_couple_rand_adv_comp1' _ _ _ _  ε2' with "Hε1").
+    { rewrite SeriesC_finite_foldr /ε2' /=. lra. }
+    iIntros "!>" (n) "Hε2".
+    assert (ε2' n = ε2 (Z_to_bool n)) as ->.
+    { inv_fin n; [eauto|]. intros n. inv_fin n; [eauto|]. intros n. inv_fin n. }
+    iMod ("HQ" with "Hε2") as "HQ". iModIntro.
+    wp_apply (conversion.wp_int_to_bool); [done|].
+    iIntros "_".
+    by iApply "HΨ".
+  Qed.
+
+End HOCAP_alt.
