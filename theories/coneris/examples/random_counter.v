341dacb0ad3910918f278dd416157d3575bca3a9
Hei Li <hei411@gmail.com>
2024-08-26 14:09:02 +0200

    incr counter spec

--- theories/coneris/examples/random_counter.v
+++ theories/coneris/examples/random_counter.v
@@ -7,9 +7,9 @@ Set Default Proof Using "Type*".
 Section impl1.
 
   Definition new_counter : val:= λ: "_", ref #0.
-  Definition incr_counter : val := λ: "l", let: "n" := rand #3 in (FAA "l" "n", "n").
+  Definition incr_counter : val := λ: "l", let: "n" := rand #3 in FAA "l" "n";; "n".
   Definition allocate_tape : val := λ: "_", AllocTape #3.
-  Definition incr_counter_tape :val := λ: "l" "α", let: "n" := rand("α") #3 in (FAA "l" (rand("α") #3), "n").
+  Definition incr_counter_tape :val := λ: "l" "α", let: "n" := rand("α") #3 in FAA "l" (rand("α") #3;; "n").
 
   Context `{!conerisGS Σ, !hocap_errorGS Σ, !hocap_tapesGS Σ, !inG Σ (frac_authR ZR)}.
   Definition counter_inv_pred (c:val) γ1 γ2 γ3:=
@@ -45,5 +45,42 @@ Section impl1.
     iApply "HΦ".
     iExists _, _, _. by iFrame.
   Qed. 
-  
+
+  Lemma incr_counter_spec c γ1 γ2 γ3 (ε2:R -> nat -> R) (P: iProp Σ) (T Q: nat -> iProp Σ):
+    (∀ ε n, 0<= ε -> 0<= ε2 ε n)%R->
+    (∀ (ε:R), 0<=ε -> ((ε2 ε 0%nat) + (ε2 ε 1%nat)+ (ε2 ε 2%nat)+ (ε2 ε 3%nat))/4 <= ε)%R →
+    {{{ inv counter_nroot (counter_inv_pred c γ1 γ2 γ3) ∗
+        □(∀ (ε:R) (n : nat), P ∗ ●↯ ε @ γ1 ={⊤∖↑counter_nroot}=∗ (⌜(1<=ε2 ε n)%R⌝∨●↯ (ε2 ε n) @ γ1 ∗ T n) ) ∗
+        □ (∀ (n:nat) (z:Z), T n ∗ own γ3 (●F z) ={⊤∖↑counter_nroot}=∗
+                          own γ3 (●F(z+n)%Z)∗ Q n) ∗
+        P
+    }}}
+      incr_counter c
+      {{{ (n:nat), RET #n; Q n }}}.
+  Proof.
+    iIntros (Hpos Hineq Φ) "(#Hinv & #Hvs1 & #Hvs2 & HP) HΦ".
+    rewrite /incr_counter.
+    wp_pures.
+    wp_bind (rand _)%E.
+    iInv counter_nroot as ">(%ε & %m & %l & %z & H1 & H2 & H3 & H4 & -> & H5 & H6)" "Hclose".
+    iDestruct (ec_valid with "[$]") as "[%K1 %K2]".
+    wp_apply (wp_couple_rand_adv_comp1' _ _ _ _ (λ x, ε2 ε (fin_to_nat x)) with "[$]").
+    { intros. naive_solver. }
+    { rewrite SeriesC_finite_foldr; specialize (Hineq ε K1). simpl; lra. }
+    iIntros (n) "H1".
+    iMod ("Hvs1" with "[$]") as "[%|[H2 HT]]".
+    { iExFalso. iApply ec_contradict; last done. done. }
+    iMod ("Hclose" with "[$H1 $H2 $H3 $H4 $H5 $H6]") as "_"; first done.
+    iModIntro. wp_pures.
+    clear.
+    wp_bind (FAA _ _).
+    iInv counter_nroot as ">(%ε & %m & % & %z & H1 & H2 & H3 & H4 & -> & H5 & H6)" "Hclose".
+    wp_faa.
+    iMod ("Hvs2" with "[$]") as "[H6 HQ]".
+    iMod ("Hclose" with "[$H1 $H2 $H3 $H4 $H5 $H6]") as "_"; first done.
+    iModIntro.
+    wp_pures.
+    by iApply "HΦ".
+  Qed. 
+    
 End impl1.
