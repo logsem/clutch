e97a615421a56307a0b64d05e4751c3459c766f4
Markus de Medeiros <markusdemedeiros@outlook.com>
2024-09-10 09:28:53 -0400

    update laws documentation

--- theories/prob/monad/laws.v
+++ theories/prob/monad/laws.v
@@ -26,15 +26,27 @@ Set Default Proof Using "Type".
       giryM T
         - measurable type of probability subdistributions over T
 
+      <<discr T>>
+        - measurable type for the discrete space on a pointed type T
+
       borelER
         - Borel type on the extended real numbers
 
 
+    Notations:
+      p =μ q
+        - p, q are equal on all measurable sets
+
+
     Definitions:
+    - Each function giryM_X has an evaluation function giryM_X_eval.
+    - The definition itself should not be unfolded!
 
       m_id                                            : A -mm-> A
       m_cmp (f : B -mm-> C) (g : A -mm-> B)           : A -mm-> C
       m_cst (k : B)                                   : A -mm-> B
+
+      m_discr (f : A -> B)                            : <<discr A>> -mm-> B
 
       giryM_zero                                      : giryM A
       giryM_eval (S : measurable set A)               : giryM A -mm-> \bar R
@@ -42,26 +54,34 @@ Set Default Proof Using "Type".
       giryM_map (f : A -mm-> B)                       : giryM A -mm-> giryM B
 
       giryM_ret                                       : A -mm-> giryM A
-      giryM_bind (f : A -mm-> giryM B)                : giryM A -mm-> giryM B
       giryM_join                                      : giryM (giryM A) -mm-> giryM A
 
-    Each function giryM_X has an evaluation function giryM_X_eval (the definition itself
-    should not be unfolded)
+      giryM_unif (m : nat)                            : giryM <<discr ('I_(S m))>>
+
+
+    Derived forms:
+      giryM_bind (f : A -mm-> giryM B)                : giryM A -mm-> giryM B
+      giryM_iterN (n : nat) (f : A -mm-> giryM A)     : A -mm-> giryM A
+
 
     Laws:
-      giryM_join (giryM_zero)                 = giryM_zero
-      giryM_integrate f (giryM_join m)        = giryM_integrate (giryM_integrate f) m
-      giryM_join (giryM_map (giryM_map f) m)  = giryM_map f (giryM_join m)
-      giryM_join (giryM_map giryM_join m)     = giryM_join (giryM_join m)
-      giryM_join (giryM_map giryM_ret t)      = t
-      giryM_join (giryM_ret t)                = t
-      giryM_bind f giryM_zero                 = giryM_zero
-      giryM_bind (m_cst giryM_zero) t         = giryM_zero
-      giryM_bind f (giryM_ret t)              = f t
-      giryM_bind giryM_ret m                  = m
-      giryM_bind g (giryM_bind f m)           = giryM_bind (m_cmp (giryM_bind g) f) m
-      giryM_map f giryM_zero                  = giryM_zero
-      giryM_map (m_cst k) t                   = giryM_ret R k     (only for proper distributions)
+      giryM_join (giryM_zero)                 =    giryM_zero
+      giryM_integrate f (giryM_join m)        =    giryM_integrate (giryM_integrate f) m  ** for f nonnegative
+      giryM_map f giryM_zero                  =    giryM_zero
+      giryM_map (m_cst k) t                   =    giryM_ret R k                          ** for t proper
+      giryM_integrate g (giryM_map f t)       =    giryM_integrate (m_cmp g h)            ** for g, (g ∘ h) nonnegative
+      giryM_join (giryM_map (giryM_map f) m)  =μ   giryM_map f (giryM_join m)
+      giryM_join (giryM_map giryM_join m)     =    giryM_join (giryM_join m)              !! Aborted !!
+      giryM_join (giryM_map giryM_ret t)      =μ   t
+      giryM_join (giryM_ret t)                =μ   t
+      giryM_bind f giryM_zero                 =    giryM_zero
+      giryM_bind (m_cst giryM_zero) t         =μ   giryM_zero
+      giryM_bind f m S                        =    \int[m]_x (f x S)                      !! Refactor me !!
+      giryM_bind f (giryM_ret t)              =μ   f t
+      giryM_bind giryM_ret m                  =μ   m
+      giryM_bind g (giryM_bind f m)           =μ   giryM_bind (m_cmp (giryM_bind g) f) m
+      giryM_join m                            =    giryM_bind m_id m
+
 
  *)
 
@@ -478,9 +498,9 @@ Section giry_iterM.
   Local Open Scope classical_set_scope.
 
   (* Monadic iteration *)
-  Fixpoint giry_iterM (n : nat) (f : measurable_map T (giryM T)) : measurable_map T (giryM T)
+  Fixpoint giryM_iterN (n : nat) (f : measurable_map T (giryM T)) : measurable_map T (giryM T)
     := match n with
          O => giryM_ret R
-       | (S n) => m_cmp (giryM_bind f) (giry_iterM n f)
+       | (S n) => m_cmp (giryM_bind f) (giryM_iterN n f)
        end.
 End giry_iterM.
