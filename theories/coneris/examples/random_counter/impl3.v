f7b3c8964298cb01393e86be07a7648ba5577a97
Hei Li <hei411@gmail.com>
2024-09-13 15:32:29 +0200

    stronger adequacy theorem

---
+++ theories/coneris/examples/random_counter/impl3.v
@@ -0,0 +1,5 @@
+From iris.algebra Require Import frac_auth.
+From iris.base_logic.lib Require Import invariants.
+From clutch.coneris Require Import coneris hocap random_counter.
+
+Set Default Proof Using "Type*".
