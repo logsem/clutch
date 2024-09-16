547516c7ae9fe2a1f1f1606a517b2fc5cb5c60a0
Hei Li <hei411@gmail.com>
2024-09-16 12:50:38 +0200

    new_counter_spec3

--- theories/coneris/examples/random_counter/impl3.v
+++ theories/coneris/examples/random_counter/impl3.v
@@ -5,7 +5,7 @@ From clutch.coneris Require Import coneris hocap random_counter.
 Set Default Proof Using "Type*".
 
 Section filter.
-  Definition filter_f (n:fin 4%nat):= bool_decide (fin_to_nat n<4)%nat.
+  Definition filter_f (n:nat):= bool_decide (n<4)%nat.
   Definition filtered_list (l:list _) := filter filter_f l.
 End filter.
 
@@ -24,5 +24,38 @@ Section impl3.
                                    counterG2_frac_authR:: inG Σ (frac_authR natR) }.
 
   Context `{!conerisGS Σ, !hocap_errorGS Σ, !hocap_tapesGS Σ, !inG Σ (frac_authR natR)}.
-  
+  Definition counter_inv_pred3 (c:val) γ1 γ2 γ3:=
+    (∃ (ε:R) (m:gmap loc (nat * list nat)) (l:loc) (z:nat),
+        ↯ ε ∗ ●↯ ε @ γ1 ∗
+        ([∗ map] α ↦ t ∈ m, ∃ (ls:list nat), ⌜ (filter filter_f ls) = t.2⌝ ∗ α ↪N ( 4%nat ; ls) )
+        ∗ ●m@γ2 ∗  
+        ⌜c=#l⌝ ∗ l ↦ #z ∗ own γ3 (●F z)
+    )%I.
+
+  Lemma new_counter_spec3 E ε N:
+    {{{ ↯ ε }}}
+      new_counter3 #() @ E
+      {{{ (c:val), RET c;
+          ∃ γ1 γ2 γ3, inv N (counter_inv_pred3 c γ1 γ2 γ3) ∗
+                      ◯↯ε @ γ1 ∗ own γ3 (◯F 0%nat)
+      }}}.
+  Proof.
+    rewrite /new_counter3.
+    iIntros (Φ) "Hε HΦ".
+    wp_pures.
+    wp_alloc l as "Hl".
+    iDestruct (ec_valid with "[$]") as "%".
+    unshelve iMod (hocap_error_alloc (mknonnegreal ε _)) as "[%γ1 [H1 H2]]".
+    { lra. }
+    simpl.
+    iMod (hocap_tapes_alloc (∅:gmap _ _)) as "[%γ2 [H3 H4]]".
+    iMod (own_alloc (●F 0%nat ⋅ ◯F 0%nat)) as "[%γ3[H5 H6]]".
+    { by apply frac_auth_valid. }
+    replace (#0) with (#0%nat) by done.
+    iMod (inv_alloc N _ (counter_inv_pred3 (#l) γ1 γ2 γ3) with "[$Hε $Hl $H1 $H3 $H5]") as "#Hinv".
+    { iSplit; last done. by iApply big_sepM_empty. }
+    iApply "HΦ".
+    iExists _, _, _. by iFrame.
+  Qed. 
+    
 End impl3. 
