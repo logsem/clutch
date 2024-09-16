25890e4dbcb10a5c2070faae1b6715656bd5d619
Hei Li <hei411@gmail.com>
2024-09-16 14:38:14 +0200

    read_counter_spec3

--- theories/coneris/examples/random_counter/impl3.v
+++ theories/coneris/examples/random_counter/impl3.v
@@ -94,5 +94,130 @@ Section impl3.
     iApply "HΦ".
     by iFrame.
   Qed.
+
+  Lemma incr_counter_tape_spec_some3 N E c γ1 γ2 γ3 (P: iProp Σ) (Q:nat->iProp Σ) (α:loc) (n:nat) ns:
+    ↑N⊆E -> 
+    {{{ inv N (counter_inv_pred3 c γ1 γ2 γ3) ∗
+        □ (∀ (z:nat), P ∗ own γ3 (●F z) ={E∖↑N}=∗
+                          own γ3 (●F(z+n)%nat)∗ Q z) ∗
+        P ∗ α ◯↪N (3%nat; n::ns) @ γ2
+    }}}
+      incr_counter_tape3 c #lbl:α @ E
+      {{{ (z:nat), RET (#z, #n); Q z ∗ α ◯↪N (3%nat; ns) @ γ2}}}.
+  Proof.
+    iIntros (Hsubset Φ) "(#Hinv & #Hvs & HP & Hα) HΦ".
+  Admitted.
+  (*   rewrite /incr_counter_tape3. *)
+  (*   wp_pures. *)
+  (*   wp_bind (rand(_) _)%E. *)
+  (*   iInv N as ">(%ε & %m & %l & %z & H1 & H2 & H3 & H4 & -> & H5 & H6)" "Hclose". *)
+  (*   iDestruct (hocap_tapes_agree with "[$][$]") as "%". *)
+  (*   erewrite <-(insert_delete m) at 1; last done. *)
+  (*   rewrite big_sepM_insert; last apply lookup_delete. *)
+  (*   simpl. *)
+  (*   iDestruct "H3" as "[Htape H3]". *)
+  (*   wp_apply (wp_rand_tape with "[$]"). *)
+  (*   iIntros "[Htape %]". *)
+  (*   iMod (hocap_tapes_pop with "[$][$]") as "[H4 Hα]". *)
+  (*   iMod ("Hclose" with "[$H1 $H2 H3 $H4 $H5 $H6 Htape]") as "_". *)
+  (*   { iSplitL; last done. *)
+  (*     erewrite <-(insert_delete m) at 2; last done. *)
+  (*     iNext. *)
+  (*     rewrite insert_insert. *)
+  (*     rewrite big_sepM_insert; last apply lookup_delete. iFrame. *)
+  (*   } *)
+  (*   iModIntro. *)
+  (*   wp_pures. *)
+  (*   clear -Hsubset. *)
+  (*   wp_bind (FAA _ _). *)
+  (*   iInv N as ">(%ε & %m & % & %z & H1 & H2 & H3 & H4 & -> & H5 & H6)" "Hclose". *)
+  (*   wp_faa. *)
+  (*   iMod ("Hvs" with "[$]") as "[H6 HQ]". *)
+  (*   replace (#(z+n)) with (#(z+n)%nat); last first. *)
+  (*   { by rewrite Nat2Z.inj_add. } *)
+  (*   iMod ("Hclose" with "[$H1 $H2 $H3 $H4 $H5 $H6]") as "_"; first done. *)
+  (*   iModIntro. wp_pures. *)
+  (*   iApply "HΦ". *)
+  (*   by iFrame. *)
+  (* Qed.  *)
+
+  Lemma counter_presample_spec3 NS E ns α
+     (ε2 : R -> nat -> R)
+    (P : iProp Σ) T γ1 γ2 γ3 c:
+    ↑NS ⊆ E ->
+    (∀ ε n, 0<= ε -> 0<=ε2 ε n)%R ->
+    (∀ (ε:R), 0<= ε ->SeriesC (λ n, if (bool_decide (n≤3%nat)) then 1 / (S 3%nat) * ε2 ε n else 0%R)%R <= ε)%R->
+    inv NS (counter_inv_pred3 c γ1 γ2 γ3) -∗
+    (□∀ (ε:R) n, (P ∗ ●↯ ε@ γ1) ={E∖↑NS}=∗
+        (⌜(1<=ε2 ε n)%R⌝ ∨(●↯ (ε2 ε n) @ γ1 ∗ T (n)))) 
+        -∗
+    P -∗ α ◯↪N (3%nat; ns) @ γ2 -∗
+        wp_update E (∃ n, T (n) ∗ α◯↪N (3%nat; ns++[n]) @ γ2).
+  Proof.
+    iIntros (Hsubset Hpos Hineq) "#Hinv #Hvs HP Hfrag".
+  Admitted.
+  (*   iApply wp_update_state_step_coupl. *)
+  (*   iIntros (σ ε) "((Hheap&Htapes)&Hε)". *)
+  (*   iMod (inv_acc with "Hinv") as "[>(% & % & % & % & H1 & H2 & H3 & H4 & -> & H5 & H6) Hclose]"; [done|]. *)
+  (*   iDestruct (hocap_tapes_agree with "[$][$]") as "%". *)
+  (*   erewrite <-(insert_delete m) at 1; last done. *)
+  (*   rewrite big_sepM_insert; last apply lookup_delete. *)
+  (*   simpl. *)
+  (*   iDestruct "H3" as "[Htape H3]". *)
+  (*   iDestruct (tapeN_lookup with "[$][$]") as "(%&%&%)". *)
+  (*   iDestruct (ec_supply_bound with "[$][$]") as "%". *)
+  (*   iMod (ec_supply_decrease with "[$][$]") as (ε1' ε_rem -> Hε1') "Hε_supply". subst. *)
+  (*   iApply fupd_mask_intro; [set_solver|]; iIntros "Hclose'". *)
+  (*   iApply state_step_coupl_state_adv_comp_con_prob_lang; first done. *)
+  (*   unshelve iExists (λ x, mknonnegreal (ε2 ε1' x) _). *)
+  (*   { apply Hpos. apply cond_nonneg. } *)
+  (*   iSplit. *)
+  (*   { iPureIntro.  *)
+  (*     unshelve epose proof (Hineq ε1' _) as H1; first apply cond_nonneg. *)
+  (*     by rewrite SeriesC_nat_bounded_fin in H1. } *)
+  (*   iIntros (sample). *)
+    
+  (*   destruct (Rlt_decision (nonneg ε_rem + (ε2 ε1' sample))%R 1%R) as [Hdec|Hdec]; last first. *)
+  (*   { apply Rnot_lt_ge, Rge_le in Hdec. *)
+  (*     iApply state_step_coupl_ret_err_ge_1. *)
+  (*     simpl. simpl in *. lra. *)
+  (*   } *)
+  (*   iApply state_step_coupl_ret. *)
+  (*   unshelve iMod (ec_supply_increase _ (mknonnegreal (ε2 ε1' sample) _) with "Hε_supply") as "[Hε_supply Hε]". *)
+  (*   { apply Hpos. apply cond_nonneg. } *)
+  (*   { simpl. done. } *)
+  (*   iMod (tapeN_update_append _ _ _ _ sample with "[$][$]") as "[Htapes Htape]". *)
+  (*   iMod (hocap_tapes_presample _ _ _ _ _ (fin_to_nat sample) with "[$][$]") as "[H4 Hfrag]". *)
+  (*   iMod "Hclose'" as "_". *)
+  (*   iMod ("Hvs" with "[$]") as "[%|[H2 HT]]". *)
+  (*   { iExFalso. iApply (ec_contradict with "[$]"). exact. } *)
+  (*   iMod ("Hclose" with "[$Hε $H2 Htape H3 $H4 $H5 $H6]") as "_". *)
+  (*   { iNext. iSplit; last done. *)
+  (*     rewrite big_sepM_insert_delete; iFrame. *)
+  (*   } *)
+  (*   iApply fupd_mask_intro_subseteq; first set_solver. *)
+  (*   iFrame. *)
+  (* Qed.  *)
+
+  Lemma read_counter_spec3 N E c γ1 γ2 γ3 P Q:
+    ↑N ⊆ E ->
+    {{{  inv N (counter_inv_pred3 c γ1 γ2 γ3) ∗
+        □ (∀ (z:nat), P ∗ own γ3 (●F z) ={E∖↑N}=∗
+                    own γ3 (●F z)∗ Q z)
+         ∗ P
+    }}}
+      read_counter3 c @ E
+      {{{ (n':nat), RET #n'; Q n'
+      }}}.
+  Proof.
+    iIntros (Hsubset Φ) "(#Hinv & #Hvs & HP) HΦ".
+    rewrite /read_counter3.
+    wp_pure.
+    iInv N as ">(%ε & %m & %l & %z & H1 & H2 & H3 & H4 & -> & H5 & H6)" "Hclose".
+    wp_load.
+    iMod ("Hvs" with "[$]") as "[H6 HQ]".
+    iMod ("Hclose" with "[$H1 $H2 $H3 $H4 $H5 $H6]"); first done.
+    iApply ("HΦ" with "[$]").
+  Qed. 
     
 End impl3. 
