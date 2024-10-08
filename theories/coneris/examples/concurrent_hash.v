80754b9f91b8ae64627cda8002d84bb4f42578e7
Hei Li <hei411@gmail.com>
2024-10-08 14:48:17 +0200

    Using tokens for concurrent hash

--- theories/coneris/examples/concurrent_hash.v
+++ theories/coneris/examples/concurrent_hash.v
@@ -34,10 +34,10 @@ Section concurrent_hash.
     multiple_parallel #insert_num (hash_once_prog "h" "lock");;
     "h".
 
-  Context `{!conerisGS Σ, !spawnG Σ, !lockG Σ}.
+  Context `{!conerisGS Σ, !spawnG Σ, !lockG Σ, inG Σ (authR natR)}.
 
-  Lemma hash_once_prog_spec γ l f:
-    {{{ is_lock γ l (∃ m, coll_free_hashfun_amortized val_size max_hash_size f m) ∗ ↯ err }}}
+  Lemma hash_once_prog_spec γlock γ l f:
+    {{{ is_lock γlock l (∃ m, coll_free_hashfun_amortized val_size max_hash_size f m γ) ∗ ↯ err ∗ own γ (◯ 1%nat) }}}
       hash_once_prog f l #()
       {{{ (v:val), RET v; True }}}.
   Proof.
@@ -49,25 +49,24 @@ Section concurrent_hash.
     wp_apply wp_rand; first done.
     iIntros.
     wp_apply (wp_insert_amortized with "[$]").
-    - admit.
-    - iIntros (?) "(%&?&%)". wp_pures.
-      wp_apply (release_spec with "[-HΦ]").
-      + iFrame. iSplitR; first iApply "H". iFrame.
-      + done.
-  Admitted.
+    iIntros (?) "(%&?&%)". wp_pures.
+    wp_apply (release_spec with "[-HΦ]").
+    - iFrame. iSplitR; first iApply "H". iFrame.
+    - done.
+  Qed.
 
-  Lemma hash_once_prog_spec' γ (l:val) (f:val) E:
-    ⊢ <<{ is_lock γ l (∃ m, coll_free_hashfun_amortized val_size max_hash_size f m) ∗ ↯ err }>>
-      hash_once_prog f l #() @ E
-      <<{ ∃∃ (v:val), True | RET v }>>.
-  Proof.
-  Admitted. 
+  (* Lemma hash_once_prog_spec' γ (l:val) (f:val) E: *)
+  (*   ⊢ <<{ is_lock γ l (∃ m, coll_free_hashfun_amortized val_size max_hash_size f m) ∗ ↯ err }>> *)
+  (*     hash_once_prog f l #() @ E *)
+  (*     <<{ ∃∃ (v:val), True | RET v }>>. *)
+  (* Proof. *)
+  (* Admitted.  *)
         
   
   Lemma concurrent_hash_spec :
     {{{ ↯ (INR insert_num * err)%R }}}
       concurrent_hash_prog
-      {{{ (f:val), RET f; ∃ m, inv nroot (coll_free_hashfun_amortized val_size max_hash_size f m)}}}.
+      {{{ (f:val), RET f; ∃ γ γlock l,  is_lock γlock l (∃ m, coll_free_hashfun_amortized val_size max_hash_size f m γ)}}}.
   Proof.
     rewrite /concurrent_hash_prog.
     iIntros (Φ) "Herr HΦ".
