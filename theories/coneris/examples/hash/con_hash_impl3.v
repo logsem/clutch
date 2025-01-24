606855a209a72047607be4761b1f46e6e9f44ed0
Hei Li <hei411@gmail.com>
2025-01-24 23:04:38 +0100

    Nearly done with con hash 3

--- theories/coneris/examples/hash/con_hash_impl3.v
+++ theories/coneris/examples/hash/con_hash_impl3.v
@@ -48,6 +48,55 @@ Section con_hash_impl3.
     inv (N.@"2") (∃ n, hash_set n γ2 γ5 γ_token))%I
   .
 
+  Local Lemma err_pos (s:nat): (0 <= s / (val_size + 1))%R.
+  Proof.
+    apply Rcomplements.Rdiv_le_0_compat.
+    - apply pos_INR.
+    - pose proof (pos_INR val_size). lra.
+  Qed.
+
+  Local Lemma amortized_inequality (k : nat):
+    (k <= max_hash_size)%nat ->
+   ( 0 <= ((max_hash_size-1)%R * k / 2 - sum_n_m (λ x : nat, INR x) 0 (k - 1)) / (val_size + 1))%R.
+  Proof.
+    intros H.
+    pose proof (pos_INR max_hash_size) as H1.
+    pose proof (pos_INR val_size) as H2.
+    pose proof (pos_INR k) as H3.
+    apply Rcomplements.Rdiv_le_0_compat; last lra.
+    assert (sum_n_m (λ x : nat, INR x) 0 (k - 1) = (k-1)*k/2)%R as ->.
+    - clear.
+      induction k.
+      + simpl. rewrite sum_n_n.
+        rewrite Rmult_0_r. by assert (0/2 = 0)%R as -> by lra.
+      + clear. induction k.
+        * simpl. rewrite sum_n_n.
+          replace (_-_)%R with 0%R by lra.
+          rewrite Rmult_0_l. by assert (0/2 = 0)%R as -> by lra.
+        * assert (S (S k) - 1 = S (S k - 1))%nat as -> by lia.
+          rewrite sum_n_Sm; last lia.
+          rewrite IHk.
+          replace (plus _ _) with (((S k - 1) * S k / 2) + (S (S k - 1))) by done.
+          assert (∀ k, (INR (S k) - 1) = (INR k))%R as H'.
+          { intros. simpl. case_match; first replace (1 - 1)%R with 0%R by lra.
+            - done.
+            - by replace (_+1-1)%R with (INR (S n)) by lra.
+          }
+          rewrite !H'.
+          replace (S k - 1)%nat with (k)%nat by lia.
+          replace (Hierarchy.plus _ _) with ((k * S k / 2)%R + (S k))%R by done.
+          assert (k * (S k) + S k + S k = S k * S (S k))%R; last lra.
+          assert (k * S k + S k + S k = S k * (k+1+1))%R as ->; try lra.
+          assert (k+1+1 = S (S k))%R.
+          -- rewrite !S_INR. lra.
+          -- by rewrite H.
+    - rewrite -!Rmult_minus_distr_r.
+      apply Rcomplements.Rdiv_le_0_compat; try lra.
+      apply Rmult_le_pos; try lra.
+      assert (INR k <= INR max_hash_size)%R; try lra.
+      by apply le_INR.
+  Qed.
+
   Lemma hash_tape_presample m γ γ_set γ_set' γ6 α ns s E:
   hash_tape_auth m γ_set γ -∗
   hash_tape α ns γ_set γ-∗
@@ -61,17 +110,30 @@ Section con_hash_impl3.
       ).
   Proof.
     rewrite /hash_tape_auth/hash_tape/hash_token/hash_set.
-    iIntros "Htauth Ht Herr Htoken (%&?&?&Htokens&%H&?)".
+    iIntros "Htauth Ht Herr Htoken (%&?&Hauth&Htokens&%H&?)".
     iDestruct (ec_combine with "[$]") as "Herr".
     rewrite /amortized_error/=H.
     iAssert (↯ (s/(val_size+1)+(((max_hash_size-1) * (s+1))/2 - sum_n_m (λ x, INR x) 0%nat (s))%R / (val_size + 1))%R )%I with "[Herr]" as "Herr".
     { iApply ec_eq; last done.
       admit. }
+    iAssert (⌜s+1 <=max_hash_size⌝)%I as "%Hineq'".
+    { iCombine "Htoken Htokens" as "H".
+      iCombine "Hauth H" gives "H0".
+      rewrite auth_both_validI.
+      iDestruct "H0" as "[[% %H']_]". simpl in *.
+      rewrite nat_op in H'. iPureIntro. lia.
+    }
     iDestruct (ec_split with "[$]") as "[Herr ?]".
-    { admit. }
-    { admit. }
+    { apply err_pos. }
+    { pose proof  amortized_inequality (s+1) as K.
+      apply K in Hineq'.
+      replace (_+_-_) with s in Hineq'; last lia.
+      rewrite plus_INR in Hineq'. simpl in *. done. }
     iMod (con_hash_interface2.hash_tape_presample _ _ _ _ _ _ _ (mknonnegreal _ _) 0%NNR with "[$][$][Herr][$]") as "(%&[? _]&?&?&?)"; [|iApply ec_eq; last done; simpl; done|].
-    - simpl. admit.
+    - simpl. rewrite Rmult_0_l Rplus_0_r.
+      rewrite -Rmult_div_swap.
+      rewrite Rmult_div_l; first done.
+      pose proof pos_INR val_size. lra.
     - iCombine "Htoken Htokens" as "Htokens".
       iFrame.
       iModIntro.
@@ -79,8 +141,9 @@ Section con_hash_impl3.
       iExists (mknonnegreal _ _); iSplit; try done. simpl.
       replace (_+_-_)%nat with s by lia. rewrite plus_INR. simpl. iFrame.
       Unshelve.
-      + admit.
-      + admit.
+      + apply err_pos. 
+      + pose proof  amortized_inequality (s+1) as K.
+        apply K in Hineq'. done.
   Admitted.
 
 
@@ -138,47 +201,108 @@ Section con_hash_impl3.
     iApply "HΦ". iFrame.
     by iFrame "Hinv".
   Qed.
+
+  Lemma con_hash_alloc_tape N f l hm P {HP: ∀ m m', Timeless (P m m')} γ1 γ2 γ3 γ4 γ5 γ_token γ_lock Q:
+  {{{ con_hash_inv N f l hm P γ1 γ2 γ3 γ4 γ5 γ_token γ_lock ∗
+      (∀ m m' α, P m m' -∗ ⌜α∉dom m'⌝ -∗ |={⊤∖↑N}=> P m (<[α:=[]]>m') ∗ Q α)
+  }}}
+      allocate_tape #()
+      {{{ (α: val), RET α; hash_tape α [] γ2 γ3 ∗ Q α }}}.
+  Proof.
+    iIntros (Φ) "(#[H1 H2] & Hvs) HΦ".
+    rewrite /allocate_tape.
+    wp_apply (con_hash_alloc_tape2 with "[$H1 Hvs]"); last done.
+    iIntros. iMod (fupd_mask_subseteq ) as "Hclose"; last iMod ("Hvs" with "[$][//]").
+    { apply difference_mono_l. apply nclose_subseteq. }
+    iFrame.
+  Qed.
+
+  Lemma con_hash_spec N f l hm P {HP: ∀ m m', Timeless (P m m')} γ1 γ2 γ3 γ4 γ5 γ_token γ_lock Q1 Q2 α ns (v:nat):
+  {{{ con_hash_inv N f l hm P γ1 γ2 γ3 γ4 γ5 γ_token γ_lock ∗ hash_tape α (ns) γ2 γ3∗
+      ( ∀ m m', P m m' -∗
+                hash_auth m γ1 γ2 γ4 γ5-∗
+                ⌜m'!!α=Some ns⌝ -∗
+                |={⊤∖↑N}=>
+             match m!!v with
+             | Some res => P m m' ∗ hash_auth m γ1 γ2 γ4 γ5 ∗ Q1 res
+             | None => ∃ n ns', ⌜n::ns'=ns⌝ ∗ P (<[v:=n]> m) (<[α:=ns']> m')∗ hash_auth (<[v:=n]> m) γ1 γ2 γ4 γ5 ∗
+                                 Q2
+             end                                        
+      )
+  }}}
+      f #v α
+      {{{ (res:nat), RET (#res);  (hash_tape α (ns) γ2 γ3 ∗ Q1 res ∨
+                                 ∃ n ns', ⌜n::ns'=ns⌝ ∗ hash_tape α ns' γ2 γ3 ∗ ⌜res=n⌝ ∗ Q2 
+                                )
+      }}}.
+  Proof.
+    iIntros (Φ) "(#[H1 H2]& Ht & Hvs) HΦ".
+    iApply (con_hash_spec2 with "[$H1 $Ht Hvs]"); last done.
+    iIntros. iMod (fupd_mask_subseteq ) as "Hclose"; last iMod ("Hvs" with "[$][$][//]").
+    { apply difference_mono_l. apply nclose_subseteq. }
+    iFrame.
+  Qed.
+    
     
   
-  (* Program Definition con_hash_impl3 : con_hash3 val_size max_hash_size max_hash_size_pos := *)
-  (*   {| init_hash3:=init_hash; *)
-  (*     allocate_tape3:=allocate_tape; *)
-  (*     compute_hash3:=compute_hash; *)
+  Program Definition con_hash_impl3 : con_hash3 val_size max_hash_size Hpos :=
+    {| init_hash3:=init_hash;
+      allocate_tape3:=allocate_tape;
+      compute_hash3:=compute_hash;
       
-  (*     con_hash_inv3 := con_hash_inv;   *)
-  (*     hash_tape3:=hash_tape; *)
-  (*     hash_frag3:=hash_frag; *)
-  (*     hash_auth3:=hash_auth; *)
-  (*     hash_tape_auth3 := hash_tape_auth; *)
-  (*     hash_set3:=hash_set;  *)
-  (*     hash_set_frag3:=hash_set_frag; *)
-  (*     hash_token3 := hash_token; *)
-  (*   |}. *)
+      con_hash_inv3 := con_hash_inv;
+      hash_tape3:=hash_tape;
+      hash_frag3:=hash_frag;
+      hash_auth3:=hash_auth;
+      hash_tape_auth3 := hash_tape_auth;
+      hash_set3:=hash_set;
+      hash_set_frag3:=hash_set_frag;
+      hash_token3 := hash_token;
+      con_hash_interface3.hash_tape_presample := hash_tape_presample;
+      con_hash_presample3 := con_hash_presample;
+      con_hash_init3 := con_hash_init;
+      con_hash_alloc_tape3 := con_hash_alloc_tape;
+      con_hash_spec3:=con_hash_spec
+    |}.
+  Next Obligation.
+    iIntros (??????) "??".
+    iApply (con_hash_interface2.hash_auth_exclusive with "[$][$]").
+  Qed.
+  Next Obligation.
+    iIntros.
+    iApply (con_hash_interface2.hash_auth_frag_agree with "[$][$]").
+  Qed.
+  Next Obligation.
+    iIntros.
+    by iApply (con_hash_interface2.hash_auth_duplicate with "[$]").
+  Qed.
+  Next Obligation.
+    iIntros.
+    iApply (con_hash_interface2.hash_auth_coll_free with "[$]").
+  Qed.
+  Next Obligation.
+    iIntros (???????) "H1 H2".
+    by iDestruct (con_hash_interface2.hash_frag_frag_agree with "[$H1][$H2]") as "?".
+  Qed.
+  Next Obligation.
+    iIntros.
+    by iApply (con_hash_interface2.hash_auth_insert with "[$][$]").
+  Qed.
+  Next Obligation.
+    iIntros.
+    iApply (con_hash_interface2.hash_tape_valid with "[$]").
+  Qed.
+  Next Obligation.
+    iIntros.
+    iApply (con_hash_interface2.hash_tape_exclusive with "[$][$]").
+  Qed.
+  Next Obligation.
+    iIntros.
+    rewrite -own_op. rewrite nat_is_op. rewrite /hash_token.
+    rewrite auth_frag_op.
+    iSplit; by iIntros.
+  Qed.
   
-        
-  
-  (* Program Definition con_hash_impl2 : con_hash2 val_size := *)
-  (*   {| init_hash2:=init_hash; *)
-  (*     allocate_tape2:=allocate_tape; *)
-  (*     compute_hash2:=compute_hash; *)
-      
-  (*     con_hash_inv2 := con_hash_inv;  *)
-  (*     hash_tape2:=hash_tape; *)
-  (*     hash_frag2:=hash_frag; *)
-  (*     hash_auth2:=hash_auth; *)
-  (*     hash_tape_auth2 := hash_tape_auth; *)
-  (*     hash_set2:=hash_set;  *)
-  (*     hash_set_frag2:=hash_set_frag;  *)
-  (*     con_hash_interface2.hash_tape_presample := hash_tape_presample;  *)
-  (*     con_hash_presample2 := con_hash_presample;  *)
-  (*     con_hash_init2 := con_hash_init;  *)
-  (*     con_hash_alloc_tape2 := con_hash_alloc_tape;  *)
-  (*     con_hash_spec2:=con_hash_spec *)
-  (*   |} *)
-  (* . *)
-  (* Next Obligation. *)
-  (*   iIntros (??????) "[??][??]". *)
-  (*   iApply (hv_auth_exclusive with "[$][$]"). *)
   (* Qed. *)
   (* Next Obligation. *)
   (*   iIntros (???????)"[??][??]". *)
