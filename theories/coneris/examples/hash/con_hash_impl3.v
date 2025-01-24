23e176f5cb217eb00cbe408de2cc0d33336928fd
Hei Li <hei411@gmail.com>
2025-01-24 20:40:13 +0100

    Progress in con_hash_impl3

--- theories/coneris/examples/hash/con_hash_impl3.v
+++ theories/coneris/examples/hash/con_hash_impl3.v
@@ -1,9 +1,9 @@
-From stdpp Require Import namespaces finite fin_sets.
+From Coquelicot Require Import Hierarchy.
+From stdpp Require Import namespaces finite fin_sets namespaces.
 From iris.proofmode Require Import proofmode.
 From iris.algebra Require Import excl_auth numbers gset_bij.
 From clutch.prelude Require Import stdpp_ext.
 From clutch.coneris Require Import coneris con_hash_interface2 con_hash_interface3.
-Import Hierarchy.
 
 Set Default Proof Using "Type*".
 
@@ -11,7 +11,7 @@ Section con_hash_impl3.
   
   Variable val_size:nat.
   Variable max_hash_size:nat.
-  Hypothesis max_hash_size_pos: (0<max_hash_size)%nat.
+  Hypothesis Hpos: (0<max_hash_size)%nat.
   Context `{Hc: conerisGS Σ, 
               hash2: !con_hash2 val_size,
                  Htoken:inG Σ (authR natR)
@@ -48,6 +48,98 @@ Section con_hash_impl3.
     inv (N.@"2") (∃ n, hash_set n γ2 γ5 γ_token))%I
   .
 
+  Lemma hash_tape_presample m γ γ_set γ_set' γ6 α ns s E:
+  hash_tape_auth m γ_set γ -∗
+  hash_tape α ns γ_set γ-∗
+  ↯ (amortized_error val_size max_hash_size Hpos) -∗
+  hash_token 1%nat γ6-∗
+    hash_set s γ_set γ_set' γ6-∗
+    state_update E E (∃ (n:fin(S val_size)), 
+          ( hash_set (s+1)%nat γ_set γ_set' γ6 )∗
+          hash_tape_auth (<[α:=ns++[fin_to_nat n]]>m) γ_set γ ∗
+          hash_tape α (ns++[fin_to_nat n]) γ_set γ ∗ hash_set_frag (fin_to_nat n) γ_set γ_set'
+      ).
+  Proof.
+    rewrite /hash_tape_auth/hash_tape/hash_token/hash_set.
+    iIntros "Htauth Ht Herr Htoken (%&?&?&Htokens&%H&?)".
+    iDestruct (ec_combine with "[$]") as "Herr".
+    rewrite /amortized_error/=H.
+    iAssert (↯ (s/(val_size+1)+(((max_hash_size-1) * (s+1))/2 - sum_n_m (λ x, INR x) 0%nat (s))%R / (val_size + 1))%R )%I with "[Herr]" as "Herr".
+    { iApply ec_eq; last done.
+      admit. }
+    iDestruct (ec_split with "[$]") as "[Herr ?]".
+    { admit. }
+    { admit. }
+    iMod (con_hash_interface2.hash_tape_presample _ _ _ _ _ _ _ (mknonnegreal _ _) 0%NNR with "[$][$][Herr][$]") as "(%&[? _]&?&?&?)"; [|iApply ec_eq; last done; simpl; done|].
+    - simpl. admit.
+    - iCombine "Htoken Htokens" as "Htokens".
+      iFrame.
+      iModIntro.
+      rewrite Nat.add_comm. iFrame.
+      iExists (mknonnegreal _ _); iSplit; try done. simpl.
+      replace (_+_-_)%nat with s by lia. rewrite plus_INR. simpl. iFrame.
+      Unshelve.
+      + admit.
+      + admit.
+  Admitted.
+
+
+  Lemma con_hash_presample  N f l hm P {HP: ∀ m m', Timeless (P m m')} γ_hv γ_set γ_tape γ_hv' γ_set' γ_token γ_lock Q
+    E  :
+    ↑N ⊆ E ->
+    con_hash_inv N f l hm P γ_hv γ_set γ_tape γ_hv' γ_set' γ_token γ_lock -∗
+    (∀ m m' s, P m m'  -∗
+               hash_set s γ_set γ_set' γ_token -∗
+             hash_tape_auth m' γ_set γ_tape -∗
+             state_update (E∖↑N) (E∖↑N)
+             (∃ m'' s', P m m'' ∗ hash_tape_auth m'' γ_set γ_tape ∗ hash_set s' γ_set γ_set' γ_token ∗ Q m m' m'' s')
+    ) -∗
+    state_update E E (
+        ∃ m m' m'' s , Q m m' m'' s
+      ).
+  Proof.
+    iIntros (?)"#[H1 H2] Hvs".
+    iApply (state_update_inv_acc with "[$H2][-]").
+    { by apply nclose_subseteq'. }
+    iIntros ">(%&?)".
+    iMod (con_hash_presample2 _ _ _ _ _ _ _ _ _ _ _ (λ _ _ _ , ∃ s', hash_set s' _ _ _∗ Q _ _ _ _)%I with "[//][-]") as "Hcont".
+    { apply subseteq_difference_r; last by apply nclose_subseteq'.
+      by apply ndot_ne_disjoint.
+    }
+    - iIntros (??) "HP Htauth".
+      iApply (state_update_mono_fupd' (E∖↑N)).
+      { rewrite difference_difference_l_L. apply difference_mono_l.
+        apply union_least; by apply nclose_subseteq'. 
+      }
+      by iMod ("Hvs" with "[$][$][$]") as "(%&%&$&$&$&$)".
+    - by iDestruct "Hcont" as "(%&%&%&%&$&$)".
+  Qed.
+
+  Lemma con_hash_init N P {HP: ∀ m m', Timeless (P m m')}:
+    {{{ P ∅ ∅ }}}
+      init_hash #()
+      {{{ (f:val), RET f; ∃ l hm γ1 γ2 γ3 γ4 γ5 γ_token γ_lock, con_hash_inv N f l hm P γ1 γ2 γ3 γ4 γ5 γ_token γ_lock ∗  hash_token max_hash_size γ_token
+      }}}.
+  Proof.
+    iIntros (Φ)"HP HΦ".
+    iApply fupd_pgl_wp.
+    iMod (own_alloc (● max_hash_size ⋅ ◯ (0+max_hash_size)%nat)) as (γ_token) "[H● [H0 H◯]]"; first (simpl; by apply auth_both_valid_2).
+    iModIntro.
+    iApply pgl_wp_fupd.
+    rewrite /init_hash.
+    iApply (con_hash_init2 with "HP").
+    iNext.
+    iIntros (?)"(%&%&%&%&%&%&%&%&#Hinv&Hs)".
+    rewrite /con_hash_inv.
+    iMod (ec_zero) as "H0err".
+    iMod (inv_alloc _ _ (∃ n : nat, hash_set n _ _ γ_token)%I with "[$H0 $H● $Hs H0err]").
+    { iExists nnreal_zero. iFrame.
+      iPureIntro. simpl. rewrite sum_n_n. simpl. lra.  }
+    iApply "HΦ". iFrame.
+    by iFrame "Hinv".
+  Qed.
+    
+  
   (* Program Definition con_hash_impl3 : con_hash3 val_size max_hash_size max_hash_size_pos := *)
   (*   {| init_hash3:=init_hash; *)
   (*     allocate_tape3:=allocate_tape; *)
