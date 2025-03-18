4093d35e1ec697dd9f87b9f8f05dc4f95cacdbec
Félix SASSUS BOURDA <felix.sassus-bourda@proton.me>
2025-03-18 16:26:32 +0100

    Some cleanup

--- theories/eris/lib/sampling/bernouilli.v
+++ theories/eris/lib/sampling/bernouilli.v
@@ -1,13 +1,116 @@
 From clutch.eris Require Import eris.
 From clutch.eris.lib.sampling Require Import utils.
+Local Open Scope R.
 
 Local Ltac done ::= lia || lra || nra || real_solver || tactics.done || auto.
 Ltac add_hint t := let n := fresh "hint" in have n := t.
 
 
+Section Lemmas.
+
+  Lemma foldr_plus_app (l1 l2 : list R) (acc : R) :
+    foldr Rplus acc (l1 ++ l2) = foldr Rplus 0 l1 + foldr Rplus acc l2.
+  Proof.
+    elim: l1 =>> //=.
+  Qed.
+
+
+  Lemma fmap_prop {A B : Type} (l : list A) (f : A -> B) (P1 : A -> Prop) (P2 : B -> Prop) :
+    (∀ a, P1 a -> P2 (f a)) ->
+    (∀ a, a ∈ l -> P1 a) ->
+    ∀ b, b ∈ (f <$> l) -> P2 b.
+  Proof.
+    move=> HPs.
+    elim: l.
+    - move=> _ b /= HinNil.
+      by apply not_elem_of_nil in HinNil.
+    - move=> a l IH Hforall /= b Hin.
+      rewrite elem_of_cons in Hin.
+      case: Hin.
+      + move=> ->.
+        apply HPs, Hforall, elem_of_list_here.
+      + move=> Hin.
+        apply IH => //.
+        move=> a' Ha'.
+        by apply Hforall, elem_of_list_further.
+  Qed.
+
+  Lemma forall_list_eq {A : Type} (l : list A) (a : A) :
+    (∀ e, e ∈ l -> e = a) ->
+    l = repeat a (length l).
+  Proof.
+    add_hint @elem_of_list_here.
+    add_hint @elem_of_list_further.
+    elim: l => //=.
+    move=> e l IH Hl.
+    rewrite (Hl e) // -IH //.
+  Qed.
+
+  Lemma map_seq_if_lt {A : Type} (e1 e2 : A) (N : nat):
+    (λ x, if bool_decide (x < N)%nat then e1 else e2) <$> seq 0 N = repeat e1 N.
+  Proof.
+    set f := (λ x : nat, if bool_decide (x < N)%nat then e1 else e2).
+    assert (Heq: ∀ e, e ∈ f <$> seq 0 N -> e = e1). {
+      apply (fmap_prop _ f (λ n, n < N)%nat).
+      - move=> a Ha /=.
+        rewrite /f bool_decide_eq_true_2 //.
+      - move=> a Ha.
+        by apply elem_of_seq in Ha as [_ Ha].
+    }
+    replace N with (length (f <$> seq 0 N)) at 2 by rewrite fmap_length seq_length //.
+    exact: forall_list_eq Heq.
+  Qed.
+
+  Lemma map_seq_if_ge {A : Type} (e1 e2 : A) (N L : nat):
+    (λ x, if bool_decide (x < N)%nat then e1 else e2) <$> seq N L = repeat e2 L.
+  Proof.
+    set f := (λ x : nat, if bool_decide (x < N)%nat then e1 else e2).
+    assert (Heq: ∀ e, e ∈ f <$> seq N L -> e = e2). {
+      apply (fmap_prop _ f (λ n, n >= N)%nat) => a Ha.
+      - rewrite /f bool_decide_eq_false_2 //.
+      - by apply elem_of_seq in Ha as [].
+    }
+    replace L with (length (f <$> seq N L)) at 2 by rewrite fmap_length seq_length //.
+    exact: forall_list_eq Heq.
+  Qed.
+
+
+  Lemma foldr_plus_repeat (ε : R) (L : nat) :
+    foldr Rplus 0 (repeat ε L) = ε * L.
+  Proof.
+    elim: L =>> //.
+    rewrite S_INR //=.
+  Qed.
+
+  Lemma SeriesC_case (N M : nat) (ε1 ε2 : R) :
+    (N <= S M)%nat ->
+    SeriesC (
+      λ x : fin (S M), 
+      if bool_decide (fin_to_nat x < N)%nat
+      then ε2
+      else ε1
+    ) = (ε1 * (S M - N) + ε2 * N)%R.
+  Proof.
+    move=> HNleM.
+    rewrite SeriesC_finite_foldr -foldr_fmap.
+    transitivity (
+      foldr Rplus 0
+      ((λ x : nat, if bool_decide (x < N)%nat then ε2 else ε1) ∘ fin_to_nat <$> enum (fin (S M)))
+    ).
+    { reflexivity. }
+    rewrite list_fmap_compose fin.enum_fin_seq.
+    assert (seq 0 (S M) = seq 0 N ++ seq N (S M - N)) as ->. 
+    { replace (S M)%nat with (N + (S M - N))%nat at 1 by lia.
+      apply seq_app. }
+    rewrite fmap_app foldr_plus_app Rplus_comm.
+    rewrite map_seq_if_ge map_seq_if_lt.
+    rewrite !foldr_plus_repeat minus_INR //.
+  Qed.
+End Lemmas.
+
+
 Section Bernoulli.
   Context `{!erisGS Σ}.
-  Local Open Scope R.
   Program Definition bernoulli_distr (p : R) (p_pos : 0 <= p <= 1) : distr (fin 2) := {|
     pmf := λ x, if bool_decide (x = 1)%fin then p else (1 - p)%R
   |}.
@@ -88,12 +191,11 @@ Section Bernoulli.
   Proof.
     iIntros "%Φ Herr HΦ".
     destruct (decide (S M < N)%nat) as [Hlt |Hge%not_lt].
-    { iDestruct (ec_contradict_lt0 with "Herr") as "[]".
-      rewrite Rcomplements.Rlt_minus_l Rplus_0_l.
+    { cred_contra. rewrite Rcomplements.Rlt_minus_l.
       simpl_expr. }
     wp_apply (twp_bernoulli_scale _ _ _ 1 0 with "[$Herr]")%R; [lia | lra..|].
     iIntros "%k [(_ & Herr) | (-> & Herr)]".
-    - iDestruct (ec_contradict with "Herr") as "[]"; simplra.
+    - cred_contra. 
     - by iApply "HΦ".
   Qed.
 
@@ -104,12 +206,11 @@ Section Bernoulli.
   Proof.
     iIntros "%Φ Herr HΦ".
     destruct (decide (S M < N)%nat) as [Hlt |Hge%not_lt].
-    { iDestruct (ec_contradict with "Herr") as "[]".
-      simpl_expr. }
+    { cred_contra. simpl_expr. }
     wp_apply (twp_bernoulli_scale _ _ (N / S M) 0 1 with "[$Herr]")%R => //.
     iIntros "%k [(-> & Herr) | (-> & Herr)]".
-    - by iApply "HΦ". 
-    - iDestruct (ec_contradict with "Herr") as "[]"; simplra.
+    - by iApply "HΦ".
+    - find_contra.
   Qed.
 
   Lemma bernoulli_spec (N M : nat) :
@@ -125,4 +226,3 @@ Section Bernoulli.
   Qed.
 
 End Bernoulli.
-
--- theories/eris/lib/sampling/utils.v
+++ theories/eris/lib/sampling/utils.v
@@ -1,17 +1,12 @@
 From clutch.eris Require Import eris.
 
 Local Open Scope R.
-Local Ltac done ::= lia || lra || nra || real_solver || tactics.done || auto.
+Local Ltac done ::= 
+  lia || lra || nra || real_solver || tactics.done || auto.
 Ltac add_hint t := let n := fresh "hint" in have n := t.
 
-Lemma INR_sub M N : (M >= N)%nat -> INR M - INR N = INR (M - N).
-Proof.
-  elim: M N.
-  - case=> //.
-  - move=> M IH [|N] Hge.
-    + rewrite Nat.sub_0_r Rminus_0_r //.
-    + rewrite -IH // !S_INR Rminus_plus_r_r //.
-Qed.
+Set Printing Coercions.
+
 
 Lemma INR_S_not_0 (n : nat) : 
   INR (S n) ≠ 0.
@@ -23,11 +18,30 @@ Proof.
   Unset Printing Coercions. 
 Qed.
 
+Lemma ec_contradict_lt0 `{!erisGS Σ} (ε : R) : (ε < 0)%R -> ↯ ε ⊢ False.
+Proof.
+  iIntros "%ε_nonpos Herr".
+  iPoseProof (ec_valid with "Herr") as "[%Hε _]" => //.
+Qed.
 
-Ltac simplra := simpl; lra.
+Ltac cred_contra := 
+    match goal with 
+    | |- context[↯ 0] => 
+          iAssert (False)%I as "[]";
+          iApply ec_contradict_lt0; last iFrame; done
+    | |- context[↯ (1 - _)%R] => 
+          iAssert (False)%I as "[]";
+          iApply ec_contradict_lt0; last iFrame; done
+    | |- context[↯ _] => 
+          iAssert (False)%I as "[]";
+          iApply ec_contradict; last iFrame; done
+    end.
+  
+
 Ltac find_contra :=
   solve
   [ contradiction
+  | solve[cred_contra] 
   | match goal with 
     | H : ¬ _ |- _ => exfalso; apply H; by eauto
     end ].
@@ -76,7 +90,6 @@ Ltac simpl_expr :=
       solve[apply INR_S_not_0]
     ).
 
-
 Lemma Rmult_le_1_le_r (r1 r2 : R) :
   0 <= r1 <= 1 -> 
   0 <= r2 ->
@@ -93,7 +106,6 @@ Proof.
   real_solver.
 Qed.
 
-
 Lemma Rmult_le_1 (r1 r2 : R) :
   0 <= r1 <= 1 -> 
   0 <= r2 <= 1 ->
@@ -107,111 +119,4 @@ Lemma Rpow_le_1 (r : R) (k : nat) :
   0 <= r ^ k <= 1.
 Proof.
   elim: k => // => n IH //=.
-Qed.
-
-
-Lemma ec_contradict_lt0 `{!erisGS Σ} (ε : R) : (ε < 0)%R -> ↯ ε ⊢ False.
-Proof.
-  iIntros "%ε_nonpos Herr".
-  iPoseProof (ec_valid with "Herr") as "[%Hε _]" => //.
-Qed.
-
-
-Lemma foldr_plus_app (l1 l2 : list R) (acc : R) :
-  foldr Rplus acc (l1 ++ l2) = foldr Rplus 0 l1 + foldr Rplus acc l2.
-Proof.
-  elim: l1 =>> //=.
-Qed.
-
-
-Lemma fmap_prop {A B : Type} (l : list A) (f : A -> B) (P1 : A -> Prop) (P2 : B -> Prop) :
-  (∀ a, P1 a -> P2 (f a)) ->
-  (∀ a, a ∈ l -> P1 a) ->
-  ∀ b, b ∈ (f <$> l) -> P2 b.
-Proof.
-  move=> HPs.
-  elim: l.
-  - move=> _ b /= HinNil.
-    by apply not_elem_of_nil in HinNil.
-  - move=> a l IH Hforall /= b Hin.
-    rewrite elem_of_cons in Hin.
-    case: Hin.
-    + move=> ->.
-      apply HPs, Hforall, elem_of_list_here.
-    + move=> Hin.
-      apply IH => //.
-      move=> a' Ha'.
-      by apply Hforall, elem_of_list_further.
-Qed.
-
-Lemma forall_list_eq {A : Type} (l : list A) (a : A) :
-  (∀ e, e ∈ l -> e = a) ->
-  l = repeat a (length l).
-Proof.
-  add_hint @elem_of_list_here.
-  add_hint @elem_of_list_further.
-  elim: l => //=.
-  move=> e l IH Hl.
-  rewrite (Hl e) // -IH //.
-Qed.
-
-Lemma map_seq_if_lt {A : Type} (e1 e2 : A) (N : nat):
-  (λ x, if bool_decide (x < N)%nat then e1 else e2) <$> seq 0 N = repeat e1 N.
-Proof.
-  set f := (λ x : nat, if bool_decide (x < N)%nat then e1 else e2).
-  assert (Heq: ∀ e, e ∈ f <$> seq 0 N -> e = e1). {
-    apply (fmap_prop _ f (λ n, n < N)%nat).
-    - move=> a Ha /=.
-      rewrite /f bool_decide_eq_true_2 //.
-    - move=> a Ha.
-      by apply elem_of_seq in Ha as [_ Ha].
-  }
-  replace N with (length (f <$> seq 0 N)) at 2 by rewrite fmap_length seq_length //.
-  exact: forall_list_eq Heq.
-Qed.
-
-Lemma map_seq_if_ge {A : Type} (e1 e2 : A) (N L : nat):
-  (λ x, if bool_decide (x < N)%nat then e1 else e2) <$> seq N L = repeat e2 L.
-Proof.
-  set f := (λ x : nat, if bool_decide (x < N)%nat then e1 else e2).
-  assert (Heq: ∀ e, e ∈ f <$> seq N L -> e = e2). {
-    apply (fmap_prop _ f (λ n, n >= N)%nat) => a Ha.
-    - rewrite /f bool_decide_eq_false_2 //.
-    - by apply elem_of_seq in Ha as [].
-  }
-  replace L with (length (f <$> seq N L)) at 2 by rewrite fmap_length seq_length //.
-  exact: forall_list_eq Heq.
-Qed.
-
-
-Lemma foldr_plus_repeat (ε : R) (L : nat) :
-  foldr Rplus 0 (repeat ε L) = ε * L.
-Proof.
-  elim: L =>> //.
-  rewrite S_INR //=.
-Qed.
-
-Lemma SeriesC_case (N M : nat) (ε1 ε2 : R) :
-  (N <= S M)%nat ->
-  SeriesC (
-    λ x : fin (S M), 
-    if bool_decide (fin_to_nat x < N)%nat
-    then ε2
-    else ε1
-  ) = (ε1 * (S M - N) + ε2 * N)%R.
-Proof.
-  move=> HNleM.
-  rewrite SeriesC_finite_foldr -foldr_fmap.
-  transitivity (
-    foldr Rplus 0
-    ((λ x : nat, if bool_decide (x < N)%nat then ε2 else ε1) ∘ fin_to_nat <$> enum (fin (S M)))
-  ).
-  { reflexivity. }
-  rewrite list_fmap_compose fin.enum_fin_seq.
-  assert (seq 0 (S M) = seq 0 N ++ seq N (S M - N)) as ->. 
-  { replace (S M)%nat with (N + (S M - N))%nat at 1 by lia.
-    apply seq_app. }
-  rewrite fmap_app foldr_plus_app Rplus_comm.
-  rewrite map_seq_if_ge map_seq_if_lt.
-  rewrite !foldr_plus_repeat INR_sub //.
 Qed.
