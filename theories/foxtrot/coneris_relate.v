e6d1dbe92482eef05eae0664d66dbf5368262900
Hei Li <hei411@gmail.com>
2025-05-28 17:04:16 +0200

    NIT

--- theories/foxtrot/coneris_relate.v
+++ theories/foxtrot/coneris_relate.v
@@ -15,13 +15,12 @@ Import uPred.
 Set Default Proof Using "Type*".
 
 Section relate.
-  Context `{H:!conerisWpGS con_prob_lang Σ}.
-  Global Instance conerisGS_foxtrotGS : foxtrotWpGS con_prob_lang Σ := {
-      foxtrotWpGS_invGS := conerisWpGS_invGS;
-      state_interp := coneris.weakestpre.state_interp ;
-      fork_post :=  coneris.weakestpre.fork_post;
-      err_interp := coneris.weakestpre.err_interp;
-      spec_interp :=(λ _, True)%I
+  Context `{H:!foxtrotWpGS con_prob_lang Σ}.
+  Global Instance foxtrotGS_conerisGS : conerisWpGS con_prob_lang Σ := {
+      conerisWpGS_invGS := foxtrotWpGS_invGS;
+      coneris.weakestpre.state_interp := state_interp ;
+      coneris.weakestpre.fork_post :=  fork_post;
+      coneris.weakestpre.err_interp := err_interp;
     }.
 
   Definition foxtrot_wp := wp (Wp :=wp').
@@ -29,19 +28,19 @@ Section relate.
 
   
   Lemma state_step_coupl_implies_spec_coupl σ ρ ε Φ1 Φ2:
-    □(∀ σ' ρ' ε', Φ1 σ' ε' -∗ Φ2 σ' ρ' ε') -∗
-     state_step_coupl σ ε Φ1 -∗ spec_coupl σ ρ ε Φ2.
+    □(∀ σ' ε', spec_interp ρ -∗ Φ1 σ' ε' -∗ Φ2 σ' ρ ε') -∗
+     state_step_coupl σ ε Φ1 -∗ spec_interp ρ -∗ spec_coupl σ ρ ε Φ2.
   Proof.
     iIntros "#H H'".
     rewrite /state_step_coupl/state_step_coupl'.
-    iApply (least_fixpoint_ind _ (λ '(σ, ε), spec_coupl σ ρ ε Φ2) with "[][$H']").
+    iApply (least_fixpoint_ind _ (λ '(σ, ε), spec_interp ρ -∗ spec_coupl σ ρ ε Φ2)%I with "[][$H']").
     iModIntro.
     iIntros ([σ1 ε1]).
-    iIntros "[%H'| [H'|[H'|H']]]".
+    iIntros "[%H'| [H'|[H'|H']]] Hspec".
     - by iApply spec_coupl_ret_err_ge_1.
-    - iApply spec_coupl_ret. by iApply "H".
+    - iApply spec_coupl_ret. by iApply ("H" with "[$]").
     - iApply spec_coupl_amplify. iIntros. iDestruct ("H'" with "[//]") as "[H' _]".
-      done.
+      by iApply "H'".
     - iDestruct "H'" as "(%μ & %ε2 & % & [%r %] & %Hineq  &H')".
       iApply (spec_coupl_step_l_dret_adv (λ _, True) μ 0%NNR (λ σ, if bool_decide (μ σ > 0)%R then ε2 σ else 1)%NNR); try done.
       + exists (Rmax r 1).
@@ -57,27 +56,28 @@ Section relate.
         * simpl. rewrite -K. lra.
       +  by apply pgl_trivial.
       + iIntros. case_bool_decide.
-        * by iDestruct ("H'" $!_ ) as "[H' _]".
+        * iDestruct ("H'" $!_ ) as "[H' _]". by iApply "H'".
         * by iApply spec_coupl_ret_err_ge_1.
           Unshelve.
           -- apply state_step_coupl_pre_mono.
           -- intros ?[][][].
-             by f_equiv.
+             by do 2 f_equiv.
   Qed. 
   
   Lemma coneris_prog_coupl_implies_prog_coupl e σ ρ ε Φ1 Φ2:
     □ (∀ e2  σ2 efs,
       Φ1 e2 σ2 efs 1%NNR) -∗
-    (∀ e' σ' efs ρ' ε', Φ1 e' σ' efs ε' -∗ Φ2 e' σ' efs ρ' ε') -∗
+    (∀ e' σ' efs ε', spec_interp ρ -∗ Φ1 e' σ' efs ε' -∗ Φ2 e' σ' efs ρ ε') -∗
     coneris.weakestpre.prog_coupl e σ ε Φ1-∗
+    spec_interp ρ -∗ 
     prog_coupl e σ ρ ε Φ2 .
   Proof.
-    iIntros "#? H Hprog".
+    iIntros "#? H Hprog Hspec".
     iDestruct (prog_coupl_equiv1 with "[$][$]") as "H'".
     iDestruct "H'" as "(%R2&%ε1&%ε2&%&% & %Hineq & %Hpgl & H')".
     iApply prog_coupl_step_l_dret_adv; try done.
     iIntros.
-    iApply "H".
+    iApply ("H" with "[$]").
     by iApply "H'".
   Qed.
   
@@ -88,23 +88,23 @@ Section relate.
     iLöb as "IH" forall (e E Φ).
     rewrite pgl_wp_unfold wp_unfold.
     rewrite /pgl_wp_pre/wp_pre.
-    iIntros "H" (???) "(H1&_&H3)".
+    iIntros "H" (???) "(H1&H2&H3)".
     unshelve iMod ("H" with "[$]") as "H". 
     iModIntro.
-    iApply (state_step_coupl_implies_spec_coupl with "[][$]").
-    iIntros (???).
+    iApply (state_step_coupl_implies_spec_coupl with "[][$][$]").
+    iModIntro.
+    iIntros (??) "Hspec".
     case_match.
-    { iModIntro. iIntros ">(?&?&?)". by iFrame. }
-    iModIntro. 
+    { iIntros ">(?&?&?)". by iFrame. }
     iIntros.
-    iApply (coneris_prog_coupl_implies_prog_coupl with "[][][$]").
+    iApply (coneris_prog_coupl_implies_prog_coupl with "[][][$][$]").
     { iModIntro. iIntros. iNext.
       by iApply state_step_coupl_ret_err_ge_1.
     }
-    iIntros (?????) "H !>".
-    iApply (state_step_coupl_implies_spec_coupl with "[][$]").
+    iIntros (????) "Hspec H !>".
+    iApply (state_step_coupl_implies_spec_coupl with "[][$][$]").
     iModIntro. 
-    iIntros (???) ">(?&?&H&Hefs)".
+    iIntros (??) " Hspec >(?&?&H&Hefs)".
     iFrame.
     iModIntro. iSplitL "H".
     { by iApply "IH". }
