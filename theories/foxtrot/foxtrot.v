47e30b7338e8b1e6d2b51e12a7b838c5c1a32bc3
Hei Li <hei411@gmail.com>
2025-07-16 13:05:02 +0200

    Par LHS

---
+++ theories/foxtrot/foxtrot.v
@@ -0,0 +1,6 @@
+From stdpp Require Import namespaces.
+From clutch.con_prob_lang Require Export notation tactics metatheory.
+From clutch.con_prob_lang Require Export lang.
+From clutch.con_prob_lang.spec Require Export spec_tactics.
+From clutch.foxtrot Require Export weakestpre lifting ectx_lifting primitive_laws proofmode coupling_rules
+    model compatibility app_rel_rules rel_tactics interp soundness.
