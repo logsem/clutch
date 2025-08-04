979fcbb54f96ab910806203358d27977a5369190
Hei Li <hei411@gmail.com>
2025-08-04 13:07:26 +0200

    wp_rand_prog'_von_neumann_con_prog

--- theories/foxtrot/examples/von_neumann.v
+++ theories/foxtrot/examples/von_neumann.v
@@ -238,7 +238,93 @@ Section von_neumann.
       WP (Val von_neumann_con_prog)
         {{ v, ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}.
     Proof.
-    Admitted. 
+      iIntros "Hspec".
+      rewrite /von_neumann_con_prog'.
+      rewrite /von_neumann_con_prog.
+      wp_pures.
+      iModIntro.
+      iFrame.
+      iModIntro.
+      iIntros (??) "#Hinv".
+      unfold_rel.
+      clear -Hspawn.
+      iIntros (K j) "Hspec".
+      wp_pures. tp_pures j.
+      wp_alloc l as "Hl".
+      tp_alloc j as l' "Hl'".
+      wp_pures. tp_pures j.
+      iMod (inv_alloc _ _ ((∃ v0 v3 : val, l ↦ v0 ∗ l' ↦ₛ v3 ∗ lrel_nat v0 v3))%I with "[Hl Hl']") as "#Hinv'".
+      { iFrame. iNext. iExists 0; iPureIntro; split; f_equal. }
+      tp_bind j (Fork _).
+      iMod (pupd_fork with "[$]") as "[Hspec [%j' Hspec']]".
+      wp_apply (wp_fork with "[Hspec']").
+      { iNext.
+        iApply (wp_wand with "[Hspec']"); first (iApply ("Hinv" with "[][Hspec']")).
+        - iExists _, _. by repeat iSplit. 
+        - by erewrite fill_empty.
+        - by iIntros.
+      }
+      simpl.
+      tp_pures j.
+      wp_pures.
+      iFrame.
+      iModIntro.
+      iModIntro. 
+      iIntros (??) "[-> ->]".
+      unfold_rel.
+      clear -Hspawn.
+      iIntros (K j) "Hspec".
+      iLöb as "IH".
+      wp_pures.
+      tp_pures j.
+      wp_bind (! _)%E.
+      iInv "Hinv'" as ">(%&%&?&?&[%[-> ->]])" "Hclose".
+      tp_load j.
+      wp_load.
+      iMod ("Hclose" with "[-Hspec]").
+      { iFrame. by iExists _. }
+      iModIntro.
+      wp_apply (wp_min_prog); first done.
+      tp_bind j (min_prog _ _)%E.
+      iMod (spec_min_prog with "[$]") as "Hspec".
+      iIntros (? ->).
+      simpl.
+      wp_pures.
+      tp_pures j.
+      tp_allocnattape j α as "Hα".
+      tp_pures j.
+      tp_allocnattape j β as "Hβ".
+      do 2 tp_pure j.
+      tp_bind j (_|||_)%E.
+      iMod (tp_par with "[$]") as "(%j1&%j2&%K1&%K2&Hspec1&Hspec2&Hcont)".
+      wp_apply (wp_par (λ v, ∃ (b:bool), ⌜v=#b⌝ ∗ j1 ⤇ fill K1 v)%I (λ v, ∃ (b:bool), ⌜v=#b⌝ ∗ j2 ⤇ fill K2 v)%I with "[Hα Hspec1][Hβ Hspec2]").
+      - tp_bind j1 (rand(_) _)%E.
+        wp_apply (wp_couple_rand_rand_lbl with "[$Hα $Hspec1]"); first naive_solver.
+        iIntros (?) "(?&Hspec1&%)".
+        simpl.
+        tp_pures j1.
+        wp_pures.
+        iFrame.
+        by iExists _.
+      - tp_bind j2 (rand(_) _)%E.
+        wp_apply (wp_couple_rand_rand_lbl with "[$Hβ $Hspec2]"); first naive_solver.
+        iIntros (?) "(?&Hspec2&%)".
+        simpl.
+        tp_pures j2.
+        wp_pures.
+        iFrame.
+        by iExists _.
+      - iIntros (??) "[(%&->&Hspec1) (%&->&Hspec2)]".
+        iNext.
+        iMod ("Hcont" with "[$]").
+        simpl.
+        tp_pures j; first solve_vals_compare_safe.
+        wp_pures.
+        case_bool_decide.
+        + tp_pure j. wp_pure. by iApply "IH".
+        + tp_pures j. wp_pure. iFrame.
+          by iExists _.
+    Qed. 
     
     Lemma wp_von_neumann_con_prog'_von_neumann_prog K j:
       j ⤇ fill K (Val von_neumann_prog) -∗
