aebb693349a5f56898eed232a706846ebd91b2f0
Hei Li <hei411@gmail.com>
2025-08-04 12:43:25 +0200

    wp_von_neumann_prog_von_neumann_prog'

--- theories/foxtrot/examples/von_neumann.v
+++ theories/foxtrot/examples/von_neumann.v
@@ -15,8 +15,8 @@ Section von_neumann.
       Fork ("ad" "l") ;;
       (rec: "f" "_" :=
          let: "bias" := min_prog (! "l") #N in
-         let: "x" := rand #(S N) in
-         let: "y" := rand #(S N) in
+         let: "x" := (rand #(S N)) ≤ "bias" in
+         let: "y" := (rand #(S N)) ≤ "bias" in
          if: "x" = "y" then "f" #() else "x"
       ).
 
@@ -28,8 +28,8 @@ Section von_neumann.
          let: "bias" := min_prog (! "l") #N in
          let: "α" := alloc #(S N)in
          let: "β" := alloc #(S N) in
-         let: "x" := rand("α") #(S N) in
-         let: "y" := rand("β") #(S N) in
+         let: "x" := (rand("α") #(S N)) ≤ "bias" in
+         let: "y" := (rand("β") #(S N)) ≤ "bias" in
          if: "x" = "y" then "f" #() else "x"
       ).
 
@@ -39,9 +39,19 @@ Section von_neumann.
       Fork ("ad" "l") ;;
       (rec: "f" "_" :=
          let: "bias" := min_prog (! "l") #N in
+         let, ("x", "y") := (((rand #(S N))≤ "bias") ||| (rand #(S N))≤ "bias") in
+         if: "x" = "y" then "f" #() else "x"
+      ).
+  
+  Definition von_neumann_con_prog': val :=
+    λ: "ad",
+      let: "l" := ref #0 in
+      Fork ("ad" "l") ;;
+      (rec: "f" "_" :=
+         let: "bias" := min_prog (! "l") #N in
          let: "α" := alloc #(S N)in
          let: "β" := alloc #(S N) in
-         let, ("x", "y") := ((rand("α") #(S N)) ||| rand("β") #(S N)) in
+         let, ("x", "y") := (((rand("α") #(S N))≤ "bias") ||| (rand("β") #(S N))≤ "bias") in
          if: "x" = "y" then "f" #() else "x"
       ).
 
@@ -56,16 +66,88 @@ Section von_neumann.
 
     
     Lemma wp_von_neumann_prog_von_neumann_prog' K j:
-    {{{ j ⤇ fill K (Val von_neumann_prog') }}}
-      (Val von_neumann_prog)
-      {{{ v, RET v; ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}}.
+     j ⤇ fill K (Val von_neumann_prog') -∗
+     WP (Val von_neumann_prog)
+      {{ v, ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}.
     Proof.
-    Admitted. 
+      iIntros "Hspec".
+      rewrite /von_neumann_prog'.
+      rewrite /von_neumann_prog.
+      wp_pures.
+      iModIntro.
+      iFrame.
+      iModIntro.
+      iIntros (??) "#Hinv".
+      unfold_rel.
+      clear.
+      iIntros (K j) "Hspec".
+      wp_pures. tp_pures j.
+      wp_alloc l as "Hl".
+      tp_alloc j as l' "Hl'".
+      wp_pures. tp_pures j.
+      iMod (inv_alloc _ _ ((∃ v0 v3 : val, l ↦ v0 ∗ l' ↦ₛ v3 ∗ lrel_nat v0 v3))%I with "[Hl Hl']") as "#Hinv'".
+      { iFrame. iNext. iExists 0; iPureIntro; split; f_equal. }
+      tp_bind j (Fork _).
+      iMod (pupd_fork with "[$]") as "[Hspec [%j' Hspec']]".
+      wp_apply (wp_fork with "[Hspec']").
+      { iNext.
+        iApply (wp_wand with "[Hspec']"); first (iApply ("Hinv" with "[][Hspec']")).
+        - iExists _, _. by repeat iSplit. 
+        - by erewrite fill_empty.
+        - by iIntros.
+      }
+      simpl.
+      tp_pures j.
+      wp_pures.
+      iFrame.
+      iModIntro.
+      iModIntro. 
+      iIntros (??) "[-> ->]".
+      unfold_rel.
+      clear.
+      iIntros (K j) "Hspec".
+      iLöb as "IH".
+      wp_pures.
+      tp_pures j.
+      wp_bind (! _)%E.
+      iInv "Hinv'" as ">(%&%&?&?&[%[-> ->]])" "Hclose".
+      tp_load j.
+      wp_load.
+      iMod ("Hclose" with "[-Hspec]").
+      { iFrame. by iExists _. }
+      iModIntro.
+      wp_apply (wp_min_prog); first done.
+      tp_bind j (min_prog _ _)%E.
+      iMod (spec_min_prog with "[$]") as "Hspec".
+      iIntros (? ->).
+      simpl.
+      wp_pures.
+      tp_pures j.
+      tp_allocnattape j α as "Hα".
+      tp_pures j.
+      tp_allocnattape j β as "Hβ".
+      tp_pures j.
+      tp_bind j (rand(_) _)%E.
+      wp_apply (wp_couple_rand_rand_lbl with "[$Hα $Hspec]"); first naive_solver.
+      iIntros (?) "(?&Hspec&%)".
+      simpl.
+      wp_pures. tp_pures j.
+      tp_bind j (rand(_) _)%E.
+      wp_apply (wp_couple_rand_rand_lbl with "[$Hβ $Hspec]"); first naive_solver.
+      iIntros (?) "(?&Hspec&%)".
+      simpl.
+      tp_pures j; first solve_vals_compare_safe.
+      wp_pures.
+      case_bool_decide.
+      - tp_pure j. wp_pure. by iApply "IH".
+      - tp_pures j. wp_pure. iFrame.
+        by iExists _.
+    Qed. 
     
     Lemma wp_von_neumann_prog'_rand_prog K j:
-    {{{ j ⤇ fill K (Val rand_prog) }}}
-      (Val von_neumann_prog')
-      {{{ v, RET v; ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}}.
+     j ⤇ fill K (Val rand_prog) -∗
+     WP (Val von_neumann_prog')
+      {{ v, ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}.
     Proof.
     Admitted. 
     (*   iIntros (Φ) "Hspec HΦ". *)
@@ -98,9 +180,9 @@ Section von_neumann.
     Context `{!foxtrotRGS Σ, !spawnG Σ}.
 
     Lemma wp_rand_prog_rand_prog' K j:
-    {{{ j ⤇ fill K (Val rand_prog') }}}
-      (Val rand_prog)
-      {{{ v, RET v; ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}}.
+     j ⤇ fill K (Val rand_prog') -∗
+     WP (Val rand_prog)
+      {{ v, ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}.
     Proof.
     Admitted. 
     (*   iIntros (Φ) "Hspec HΦ". *)
@@ -120,16 +202,23 @@ Section von_neumann.
 
     
     Lemma wp_rand_prog'_von_neumann_con_prog K j:
-    {{{ j ⤇ fill K (Val von_neumann_con_prog) }}}
-      (Val rand_prog')
-      {{{ v, RET v; ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}}.
+      j ⤇ fill K (Val von_neumann_con_prog) -∗
+      WP (Val rand_prog')
+      {{ v, ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}.
+    Proof.
+    Admitted. 
+
+    Lemma wp_von_neumann_con_prog_von_neumann_con_prog' K j:
+      j ⤇ fill K (Val von_neumann_con_prog') -∗
+      WP (Val von_neumann_con_prog)
+        {{ v, ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}.
     Proof.
     Admitted. 
     
-    Lemma wp_von_neumann_con_prog_von_neumann_prog K j:
-    {{{ j ⤇ fill K (Val von_neumann_prog) }}}
-      (Val von_neumann_con_prog)
-      {{{ v, RET v; ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}}.
+    Lemma wp_von_neumann_con_prog'_von_neumann_prog K j:
+      j ⤇ fill K (Val von_neumann_prog) -∗
+      WP (Val von_neumann_con_prog')
+        {{ v, ∃ v' : val, j ⤇ fill K v' ∗ ((ref lrel_nat → ()) → () → lrel_bool)%lrel v v' }}.
     Proof.
     Admitted. 
     (*   iIntros (Φ) "Hspec HΦ". *)
@@ -182,11 +271,12 @@ Section von_neumann.
   Lemma rand_prog_refines_von_neumann_prog :
     ∅ ⊨ rand_prog ≤ctx≤ von_neumann_prog : ((TRef TNat → TUnit) → TUnit →TBool).
   Proof.
-    eapply ctx_refines_transitive with rand_prog'; last eapply ctx_refines_transitive with von_neumann_con_prog;
+    eapply ctx_refines_transitive with rand_prog'; last eapply ctx_refines_transitive with von_neumann_con_prog; last eapply ctx_refines_transitive with von_neumann_con_prog';
       apply (refines_sound (#[spawnΣ; foxtrotRΣ])); iIntros; unfold_rel; iIntros (K j) "spec"; simpl.
     - wp_apply (wp_rand_prog_rand_prog' with "[$]"); by iIntros.
     - wp_apply (wp_rand_prog'_von_neumann_con_prog with "[$]"); by iIntros.
-    - wp_apply (wp_von_neumann_con_prog_von_neumann_prog with "[$]"); by iIntros.
+    - wp_apply (wp_von_neumann_con_prog_von_neumann_con_prog' with "[$]"); by iIntros.
+    - wp_apply (wp_von_neumann_con_prog'_von_neumann_prog with "[$]"); by iIntros.
   Qed.
   
   Lemma von_neumann_prog_eq_rand_prog :
