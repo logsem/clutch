86b00e9dd17e9e93eca3521a017adf9568b9db72
Alejandro Aguirre <aleagui1992@gmail.com>
2025-09-12 10:30:32 +0200

    Deleting non-eris files

--- theories/coneris/coneris.v
+++
@@ -1,5 +0,0 @@
-From stdpp Require Import namespaces.
-From clutch.con_prob_lang Require Export notation tactics metatheory.
-From clutch.con_prob_lang Require Export lang.
-From clutch.coneris Require Export lifting ectx_lifting primitive_laws derived_laws
-  proofmode error_rules.
