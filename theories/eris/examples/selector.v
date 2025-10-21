d4210675bba40f3724787ccc29c22b074a8fe868
Markus de Medeiros <markusdemedeiros@outlook.com>
2025-10-21 15:53:27 -0400

    S

--- theories/eris/examples/selector.v
+++ theories/eris/examples/selector.v
@@ -44,6 +44,13 @@ Section credits.
   Definition Bii_CreditV (F : bool → R) k x :=
     Bii_μ k x false * F false + Bii_μ k x true * F true.
 
+  Definition S_CreditV (F : nat → R) k x y N : R :=
+    SeriesC (fun n => S_μ k x y N n * F n).
+
+  Definition B_CreditV (F : bool → R) k x : R :=
+    (exp (-(2*k+x)/(2*k+2))) * F true +
+    (exp (-(2*k+x)/(2*k+2))) * F false.
+
   Definition Bii_g F x : R → R := fun r =>
     Iverson (Rle x) r * F true +
     Iverson (Rge x) r * F false.
@@ -53,10 +60,24 @@ Section credits.
     Iverson (eq 1) n * RInt (Bii_g F x) 0 1 +
     Iverson (eq 2) n * F false.
 
+  Definition S_hz F k x N z : bool → R := fun b =>
+    Iverson is_true b * F N +
+    Iverson (not ∘ is_true) b * S_CreditV F k x z (N + 1).
+
+  Definition S_g F k x y N : R → R := fun z =>
+    Iverson (Rle y) z * F N +
+    Iverson (Rge y) z * (Bii_μ k x false * S_hz F k x N z false + Bii_μ k x true * S_hz F k x N z true).
+
   Lemma C_CreditV_nn {F m} (Hnn : ∀ r, 0 <= F r) : 0 <= C_CreditV F m.
   Proof. Admitted.
 
   Lemma Bii_CreditV_nn {F k x} (Hnn : ∀ r, 0 <= F r) : 0 <= Bii_CreditV F k x.
+  Proof. Admitted.
+
+  Lemma S_CreditV_nn {F k x y N} (Hnn : ∀ r, 0 <= F r) : 0 <= S_CreditV F k x y N.
+  Proof. Admitted.
+
+  Lemma B_CreditV_nn {F k x} (Hnn : ∀ r, 0 <= F r) : 0 <= B_CreditV F k x.
   Proof. Admitted.
 
   Lemma Bii_h_nn {F k x} (Hnn : ∀ r, 0 <= F r) : 0 <= Bii_h F k x.
@@ -76,8 +97,20 @@ Section credits.
   Lemma Bii_g_correct {F x} : is_RInt (Bii_g F x) 0 1 (RInt (Bii_g F x) 0 1).
   Proof. Admitted.
 
-End credits.
+  Lemma S_hz_nn {F k x N z w} (Hnn : ∀ r, 0 <= F r) : 0 <= S_hz F k x N z w.
+  Proof. Admitted.
 
+  Lemma S_g_nn {F k x z N r} (Hnn : ∀ r, 0 <= F r) : 0 <= S_g F k x z N r.
+  Proof. Admitted.
+
+  Lemma S_nn_1 {F k x z N} (Hnn : ∀ r, 0 <= F r) :
+    0 <= Bii_μ k x false * S_hz F k x N z false + Bii_μ k x true * S_hz F k x N z true.
+  Proof. Admitted.
+
+  Lemma S_g_expectation {F k x y N} : is_RInt (S_g F k x y N) 0 1 (S_CreditV F k x y N).
+  Proof. Admitted.
+
+End credits.
 
 Section program.
   Context `{!erisGS Σ}.
@@ -93,19 +126,26 @@ Section program.
       let: "r" := init #() in
       ("f" = #0) || (("f" = #1) && (cmp "x" "r" = #(-1))).
 
+  Definition S : val :=
+    rec: "trial" "k" "x" "y" "N" :=
+      let: "z" := init #() in
+      if: (cmp "y" "z" = #(-1)) || (Bii "k" "x") then "N" else "trial" "k" "x" "z" ("N" + #1%nat).
+
+  Definition B : val :=
+    λ: "k" "x", (S "k" "x" "x" #0%nat) `rem` #2 = #0.
+
   Theorem wp_C {E F} (m : nat) (Hnn : ∀ r, 0 <= F r) :
     ↯(C_CreditV F m) -∗ WP C #m @ E {{ vn, ∃ n : nat, ⌜vn = #n ⌝ ∗ ⌜n = 0%nat \/ n = 1%nat \/ n = 2%nat⌝∗ ↯(F n) }}.
   Proof.
     iIntros "Hε".
     rewrite /C.
     wp_pures.
-
     (* I hate fin >:( *)
-    have HF0 : (0 < (S (Z.to_nat (m + 1))))%nat by lia.
-    have HF1 : (1 < (S (Z.to_nat (m + 1))))%nat by lia.
-    pose F0 : fin (S (Z.to_nat (m + 1))) := Fin.of_nat_lt HF0.
-    pose F1 : fin (S (Z.to_nat (m + 1))) := Fin.of_nat_lt HF1.
-    pose C_F := fun (n : fin (S (Z.to_nat (m + 1)))) =>
+    have HF0 : (0 < (Coq.Init.Datatypes.S (Z.to_nat (m + 1))))%nat by lia.
+    have HF1 : (1 < (Coq.Init.Datatypes.S (Z.to_nat (m + 1))))%nat by lia.
+    pose F0 : fin (Coq.Init.Datatypes.S  (Z.to_nat (m + 1))) := Fin.of_nat_lt HF0.
+    pose F1 : fin (Coq.Init.Datatypes.S (Z.to_nat (m + 1))) := Fin.of_nat_lt HF1.
+    pose C_F := fun (n : fin (Coq.Init.Datatypes.S  (Z.to_nat (m + 1)))) =>
       Iverson (eq F0) n * (1 / (m + 2) * F 0%nat) +
       Iverson (eq F1) n * (1 / (m + 2) * F 1%nat) +
       Iverson (fun n' => ¬ n' = F0 ∧ ¬ n' = F1) n * (m / (m + 2) * F 2%nat).
@@ -200,5 +240,92 @@ Section program.
       lra.
     }
   Qed.
+
+  Theorem wp_S {E F} (k : nat) xα x (Hnn : ∀ r, 0 <= F r) :
+    ∀ yα y N , ↯(S_CreditV F k x y N) ∗ lazy_real xα x ∗ lazy_real yα y -∗
+    WP S #k xα yα #N @ E {{ vn, ∃ n : nat, ⌜vn = #n ⌝ ∗ ↯(F n) ∗ lazy_real xα x }}.
+  Proof.
+    iLöb as "IH".
+    iIntros (yα y N) "(Hε & Hx & Hy)".
+    rewrite {2}/S.
+    wp_pures.
+    wp_apply wp_init; first done.
+    iIntros (zα) "Hz".
+    iApply (wp_lazy_real_presample_adv_comp _ _ zα _ (S_CreditV F k x y N) (S_g F k x y N)); auto.
+    { intros ??. apply S_g_nn; auto. }
+    { apply S_g_expectation. }
+    iSplitL "Hz"; [done|].
+    iSplitL "Hε"; [done|].
+    iIntros (z) "(Hε & Hz)".
+    wp_pures.
+    wp_apply (wp_cmp with "[Hy Hz]"); first iFrame.
+    iIntros (c) "(Hy & Hz & %Hcmp)".
+    destruct Hcmp as [[-> Hc'] | [-> Hc']].
+    { wp_pures.
+      iModIntro; iExists N; iFrame; iSplitR; first done.
+      rewrite /S_g.
+      iPoseProof (ec_split _ _ with "Hε") as "(Hε & _)".
+      { apply Rmult_le_pos; [apply Iverson_nonneg | apply Hnn ]. }
+      { apply Rmult_le_pos; [apply Iverson_nonneg | apply S_nn_1; auto ]. }
+      rewrite Iverson_True; [rewrite Rmult_1_l|done].
+      done.
+    }
+    { wp_pures.
+      rewrite /S_g.
+      iPoseProof (ec_split _ _ with "Hε") as "(_ & Hε)".
+      { apply Rmult_le_pos; [apply Iverson_nonneg | apply Hnn ]. }
+      { apply Rmult_le_pos; [apply Iverson_nonneg | apply S_nn_1; auto ]. }
+      rewrite Iverson_True; [rewrite Rmult_1_l|lra].
+      wp_bind (Bii _ _).
+      iApply (pgl_wp_mono_frame (□ _ ∗ _  ∗ _)%I with "[Hx Hε ] [IH Hy Hz]"); last first.
+      { iSplitR; first iExact "IH". iSplitL "Hy"; first iExact "Hy". iExact "Hz". }
+      { iApply (@wp_Bii _ (S_hz F k x N z)); last iFrame.
+        iIntros (?). apply S_hz_nn; auto. }
+      iIntros (bv) "((#IH & Hy & Hz) & [%b [-> [Hε Hx]]])".
+      destruct b.
+      { wp_pures.
+        iModIntro; iExists N; iFrame; iSplitR; first done.
+        rewrite /S_hz.
+        iPoseProof (ec_split _ _ with "Hε") as "(Hε & _)".
+        { apply Rmult_le_pos; [apply Iverson_nonneg | apply Hnn ]. }
+        { apply Rmult_le_pos; [apply Iverson_nonneg | apply S_CreditV_nn; auto ]. }
+        rewrite Iverson_True; [rewrite Rmult_1_l|intuition].
+        done.
+      }
+      { do 2 wp_pure.
+        rewrite -Nat2Z.inj_add.
+        iApply "IH".
+        iFrame.
+        rewrite /S_hz.
+        iPoseProof (ec_split _ _ with "Hε") as "(_ & Hε)".
+        { apply Rmult_le_pos; [apply Iverson_nonneg | apply Hnn ]. }
+        { apply Rmult_le_pos; [apply Iverson_nonneg | apply S_CreditV_nn; auto ]. }
+        rewrite Iverson_True; [rewrite Rmult_1_l|intuition].
+        done.
+      }
+    }
+  Qed.
+
+  Theorem wp_B {E F} (k : nat) xα x (Hnn : ∀ r, 0 <= F r) :
+    ↯(B_CreditV F k x) ∗ lazy_real xα x  -∗
+    WP B #k xα @ E {{ vb, ∃ b : bool, ⌜vb = #b ⌝ ∗ ↯(F b) ∗ lazy_real xα x }}.
+  Proof.
+    iIntros "(Hε & Hx)".
+    rewrite /B.
+    wp_pures.
+    wp_bind (S _ _ _ _).
+    iApply (pgl_wp_mono with "[Hx Hε] "); last first.
+    { iApply wp_S.
+      (* TODO: Credit distribution for B *)
+      Unshelve.
+
+      (* TODO: FIXME: lazy_real being duplicated is a problem!
+         We could add a wrapper function that does the 0th iterate, and only calls S
+         once a fresh y is allocated.
+
+         Note that x must be in the precondition and postcondition of this lemma, but
+         y can be thrown away. *)
+    (* iIntros (v) "[%n [-> [Hε Hx]]]". *)
+  Admitted.
 
 End program.
