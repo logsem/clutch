47a0434fa6f6817a03265e381b91941c4f14830e
Markus de Medeiros <markusdemedeiros@outlook.com>
2025-11-23 13:32:26 -0500

    checkpoint foob2

--- theories/eris/examples/foob2.v
+++ theories/eris/examples/foob2.v
@@ -91,6 +91,126 @@ Definition Darboux_sup_seq (f : R → R) L : Darboux_seq :=
 Definition Darboux_inf_seq (f : R → R) L : Darboux_seq :=
   SF_seq_f2 (fun x y => real $ Inf_fct f x y)  L.
 
+(* Darboux sums *)
+Definition Darboux_sum (ptd : Darboux_seq) : R :=
+  seq.foldr plus zero (seq.pairmap (fun x y => scal (fst y - fst x) (snd y)) (SF_h ptd, zero) (SF_t ptd)).
+
+Lemma Riemann_pairmap_junk (x x' : R) {L} {ptd : @SF_seq R} :
+  (seq.pairmap (λ x y : R * R, scal (y.1 - x.1) (y.2)) (SF_h ptd, x) L) =
+  (seq.pairmap (λ x y : R * R, scal (y.1 - x.1) (y.2)) (SF_h ptd, x') L).
+Proof. induction L; rewrite //=. Qed.
+
+
+(* Lower sum for a list a :: L *)
+Definition DarbouxLowerSum (f : R → R) (a b : R) (L : list R) : R :=
+  Darboux_sum (Darboux_inf_seq f (a :: L ++ (cons b nil))).
+
+(* Upper sum for a list a :: L *)
+Definition DarbouxUpperSum (f : R → R) (a b : R) (L : list R) : R :=
+  Darboux_sum (Darboux_sup_seq f (a :: L ++ (cons b nil))).
+
+(* Lower sums <= Upper sums *)
+Theorem DarbouxSum_Lower_Upper {f a b} (L : list R) (Hwf : Part_wf a b L) (HB : Bounded f a b) :
+  DarbouxLowerSum f a b L <= DarbouxUpperSum f a b L.
+Proof.
+  rewrite /DarbouxLowerSum /DarbouxUpperSum /Darboux_sum /Darboux_inf_seq /Darboux_sup_seq /SF_seq_f2 //=.
+  destruct Hwf as [Hord].
+  induction L as [| x L IHL] in a, b, Hord, HB |-*.
+  - simpl.
+    admit. (* Base case: single interval [a,b] *)
+  - simpl.
+    assert (Hjunk1: seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
+      (x, real (Inf_fct f a x)) (seq.pairmap (λ x0 y : R, (y, real (Inf_fct f x0 y))) x (L ++ cons b nil))) =
+      seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
+      (x, zero) (seq.pairmap (λ x0 y : R, (y, real (Inf_fct f x0 y))) x (L ++ cons b nil)))).
+    { induction L; simpl; auto. }
+    assert (Hjunk2: seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
+      (x, real (Sup_fct f a x)) (seq.pairmap (λ x0 y : R, (y, real (Sup_fct f x0 y))) x (L ++ cons b nil))) =
+      seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
+      (x, zero) (seq.pairmap (λ x0 y : R, (y, real (Sup_fct f x0 y))) x (L ++ cons b nil)))).
+    { induction L; simpl; auto. }
+    rewrite Hjunk1 Hjunk2.
+    apply Rplus_le_compat.
+    + admit. (* Need: Inf_fct f a x <= Sup_fct f a x and scal preserves inequality *)
+    + apply: IHL.
+      * admit. (* Need to extract sorted (x :: L ++ [b]) from sorted (a :: x :: L ++ [b]) *)
+      * admit. (* bounded *)
+Admitted.
+
+Theorem DarbouxSum_Lower_MonoFun {f g a b} (L : list R) (Hwf : Part_wf a b L) (HBf : Bounded f a b)
+  (HBg : Bounded g a b) (Hfg : FunLe f g a b) : DarbouxLowerSum f a b L <= DarbouxLowerSum g a b L.
+Proof.
+  rewrite /DarbouxLowerSum /Darboux_sum /Darboux_inf_seq /SF_seq_f2 //=.
+  destruct Hwf as [Hord].
+  induction L as [| x L IHL] in a, b, Hord, HBf, HBg, Hfg |-*.
+  - simpl. admit. (* Base case: f ≤ g implies Inf_fct f a b ≤ Inf_fct g a b *)
+  - simpl.
+    assert (Hjunk1: seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
+      (x, real (Inf_fct f a x)) (seq.pairmap (λ x0 y : R, (y, real (Inf_fct f x0 y))) x (L ++ cons b nil))) =
+      seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
+      (x, zero) (seq.pairmap (λ x0 y : R, (y, real (Inf_fct f x0 y))) x (L ++ cons b nil)))).
+    { induction L; simpl; auto. }
+    assert (Hjunk2: seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
+      (x, real (Inf_fct g a x)) (seq.pairmap (λ x0 y : R, (y, real (Inf_fct g x0 y))) x (L ++ cons b nil))) =
+      seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
+      (x, zero) (seq.pairmap (λ x0 y : R, (y, real (Inf_fct g x0 y))) x (L ++ cons b nil)))).
+    { induction L; simpl; auto. }
+    rewrite Hjunk1 Hjunk2.
+    apply Rplus_le_compat.
+    + admit. (* Need: Inf_fct_mono for f ≤ g and scal preserves inequality *)
+    + apply: IHL.
+      * admit. (* sorted (x :: L ++ [b]) from sorted (a :: x :: L ++ [b]) *)
+      * admit. (* Bounded f x b from Bounded f a b *)
+      * admit. (* Bounded g x b from Bounded g a b *)
+      * admit. (* FunLe f g x b from FunLe f g a b *)
+Admitted.
+
+Theorem DarbouxSum_Upper_MonoFun {f g a b} (L : list R) (Hwf : Part_wf a b L) (HBf : Bounded f a b)
+  (HBg : Bounded g a b) (Hfg : FunLe f g a b) : DarbouxUpperSum f a b L <= DarbouxUpperSum g a b L.
+Proof. (* Similar to DarbouxSum_Lower_MonoFun *) Admitted.
+
+End Darboux_sum.
+
+(** 1D Darboux Integrals *)
+
+Section DarbouxInt1.
+
+Definition DarbouxInt_Lower (f : R → R) (a b : R) : Rbar :=
+  Glb_Rbar (fun (IV : R) => exists L : list R, Part_wf a b L ∧ IV = DarbouxLowerSum f a b L).
+
+Definition DarbouxInt_Upper (f : R → R) (a b : R) : Rbar :=
+  Lub_Rbar (fun (IV : R) => exists L : list R, Part_wf a b L ∧ IV = DarbouxUpperSum f a b L).
+
+Definition DarbouxIntegrable (f : R → R) (a b : R) : Prop :=
+  DarbouxInt_Lower f a b = DarbouxInt_Upper f a b.
+
+(* If the function is bounded, then the lower interal is finite. This allows conversion to a real number. *)
+Theorem DarbouxInt_Lower_Finite {f a b} (HB : Bounded f a b) : is_finite (DarbouxInt_Lower f a b).
+Proof. Admitted.
+
+(* If the function is bounded, then the upper interal is finite. This allows conversion to a real number. *)
+Theorem DarbouxInt_Upper_Finite {f a b} (HB : Bounded f a b) : is_finite (DarbouxInt_Upper f a b).
+Proof. Admitted.
+
+Theorem DarbouxInt_Lower_Upper {f a b} (HB : Bounded f a b) :
+  DarbouxInt_Lower f a b <= DarbouxInt_Upper f a b.
+Proof. (* Pointwise bound, plus existence to strip the real *) Admitted.
+
+Theorem DarbouxInt_Lower_MonoFun {f g a b} (HBf : Bounded f a b) (HBg : Bounded g a b)
+  (Hfle : FunLe f g a b) : DarbouxInt_Lower f a b <= DarbouxInt_Lower g a b.
+Proof. Admitted.
+
+Theorem DarbouxInt_Upper_MonoFun {f g a b} (HBf : Bounded f a b) (HBg : Bounded g a b)
+  (Hfle : FunLe f g a b) : DarbouxInt_Upper f a b <= DarbouxInt_Upper g a b.
+Proof. Admitted.
+
+End DarbouxInt1.
+
+
+(** Riemann Integrability compat *)
+
+Section DarbouxRiemann.
+
 (* Compat: a Darboux sequence that is pointwise greater than the given step function P *)
 Definition Darboux_raise_seq (f : R → R) (P : @SF_seq R) :=
   Darboux_sup_seq f (SF_lx P).
@@ -103,17 +223,8 @@ Definition Darboux_lower_seq (f : R → R) (P : @SF_seq R) :=
 Definition Darboux_seq_of_Riemann (f : R → R) (ptd : @SF_seq R) : Darboux_seq :=
   SF_map f ptd.
 
-(* Darboux sums *)
-Definition Darboux_sum (ptd : Darboux_seq) : R :=
-  seq.foldr plus zero (seq.pairmap (fun x y => scal (fst y - fst x) (snd y)) (SF_h ptd, zero) (SF_t ptd)).
-
-Lemma Riemann_pairmap_junk (x x' : R) {L} {ptd : @SF_seq R} :
-  (seq.pairmap (λ x y : R * R, scal (y.1 - x.1) (y.2)) (SF_h ptd, x) L) =
-  (seq.pairmap (λ x y : R * R, scal (y.1 - x.1) (y.2)) (SF_h ptd, x') L).
-Proof. induction L; rewrite //=. Qed.
-
 (* Riemann sums can be converted into Darboux sums *)
-Lemma Darboux_Riemann_compat (f : R → R) (ptd : @SF_seq R) :
+Lemma Darboux_Riemann_sum_compat (f : R → R) (ptd : @SF_seq R) :
    Riemann_sum f ptd = Darboux_sum (Darboux_seq_of_Riemann f ptd).
 Proof.
   rewrite /Riemann_sum /Darboux_sum /Darboux_seq_of_Riemann //=.
@@ -138,67 +249,60 @@ Proof.
   by rewrite X.
 Qed.
 
-(* Lower sum for a list a :: L *)
-Definition DarbouxLowerSum (f : R → R) (a b : R) (L : list R) : R :=
-  Darboux_sum (Darboux_inf_seq f (a :: L ++ (cons b nil))).
-
-(* Upper sum for a list a :: L *)
-Definition DarbouxUpperSum (f : R → R) (a b : R) (L : list R) : R :=
-  Darboux_sum (Darboux_sup_seq f (a :: L ++ (cons b nil))).
-
-(* Lower sums <= Upper sums *)
-Theorem DarbouxSum_Lower_Upper {f a b} (L : list R) (Hwf : Part_wf a b L) (HB : Bounded f a b) :
-  DarbouxLowerSum f a b L <= DarbouxUpperSum f a b L.
+(*
+Lemma filterlim_real_to_Rbar {T : Type} (f : T → R) (F : (T → Prop) → Prop) (r : R) :
+  filterlim (fun t => Finite $ f t) F (Rbar_locally (Finite r)) →
+  filterlim f F (locally r).
 Proof.
-  (*
-  rewrite /DarbouxLowerSum /DarbouxUpperSum /Darboux_sum /Darboux_inf_seq /Darboux_sup_seq /SF_seq_f2 //=.
-  destruct Hwf as [Hord Hright].
-  induction L as [| x L IHL] in a, b, Hord, HB |-*.
-  - simpl. lra.
-  - simpl.
-    assert (Hjunk1: seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
-      (x, real (Inf_fct f a x)) (seq.pairmap (λ x0 y : R, (y, real (Inf_fct f x0 y))) x L)) =
-      seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
-      (x, zero) (seq.pairmap (λ x0 y : R, (y, real (Inf_fct f x0 y))) x L))).
-    { induction L; simpl; auto. }
-    assert (Hjunk2: seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
-      (x, real (Sup_fct f a x)) (seq.pairmap (λ x0 y : R, (y, real (Sup_fct f x0 y))) x L)) =
-      seq.foldr plus zero (seq.pairmap (λ x0 y : R * R, scal (y.1 - x0.1) y.2)
-      (x, zero) (seq.pairmap (λ x0 y : R, (y, real (Sup_fct f x0 y))) x L))).
-    { induction L; simpl; auto. }
-    rewrite Hjunk1 Hjunk2.
-    apply Rplus_le_compat.
-    + admit. (* Need: Inf_fct f a x <= Sup_fct f a x and scal preserves inequality *)
-    + apply: IHL.
-      * rewrite //= in Hord.
-        destruct L; simpl; [done | ].
-        admit.
-      * admit.
-  *)
+  intros ?.
+A dmitted.
+*)
+
+(* TODO: Need special case for a = b, where the Riemann integral becomes 0 *)
+Lemma Darboux_Riemann {f : R → R} {a b : R} :
+  a < b →
+  Bounded f a b →
+  DarbouxIntegrable f a b →
+  is_RInt f a b (real $ DarbouxInt_Lower f a b).
+Proof.
+  intros Hab HB HD.
+  rewrite /is_RInt.
+
+  (* Simplify the sign expression *)
+  rewrite Rcomplements.sign_eq_1; [|lra].
+  rewrite /scal//= /mult//=.
+  replace (fun ptd : @SF_seq R => Rmult (IZR (Zpos xH)) (@Riemann_sum (NormedModule.ModuleSpace R_AbsRing R_NormedModule) f ptd))
+     with (fun ptd : @SF_seq R => (@Riemann_sum (NormedModule.ModuleSpace R_AbsRing R_NormedModule) f ptd)).
+  2: { apply functional_extensionality. intros ?. lra. }
+
+  (* Apply the squeeze theorem *)
+  suffices Hsqueeze :
+    filterlim (fun ptd : SF_seq => Darboux_sum (Darboux_lower_seq f ptd)) (Riemann_fine a b) (Rbar_locally (DarbouxInt_Lower f a b)) ∧
+    filterlim (fun ptd : SF_seq => Darboux_sum (Darboux_raise_seq f ptd)) (Riemann_fine a b) (Rbar_locally (DarbouxInt_Lower f a b)).
+  { destruct Hsqueeze as [H1 H2].
+    have HsqueezeF : Riemann_fine a b (λ x : SF_seq, Darboux_sum (Darboux_lower_seq f x) <= Riemann_sum f x <= Darboux_sum (Darboux_raise_seq f x)).
+    { (* Inequality between functions *) admit. }
+    have HF : Filter (Riemann_fine a b).
+    { apply filter_filter. }
+    have X :=
+      @Lim_seq.filterlim_le_le _ (Riemann_fine a b) (filter_filter)
+        (λ ptd : SF_seq, Darboux_sum (Darboux_lower_seq f ptd))
+        (λ ptd : SF_seq, Riemann_sum f ptd)
+        (λ ptd : SF_seq, Darboux_sum (Darboux_raise_seq f ptd))
+        (DarbouxInt_Lower f a b)
+        HsqueezeF
+        H1 H2.
+    (* Prove that the Finite Rbar and R filters are equal *)
+    admit.
+  }
+  (* Now we need to connect the filterlims to the infima/suprema *)
+  split.
+  { admit. }
+  { admit. }
 Admitted.
 
-Theorem DarbouxSum_Lower_MonoFun {f g a b} (L : list R) (Hwf : Part_wf a b L) (HBf : Bounded f a b)
-  (HBg : Bounded g a b) (Hfg : FunLe f g a b) : DarbouxLowerSum f a b L <= DarbouxLowerSum g a b L.
-Proof. Admitted.
+End DarbouxRiemann.
 
-Theorem DarbouxSum_Upper_MonoFun {f g a b} (L : list R) (Hwf : Part_wf a b L) (HBf : Bounded f a b)
-  (HBg : Bounded g a b) (Hfg : FunLe f g a b) : DarbouxUpperSum f a b L <= DarbouxUpperSum g a b L.
-Proof. Admitted.
-
-End Darboux_sum.
-
-(** 1D Darboux Integrals *)
-
-Section DarbouxInt1.
-
-Definition DarbouxInt_Lower (f : R → R) (a b : R) : R :=
-  real $ Glb_Rbar (fun (IV : R) => exists L : list R, Part_wf a b L ∧ IV = DarbouxLowerSum f a b L).
-
-Definition DarbouxInt_Upper (f : R → R) (a b : R) : R :=
-  real $ Lub_Rbar (fun (IV : R) => exists L : list R, Part_wf a b L ∧ IV = DarbouxUpperSum f a b L).
-
-
-End DarbouxInt1.
 
 (** 2D Darboux Sums *)
 
@@ -250,13 +354,3 @@ Ineuqality between lower and upper sums
 
 I can also define the 2D Darboux lower and upper sums, similar lemmas
 *)
-
-
-
-
-
-
-
-
-
-
