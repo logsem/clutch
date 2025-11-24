0b2c39e8769c7d346917919c67d1901c2e949d25
Hei Li <hei411@gmail.com>
2025-11-24 15:24:26 +0100

    Well formness and support_set functions

--- theories/elton/adequacy.v
+++ theories/elton/adequacy.v
@@ -12,19 +12,4 @@ From clutch.elton Require Import weakestpre primitive_laws.
 From clutch.prob Require Import distribution.
 Import uPred.
 
-Fixpoint distr_urns_f (m:list(loc*urn)) : distr (gmap loc nat) :=
-  match m with
-  | [] => dret ∅
-  | (k, u) :: rest =>
-      let l:=elements u in
-      if bool_decide (l=[]) then distr_urns_f rest
-      else let x := distr_urns_f rest in
-           x ≫= (λ m, dunifP (size u+1)%nat ≫=
-                        (λ n, match l!!(fin_to_nat n) with
-                              | Some num =>dret (<[k:=num]> m)
-                              | None => dzero (* impossible *)
-                              end
-             ))
-  end.
-
 
