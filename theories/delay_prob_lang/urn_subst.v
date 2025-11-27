1cf3a96b4f3379f079e094ef94b2eb299329293c
Hei Li <hei411@gmail.com>
2025-11-27 13:55:47 +0100

    Add file for rupd and urn_subst lemmas

---
+++ theories/delay_prob_lang/urn_subst.v
@@ -0,0 +1,41 @@
+From Coq Require Import Reals Psatz.
+From stdpp Require Import functions gmap stringmap fin_sets.
+From clutch.prelude Require Import stdpp_ext NNRbar fin uniform_list.
+From clutch.delay_prob_lang Require Import tactics notation.
+From clutch.delay_prob_lang Require Export lang.
+From iris.prelude Require Import options.
+Set Default Proof Using "Type*".
+
+(* In lang.v, we defined functions and lemmas for substituing for baselits, 
+   now we do it for expressions and values*)
+
+Fixpoint urn_subst_expr (f: gmap loc nat) (e : expr) : option expr :=
+  match e with
+  | Val v => v' ← urn_subst_val f v; Some (Val v') 
+| Var x => Some $ Var x
+| Rec f' x e => e' ← urn_subst_expr f e; Some (Rec f' x e')
+| App e1 e2 => e1' ← urn_subst_expr f e1; e2' ← urn_subst_expr f e2; Some (App e1' e2')
+| UnOp op e => e' ← urn_subst_expr f e; Some (UnOp op e')
+| BinOp op e1 e2 => e1' ← urn_subst_expr f e1; e2' ← urn_subst_expr f e2; Some (BinOp op e1' e2')
+| If e0 e1 e2 => e0' ← urn_subst_expr f e0; e1' ← urn_subst_expr f e1; e2' ← urn_subst_expr f e2; Some (If e0' e1' e2')
+| Pair e1 e2 => e1' ← urn_subst_expr f e1; e2' ← urn_subst_expr f e2; Some (Pair e1' e2')
+| Fst e => e' ← urn_subst_expr f e; Some (Fst e')
+| Snd e => e' ← urn_subst_expr f e; Some (Snd e')
+| InjL e => e' ← urn_subst_expr f e; Some (InjL e')
+| InjR e => e' ← urn_subst_expr f e; Some (InjR e')
+| Case e0 e1 e2 => e0' ← urn_subst_expr f e0; e1' ← urn_subst_expr f e1; e2' ← urn_subst_expr f e2; Some (Case e0' e1' e2')
+| AllocN e1 e2 => e1' ← urn_subst_expr f e1; e2' ← urn_subst_expr f e2; Some (AllocN e1' e2')
+| Load e => e' ← urn_subst_expr f e; Some (Load e')
+| Store e1 e2 => e1' ← urn_subst_expr f e1; e2' ← urn_subst_expr f e2; Some (Store e1' e2')
+| Rand e => e' ← urn_subst_expr f e; Some (Rand e')
+| DRand e => e' ← urn_subst_expr f e; Some (DRand e')
+end
+with urn_subst_val f v : option val :=
+       match v with
+       | LitV l => l' ← urn_subst f l; Some $ LitV l'
+| RecV f' x e => e' ← urn_subst_expr f e; Some $ RecV f' x e'
+| PairV v1 v2 => v1' ← urn_subst_val f v1; v2' ← urn_subst_val f v2; Some $ PairV v1' v2'
+| InjLV v =>  v' ← urn_subst_val f v; Some $ InjLV v'
+| InjRV v => v' ← urn_subst_val f v; Some $ InjRV v'
+end
+.
---
+++ theories/elton/rupd.v
@@ -0,0 +1,21 @@
+From iris.base_logic.lib Require Export fancy_updates invariants.
+From clutch.prelude Require Import iris_ext.
+From iris.proofmode Require Import base tactics classes.
+From clutch.delay_prob_lang Require Import lang urn_subst.
+From clutch.elton Require Import weakestpre primitive_laws.
+
+Section rupd.
+  Context `{H:!eltonGS Σ}.
+  (* Do we need to open invariants? *)
+  Definition rupd_def (P:val -> Prop) v:=
+    (∀ σ1, state_interp σ1 -∗
+           ⌜∀ f, urns_f_valid (urns σ1) f ->
+                ∃ v', urn_subst_val f v = Some v' /\ P v'⌝).
+  
+  Local Definition rupd_aux : seal (@rupd_def). Proof. by eexists. Qed.
+  Definition rupd := rupd_aux.(unseal).
+  Lemma rupd_unseal : rupd = rupd_def.
+  Proof. rewrite -rupd_aux.(seal_eq) //. Qed.
+
+  
+End rupd. 
