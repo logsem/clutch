3c6ff63e3df844e5ccf5b89714c1d300185eb323
Markus de Medeiros <markusdemedeiros@outlook.com>
2025-12-14 21:27:12 -0500

    set doc

--- theories/eris/examples/math/sets.v
+++ theories/eris/examples/math/sets.v
@@ -4,29 +4,35 @@ Import Hierarchy.
 Set Default Proof Using "Type*".
 #[local] Open Scope R.
 
-(** Basic definitions for some sets *)
-
+(** Open open interval *)
 Definition Ioo (a b : R) : R → Prop :=
   fun x => Rmin a b < x < Rmax a b.
 
+(** Closed closed interval *)
 Definition Icc (a b : R) : R -> Prop :=
   fun t => Rmin a b <= t <= Rmax a b.
 
+(** Closed infinite interval *)
 Definition Ici (a : R) : R -> Prop :=
   fun t => a <= t.
 
+(** Infinite closed interval *)
 Definition Iic (b : R) : R -> Prop :=
   fun t => t <= b.
 
+(** Open infinite interval *)
 Definition Ioi (a : R) : R -> Prop :=
   fun t => a < t.
 
+(** Infinite open interval *)
 Definition Iio (b : R) : R -> Prop :=
   fun t => t < b.
 
+(** Infinite Infinite interval *)
 Definition Iii : R -> Prop :=
   fun t => True.
 
+(** Cartesian product of sets *)
 Definition RII (X Y : R -> Prop) : R * R -> Prop :=
   fun '(tx, ty) => X tx /\ Y ty.
 
@@ -39,5 +45,6 @@ Definition Int {T} (S U : T -> Prop) : T -> Prop :=
 Definition Bounded (f : R * R -> R) (M : R) : R * R -> Prop :=
   fun t => Rabs (f t) <= M.
 
+(** Inclusion in (xa, xb) implies inclusion in [xa, xb] *)
 Lemma Ioo_Icc {xa xb x} : Ioo xa xb x → Icc xa xb x.
 Proof. rewrite /Ioo/Icc//=. lra. Qed.
