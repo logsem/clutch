40756d0f261e8385bf6153c2ec7bce40234489d0
Philipp G. Haselwarter <philipp@haselwarter.org>
2026-01-09 11:15:02 +0100

    qs nits

--- theories/eris/tutorial/quicksort.v
+++ theories/eris/tutorial/quicksort.v
@@ -6,6 +6,8 @@ From clutch.eris Require Import eris.
 From clutch.eris.lib Require Import list.
 Set Default Proof Using "Type*".
 
+Local Notation sorted := (StronglySorted Z.le).
+
 Section quicksort.
 
   Context `{!erisGS Σ}.
@@ -27,7 +29,6 @@ Section quicksort.
         let, ("les", "gts") := ("qs" "le", "qs" "gt") in
         list_append "les" (list_cons "p"  "gts").
 
-
   Lemma filter_split_perm {A} (l : list A) f :
     l ≡ₚ List.filter f l ++ List.filter (fun x=>negb (f x)) l.
   Proof.
@@ -35,73 +36,62 @@ Section quicksort.
     destruct (f a) => /= ; rewrite -?Permutation_middle -IHl //.
   Qed.
 
-  Lemma Partition (xs : list Z) l (e : Z) :
-    {{{ ⌜is_list xs l⌝ }}}
-      partition l (Val #e)
-    {{{ le gt, RET (le, gt);
-        ∃ xsle xsgt : list Z,
-          ⌜is_list xsle le ∧ is_list xsgt gt
-          ∧ app xsle xsgt ≡ₚ xs ⌝
-          ∧ ⌜ ∀ x, In x xsle → (x ≤ e)%Z ⌝
-                   ∧ ⌜ ∀ x, In x xsgt → (e < x)%Z ⌝
-    }}}.
-  Proof.
-    iIntros (φ Lxs) "hφ".
-    rewrite /partition. subst.
-    wp_pures.
-    wp_bind (list_filter _ _).
-    iApply (wp_list_filter _ (λ x, bool_decide (e < x)%Z)).
-    { iSplit => //. iIntros (x ψ) "_ !> hψ".
-      simpl. wp_pures. iApply "hψ". eauto. }
-    iIntros "!>" (gt egt). wp_pures.
-    wp_bind (list_filter _ _).
-    iApply (wp_list_filter _ (λ x, negb $ bool_decide (e < x)%Z)).
-    { iSplit => //. iIntros (x ψ) "_ !> hψ".
-      simpl. wp_pures.
-      case_bool_decide ; wp_pures ; by iApply "hψ". }
-    iIntros "!>" (le ele).
-    wp_pures. iApply "hφ". iPureIntro.
-    set (xs_gt := (List.filter (λ x : Z, bool_decide (e < x)%Z) xs)) in *.
-    set (xs_le := (List.filter (λ x : Z, negb $ bool_decide (e < x)%Z) xs)) in *.
-    exists xs_le, xs_gt. intuition auto.
-    2:{ epose proof (filter_In _ x xs) as [h _]. destruct (h H) as [_ hle].
-        destruct (bool_decide (e < x)%Z) eqn:hlt => //.
-        apply bool_decide_eq_false in hlt.
-        apply Z.nlt_ge. done.
-    }
-    2:{ epose proof (filter_In _ x xs) as [h _]. destruct (h H) as [_ hgt].
-        by apply bool_decide_eq_true in hgt.
-    }
-    epose proof (filter_split_perm xs _) as xs_split.
-    symmetry. subst xs_le xs_gt.
-    rewrite Permutation_app_comm. apply xs_split.
-  Qed.
-
-  Local Notation sorted := (StronglySorted Z.le).
-
   Fact sorted_append pre post p :
     sorted pre → sorted post →
     (∀ x, In x pre → (x <= p)%Z) → (∀ x, In x post → (p < x)%Z) →
     sorted (pre ++ p :: post).
   Proof.
-    intros Spre Spost ppre ppost.
-    induction pre => /=.
-    - apply SSorted_cons, List.Forall_forall => // ; intros.
-      by apply Z.lt_le_incl, ppost.
-    - apply SSorted_cons, List.Forall_forall => // ; [| clear IHpre].
-      { apply IHpre.
-        * apply StronglySorted_inv in Spre. by destruct Spre.
-        * intros. apply ppre. set_solver. }
-      intros x xppp.
-      destruct (in_app_or _ _ _ xppp) as [x_pre | x_post] ; clear xppp.
-      + apply StronglySorted_inv in Spre. destruct Spre.
-        by apply (List.Forall_forall _ pre).
-      + assert (a ≤ p)%Z as ap by (apply ppre ; constructor => //).
-        assert (∀ z, In z (p::post) -> (p ≤ z)%Z) as ppost' ; last first.
-        { etrans => //. apply ppost' => //. }
-        intros z hz.
-        destruct (in_inv hz) as [-> | zpost] => //.
+    intros Spre Spost ppre ppost. apply StronglySorted_app. repeat split ; auto.
+    - intros x1 x2 x1pre x2post. trans p.
+      + apply ppre, elem_of_list_In ; auto.
+      + apply elem_of_list_In in x2post.
+        destruct (in_inv x2post) as [-> | zpost] => //.
         apply Z.lt_le_incl, ppost => //.
+    - apply SSorted_cons, List.Forall_forall => // ; intros.
+      apply Z.lt_le_incl, ppost. assumption.
+  Qed.
+
+  Lemma Partition (xs : list Z) (l_xs : val) (e : Z) :
+    {{{ ⌜is_list xs l_xs⌝ }}}
+      partition l_xs (Val #e)
+    {{{ le gt, RET (le, gt);
+        ∃ xsle xsgt : list Z,
+          ⌜is_list xsle le ∧ is_list xsgt gt
+          ∧ xsle ++ xsgt ≡ₚ xs ⌝
+          ∧ ⌜ ∀ x, In x xsle → (x ≤ e)%Z ⌝
+                   ∧ ⌜ ∀ x, In x xsgt → (e < x)%Z ⌝
+    }}}.
+  Proof.
+    iIntros (post Lxs) "Hpost".
+    unfold partition.
+    wp_pures.
+    (* filter the xs greater than e *)
+    wp_apply (wp_list_filter xs (λ x, bool_decide (e < x)%Z)).
+    { iSplit ; auto. iIntros (x ψ) "_ !> hψ".
+      simpl. wp_pures. iApply "hψ". eauto. }
+    iIntros (l_xs_gt H_xs_gt). wp_pures.
+    (* filter the xs smaller/equal to e *)
+    wp_apply (wp_list_filter _ (λ x, negb $ bool_decide (e < x)%Z)).
+    { iSplit => //. iIntros (x ψ) "_ !> hψ".
+      simpl. wp_pures.
+      case_bool_decide ; wp_pures ; by iApply "hψ". }
+    iIntros (l_xs_le H_xs_le).
+    (* prove the postcondition from the facts we know about xs_le and xs_gt *)
+    wp_pures. iApply "Hpost". iPureIntro.
+    set (xs_gt := (List.filter (λ x : Z, bool_decide (e < x)%Z) xs)) in *.
+    set (xs_le := (List.filter (λ x : Z, negb $ bool_decide (e < x)%Z) xs)) in *.
+    exists xs_le, xs_gt.
+    repeat split ; auto.
+    (* the result is a permutation of the input *)
+    - rewrite Permutation_app_comm. rewrite -filter_split_perm. reflexivity.
+    (* xs_le are indeed smaller than e *)
+    - intros x H. epose proof (filter_In _ x xs) as [h _]. destruct (h H) as [_ hle].
+      destruct (bool_decide (e < x)%Z) eqn:hlt => //.
+      apply bool_decide_eq_false in hlt.
+      apply Z.nlt_ge. done.
+    (* xs_gt are indeed larger than e *)
+    - intros x H. epose proof (filter_In _ x xs) as [h _]. destruct (h H) as [_ hgt].
+      by apply bool_decide_eq_true in hgt.
   Qed.
 
   Lemma qs_sorted : ∀ (xs : list Z) (l : val),
@@ -132,20 +122,20 @@ Section quicksort.
     repeat (wp_pure ; unfold partition ; progress fold partition).
     (* partition xs \ p into smaller and larger elements *)
     wp_apply Partition => //.
-    iIntros (le gt (xsle & xsgt & (hle & hgt & hperm) & ple & pgt)). wp_pures.
+    iIntros (le gt (xs_le & xs_gt & (hle & hgt & hperm) & ple & pgt)). wp_pures.
     (* sort xs_gt *)
-    wp_apply "Hqs" => //. iIntros (gts (xs_gt_s & Lgts & Pgts & Sgts)).
+    wp_apply "Hqs" => //. iIntros (gts (xs_gt_sorted & Lgts & Pgts & Sgts)).
     (* sort xs_le *)
-    wp_apply "Hqs" => //. iIntros (les (xs_le_s & Lles & Ples & Sles)). wp_pures.
+    wp_apply "Hqs" => //. iIntros (les (xs_le_sorted & Lles & Ples & Sles)). wp_pures.
     (* re-assemble the sorted results *)
     replace (#p) with (inject p) by auto.
-    wp_apply wp_list_cons => //. iIntros (p_xs_gt_s h_p_xs_gt).
+    wp_apply wp_list_cons => //. iIntros (p_xs_gt_sorted h_p_xs_gt).
     iApply wp_list_append => //. iIntros "!>" (xs_le_p_gt_s L).
     iApply "hφ".
-    iExists (xs_le_s ++ p :: xs_gt_s). iPureIntro. repeat split => //.
+    iExists (xs_le_sorted ++ p :: xs_gt_sorted). iPureIntro. repeat split => //.
     - clear -Ples Pgts hperm hpart.
       rewrite Pgts Ples. rewrite -Permutation_middle.
-      rewrite hperm. rewrite Permutation_middle. rewrite -hpart. done.
+      rewrite hperm. rewrite Permutation_middle. rewrite -hpart. reflexivity.
     - clear -Sles Sgts ple pgt Ples Pgts. apply sorted_append => // ; intros.
       + apply ple. eapply Permutation_in => //.
       + apply pgt. eapply Permutation_in => //.
