5a260104f469ffe6b6f0bb5354db4af8bc6e5b74
Philipp G. Haselwarter <philipp@haselwarter.org>
2026-01-23 10:39:23 +0100

    nits

--- theories/eris/tutorial/basic_eris.v
+++ theories/eris/tutorial/basic_eris.v
@@ -23,44 +23,50 @@ Section eris_introduction.
       1. Error credits [↯ ε], and
       2. Hoares triple [{{{ P }}} e {{{ v, Q v }}}].
 
-      that we will use to reason about said programs. *)
+      that we will use to reason about said programs. xx *)
 
   (** An error credit is separation logic resource, written [↯ ε], where [ε] is
-      (non-negative) real number. Error credits can be split additively. *)
+      (non-negative) real number. Error credits can be split additively. xx *)
   Lemma error_credit_split :
     ↯ (1/4 + 1/4) ⊢ ↯ (1/4) ∗ ↯ (1/4).
   Proof.
     iIntros "H".
     (** The [ec_split] lemma tells us that [↯ (n + m)] can be split into [↯ n]
-        and [↯ m] as long as [0 <= n] and [0 <= m]. We apply it directly. *)
+        and [↯ m] as long as [0 <= n] and [0 <= m]. We apply it directly. xx *)
     iApply ec_split.
     { real_solver. }
     { real_solver. }
     iApply "H".
   Qed.
 
-  (** Similarly, error credits can be combined. *)
+  (** Similarly, error credits can be combined. xx *)
   Lemma error_credit_combine :
     ↯ (1/4) ∗ ↯ (1/4) ⊢ ↯ (1/2).
   Proof.
     iIntros "[H1 H2]".
+    (* xx *)
     iDestruct (ec_combine with "[H1 H2]") as "H".
-    { iFrame. }
+    { (* xx *)
+      iFrame. }
+    (* xx *)
     assert (1/4 + 1/4 = 1/2)%R as -> by real_solver.
     iApply "H".
   Qed.
 
-  (** Interestingly, if we own [↯ ε] and [1 <= ε] then we can prove [False]! *)
+  (** Interestingly, if we own [↯ ε] and [1 <= ε] then we can prove [False]! xx *)
   Lemma error_credit_False ε :
     (1 <= ε)%R →
     ↯ ε ⊢ False.
   Proof.
     intros Hr.
+    (* xx *)
     iApply ec_contradict.
     apply Hr.
   Qed.
 
-  (** If we can combine enough error credits to get above 1, any conclusion follows. *)
+  (** Exercise:
+      If we can combine enough error credits to get above 1, any conclusion follows.
+      xx *)
   Lemma error_credit_accumulate P Q :
     ↯ (2/3) ∗ (P -∗ ↯ (1/2)) -∗ P -∗ Q.
   Proof.
@@ -99,14 +105,13 @@ Section eris_introduction.
 
       means that probability of [e] terminating in a value [v] that *does not*
       satisfy [Q], is at most [ε]. This may seem somewhat counterintutive but
-      will be clearer in just a moment. *)
+      will be clearer in just a moment. xx *)
 
   (** The programming language [ProbLang] we consider in this tutorial has a
       single probabilistic connective [rand #N]. The expression [rand #N]
       evalautes uniformly at random to a value in the set [{0, 1, ..., N}]. For
       example, the expression [rand #1] corresponds to a coin flip, reducing
-      with probability [1/2] to either [0] or [1]. *)
-
+      with probability [1/2] to either [0] or [1]. xx *)
   Lemma wp_coin_flip :
     {{{ True }}} rand #1 {{{ (n : nat), RET #n; ⌜n = 0 ∨ n = 1⌝ }}}.
   Proof.
@@ -123,8 +128,9 @@ Section eris_introduction.
         in other proofs, because the post-condition does not have to
         syntactically match [Q]. Using this way of stating specifications, the
         consequence and framing rule is implicitly applied in the
-        post-condition. *)
+        post-condition. xx *)
     iIntros "%Φ HP HΦ".
+    (* xx *)
     wp_apply wp_rand.
     iIntros (n) "%Hn".
     iApply "HΦ".
@@ -134,11 +140,11 @@ Section eris_introduction.
 
   (** Let's try another example: The expression [rand #2] samples uniformly from
       the set [{0, 1, 2}]. The program therefore returns [false] with probability
-      [1/3], and [true] with probability [2/3]. *)
+      [1/3], and [true] with probability [2/3]. xx *)
   Definition unif_3_eq : expr :=
     if: rand #2 = #0 then #false else #true.
 
-  (** Let's write a spec using error credits that captures this idea. *)
+  (** Let's write a spec using error credits that captures this idea. xx *)
   Lemma wp_unif_3 :
     {{{ ↯ (1 / 3) }}} unif_3_eq {{{ (b : bool), RET #b; ⌜b = true⌝ }}}.
   Proof.
@@ -146,18 +152,18 @@ Section eris_introduction.
     unfold unif_3_eq.
     (** Here we apply [wp_rand_err] that allows us "spend" [1 / (N + 1)] error
         credits to avoid a concrete outcome in the range [0..N]. We choose [0]
-        to be the outcome we want to avoid. *)
+        to be the outcome we want to avoid. xx *)
     wp_apply (wp_rand_err 0 with "[Hε]").
     { iApply (ec_eq with "Hε"). simpl. real_solver. }
     iIntros (x) "[% %]".
     (** the [wp_pures] tactic progresses the proof by stepping through pure
-        evaluations steps such as equality tests *)
+        evaluations steps such as equality tests xx *)
     wp_pures.
     rewrite bool_decide_eq_false_2; last first.
     { inversion 1. nat_solver. }
     wp_pures.
     (** The update modality allows us to update ghost resources—we won't need
-        this here and will just introduce it using the [iModIntro] tactic. *)
+        this here and will just introduce it using the [iModIntro] tactic. xx *)
     iModIntro.
     iApply "HΦ".
     done.
@@ -179,12 +185,18 @@ Section eris_introduction.
   Proof.
     iIntros (Φ) "Hε HΦ".
     unfold twoflip.
+    (* xx *)
     set (F (n : nat) := if bool_decide (n = 1) then 0%R else (1/2)%R).
+    (* We perform the first sampling, and distribute ε according to F. xx *)
     wp_apply (wp_rand_exp F with "Hε").
     { intros n. unfold F. real_solver. }
-    { unfold F. simpl. real_solver. }
+    {
+      (* The errors distributed according to F are indeed bounded by our initial
+         error credit budget. *)
+      unfold F. simpl. real_solver. }
     iIntros (n) "[%Hn Hε]".
     wp_pures.
+    (* We observe the outcome of the first flip. *)
     case_bool_decide; simplify_eq.
     - wp_pures.
       iApply "HΦ".
@@ -192,12 +204,13 @@ Section eris_introduction.
     - unfold F.
       rewrite bool_decide_eq_false_2; last first.
       { intros ->. done. }
-      (** Now we have [1 / 2] error credits and we progress like in the previous
-          example using [wp_rand_err]. *)
       wp_pures.
+      (** Now we have [1 / 2] error credits and we progress like in the previous
+          example using [wp_rand_err]. xx *)
       wp_apply (wp_rand_err 0 with "[Hε]").
       { iApply (ec_eq with "Hε"). simpl. real_solver. }
       iIntros (m) "[%Hm %Hm']".
+      (* by now, we know [m ≠ 0], so we will definitely return [true]. *)
       wp_pures.
       assert (m = 1) as -> by nat_solver.
       rewrite bool_decide_eq_true_2; [|done].
