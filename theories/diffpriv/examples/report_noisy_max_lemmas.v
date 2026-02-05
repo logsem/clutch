b6f65cb7c53cb25718ffdb6bcc3821bb156b9cda
Hei Li <hei411@gmail.com>
2026-02-05 13:51:05 +0100

    pw_list_Z_max_correct

--- theories/diffpriv/examples/report_noisy_max_lemmas.v
+++ theories/diffpriv/examples/report_noisy_max_lemmas.v
@@ -20,10 +20,17 @@ Fixpoint list_Z_max' l x a i:=
 
 Definition list_Z_max l :=
   match l with
-  | [] => 0%Z
+  | [] => 0%nat
   | x::xs => (list_Z_max' (x::xs) 0%nat x 0%nat)
   end.
 
+Lemma list_Z_max_cons hd tl x a i:
+  list_Z_max' (hd::tl) x a i =
+  if bool_decide (hd>a)%Z then
+    list_Z_max' tl (x+1) hd x
+  else list_Z_max' tl (x+1)%nat a i.
+Proof. done. Qed. 
+  
 Lemma pw_list_Z_max_bound l x a i:
   i<= x -> 
   i=list_Z_max' l x a i \/ x <= list_Z_max' l x a i.
@@ -39,20 +46,21 @@ Proof.
     destruct!/=; lia.
 Qed. 
 
-Lemma pw_list_Z_max_after l l' x j a a' i:
+Lemma pw_list_Z_max_after l l' x j a a' i i':
   i<= x->
+  i'<=x ->
   length l = length l' ->
   (∀ p, p ∈ zip_with (λ x y, (x,y)) l l' -> (dZ p.1 p.2 <= 1)%R) ->
-  (i=j -> a+1=a')%Z ->
+  (i=j -> a+1=a' /\ i = i')%Z ->
   (dZ a a' <=1)%R ->
   x>j ->
   list_Z_max' l x a i = j ->
-  list_Z_max' l' x a' i = j.
+  list_Z_max' l' x a' i' = j.
 Proof.
-  revert l' x j a a' i.
+  revert l' x j a a' i i'.
   induction l as [|z]; first (intros []; naive_solver).
   intros [|z' ]; first naive_solver.
-  intros x j a a' i Hineq Hlen H H1 H2 H3 H4.
+  intros x j a a' i i' Hineq Hineq' Hlen H H1 H2 H3 H4.
   simpl. simpl in *.
   case_bool_decide.
   { unshelve epose proof pw_list_Z_max_bound l (x+1) z x _; lia. }
@@ -65,23 +73,24 @@ Proof.
     subst.
     unshelve epose proof H (z, z') _ as H4; first (rewrite elem_of_cons; naive_solver).
     apply dZ_bounded_cases in H4. simpl in *. lia.
-  - eapply IHl; try done; try lia.
+  - eapply IHl; last first; try done; try lia.
     intros.
     apply H.
     rewrite elem_of_cons. naive_solver.
 Qed.
 
-Lemma pw_list_Z_max_current l l' j a a' i z z':
+Lemma pw_list_Z_max_current l l' j a a' i i' z z':
   i<= j->
+  i'<=j ->
   length l = length l' ->
   (∀ p, p ∈ zip_with (λ x y, (x,y)) l l' -> (dZ p.1 p.2 <= 1)%R) ->
   (z+1=z')%Z ->
-  (i=j -> a+1=a')%Z ->
+  (i=j -> a+1=a' /\ i=i')%Z ->
   (dZ a a' <=1)%R ->
   list_Z_max' (z::l) j a i = j ->
-  list_Z_max' (z'::l') j a' i = j.
+  list_Z_max' (z'::l') j a' i' = j.
 Proof.
-  intros H1 H2 H3 H4 H5 H6. subst.
+  intros H1 H1' H2 H3 H4 H5 H6. subst.
   apply dZ_bounded_cases in H6 as H7.
   simpl. 
   intros H.
@@ -101,129 +110,134 @@ Proof.
     + eapply pw_list_Z_max_after; last first; try done; lia.
 Qed. 
 
-
-
-
-
-(* Definition list_Z_max l := *)
-(*   match l with *)
-(*   | [] => 0%Z *)
-(*   | x::xs => fold_left Z.max (x::xs) x *)
-(*   end. *)
-
-(* Lemma Zmax_rewrite x y: *)
-(*   Z.max x y=if bool_decide (y>x)%Z then y else x. *)
-(* Proof. *)
-(*   case_bool_decide. *)
-(*   - rewrite Z.max_r; lia. *)
-(*   - rewrite Z.max_l; lia. *)
-(* Qed.  *)
-
-(* Lemma list_Z_max_witness' l x: *)
-(*   fold_left Z.max l x = x \/ In (fold_left Z.max l x) l . *)
-(* Proof. *)
-(*   revert x. *)
-(*   induction l as [|a ? IHl]; first naive_solver. *)
-(*   simpl. *)
-(*   intros x. *)
-(*   rewrite (Zmax_rewrite). *)
-(*   case_bool_decide. *)
-(*   - pose proof IHl a. *)
-(*     naive_solver. *)
-(*   - pose proof IHl x. naive_solver. *)
-(* Qed. *)
-
-(* Lemma list_Z_max_witness l: *)
-(*   (0<length l)%Z -> *)
-(*   In (list_Z_max l) l. *)
-(* Proof. *)
-(*   intros H. *)
-(*   destruct l as [|z]; first (simpl in *; lia). *)
-(*   simpl. *)
-(*   pose proof list_Z_max_witness' l z. *)
-(*   rewrite Zmax_rewrite. case_bool_decide; naive_solver. *)
-(* Qed. *)
-
-(* Lemma list_Z_max_bound' x l: *)
-(*   (x<=fold_left Z.max l x /\ (∀ y, In y l -> y <= fold_left Z.max l y))%Z. *)
-(* Proof. *)
-(*   revert x. *)
-(*   induction l as [|?? IHl]; first naive_solver. *)
-(*   intros x. *)
-(*   simpl. *)
-(*   rewrite Zmax_rewrite. *)
-(*   case_bool_decide. *)
-(*   - pose proof IHl a. *)
-(*     destruct!/=. *)
-(*     split; first lia. *)
-(*     intros. *)
-(*     destruct!/=. *)
-(*     + rewrite Zmax_rewrite. *)
-(*       rewrite bool_decide_eq_false_2; [done|lia]. *)
-(*     + rewrite Zmax_rewrite. *)
-(*       case_bool_decide. *)
-(*       * admit. *)
-(*       * naive_solver. *)
-(*   - pose proof IHl x. *)
-(*     destruct!/=. *)
-(*     split; first naive_solver. *)
-(*     intros. destruct!/=. *)
-(*     + rewrite Zmax_rewrite. *)
-(*       rewrite bool_decide_eq_false_2; last lia. *)
-(*       naive_solver. *)
-(*     + admit. *)
-(* Admitted. *)
-    
-
-(* Fixpoint max_index' (l: list Z) (j:nat) (prev: Z) (ans : nat):= *)
-(*   match l with *)
-(*   | [] => ans%nat *)
-(*   | x :: xs => if bool_decide (prev < x)%Z *)
-(*              then max_index' xs (j+1)%nat x j *)
-(*              else max_index' xs (j+1)%nat prev ans *)
-(*   end. *)
-               
-(* Definition max_index l := *)
-(*   match l with *)
-(*   | [] => 0%nat *)
-(*   | x :: xs => max_index' xs 1%nat x 0%nat *)
-(*   end. *)
-
-(* Lemma max_index_range l: *)
-(*   0< length l -> *)
-(*   (max_index l< length l). *)
-(* Proof. *)
-(*   intros Hl. *)
-(*   destruct l as [|x xs]; first (simpl in *; lia). *)
-(*   clear Hl. *)
-(*   simpl. *)
-(*   assert (∀ xs j a ans, *)
-(*             ans<j+length xs -> *)
-(*             max_index' xs j a ans < j+length xs *)
-(*          ). *)
-(*   { clear. *)
-(*     intros xs. *)
-(*     induction xs as [|? ? IHxs]; first naive_solver. *)
-(*     simpl. *)
-(*     intros. *)
-(*     case_bool_decide. *)
-(*     + eapply Nat.lt_le_trans; first apply IHxs; lia. *)
-(*     + eapply Nat.lt_le_trans; first apply IHxs; lia. *)
-(*   } *)
-(*   apply H. lia. *)
-(* Qed.  *)
-
-(* Lemma max_index_correct l z_large: *)
-(*   (0<length l)%nat -> *)
-(*   (l!!max_index l = Some z_large) ->  *)
-(*   (∀ i z, l!!i=Some z -> *)
-(*           (i<max_index l /\ (z<z_large)%Z) *)
-(*           \/ (max_index l <=i /\ (z<=z_large)%Z)  *)
-(*   ). *)
-(* Proof. *)
-(*   intros Hl. *)
-(* Admitted.  *)
+Lemma pw_list_Z_max_before l l' i i' x z a a' j:
+  i<= x->
+  (i'<=j)%nat ->
+  (x<=j) ->
+  length l = length l' ->
+  (l!!(j-x)=Some z) ->
+  (l'!!(j-x)=Some (z+1)%Z)->
+  (∀ p, p ∈ zip_with (λ x y, (x,y)) l l' -> (dZ p.1 p.2 <= 1)%R) ->
+  (i=j -> a+1=a'/\i'=j)%Z ->
+  (dZ a a' <=1)%R ->
+  list_Z_max' (l) x a i = j ->
+  list_Z_max' (l') x a' i' = j.
+Proof.
+  revert l' i i' x z a a' j.
+  induction l as [|hd tl IHl]; first (by intros []).
+  intros [|hd' tl']; first done.
+  intros i i' x z a a' j H1 H1' H2 H3 H4 H5 H6 H7 H8 H9.
+  rewrite Nat.le_lteq in H2.
+  destruct H2 as [|<-]; last first.
+  { eapply pw_list_Z_max_current; last done; try done.
+    - by simplify_eq.
+    - intros. apply H6.
+      simpl.
+      rewrite elem_of_cons. naive_solver.
+    - rewrite Nat.sub_diag in H5 H4.
+      simpl in *. by simplify_eq.
+    - intros. naive_solver.
+  }
+  simpl in *.
+  case_bool_decide.
+  { unshelve epose proof pw_list_Z_max_bound tl (x+1) hd x _ as H10; first lia.
+    destruct H10; first lia.
+    case_bool_decide.
+    - eapply IHl; last done; try done; try lia.
+      + rewrite lookup_cons_Some in H4.
+        rewrite Nat.sub_add_distr.
+        destruct H4; first lia.
+        naive_solver.
+      + rewrite lookup_cons_Some in H5.
+        rewrite Nat.sub_add_distr.
+        destruct H5; first lia.
+        naive_solver.
+      + intros.
+        apply H6. rewrite elem_of_cons; naive_solver.
+      + unshelve epose proof H6 (hd, hd') _; last done.
+        rewrite elem_of_cons. naive_solver.
+    - eapply IHl; last done; last first; try done; try lia.
+      + unshelve epose proof H6 (hd, hd') _ as H11; first (rewrite elem_of_cons; naive_solver).
+        simpl in *.
+        apply dZ_bounded_cases in H8, H11.
+        rewrite Rcomplements.Rabs_le_between.
+        split.
+        * replace (-(1))%R with (IZR (-1))%Z by done.
+          apply IZR_le. lia.
+        * apply IZR_le. lia.
+      + intros.
+        apply H6. rewrite elem_of_cons; naive_solver.
+      + rewrite lookup_cons_Some in H5.
+        rewrite Nat.sub_add_distr.
+        destruct H5; first lia.
+        naive_solver.
+      + rewrite lookup_cons_Some in H4.
+        rewrite Nat.sub_add_distr.
+        destruct H4; first lia.
+        naive_solver.
+  }
+  case_bool_decide.
+  - eapply IHl; last done; try lia.
+    + rewrite lookup_cons_Some in H4.
+      rewrite Nat.sub_add_distr.
+      destruct H4; first lia.
+      naive_solver.
+    + rewrite lookup_cons_Some in H5.
+      rewrite Nat.sub_add_distr.
+      destruct H5; first lia.
+      naive_solver.
+    + intros.
+      apply H6. rewrite elem_of_cons; naive_solver.
+    + unshelve epose proof H6 (hd, hd') _ as H11; first (rewrite elem_of_cons; naive_solver).
+        simpl in *.
+        apply dZ_bounded_cases in H8, H11.
+        rewrite Rcomplements.Rabs_le_between.
+        split.
+        * replace (-(1))%R with (IZR (-1))%Z by done.
+          apply IZR_le. lia.
+        * apply IZR_le. lia.
+  - eapply IHl; last done; try lia.
+    + rewrite lookup_cons_Some in H4.
+      rewrite Nat.sub_add_distr.
+      destruct H4; first lia.
+      naive_solver.
+    + rewrite lookup_cons_Some in H5.
+      rewrite Nat.sub_add_distr.
+      destruct H5; first lia.
+      naive_solver.
+    + intros.
+      apply H6. rewrite elem_of_cons; naive_solver.
+    + unshelve epose proof H6 (hd, hd') _ as H11; first (rewrite elem_of_cons; naive_solver).
+        simpl in *.
+        apply dZ_bounded_cases in H8, H11.
+        rewrite Rcomplements.Rabs_le_between.
+        split.
+        * replace (-(1))%R with (IZR (-1))%Z by done.
+          apply IZR_le. lia.
+        * apply IZR_le. lia.
+Qed. 
   
-
   
-
+Definition pw_list_Z_max_correct l l' z (j:nat):
+  length l > 0 ->
+  length l = length l' -> 
+  (∀ p, p ∈ zip_with (λ x y, (x,y)) l l' -> (dZ p.1 p.2 <= 1)%R) ->
+  (l!!(j)=Some z) ->
+  (l'!!(j)=Some (z+1)%Z)->
+  list_Z_max l = j ->
+  list_Z_max l' = j.
+Proof.
+  destruct l; first (simpl; lia).
+  destruct l'; first done.
+  rewrite /list_Z_max.
+  intros H1 H2 H3 H4 H5 H6.
+  eapply pw_list_Z_max_before; last done; try done; try lia.
+  - by rewrite Nat.sub_0_r. 
+  - by rewrite Nat.sub_0_r.
+  - intros H. subst.
+    split; last done.
+    rewrite -H in H5, H4.
+    simpl in *. by simplify_eq.
+  - unshelve epose proof H3 (_,_) _ as K; last apply K.
+    rewrite elem_of_cons. naive_solver.
+Qed. 
