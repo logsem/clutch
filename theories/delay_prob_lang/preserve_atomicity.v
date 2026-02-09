52a88e197d6953c207c5ff096e5aa9771a144e3f
Hei Li <hei411@gmail.com>
2026-02-09 14:41:46 +0100

    NIT

--- theories/delay_prob_lang/preserve_atomicity.v
+++ theories/delay_prob_lang/preserve_atomicity.v
@@ -4,6 +4,14 @@ From clutch.delay_prob_lang Require Export lang.
 From clutch.prob Require Import distribution.
 Set Default Proof Using "Type*".
 
+Lemma list_destruct_rev {A: Type} (l:list A) :
+  l= [] \/ ∃ x xs, l = xs ++ [x].
+Proof.
+  induction l using rev_ind; destruct!/=; first naive_solver.
+  - right. eexists _, []. naive_solver.
+  - right. naive_solver.
+Qed. 
+
 Lemma value_promote_preserves_atomicity K f v v':
   Atomic StronglyAtomic (fill K (Val v)) ->
   urn_subst_val f v = Some v' ->
@@ -21,6 +29,35 @@ Proof.
       fill K0 e1' = fill K v  -> head_step e1' σ (e2', σ') > 0
       → is_Some (to_val (fill K0 e2'))) as H1' by naive_solver.
   clear H1.
-  inversion H4.
-  - 
+  assert (K' = []) as ->; last first.
+  { simpl.
+    inversion H4; simpl; subst; simpl in *; try done.
+    - simpl in *.
+      destruct (list_destruct_rev K) as [|[K1 [K2]]]; simplify_eq.
+      rewrite fill_app in H3.
+      simpl in *.
+      destruct K1; simplify_eq; simpl in *; simplify_eq.
+      + destruct (list_destruct_rev K2) as [|[K1' [K2']]]; simplify_eq; last first. 
+        * rewrite fill_app in H3. simpl in *. destruct K1'; simplify_eq.
+        * simpl in *. simplify_eq.
+          destruct v; simpl in *; repeat setoid_rewrite bind_Some in H2; destruct!/=.
+          unshelve epose proof H1' σ' σ' [] _ _ _ _; [| |done|..].
+          2:{ rewrite head_step_support_equiv_rel.
+              by apply BetaS. }
+          simpl in *.
+          admit. 
+      + unshelve epose proof (fill_to_val K2 _ _) as ->; [|by erewrite <-H|].
+        simpl in *. simplify_eq.
+        unshelve epose proof H1' σ' σ' [] _ _ _ _; [| |done|..].
+        2:{ rewrite head_step_support_equiv_rel.
+            by apply BetaS.
+        }
+        simpl in *.
+        (* obvious, induct on subst f0 _ *)
+        admit. 
+    - admit.
+    - admit.
+    - admit.
+    - admit.
+  }
 Admitted. 
