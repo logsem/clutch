d4bb409a46e293baa10d685fe298f8aa4a9f6c10
Hei Li <hei411@gmail.com>
2026-02-27 15:57:07 +0100

    Finish multiple guess example

--- theories/delay_prob_lang/urn_erasable.v
+++ theories/delay_prob_lang/urn_erasable.v
@@ -80,5 +80,82 @@ Section urn_erasable_functions.
     m!!u=Some (urn_unif s)->
     urn_erasable (dmap (λ b, if b then (<[u:= urn_unif s1]> m) else (<[u:= urn_unif s2]> m)) (biased_coin _ H)) m.
   Proof.
-  Admitted. 
+    intros Hnonempty1 Hnonempty2 Hdisjoint Hunion Hlookup.
+    rewrite /urn_erasable.
+    rewrite /dmap -dbind_assoc'.
+    apply distr_ext.
+    intros f.
+    replace (m) with (<[u:=urn_unif s]> (delete u m)) at 1; last first. 
+    { apply map_eq.
+      intros u'.
+      destruct (decide (u=u')); subst; by simplify_map_eq.
+    }
+    rewrite urns_f_distr_insert; simpl; simplify_map_eq; try done; last by apply union_positive_l_alt_L.
+    rewrite dbind_comm.
+    replace (urns_f_distr_compute_distr _) with (dbind (λ b, if b then urns_f_distr_compute_distr (urn_unif s1) else urns_f_distr_compute_distr (urn_unif s2)) (biased_coin (size s1/size (s1 ∪ s2)) H)); last first.
+    - apply distr_ext.
+      intros x.
+      destruct (decide (x∈s1)).
+      + rewrite {1}/dbind/dbind_pmf{1}/pmf. rewrite SeriesC_finite_foldr/=.
+        rewrite Rplus_0_r.
+        rewrite /biased_coin/biased_coin_pmf{1 3}/pmf.
+        rewrite /urns_f_distr_compute_distr/urns_f_distr_compute/pmf.
+        rewrite bool_decide_eq_true_2; last done.
+        rewrite bool_decide_eq_false_2; last set_solver.
+        rewrite bool_decide_eq_true_2; last set_solver.
+        rewrite Rmult_0_r Rplus_0_r.
+        rewrite Rdiv_def.
+        rewrite Rmult_comm.
+        rewrite -Rmult_assoc.
+        replace (/_ * _) with 1%R; first lra.
+        rewrite Rmult_comm.
+        rewrite Rmult_inv_r; first lra.
+        apply not_0_INR.
+        intros Hcontra.
+        apply size_empty_inv in Hcontra.
+        rewrite leibniz_equiv_iff in Hcontra.
+        naive_solver.
+      + destruct (decide (x∈s2)).
+        * rewrite {1}/dbind/dbind_pmf{1}/pmf. rewrite SeriesC_finite_foldr/=.
+          rewrite Rplus_0_r.
+          rewrite /biased_coin/biased_coin_pmf{1 3}/pmf.
+          rewrite /urns_f_distr_compute_distr/urns_f_distr_compute/pmf.
+          rewrite bool_decide_eq_false_2; last set_solver.
+          rewrite bool_decide_eq_true_2; last done.
+          rewrite bool_decide_eq_true_2; last set_solver.
+          rewrite Rmult_0_r Rplus_0_l.
+          rewrite Rdiv_def.
+          rewrite -(Rdiv_diag (size s1 + size s2)); last first.
+          -- rewrite -plus_INR.
+             apply not_0_INR.
+             assert (0<size s1)%nat; last lia.
+             destruct (size s1) eqn:K; last lia.
+             exfalso.
+             apply size_empty_inv in K.
+             rewrite leibniz_equiv_iff in K. naive_solver.
+          -- rewrite Rdiv_def.
+             rewrite Rmult_plus_distr_r.
+             rewrite size_union; last done.
+             rewrite -plus_INR.
+             rewrite Rplus_minus_l.
+             rewrite Rmult_comm.
+             rewrite -Rmult_assoc.
+             rewrite Rinv_l; first lra.
+             apply not_0_INR.
+             intros K.
+             apply size_empty_inv in K.
+             set_solver.
+        * rewrite {1}/dbind/dbind_pmf{1}/pmf. rewrite SeriesC_finite_foldr/=.
+          rewrite Rplus_0_r.
+          rewrite /biased_coin/biased_coin_pmf{1 3}/pmf.
+          rewrite /urns_f_distr_compute_distr/urns_f_distr_compute/pmf.
+          rewrite bool_decide_eq_false_2; last set_solver.
+          rewrite bool_decide_eq_false_2; last done.
+          rewrite bool_decide_eq_false_2; last set_solver.
+          lra. 
+    - rewrite -dbind_assoc'. apply dbind_pmf_ext; last done; last by apply distr_ext.
+      intros []?; rewrite dret_id_left -/(urns_f_distr _);
+      rewrite -insert_delete_insert;
+        rewrite urns_f_distr_insert; try rewrite Hlookup/=; simplify_map_eq; try done; by rewrite dbind_comm.
+  Qed. 
 End urn_erasable_functions.
