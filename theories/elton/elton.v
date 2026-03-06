af91461bc60b17d589103c87ea378b43c0521e0e
Hei Li <hei411@gmail.com>
2026-03-06 15:24:52 +0100

    remove unnecessary files

--- theories/elton/elton.v
+++
@@ -1,9 +0,0 @@
-From stdpp Require Import namespaces.
-From clutch.delay_prob_lang Require Export notation tactics metatheory urn_subst.
-From clutch.elton Require Export weakestpre lifting ectx_lifting primitive_laws proofmode adequacy.
-From clutch.delay_prob_lang Require Export lang.
-
-
-From clutch.elton.unary_rel Require Export
-    unary_model unary_interp unary_fundamental.
-
