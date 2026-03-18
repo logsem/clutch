de3110f745b4c745aa61af9e0110760ca91e9cd2
Philipp G. Haselwarter <philipp@haselwarter.org>
2026-03-18 11:05:49 +0100

    remove theory directories unrelated to the diffpriv paper

--- theories/clutch.v
+++
@@ -1,7 +0,0 @@
-From stdpp Require Import namespaces.
-From clutch.prob_lang Require Export lang notation tactics.
-From iris.proofmode Require Export proofmode.
-From clutch.prob_lang.spec Require Export spec_tactics spec_rules. 
-From clutch.clutch Require Export
-  primitive_laws derived_laws proofmode
-  coupling_rules model compatibility rel_rules rel_tactics reltac2 interp soundness.
