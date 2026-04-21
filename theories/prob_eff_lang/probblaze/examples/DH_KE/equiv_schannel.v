202069f28fdb45da6607ae618e37e8fea1a9d6af
Kinnari Vijay Dave <au809690@Kinnaris-MacBook-Pro.local>
2026-04-21 12:54:14 +0200

    WIP

--- theories/prob_eff_lang/probblaze/examples/DH_KE/equiv_schannel.v
+++ theories/prob_eff_lang/probblaze/examples/DH_KE/equiv_schannel.v
@@ -119,24 +119,27 @@ Section s_channel_verification.
 
   Program Definition RecvALSAlice : iThy Σ :=
     λ e1 e2, (λne Q,
-                ∃ m : val,
                   (⌜ e1 = do: channel (RecvV alice) ⌝%E ∗
                   ⌜  e2 = do: leaksec (RecvV alice)⌝%E) ∗ □ (Q (Val #()%V) (Val #()%V)))%I.
   Next Obligation. solve_proper. Qed.    
 
   Program Definition RecvALSBob : iThy Σ := 
     λ e1 e2, (λne Q,
-                ∃ m : val,
                   (⌜ e1 = do: channel (RecvV bob) ⌝%E ∗
                   ⌜  e2 = do: leaksec (RecvV bob)⌝%E) ∗ □ (Q (Val #()%V) (Val #()%V)))%I.
   Next Obligation. solve_proper. Qed.  
 
-
+  Program Definition ChanLeakSec : iThy Σ := iThySum (iThySum SendALSAlice SendALSBob) (iThySum RecvALSAlice RecvALSBob).
   
   Definition LblEnvSec := [ ([channel; getKey1], [leaksec; getKey2], iThyBot)    ;
                             ([keyleak1], [keyleak2],iThySum (iThySum KLeakSendAlice KLeakRecvAlice) (iThySum KLeakSendBob KLeakRecvAlice));
                             ([leakauth1], [leakauth2], iThySum (iThySum LASendAlice LASendBob) (iThySum LARecvAlice LARecvBob))].
 
+   Definition LblEnvSec' := [ ([leaksec; getKey1], [channel; getKey2], iThyBot)    ;
+                            ([keyleak1], [keyleak2],iThySum (iThySum KLeakSendAlice KLeakRecvAlice) (iThySum KLeakSendBob KLeakRecvAlice));
+                            ([leakauth1], [leakauth2], iThySum (iThySum LASendAlice LASendBob) (iThySum LARecvAlice LARecvBob))].
+
+  
   (*Definition LblEnvSec := [([leak'1], [leak'2],iThySum (iThySum SendSecBob RecvSecAlice) (iThySum RecvSecBob RecvSecAlice)); ([leak1], [leak2], iThySum LeakAlice LeakBob)].*)
 
   (*Theories relating the secure channel effects for the client*)
@@ -193,14 +196,6 @@ Section s_channel_verification.
                  □ (Q NONEV NONEV )
              )%I.
   Next Obligation. solve_proper. Qed.
-
-
-
-  (*Definition LblSecChannel γtoka atokN γfraca γseca γsecb : iLblThy Σ :=
-    [([channel1; getKey1],[channel2], (iThySum (SendSecAliceImpl γtoka γfraca γseca atokN) (RecvSecBobImpl γsecb)))].*)
-  (*    (iThySum (SendSecBobImpl γtokb γfracb γsecb btokN) (RecvSecAliceImpl γseca))))].*)
-  (*Definition SecChannelThy  γtoka atokN' γfraca γseca γsecb : iThy Σ :=
-    (iThySum (SendSecAliceImpl γtoka γfraca γseca atokN') (RecvSecBobImpl γsecb)).*)
   
    Definition SecChannelThy : iThy Σ :=
    iThySum (iThySum (SendSecAlice) (RecvSecBob)) (iThySum (SendSecBob) (RecvSecAlice)).
@@ -254,15 +249,12 @@ Section s_channel_verification.
   Definition GetKey1bot : iThy Σ :=
     λ e1 ex, (λne True, ⌜ e1 = do: getKey1 (ex) ⌝%E ∗ False)%I.
 
-  Program Definition ChanImpl : iThy Σ :=
-    λ e1 e2, (λne Q,
-                 (⌜ e1 = do: channel (RecvV bob) ⌝%E ∗
-                  ⌜ e2 = do: leaksec (RecvV bob) ⌝%E)  ∗ 
-                  □ (Q (Val #()%V) (Val #()%V)))%I.
-  Next Obligation. solve_proper. Qed.
-  
+    
   Definition LblClients : iLblThy Σ :=
-     [([schannel1; getKey1; channel],[schannel2; getKey2; leaksec], (SecChannelThy))].
+    [([schannel1; getKey1; channel],[schannel2; getKey2; leaksec], (SecChannelThy))].
+  
+  Definition LblClients' : iLblThy Σ :=
+     [([schannel1; getKey1; leaksec],[schannel2; getKey2; channel], (SecChannelThy))].
     (*[([schannel1; getKey1],[schannel2], (iThySum (GetKey1bot) (SecChannelThy)))].*)
   
 (*Verification of F_OAUTH[F_KE_L[CHAN[]]] ≤ CHAN_SIM[F_CHAN[]]*)
@@ -290,8 +282,6 @@ Section s_channel_verification.
     BREL (F_OAUTH leakauth1 channel (F_KE_L getKey1 keyleak1 (CHAN getKey1 schannel1 channel f1)))
     ≤ CHAN_SIM leakauth2 keyleak2 leaksec (F_CHAN schannel2 leaksec f2)<| LblEnvSec ++ L |> {{ (λ v1 v2, ⌜ v1 = v2 ⌝) }}.
 Proof with (repeat foldkont) using G.
-
-  (*iIntros (LblThy Xthy Hf1 Hf2) "Htoka Htoka1 Htokb1 Hsecowna Hsecownb Hauthowna Hauthownb Hf1f2". repeat simpl.*)
   iIntros (Hf1 Hf2)  "Hrelf1f2". repeat simpl.
   iApply brel_alloctape_r. iIntros (β) "Hβ". brel_pures_r. 
   iApply brel_alloc_r. iIntros (l2) "Hl2". brel_pures_r.
@@ -306,21 +296,7 @@ Proof with (repeat foldkont) using G.
   simpl. do 2 rewrite subst_is_closed_empty; try done.
   iApply brel_alloc_r. iIntros (l0) "Hl0". brel_pures_r.
   rewrite subst_is_closed_empty; try done. 
-  
-  (*use update modality, add the token atokN to the namespace of open invariants, and then allocate an invariant corresponding to either a sample is drawn from the tape or not*)
-  (*iApply fupd_brel.
-  iMod (inv_alloc atokN _ (token γtoka ∨ own γfraca DfracDiscarded)%I with "[Htoka]") as "#Hinvta".
-  { iNext; iFrame. }
-  iModIntro.
-  iApply (brel_na_alloc
-              ((β ↪ₛN (S n''; [n]) ∗ l2 ↦ₛ NONEV)
-               ∨ (β ↪ₛ□ (S n''; [])
-                  ∗ l2 ↦ₛ□ SOMEV #n)
-              )%I
-              betaN).
-   iSplitL "Hβ Hl2"; [iNext; iFrame; iLeft; iFrame|].
-   iIntros "#Hinvn".
-   set (kl1 := (match: "payload" with
+  set (kl1 := (match: "payload" with
          InjL "payload" =>
            let: "dst" := "payload" in
            let: "m" := Fst "dst" in
@@ -331,7 +307,7 @@ Proof with (repeat foldkont) using G.
                let: "key" := do: getKey1 "dst" in
                match: "key" with
                  InjL <> => "k" (InjLV #()%V)
-               | InjR "x" => let: "enc_m" := #()%V in (do: channel1 InjL ("enc_m", bob));; "k" #()%V
+               | InjR "x" => let: "enc_m" := #()%V in (do: channel InjL ("enc_m", bob));; "k" #()%V
                end
            | InjR "message" => "k" (InjLV #()%V)
            end
@@ -340,41 +316,58 @@ Proof with (repeat foldkont) using G.
          match: "key" with
            InjL <> => "k" (InjLV #()%V)
          | InjR "key" =>
-           let: "r" := do: channel1 InjR "from" in
-           match: "r" with InjL <> => "k" (InjLV #()%V) | InjR "x" => let: "enc_m" := #()%V in "k" (InjR "enc_m") end
+           let: "r" := do: channel InjR "from" in
+           match: "r" with
+             InjL <> => "k" (InjLV #()%V)
+           | InjR "x" => let: "enc_m" := #()%V in "k" (InjR "enc_m")
+           end
          end
                          end)%E).
-  set (kl2 :=  (match: "p" with
+  set (kl2 := (match: "p" with
          InjL <> =>
-           (do: leak1 bob);; 
-           let: "r" := do: channel1 InjR bob in match: "r" with InjL <> => "k" (InjLV #()%V) | InjR "w" => "k" (InjR (vgval (g ^+ n))) end
+           (do: keyleak1 InjL alice);; 
+           let: "r" := do: keyleak1 InjR bob in
+           match: "r" with InjL <> => "k" (InjLV #()%V) | InjR "w" => "k" (InjR (vgval (g ^+ n))) end
        | InjR <> =>
-         let: "r" := do: channel1 InjR alice in
-         match: "r" with InjL <> => "k" (InjLV #()%V) | InjR "w" => (do: leak1 alice);; "k" (InjR (vgval (g ^+ n))) end
+         let: "r" := do: keyleak1 InjR alice in
+         match: "r" with
+           InjL <> => "k" (InjLV #()%V)
+         | InjR "w" => (do: keyleak1 InjL bob);; "k" (InjR (vgval (g ^+ n)))
+         end
                        end)%E).
   set (kl3 := ( match: "payload" with
          InjL "payload" =>
            let: "dst" := "payload" in
            let: "m" := Fst "dst" in
            let: "dst" := Snd "dst" in
-           match: ! #l1 with InjL <> => #l1 <- InjR "m";; (do: channel1 InjL ("m", "dst"));; "k" #()%V | InjR "message" => "k" #()%V end
-       | InjR "from" => let: "r" := do: channel1 InjR "from" in match: "r" with InjL <> => "k" (InjLV #()%V) | InjR "x" => "k" ! #l1 end
-                                     end)%E).
-  set (kr1 :=  (match: "payload" with
+           match: ! #l1 with
+             InjL <> => #l1 <- InjR "m";; (do: leakauth1 InjL ("m", "dst"));; "k" #()%V
+           | InjR "message" => "k" #()%V
+           end
+       | InjR "from" =>
+         let: "r" := do: leakauth1 InjR "from" in
+         match: "r" with InjL <> => "k" (InjLV #()%V) | InjR "x" => "k" ! #l1 end
+                       end)%E).
+  set (kr1 := ( match: "payload" with
          InjL "payload" =>
            let: "dst" := "payload" in
            let: "m" := Fst "dst" in
            let: "dst" := Snd "dst" in
-           match: ! #l0 with InjL <> => #l0 <- InjR "m";; (do: channel2 InjL (#0, "dst"));; "k" #()%V | InjR "x" => "k" #()%V end
-       | InjR "from" => let: "r" := do: channel2 InjR "from" in match: "r" with InjL <> => "k" (InjLV #()%V) | InjR "x" => "k" ! #l0 end
-                                     end)%E).
-  set (kr2 :=  (match: "payload" with
+           match: ! #l0 with
+             InjL <> => #l0 <- InjR "m";; (do: leaksec InjL "dst");; "k" #()%V
+           | InjR "x" => "k" #()%V
+           end
+       | InjR "from" =>
+         let: "r" := do: leaksec InjR "from" in
+         match: "r" with InjL <> => "k" (InjLV #()%V) | InjR "x" => "k" ! #l0 end
+                       end)%E).
+  set (kr2 := ( match: "payload" with
          InjL "payload" =>
            let: "dst" := "payload" in
            let: "m" := Fst "dst" in
            let: "dst" := Snd "dst" in
-           (do: leak2 "dst");; 
-           let: "r" := do: channel2 InjR bob in
+           (do: keyleak2 InjL "dst");; 
+           let: "r" := do: keyleak2 InjR bob in
            match: "r" with
              InjL <> => "k" (InjL #()%V)
            | InjR "x" =>
@@ -382,91 +375,41 @@ Proof with (repeat foldkont) using G.
                            InjL <> => let: "m" := #()%V;; rand(#lbl:β) #(S n'') in #l2 <- InjR "m";; "m"
                          | InjR "m" => "m"
                          end in
-             let: "mA" := g ^ "m" in (do: channel2 InjL ("mA", alice));; "k" #()%V
+             let: "mA" := g ^ "m" in (do: leakauth2 InjL ("mA", alice));; "k" #()%V
            end
-       | InjR "from " =>
-         let: "r" := do: channel2 InjR "from" in
-         match: "r" with InjL <> => "k" (InjL #()%V) | InjR "x" => (do: leak2 "from");; "k" (InjR #0) end
+       | InjR "from" =>
+         let: "r" := do: keyleak2 InjR "from" in
+         match: "r" with
+           InjL <> => "k" (InjL #()%V)
+         | InjR "x" => (do: keyleak2 InjL bob);; (do: leakauth2 InjR "from");; "k" (InjR #0)
+         end
                        end)%E).
-  About brel_exhaustion.
-  Search HandleCtx.
-  Print brel_exhaustion.
-  About brel_introduction_mono.
-  Print to_iThy_le.
-  iApply (brel_exhaustion f1 f2); try simpl. 
-  About ectx_labels.
-  Print HandleCtx.
-   iApply brel_exhaustion; try set_solver...
-  1: {
-  try done.
-  iFrame "Hβ". simpl.
-  iIntros (b) "Hβ".
-  Print brel_couple_UT.
-  iIntros (n ?) "!> Hn".
-*)
-
-
-
-
-
-(*  (*-------------------------------------------------*)
-  iApply brel_alloc_r. iIntros (l0) "Hl0".
-  iApply brel_couple_UT; try done.
-  iFrame "Hα". simpl. iSplit => //. iIntros (n ?) "!> Hα".
-  (*iFrame. iSplit; first done. iIntros (n) "!>Hn Hα".*)
-  brel_pures_l. brel_exp_l. brel_pures_l. iApply brel_alloc_l.
-  iIntros (l3) "!>Hl3". brel_pures_l. repeat (rewrite subst_is_closed_empty; try done). brel_pures_r.
-  rewrite subst_is_closed_empty; try done...
-  (*Search "brel_exhaustion".
-  Search "HandleCtx".
-  About ectx.*)
- 
-  (*iApply fupd_brel.
-   iMod (inv_alloc atokN _ (token γtoka ∨ own γfraca DfracDiscarded)%I with "[Htoka]") as "#Hinvta".
-   { iNext; iLeft;iFrame. }
-   iModIntro.
-
-    iApply (brel_na_alloc
-              ((α ↪ₛN (S n''; [n]) ∗ l2 ↦ₛ NONEV)
-               ∨ (α ↪ₛ□ (S n''; [])
-                  ∗ l2 ↦ₛ□ SOMEV #n)
-              )%I
-              alphaN).
-   iSplitL "Hα Hl2"; [iNext; iFrame; iLeft; iFrame|].
-   iIntros "#Hinvn".*)
-  iApply (brel_exhaustion _ _ [HandleCtx _ _ _ _ _] [HandleCtx _ _ _ _ _] Xthy _ _ (λ v1 v2, ⌜v1=v2⌝)%I); try done.
-  {  admit.}
-  iLöb as "IH". simpl.
-
-  iSplit; [iIntros (v1 v2) "!#Hv1v2"; brel_pures; iModIntro; done|].
-  iIntros (?????) "!# %Hk1 %Hk2 [[H1 | H1'] | [H2 | H3]] #Hrel".
-  1 : { iDestruct "H1" as (m1 m2) "[%He1 [%He2 Hqv]]".
-        rewrite He1.
-        rewrite He2. 
-        brel_pures; [apply Hk1; set_solver|apply Hk2; set_solver|]...
-        
-     iApply (brel_na_inv _ _ alphaN); first set_solver.   
-     iFrame "Hinvn".
-     iIntros "([(Hα & Hla) | (#Hα & #Hla)] & Hclose)".
-        - Print brel_load_l.
-          iApply brel_load_l.
-  iDestruct "HXQ" as " "
+  set (f1' :=  (handle: f1 with
+     | effect schannel1 "payload", rec "k" as multi => kl1
+     | return "y" => #()%V
+                end)%E).
+  set (f2' := ( handle: f2 with
+     | effect schannel2 "payload", rec "k" as multi => kr1
+     | return "y" => #()%V
+                end)%E).
+  iAssert ( brel ⊤ f1' f2'
+    (([channel; getKey1], [leaksec; getKey2], ChanLeakSec)
+     :: ([keyleak1], [keyleak2],
+         iThySum (iThySum KLeakSendAlice KLeakRecvAlice) (iThySum KLeakSendBob KLeakRecvAlice))
+        :: ([leakauth1], [leakauth2],
+            iThySum (iThySum LASendAlice LASendBob) (iThySum LARecvAlice LARecvBob))
+           :: L)
+    (λ v1 v2 : val, ⌜v1 = v2⌝%I)) as "Hrelf1'f2'".
+  { admit.}
+  Search "_ ⊆ _".
+  Search "sublist".
+  About  "`sublist_of`".
+  Search "∈".
+  iApply (brel_exhaustion f1' f2'); try simpl; try auto; try (apply sublist_subseteq); try (apply singleton_sublist_l);
+    try (apply list_elem_of_In); try simpl; try auto.
+  { iSplit; [iIntros (v1 v2) "%Hv1v2"; iModIntro; brel_pures; iModIntro; done |].
+   iIntros (?????) "!# %Hk1 %Hk2 HXQ #Hrel".
   
-   (* will need induction, since send and receive can be called more than once*)
-  { iSplit.
-    + iModIntro. iIntros (v1 v2) "%Hv1v2".
-      simpl.
-      destruct v1; try (destruct v2); try (inversion Hv1v2);
-      try (brel_pures); try (iModIntro);
-        try (iPureIntro); try auto.
-    + iModIntro. iIntros (k1' k2' e1' e2' Q) "Hk1' Hk2 HXQ Hrel".
-      simpl.
-     
-  iApply (brel_exhaustion _ _ [_] [_] with "[$]").
-  iApply (brel_exhaustion _ _ [(HandleCtx Deep MS channel1 kl3 #()%V)]
-            [(HandleCtx Deep MS channel2 kr2 _)] _ _ _ _ _ _ _).
-  About PureExec.
-  About unshot.*)
 Admitted.
 
 
@@ -485,9 +428,16 @@ Admitted.
     BREL f1 ≤ f2 <| LblThy ++ L |> {{ (λ v1 v2, ⌜ v1 = v2 ⌝)  }} -∗ 
     BREL (CHAN_SIM channel1 leak1 (F_CHAN channel1 f1))
     ≤ (F_OAUTH channel2 (F_KE_L getKey2 channel2 leak2 (CHAN getKey2 channel2 f2))) <| LblEnvSec ++ L |> {{ (λ v1 v2, ⌜ v1 = v2 ⌝) }}.
-Proof.
-Admitted.   
-*)
+ *)
+ Lemma SIM_F_KE_CHAN f1 f2 L :
+    is_closed_expr ∅ f1 ->
+    is_closed_expr ∅ f2 ->
+    BREL f1 ≤ f2 <| LblClients' ++ L |> {{ (λ v1 v2, ⌜ v1 = v2 ⌝) }} -∗
+    BREL (CHAN_SIM leakauth1 keyleak1 leaksec (F_CHAN schannel1 leaksec f1)) ≤
+      (F_OAUTH leakauth2 channel (F_KE_L getKey2 keyleak2 (CHAN getKey2 schannel2 channel f1)))
+      <| LblEnvSec' ++ L |> {{ (λ v1 v2, ⌜ v1 = v2 ⌝) }}.
+ Proof.
+ Admitted.
 
   
   
