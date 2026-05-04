4bfe2f816db6bbe4c9a564542c0881749ed79285
Laurits Bligaard <bligaard@cs.au.dk>
2026-05-04 16:20:28 +0200

    ot receiver corrupt in mut handler style

--- theories/prob_eff_lang/probblaze/examples/mutual_handlers_composition.v
+++ theories/prob_eff_lang/probblaze/examples/mutual_handlers_composition.v
@@ -1,4 +1,4 @@
-From clutch.prob_eff_lang.probblaze Require Import logic notation sem_types sem_def.
+From clutch.prob_eff_lang.probblaze Require Import logic notation sem_types sem_def proofmode.
 From iris.proofmode Require Import base proofmode classes.
 From clutch.prob_eff_lang.probblaze.examples.DH_KE Require Import valgroup.
 
@@ -9,10 +9,15 @@ Section definitions.
 Definition mut_handler_constructor : val :=
   λ: "h1" "h2",
     rec: "mhandler" "f" :=
-    (* instantiate each handler-branch with the mutually recursive handler *)
+    (* instantiate the first handler-branch with the mutually recursive handler *)
     let: "mh1" := "h1" "mhandler" in 
     (* handle: handle: f with ... with ... *)
     "mh1" (λ: <>, "h2" "f").
+
+Definition mut_mut_handler_constructor : val :=
+  λ: "mhandler1" "h1" "h2",
+     let: "mh2" := "h2" "mhandler1" in
+      (mut_handler_constructor "h1") "mh2". 
 
 (* a mutually recursive handler, handling s with h *)
 Definition mut_handler (s : label) (h : expr) : val :=
@@ -28,6 +33,13 @@ Definition handle_with (s : label) (h : expr) : val :=
      | effect s "x", "k" as multi => (λ: "x" "k", h)%V "x" "k"
      | return "y" => "y"
      end.
+
+Definition handle_withV (s : label) (h : expr): val :=
+  λ: "mhandler", λ: "f",
+    handle: "f" #()%V with 
+  | effect s "x", "k" as multi => "mhandler" (λ: "x" "k", h)%V "x" "k"
+  | return "y" => "y"
+end.
 
 Definition handle_with_deep (s : label)  (h : expr) : val :=
   λ: "f",
@@ -45,6 +57,25 @@ Definition comp_handler (s1 s2 : label) (h1 h2 : expr) : val :=
   | effect s1 "x", "k" as multi => (λ: "x" "k", h1)%V "x" "k"
   | return "y" => "y"
   end. 
+
+Definition mut_mut_full sim1 hsim1 sim2 hsim2 f hf : val :=
+  rec: "mhandler1" "f" :=
+    let: "mh" :=
+      (λ: "mhandler1", (rec: "mhandler2" "f" :=
+                          handle: 
+                          handle: "f" #()%V with
+                       | effect sim2 "x", "k" as multi=> "mhandler1" (λ: <>, (λ: "x" "k", hsim2)%V "x" "k")
+                       | return "y" => "y"
+                        end with
+                       | effect sim1 "x", "k" as multi => "mhandler2" (λ: <>, (λ: "x" "k", hsim1)%V "x" "k")
+                       | return "y" => "y"
+                        end))%V "mhandler1" in
+    let: "handler" := (λ: "f", handle: "f" #()%V with
+                      | effect f "x", "k" as multi=> (λ: "x" "k", hf)%V "x" "k"
+                      | return "y" => "y"
+                       end )%V in
+     "mh" (λ: <>, "handler" "f").
+
 
 End definitions.
 
@@ -80,12 +111,12 @@ End theories.
 
 
 Section proofs.
-  Context {sim f p : label}.
+  Context {sim p : label}.
   Context `{probblazeRGS Σ}.
   (* The types used in the signatures *)
   Context {Pp Pr Ip Ir Sp Sr : (val → val → iProp Σ)}.
 
-  Lemma composition_mut_handler_1 h1 h2 h3 (f1 f2 : val) R :
+  Lemma composition_mut_handler_1 h1 h2 h3 f (f1 f2 : val) R :
     
     (* sim and f has to appear to avoid f1 and f2 to interfer with the protocol *)
     BREL f1 #()%V ≤ f2 #()%V <| [([p],[p], @client p Σ Pp Pr);([sim;f],[sim;f],⊥)] |> {{ R }} -∗
@@ -175,7 +206,7 @@ Section proofs.
     simpl. iIntros (??) "!# HR". by brel_pures. 
   Qed. 
 
-  Lemma composition_mut_handler_2 h (g g' : val) (c1 c2 : val) R: 
+  Lemma composition_mut_handler_2 f h (g g' : val) (c1 c2 : val) R: 
     □ (BREL c1 #()%V ≤ c2 #()%V <| [([p],[p],@client p Σ Pp Pr)] |> {{ R }}) -∗
 
     □ (∀ f1 f2 : val, BREL f1 #()%V ≤ f2 #()%V <| [([p],[p],@client p Σ Pp Pr); ([f],[f], @ideal f Σ Ip Ir)] |> {{ R }} -∗
@@ -208,6 +239,73 @@ Section proofs.
     iSplit; [iModIntro; iIntros (??) "HR"; by brel_pures|].
     iExact "IH".
   Qed. 
+
+  Lemma composition_mut_handler_3 sim1 sim2 Sp1 Sr1 Sp2 Sr2 (f : label) hf hsim1 hsim2  (c1 c2 : val) (R : val → val → iProp Σ) :
+    let IT := @ideal f Σ Ip Ir in
+    BREL c1 #()%V ≤ c2 #()%V <| [([f], [f], IT)] |> {{ R }} -∗
+
+     □ (∀ k1' k2' v1 v2, Ip v1 v2 -∗ (* adding the payload to the context *)
+                        (* adding the continuation to the context *)
+                        (∀ w1 w2, Ir w1 w2 -∗ BREL fill k1' w1 ≤ fill k2' w2 <| [([f],[f],IT);([sim1;sim2],[sim1;sim2], @simulator sim1 Σ Sp1 Sr1)] |> {{ R }}) -∗
+                        BREL val_subst "k" (KontV k1') (val_subst "x" v1 hf) 
+                          ≤ val_subst "k" (KontV k2') (val_subst "x" v2 hf) <| [([sim1;sim2],[sim1;sim2], @simulator sim2 Σ Sp2 Sr2)(* ; ([p],[p],⊥) *)] |> {{ R }}) -∗
+
+     □ (∀ k1' k2' v1 v2, Sp2 v1 v2 -∗ (* adding the payload to the context *)
+                         (* adding the continuation to the context *)
+                         (∀ w1 w2, Sr2 w1 w2 -∗ BREL fill k1' w1 ≤ fill k2' w2 <| [([sim1;sim2],[sim1;sim2],@simulator sim2 Σ Sp2 Sr2);([f],[f],⊥)] |> {{ R }}) -∗
+                         BREL val_subst "k" (KontV k1') (val_subst "x" v1 hsim2) 
+                           ≤ val_subst "k" (KontV k2') (val_subst "x" v2 hsim2) <| [([f],[f], IT); ([sim1;sim2],[sim1;sim2],@simulator sim1 Σ Sp1 Sr1)] |> {{ R }}) -∗
+    
+     □ (∀ k1' k2' v1 v2, Sp1 v1 v2 -∗ (* adding the payload to the context *)
+                         (* adding the continuation to the context *)
+                         (∀ w1 w2, Sr1 w1 w2 -∗ BREL fill k1' w1 ≤ fill k2' w2 <| [([sim1],[sim1],@simulator sim1 Σ Sp1 Sr1)] |> {{ R }}) -∗
+                         BREL val_subst "k" (KontV k1') (val_subst "x" v1 hsim1) 
+                           ≤ val_subst "k" (KontV k2') (val_subst "x" v2 hsim1) <| [([sim2],[sim2],@simulator sim2 Σ Sp2 Sr2)] |> {{ R }}) -∗
+
+    BREL mut_handler_constructor (mut_handler sim1 hsim1) (mut_handler_constructor (mut_handler sim2 hsim2) (handle_with f hf)) c1 ≤
+         (mut_mut_full sim1 hsim1 sim2 hsim2 f hf) c2  <| [([f],[f], ⊥);([sim1;sim2],[sim1;sim2],⊥)] |> {{ R }}.
+  Proof.    
+    iIntros (IT) "Hcc #Hhf #Hhsim2 #Hhsim1".
+    unfold mut_handler_constructor, mut_handler, handle_with, mut_mut_handler_constructor, mut_mut_full.
+    brel_pures_l. brel_pures_r. 
+
+    iApply (brel_exhaustion_abs (c1 #()%V) (c2 #()%V) with "[Hcc]"); [set_solver|set_solver| |].
+    { iApply (brel_introduction_mono [([f],[f],IT)] [([f],[f],IT);([sim1;sim2],[sim1;sim2],⊥)]). 
+      - iApply to_iThy_le_intro'. constructor. apply submseteq_nil_l.
+      - iExact "Hcc". }
+    iSplit; [iModIntro; iIntros (??) "HR"; by brel_pures|]. 
+    iLöb as "IHhf".
+    iIntros (?????) "!# %Hk1 %Hk2 (%&%&HIp&->&->&#HIrQ) #Hkont1". 
+    brel_pures. 
+    1,2 : apply neutral_ectx; set_solver.
+    
+    iApply brel_introduction_mono. { iApply to_iThy_le_intro'; apply submseteq_swap. }
+    iApply (brel_exhaustion (val_subst "k" (KontV k1') (val_subst "x" v1 hf)) (val_subst "k" (KontV k2') (val_subst "x" v2 hf)) with "[HIp]"); [set_solver|set_solver| |].
+    { iDestruct ("Hhf" with "HIp") as "Hkont". 
+      iApply brel_introduction_mono. { iApply to_iThy_le_intro'; apply submseteq_skip; apply submseteq_nil_l. }
+      iApply "Hkont". iIntros (??) "HIr". 
+      iApply (brel_introduction_mono [([f], [f], IT); ([sim1; sim2], [sim1; sim2], ⊥)]). { admit. }
+      iApply "Hkont1". by iApply "HIrQ". }
+    iSplit; [iModIntro; iIntros (??) "HR"; by brel_pures|]. 
+    iLöb as "IHsim2".
+    iIntros (?????) "!# %Hk1' %Hk2' (%&%&HSp&->&->&#HSrQ) #Hkont2". 
+    brel_pures. 
+    1,2 : apply neutral_ectx; set_solver.
+  Admitted. 
+    (* iApply (brel_exhaustion _ _ [HandleCtx _ _ _ _ _; HandleCtx _ _ _ _ _] [HandleCtx _ _ _ _ _; HandleCtx _ _ _ _ _ ; HandleCtx _ _ _ _ _] with "[HSp]"); [set_solver|set_solver| |].
+       { iApply brel_introduction_mono. { iApply to_iThy_le_intro'; apply submseteq_swap; done. }
+         iApply (brel_exhaustion _ _ [HandleCtx _ _ _ _ _] [HandleCtx _ _ _ _ _] _ _ _ _ R with "[HSp]"); [set_solver|set_solver| |].
+         { iDestruct ("Hhsim2" with "HSp") as "Hkont". iApply "Hkont". iIntros (??) "HSr". iApply "Hkont2". by iApply "HSrQ". }
+         iSplit; [iModIntro; iIntros (??) "HR"; by brel_pures|]. 
+         iIntros (?????) "!# %Hk1'' %Hk2'' (%&%&HIp2&->&->&#HIrQ2) #Hkont2'". simpl.
+         brel_pures. 
+         1,2 : apply neutral_ectx; set_solver.
+         iApply brel_introduction_mono. 
+         { iSplit.
+           - iApply iThy_le_to_iThy_2.
+           - iSplit; iModIntro; [iApply valid_submseteq|iIntros (Hd);iPureIntro; revert Hd; apply distinct_submseteq]; apply submseteq_cons; done. }
+         iDestruct ("Hhf" with "HIp2") as "Hkont". iApply "Hkont". *)
+      
 
 End proofs.    
                
