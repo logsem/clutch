8a0cbb13a0453f42b4205677b1c44426f8bc4354
Philipp G. Haselwarter <philipp@haselwarter.org>
2026-06-13 06:54:29 +0000

    gen_diffpriv/lib/laplace: add hoare_couple_laplace_exact (reflexive coupling)

--- theories/gen_diffpriv/lib/laplace.v
+++ theories/gen_diffpriv/lib/laplace.v
@@ -16,7 +16,7 @@
     and then writes [Laplace num den mean] in programs and applies
     [wp_couple_laplace] in proofs. *)
 From iris.proofmode Require Import proofmode.
-From clutch.prob Require Import differential_privacy distribution couplings_dp.
+From clutch.prob Require Import differential_privacy distribution couplings couplings_app couplings_dp.
 (* re-export the client-facing layer so [Require Import laplace] alone provides
    the WP rules, coupling seams, proof-mode tactics, notation and families *)
 From clutch.gen_diffpriv Require Export primitive_laws coupling_rules proofmode.
@@ -122,6 +122,43 @@ Section laplace.
     iApply (wp_couple_laplace loc loc' k k' Hdist num den ε ε' K E Hε εpos Hε'
               with "[$Hr $Hε]").
     iApply "HΦ".
+  Qed.
+
+  (** The exact (error-free) coupling: identical locations couple perfectly
+      (the two sides sample the SAME distribution, coupled along the diagonal at
+      zero cost — the reflexive coupling, [ARcoupl_eq] lifted to [DPcoupl]). *)
+  Lemma hoare_couple_laplace_exact (loc num den : Z) K E :
+    {{{ ⤇ fill K (Laplace #num #den #loc #()) }}}
+      Laplace #num #den #loc #() @ E
+      {{{ (z : Z), RET #z; ⤇ fill K #z }}}.
+  Proof.
+    iIntros (Φ) "Hr HΦ".
+    wp_pures. tp_pures.
+    iMod ecm_zero as "Hε".
+    iApply (wp_couple_sample_gen S lidx
+              (sf_param_to_val laplace_family (num, den, loc))
+              (sf_param_to_val laplace_family (num, den, loc))
+              (dmap (sf_inj laplace_family) (sf_sample laplace_family (num, den, loc)))
+              (dmap (sf_inj laplace_family) (sf_sample laplace_family (num, den, loc)))
+              (λ v v', ∃ z : Z, v = #z ∧ v' = #z) K E 0
+              (sig_sample_at laplace_family S (num, den, loc))
+              (sig_sample_at laplace_family S (num, den, loc)) _ with "[$Hr $Hε]").
+    { iIntros "!>" (v v') "(Hspec & %HR)". destruct HR as (z & -> & ->).
+      iApply ("HΦ" $! z with "Hspec"). }
+    (* the reflexive coupling of the Laplace draw, lifted through sf_inj *)
+    Unshelve.
+    apply (DPcoupl_map (sf_inj laplace_family) (sf_inj laplace_family)
+             (sf_sample laplace_family (num, den, loc)) (sf_sample laplace_family (num, den, loc))
+             (λ v v', ∃ z : Z, v = #z ∧ v' = #z) 0 0); [lra | lra | ].
+    eapply (DPcoupl_mono (sf_sample laplace_family (num, den, loc))
+              (sf_sample laplace_family (num, den, loc))
+              (sf_sample laplace_family (num, den, loc))
+              (sf_sample laplace_family (num, den, loc))
+              (=) (λ a a', ∃ z : Z, sf_inj laplace_family a = #z ∧ sf_inj laplace_family a' = #z)
+              0 0 0 0);
+      [ intros; reflexivity | intros; reflexivity
+      | intros a a' ->; by exists a' | lra | lra
+      | apply ARcoupl_to_DPcoupl, ARcoupl_eq ].
   Qed.
 
 End laplace.
