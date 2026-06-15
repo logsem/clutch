52d9c973e5679381519ad9de3b3563e238658dbb
Kinnari Vijay Dave <kinnari.dave@cs.au.dk>
2026-06-15 14:26:21 +0200

    some composition statements

---
+++ theories/prob_eff_lang/probblaze/examples/DH_KE/new_composition.v
@@ -0,0 +1,92 @@
+From iris.proofmode Require Import base proofmode classes.
+From iris.base_logic.lib Require Import na_invariants.
+From iris.algebra Require Import agree excl auth frac excl_auth.
+From iris.algebra.lib Require Import dfrac_agree.
+From clutch Require Import stdpp_ext.
+From clutch.prob_eff_lang.probblaze Require Import logic primitive_laws proofmode
+  spec_rules spec_ra class_instances.
+From clutch.prob_eff_lang.probblaze Require Import tactics.
+From clutch.prob_eff_lang.probblaze Require Export notation valgroup.
+From clutch.prob_eff_lang.probblaze Require Export def_dhke.
+From clutch.prob_eff_lang.probblaze Require Export newdef_schan.
+From clutch.prob_eff_lang.probblaze Require Export new_schan_ri.
+From clutch.prob_eff_lang.probblaze Require Export dhke_channel.
+From clutch.prob_eff_lang.probblaze Require Import sem_types sem_row sem_sig sem_judgement sem_def.
+
+Import fingroup.
+Import fingroup.fingroup.
+
+
+
+Import valgroup_tactics.
+
+Section new_comp_verification.
+  Context `{probblazeRGS Σ}.
+  Context (channel leaksec channel1 channel2 getKey1 getKey2 leakauth1 leakauth2 keyleak1 keyleak2 schannel1 schannel2 l1 l2 l2': label).
+  Context {vg: val_group}.
+  Context {cg: clutch_group_struct}.
+  Context {G : clutch_group (vg:=vg) (cg:=cg)}.
+  Context {vgg: @val_group_generator vg}.
+  Context `{!inG Σ (exclR unitO), !inG Σ dfracO,!inG Σ (dfrac_agreeR valO)}.
+
+  Definition REAL_CHAN_DHKE : val :=
+    λ: "f", (F_OAUTH ||ₗ F_AUTH (DH_KE)) (CHAN "f").
+
+  Definition REAL_CHAN_DH_REAL : val :=
+    λ: "f", (F_OAUTH ||ₗ F_AUTH) (C DH_real (CHAN "f")).
+
+  Definition REAL_CHAN_DH_RAND : val :=
+    λ: "f", (F_OAUTH ||ₗ F_AUTH) (C DH_rand (CHAN "f")).
+  
+   (* F_OAUTH[ F_AUTH [DH_KE [CHAN []]]] ≤ F_OAUTH[ F_AUTH [C[DH_real][CHAN []]]] *)
+  (*---------------------------------------------------------------------------*)
+  Lemma F_OAUTH_DHKE_C_REAL (f1 f2 : val) (L : sem_row Σ) :
+    sem_val_typed f1 f2 (∀ᵣ θₕ, (((⊤ × (sem_ty_sum 𝟙 𝟙)) -{ θₕ }-> (Option  ⊤)) × ((sem_ty_sum 𝟙 𝟙) -{ θₕ }-> (Option  ⊤))) -{ sem_row_union  θₕ L }-> 𝟙)%T -∗    
+    BREL (REAL_CHAN_DHKE f1) ≤ (REAL_CHAN_DH_REAL f2)
+     <|⊥|> {{λ v1 v2,
+                                       ∀ (leakauth1 leakauth2 keyleak1 keyleak2 : label),
+               BREL v1 ((λ: "m", do: leakauth1 (Send "m")), (λ: "m", do: leakauth1 (Recv "m")))%V (λ: "m", do: keyleak1 "m")%V ≤ v2 ((λ: "m", do: leakauth2 (Send "m")), (λ: "m", do: leakauth2 (Recv "m")))%V (λ: "m", do: keyleak2 "m")%V <| (iLblSig_to_iLblThy (envsec_row keyleak1 keyleak2 leakauth1 leakauth2 )) ++ (iLblSig_to_iLblThy L) |> {{ (λ w1 w2, 𝟙%T w1 w2)}}}}.
+  Proof.
+  Admitted.
+
+   (*F_OAUTH [ F_AUTH [C[DH_real][CHAN[]]]] ≤ F_OAUTH[ F_AUTH [DH_KE [CHAN []]]] *)
+  (*----------------------------------------------------------------------------*)
+  Lemma C_REAL_DHKE_F_OAUTH (f1 f2 : val) (L : sem_row Σ) :
+    sem_val_typed f1 f2 (∀ᵣ θₕ, (((⊤ × (sem_ty_sum 𝟙 𝟙)) -{ θₕ }-> (Option  ⊤)) × ((sem_ty_sum 𝟙 𝟙) -{ θₕ }-> (Option  ⊤))) -{ sem_row_union  θₕ L }-> 𝟙)%T -∗    
+    BREL (REAL_CHAN_DH_REAL f1) ≤ (REAL_CHAN_DHKE f2)
+     <|⊥|> {{λ v1 v2,
+                                       ∀ (leakauth1 leakauth2 keyleak1 keyleak2 : label),
+               BREL v1 ((λ: "m", do: leakauth1 (Send "m")), (λ: "m", do: leakauth1 (Recv "m")))%V (λ: "m", do: keyleak1 "m")%V ≤ v2 ((λ: "m", do: leakauth2 (Send "m")), (λ: "m", do: leakauth2 (Recv "m")))%V (λ: "m", do: keyleak2 "m")%V <| (iLblSig_to_iLblThy (envsec_row keyleak1 keyleak2 leakauth1 leakauth2 )) ++ (iLblSig_to_iLblThy L) |> {{ (λ w1 w2, 𝟙%T w1 w2)}}}}.
+  Proof.
+  Admitted.
+
+  (*F_OAUTH [F_AUTH [C[DH_real][CHAN[]]]] ≤ F_OAUTH [F_AUTH [C[DH_rand][CHAN[]]]]*)
+  (*-----------------------------------------------------------------------------*)
+   Lemma C_REAL_DH_RAND (f1 f2 : val) (L : sem_row Σ) :
+    sem_val_typed f1 f2 (∀ᵣ θₕ, (((⊤ × (sem_ty_sum 𝟙 𝟙)) -{ θₕ }-> (Option  ⊤)) × ((sem_ty_sum 𝟙 𝟙) -{ θₕ }-> (Option  ⊤))) -{ sem_row_union  θₕ L }-> 𝟙)%T -∗    
+    BREL (REAL_CHAN_DH_REAL f1) ≤ (REAL_CHAN_DH_RAND f2)
+     <|⊥|> {{λ v1 v2,
+                                       ∀ (leakauth1 leakauth2 keyleak1 keyleak2 : label),
+               BREL v1 ((λ: "m", do: leakauth1 (Send "m")), (λ: "m", do: leakauth1 (Recv "m")))%V (λ: "m", do: keyleak1 "m")%V ≤ v2 ((λ: "m", do: leakauth2 (Send "m")), (λ: "m", do: leakauth2 (Recv "m")))%V (λ: "m", do: keyleak2 "m")%V <| (iLblSig_to_iLblThy (envsec_row keyleak1 keyleak2 leakauth1 leakauth2 )) ++ (iLblSig_to_iLblThy L) |> {{ (λ w1 w2, 𝟙%T w1 w2)}}}}.
+   Proof.
+   Admitted.
+
+
+  (*F_OAUTH [F_AUTH [C[DH_rand][CHAN[]]]] ≤ F_OAUTH [F_AUTH [C[DH_real][CHAN[]]]]*)
+  (*----------------------------------------------------------------------------*)
+  Lemma C_DH_RAND_REAL (f1 f2 : val) (L : sem_row Σ) :
+    sem_val_typed f1 f2 (∀ᵣ θₕ, (((⊤ × (sem_ty_sum 𝟙 𝟙)) -{ θₕ }-> (Option  ⊤)) × ((sem_ty_sum 𝟙 𝟙) -{ θₕ }-> (Option  ⊤))) -{ sem_row_union  θₕ L }-> 𝟙)%T -∗    
+    BREL (REAL_CHAN_DH_RAND f1) ≤ (REAL_CHAN_DH_REAL f2)
+     <|⊥|> {{λ v1 v2,
+                                       ∀ (leakauth1 leakauth2 keyleak1 keyleak2 : label),
+               BREL v1 ((λ: "m", do: leakauth1 (Send "m")), (λ: "m", do: leakauth1 (Recv "m")))%V (λ: "m", do: keyleak1 "m")%V ≤ v2 ((λ: "m", do: leakauth2 (Send "m")), (λ: "m", do: leakauth2 (Recv "m")))%V (λ: "m", do: keyleak2 "m")%V <| (iLblSig_to_iLblThy (envsec_row keyleak1 keyleak2 leakauth1 leakauth2 )) ++ (iLblSig_to_iLblThy L) |> {{ (λ w1 w2, 𝟙%T w1 w2)}}}}.
+  Proof.
+  Admitted.
+
+   (*F_OAUTH [F_AUTH [DH_KE [CHAN[]]]] ≤ F_AUTH [ DH_SIM [CHAN_SIM [F_CHAN[]]]]  *)
+  (*---------------------------------------------------------------------------*)
+
+
+  (*F_AUTH[ DH_SIM [CHAN_SIM [F_CHAN[]]]] ≤ F_OAUTH [F_AUTH [DH_KE [CHAN[]]]] *)
+(*--------------------------------------------------------------------------*)
+End new_comp_verification.
