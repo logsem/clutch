1d8a8acf9227f6fd2dd31746dd4c9ea291036d9c
Langlois <robinson.langlois@ens-lyon.fr>
2026-06-25 13:58:11 +0200

    pmw data: change in the distribution

--- src/diffpriv/numeric_sparse_vector/data/gen_data.py
+++ src/diffpriv/numeric_sparse_vector/data/gen_data.py
@@ -3,7 +3,28 @@ from random import randint
 size = 1_000_000
 nb_o = 10
 
+
+
 with open("rd_data.csv", "w", encoding="utf-8") as f:
-    for i in range(size+1):
-        f.write(str(randint(0, nb_o) % nb_o)+"\n")
+    for i in range(size):
+        if randint(0, 1) == 0:
+            f.write(str(0)+"\n")
+        elif randint(0, 1) == 0:
+            f.write(str(1)+"\n")
+        elif randint(0, 1) == 0:
+            f.write(str(2)+"\n")
+        elif randint(0, 1) == 0:
+            f.write(str(3)+"\n")
+        elif randint(0, 1) == 0:
+            f.write(str(4)+"\n")
+        elif randint(0, 1) == 0:
+            f.write(str(5)+"\n")
+        elif randint(0, 1) == 0:
+            f.write(str(6)+"\n")
+        elif randint(0, 1) == 0:
+            f.write(str(7)+"\n")
+        elif randint(0, 1) == 0:
+            f.write(str(8)+"\n")
+        elif randint(0, 1) == 0:
+            f.write(str(9)+"\n")
 
