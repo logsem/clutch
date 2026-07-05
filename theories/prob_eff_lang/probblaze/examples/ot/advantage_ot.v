dbff474ded27dbd3153a69a1547cc34410900721
Laurits Bligaard <bligaard@cs.au.dk>
2026-07-05 14:17:21 +0200

    closed advantages for ot

---
+++ theories/prob_eff_lang/probblaze/examples/ot/advantage_ot.v
@@ -0,0 +1,97 @@
+From clutch.prob_eff_lang.probblaze Require Import advantage.
+From iris.algebra Require Import excl.
+From iris.algebra.lib Require Import dfrac_agree.
+From clutch.prob_eff_lang.probblaze Require Import sem_def sem_types sem_judgement sem_row syntax semantics proofmode valgroup.
+From clutch.prob_eff_lang.probblaze Require Import OT_Rcorrupt_thunk definition_thunk_receiver_corrupt adequacy. 
+Import fingroup. 
+
+Section adv_rc.
+  Context {vg : val_group} {cg : clutch_group_struct} {vgg : @val_group_generator vg}.
+  Context {G : ∀ `{!probblazeRGS Σ}, clutch_group}.
+  Context `{probblazeRGpreS Σ}.
+  Context `{!inG Σ (exclR unitO), !inG Σ dfracO, !inG Σ (dfrac_agreeR valO)}.
+  #[local] Notation n := (S (S n'')).
+  Context `{n_prime : is_true (prime.prime n)}.
+
+  Notation "'𝔾'" := sem_ty_group.   
+  Definition τ_rc `{probblazeRGS Σ} := (∀ᵣ θ, τC θ ⊸ ((((𝔾 × 𝔾) × (𝔾 × 𝔾)) -{ θ }-> 𝟙) × (𝟙 -{ θ }-> Option (𝔾 × 𝔾)))
+            -{ ¡[OS] θ}-∘ 𝟙)%T.
+
+
+  Theorem adv_ot_rc A (ε : R) :
+    (∀ `{!probblazeRGS Σ},⊢ sem_val_typed A A (τ_rc → 𝔹)%T) →
+    advantage A OT_SIM_FOT_thunk (λ: "f" "effs", F_CRS (λ: "doCRS", OT_Real_Receiver_Corrupted "f" ("effs", "doCRS"))%E)%V #true <= 3/n. 
+  Proof using n_prime G inG2 H. 
+    intros. eapply sem_typed_advantage_aff; try done; last split.
+    - apply Rcomplements.Rdiv_le_0_compat; first lra.
+      apply lt_0_INR. lia.
+    - intros Hrgs. by unshelve iApply OT_ideal_real.
+    - intros Hrgs. by unshelve iApply OT_real_ideal.
+  Qed.
+
+End adv_rc.
+
+From clutch.prob_eff_lang.probblaze Require Import OT_SCorrupt_thunk definition_thunk_sender_corrupt. 
+
+Section adv_sc.
+  Context {vg : val_group} {cg : clutch_group_struct} {vgg : @val_group_generator vg}.
+  Context {G : ∀ `{!probblazeRGS Σ}, clutch_group}.
+  Context `{probblazeRGpreS Σ}.
+  Context `{!inG Σ (exclR unitO), !inG Σ dfracO, !inG Σ (dfrac_agreeR valO)}.
+  #[local] Notation n := (S (S n'')).
+  Context `{n_prime : is_true (prime.prime n)}.
+
+  Notation "'𝔾'" := sem_ty_group.   
+  Definition τ_sc `{probblazeRGS Σ} := (∀ᵣ θ, τC θ ⊸ (((𝔾 × 𝔾) -{ θ }-> 𝟙) × (𝟙 -{ θ }-> Option ((𝔾 × 𝔾) × (𝔾 × 𝔾))))
+            -{ ¡[OS] θ}-∘ 𝟙)%T.
+
+
+  Theorem adv_ot_real A :
+    (∀ `{!probblazeRGS Σ},⊢ sem_val_typed A A (τ_sc → 𝔹)%T) →
+    advantage A 
+      (λ: "f" "effs", F_CRS (λ: "doCRS", OT_Real_Sender_corrupt "f" ("effs", "doCRS")))%V
+      (λ: "f" "effs", (reduction DH_rand) (λ: "doCRS", OT_Real_Sender_corrupt "f" ("effs", "doCRS")))%V
+        #true <= 1/n. 
+  Proof using n_prime G inG2 H. 
+    intros. eapply sem_typed_advantage_aff; try done; last split.
+    - apply Rcomplements.Rdiv_le_0_compat; first lra.
+      apply lt_0_INR. lia.
+    - intros Hrgs. by unshelve iApply OT_real_DH_real.
+    - intros Hrgs. by unshelve iApply DH_real_OT_real.
+  Qed.
+
+    Theorem adv_ot_sim A :
+    (∀ `{!probblazeRGS Σ},⊢ sem_val_typed A A (τ_sc → 𝔹)%T) →
+    advantage A (λ: "f" "effs", (reduction DH_real) (λ: "doCRS", OT_Real_Sender_corrupt "f" ("effs", "doCRS")))%V OT_SIM_FOT_thunk #true <= 1/n. 
+  Proof using n_prime G inG2 H. 
+    intros. eapply sem_typed_advantage_aff; try done; last split.
+    - apply Rcomplements.Rdiv_le_0_compat; first lra.
+      apply lt_0_INR. lia.
+    - intros Hrgs. by unshelve iApply DH_OT_sim.
+    - intros Hrgs. by unshelve iApply OT_sim_DH.
+  Qed.
+
+  Theorem adv_ot A :
+    (∀ `{!probblazeRGS Σ},⊢ sem_val_typed A A (τ_sc → 𝔹)%T) →
+    advantage A
+      (λ: "f" "effs", F_CRS (λ: "doCRS", OT_Real_Sender_corrupt "f" ("effs", "doCRS")))%V 
+      OT_SIM_FOT_thunk #true 
+    <= advantage A 
+         (λ: "f" "effs", (reduction DH_rand) (λ: "doCRS", OT_Real_Sender_corrupt "f" ("effs", "doCRS")))%V 
+         (λ: "f" "effs", (reduction DH_real) (λ: "doCRS", OT_Real_Sender_corrupt "f" ("effs", "doCRS")))%V #true + 2/n.
+  Proof using n_prime G inG2 H.
+    intros. eapply advantage_triangle.
+    - by eapply adv_ot_real.
+    - eapply advantage_triangle.
+      + apply Rle_refl.
+      + by eapply adv_ot_sim.
+      + apply Rle_refl.
+    - rewrite Rplus_comm. 
+      rewrite Rplus_assoc.
+      rewrite -Rdiv_plus_distr. 
+      assert (1 + 1 = 2) as -> by lra.
+      apply Rle_refl.
+  Qed.
+
+
+End adv_sc.
