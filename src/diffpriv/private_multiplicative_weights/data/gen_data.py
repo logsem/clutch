67697fabdf946fae4105c2486c64f548610c35d3
Langlois <robinson.langlois@ens-lyon.fr>
2026-07-06 13:00:59 +0200

    modif: distrib + gif

--- src/diffpriv/private_multiplicative_weights/data/gen_data.py
+++ src/diffpriv/private_multiplicative_weights/data/gen_data.py
@@ -1,44 +1,21 @@
+#!/usr/bin/python3
+
 from random import randint
 
-size = 1_000_000
-nb_o = 10
 
-
-
-with open("rd_data.csv", "w", encoding="utf-8") as f:
-    for i in range(size):
-        token = True
-        count = 0
-        while token and count <= 12:
-            if randint(0, 3) == 0:
-                f.write(str(count)+"\n")
-                token = False
-            count += 1
-
-
-#        if randint(0, 3) == 0:
-#            f.write(str(0)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(1)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(2)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(3)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(4)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(5)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(6)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(7)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(8)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(9)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(10)+"\n")
-#        elif randint(0, 3) == 0:
-#            f.write(str(10)+"\n")
-
+if __name__ == "__main__":
+    size = 1_000_000
+    nb_o = 13
+    
+    with open("rd_data.csv", "w", encoding="utf-8") as f:
+        for i in range(size):
+            token = True
+            count = 0
+            while token and count < nb_o:
+                if randint(0, 3) == 0:
+                    f.write(str(count)+"\n")
+                    token = False
+                count += 1
+            if token:
+                f.write(str(nb_o)+"\n")
 
--- src/diffpriv/private_multiplicative_weights/gif/render_gif.py
+++ src/diffpriv/private_multiplicative_weights/gif/render_gif.py
@@ -1,10 +1,9 @@
+#!/usr/bin/python3
+
 import csv
 import matplotlib.pyplot as plt
 from PIL import Image
 import io
-
-images = []
-
 
 def max_l(l):
     a = 0
@@ -14,42 +13,44 @@ def max_l(l):
     return a
 
 
-with open("data/len.csv", "r") as flen:
-    datalen = csv.reader(flen)
-    nb_images = 0
-    for row in datalen:
-        nb_images = int(row[0])
-    with open("data/gif0.csv", "r") as f0:
-        data0 = csv.reader(f0)
-        x0 = []
-        y0 = []
-        max_d = 0
-        for row in data0:
-            x0.append(row[0])
-            y0.append(float(row[1]))
-            if max_d < float(row[1]):
-                max_d = float(row[1])
-        for i in range(nb_images):
-            print(str(int(10000*i/nb_images)/100) + "%", "\r", end="")
-            with open(f"data/gif{i+1}.csv", "r") as f:
-                data = csv.reader(f)
-                x = []
-                y = []
-                for row in data:
-                    x.append(row[0])
-                    y.append(float(row[1]))
-                fig, ax = plt.subplots()
-                ax.bar(x0, y0, color='yellow', alpha=0.8)
-                ax.bar(x, y, color='cyan', alpha=0.5)
-                ax.set_ylim(0, max_d+0.1)
-
-                buf = io.BytesIO()
-                fig.savefig(buf, format='png')
-                buf.seek(0)
-                images.append(Image.open(buf))
-                plt.close()
-print("exporting")
-images[0].save('evolution_distrib.gif',
-               save_all=True, append_images=images[1:],
-               optimize=False, duration=30)
-print("over")
+if __name__ == "__main__":
+    images = []
+    
+    with open("data/len.csv", "r") as flen:
+        datalen = csv.reader(flen)
+        nb_images = 0
+        for row in datalen:
+            nb_images = int(row[0])
+        with open("data/gif0.csv", "r") as f0:
+            data0 = csv.reader(f0)
+            x0 = []
+            y0 = []
+            max_d = 0
+            for row in data0:
+                x0.append(row[0])
+                y0.append(float(row[1]))
+                if max_d < float(row[1]):
+                    max_d = float(row[1])
+            for i in range(nb_images):
+                print(str(int(1000*i/nb_images)/10) + "%", "\r", end="")
+                with open(f"data/gif{i+1}.csv", "r") as f:
+                    data = csv.reader(f)
+                    x = []
+                    y = []
+                    for row in data:
+                        x.append(row[0])
+                        y.append(float(row[1]))
+                    fig, ax = plt.subplots()
+                    ax.bar(x0, y0, color='yellow', alpha=0.8)
+                    ax.bar(x, y, color='cyan', alpha=0.5)
+                    ax.set_ylim(0, max_d+0.1)
+    
+                    buf = io.BytesIO()
+                    fig.savefig(buf, format='png')
+                    buf.seek(0)
+                    images.append(Image.open(buf))
+                    plt.close()
+    print("exporting")
+    images[0].save('evolution_distrib.gif',
+                   save_all=True, append_images=images[1:],
+                   optimize=False, duration=30)
