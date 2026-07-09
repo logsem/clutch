f9ab67906e8f2a434944eae970f1f472f7009bf8
Philipp G. Haselwarter <philipp@haselwarter.org>
2026-07-09 14:59:34 +0200

    fix merge?

--- theories/prob_eff_lang/probblaze/examples/DH_KE/valgroup.v
+++ theories/prob_eff_lang/probblaze/examples/DH_KE/valgroup.v
@@ -214,9 +214,9 @@ Class clutch_group `{probblazeRGS Σ} {vg : val_group} {cg : clutch_group_struct
           (BREL (fill K (vg_of_int (#x))) ≤ e @ E <|X|> {{R}})
     ; brel_vg_of_int_correct_l' : BREL_VG_OF_INT_CORRECT_L'
     ; BREL_VG_OF_INT_CORRECT_R := ∀ E K X R e x g,
-          vg_of_int_sem x = Some g ->
-          (BREL e ≤ (fill K (SOMEV (vgval g))) @ E <|X|> {{R}}) -∗
-          (BREL e ≤ (fill K (vg_of_int (#x))) @ E <|X|> {{R}})
+        vg_of_int_sem x = Some g ->
+        (BREL e ≤ (fill K (SOMEV (vgval g))) @ E <|X|> {{R}}) -∗
+        (BREL e ≤ (fill K (vg_of_int (#x))) @ E <|X|> {{R}})
     ; brel_vg_of_int_correct_r : BREL_VG_OF_INT_CORRECT_R
     ; BREL_VG_OF_INT_NONE_L := ∀ E K X R e x,
           vg_of_int_sem x = None ->
@@ -228,11 +228,6 @@ Class clutch_group `{probblazeRGS Σ} {vg : val_group} {cg : clutch_group_struct
           (BREL e ≤ (fill K NONEV) @ E <|X|> {{R}}) -∗
           (BREL e ≤ (fill K (vg_of_int (#x))) @ E <|X|> {{R}})
     ; brel_vg_of_int_none_r : BREL_VG_OF_INT_NONE_R
-    ; BREL_VG_OF_INT_CORRECT_R :=  ∀ E K X R e x g,
-                                   vg_of_int_sem x = Some g ->
-                                   (BREL  e ≤ (fill K (SOMEV (vgval g))) @ E <|X|> {{R}}) -∗
-                                   (BREL  e ≤ (fill K (vg_of_int (#x))) @ E <|X|> {{R}})
-    ; brel_vg_of_int_correct_r : BREL_VG_OF_INT_CORRECT_R
     }.
 
 Definition vg_of_cg := λ {Σ HΣ} vg cg (G : @clutch_group Σ HΣ vg cg), vg.
